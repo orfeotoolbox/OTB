d61a2181199af96ec24a2c8b6bc6ff5945c565ab
Thomas Feuvrier <thomas.feuvrier@c-s.fr>
2006-01-23 12:19:23 +0000

    Mise en place du premier test pour FeatureExtraction.

---
+++ Testing/Code/FeatureExtraction/otbLeeFilter.cxx
@@ -0,0 +1,84 @@
+/*=========================================================================
+
+  Programme :   OTB (ORFEO ToolBox)
+  Auteurs   :   CS - T.Feuvrier
+  Language  :   C++
+  Date      :   11 janvier 2005
+  Version   :   
+  Role      :   Test l'extraction d'une ROI dans une image mono canal, dont les valeurs sont codées en "unsigned char"
+  $Id$
+
+=========================================================================*/
+
+#include "itkExceptionObject.h"
+
+
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "otbExtractROI.h"
+#include "otbImage.h"
+
+
+int otbLeeFilter( int argc, char ** argv )
+{
+  try 
+    { 
+        const char * inputFilename  = argv[1];
+        const char * outputFilename = argv[2];
+        
+        unsigned int  startX((unsigned int)::atoi(argv[3]));
+        unsigned int  startY((unsigned int)::atoi(argv[4]));
+        unsigned int  sizeX((unsigned int)::atoi(argv[5]));
+        unsigned int  sizeY((unsigned int)::atoi(argv[6]));
+
+        typedef float  	                                        InputPixelType;
+        typedef float     	                                OutputPixelType;
+        const   unsigned int        	                        Dimension = 2;
+
+        typedef itk::Image< InputPixelType,  Dimension >        InputImageType;
+        typedef itk::Image< OutputPixelType, Dimension >        OutputImageType;
+
+        typedef otb::ImageFileReader< InputImageType  >         ReaderType;
+        typedef otb::ImageFileWriter< OutputImageType >         WriterType;
+
+        typedef otb::TOTO< InputImageType, 
+                                            OutputImageType >   FilterType;
+
+
+        FilterType::Pointer filter = FilterType::New();
+        
+        filter->Settototo( startX );
+
+        ReaderType::Pointer reader = ReaderType::New();
+        WriterType::Pointer writer = WriterType::New();
+
+        reader->SetFileName( inputFilename  );
+        writer->SetFileName( outputFilename );
+        
+
+
+
+        filter->SetInput( reader->GetOutput() );
+        writer->SetInput( filter->GetOutput() );
+        
+        writer->Update(); 
+
+    } 
+  catch( itk::ExceptionObject & err ) 
+    { 
+    std::cout << "Exception itk::ExceptionObject levee !" << std::endl; 
+    std::cout << err << std::endl; 
+    return EXIT_FAILURE;
+    } 
+  catch( ... ) 
+    { 
+    std::cout << "Exception levee inconnue !" << std::endl; 
+    return EXIT_FAILURE;
+    } 
+  // Software Guide : EndCodeSnippet
+
+
+  return EXIT_SUCCESS;
+}
+
+
