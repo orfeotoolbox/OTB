04d93b91fef0ff8cce25bf449e926bbcbdc0b970
Thomas Feuvrier <thomas.feuvrier@c-s.fr>
2006-02-27 08:33:12 +0000

    nomsg

--- SoftwareGuide/cmake_install.cmake
+++ SoftwareGuide/cmake_install.cmake
@@ -1,10 +1,10 @@
-# Install script for directory: /home/ORFEO/thomas/ORFEO-TOOLBOX/OTB-Documents/SoftwareGuide
+# Install script for directory: /ORFEO/thomas/ORFEO-TOOLBOX/otb/OTB-Documents/SoftwareGuide
 
-INCLUDE("/home/ORFEO/thomas/ORFEO-TOOLBOX/OTB-Documents/SoftwareGuide/Examples/cmake_install.cmake")
-INCLUDE("/home/ORFEO/thomas/ORFEO-TOOLBOX/OTB-Documents/SoftwareGuide/Art/cmake_install.cmake")
-INCLUDE("/home/ORFEO/thomas/ORFEO-TOOLBOX/OTB-Documents/SoftwareGuide/Latex/cmake_install.cmake")
+INCLUDE("/ORFEO/thomas/ORFEO-TOOLBOX/otb/OTB-Documents/SoftwareGuide/Examples/cmake_install.cmake")
+INCLUDE("/ORFEO/thomas/ORFEO-TOOLBOX/otb/OTB-Documents/SoftwareGuide/Art/cmake_install.cmake")
+INCLUDE("/ORFEO/thomas/ORFEO-TOOLBOX/otb/OTB-Documents/SoftwareGuide/Latex/cmake_install.cmake")
 
-FILE(WRITE "/home/ORFEO/thomas/ORFEO-TOOLBOX/OTB-Documents/SoftwareGuide/install_manifest.txt" "")
+FILE(WRITE "/ORFEO/thomas/ORFEO-TOOLBOX/otb/OTB-Documents/SoftwareGuide/install_manifest.txt" "")
 FOREACH(file ${CMAKE_INSTALL_MANIFEST_FILES})
-  FILE(APPEND "/home/ORFEO/thomas/ORFEO-TOOLBOX/OTB-Documents/SoftwareGuide/install_manifest.txt" "${file}\n")
+  FILE(APPEND "/ORFEO/thomas/ORFEO-TOOLBOX/otb/OTB-Documents/SoftwareGuide/install_manifest.txt" "${file}\n")
 ENDFOREACH(file)
