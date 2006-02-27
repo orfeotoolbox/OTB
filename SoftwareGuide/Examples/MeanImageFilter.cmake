825ea25e26cdc6822769ef12b952f01faec967d3
Thomas Feuvrier <thomas.feuvrier@c-s.fr>
2006-02-27 08:35:33 +0000

    nomsg

--- SoftwareGuide/Examples/MeanImageFilter.cmake
+++
@@ -1,6 +0,0 @@
-# Cmake macro to invoke: /ORFEO/thomas/ORFEO-TOOLBOX/OTB/bin/MeanImageFilter  /ORFEO/thomas/ORFEO-TOOLBOX/OTB/Examples/Data/Circle.png /home/ORFEO/thomas/ORFEO-TOOLBOX/OTB-Documents/SoftwareGuide/Art/Generated/CircleMeanOutput.png 10 10
-RUN_EXAMPLE( "MeanImageFilter" "CircleMeanOutput.png" "/ORFEO/thomas/ORFEO-TOOLBOX/OTB/Examples/StartExamples/MeanImageFilter.cxx"  /ORFEO/thomas/ORFEO-TOOLBOX/OTB/Examples/Data/Circle.png /home/ORFEO/thomas/ORFEO-TOOLBOX/OTB-Documents/SoftwareGuide/Art/Generated/CircleMeanOutput.png 10 10 )
-CONVERT_INPUT_IMG( "Circle.png" "Circle.eps" "/ORFEO/thomas/ORFEO-TOOLBOX/OTB/Examples/Data" )
-ADD_DEP_TEX_ON_EPS_FIGS( "/home/ORFEO/thomas/ORFEO-TOOLBOX/OTB-Documents/SoftwareGuide/Examples/MeanImageFilter.tex" "Circle.eps" )
-CONVERT_IMG( "CircleMeanOutput.png" "CircleMeanOutput.eps" "/home/ORFEO/thomas/ORFEO-TOOLBOX/OTB-Documents/SoftwareGuide/Art/Generated" )
-ADD_DEP_TEX_ON_EPS_FIGS( "/home/ORFEO/thomas/ORFEO-TOOLBOX/OTB-Documents/SoftwareGuide/Examples/MeanImageFilter.tex" "CircleMeanOutput.eps" )
