b8ff271ac97c8d67b4203d7be0d61a92504462df
Caroline Ruffel <caroline.ruffel@c-s.fr>
2006-03-08 12:05:47 +0000

    nomsg

---
+++ Testing/Code/ChangeDetection/otbChangeDetectionTests.cxx
@@ -0,0 +1,17 @@
+// this file defines the otbCommonTest for the test driver
+// and all it expects is that you have a function called RegisterTests
+#if defined(_MSC_VER)
+#pragma warning ( disable : 4786 )
+#endif
+#include <iostream>
+#include "otbTestMain.h" 
+
+void RegisterTests()
+{
+REGISTER_TEST(otbCBAMIChangeDetectionTest );
+REGISTER_TEST(otbCorrelChangeDetectionTest );
+REGISTER_TEST(otbMeanDiffChangeDetectionTest );
+REGISTER_TEST(otbMeanRatioChangeDetectionTest );
+REGISTER_TEST(otbLHMIChangeDetectionTest );
+REGISTER_TEST(otbJHMIChangeDetectionTest );
+}
