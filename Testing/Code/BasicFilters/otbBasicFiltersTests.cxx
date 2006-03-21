258f38cbdfb6fd391bfe509e936fdcc01227b22a
Thomas Feuvrier <thomas.feuvrier@c-s.fr>
2006-03-21 08:49:01 +0000

    nomsg

---
+++ Testing/Code/BasicFilters/otbBasicFiltersTests.cxx
@@ -0,0 +1,14 @@
+// this file defines the otbCommonTest for the test driver
+// and all it expects is that you have a function called RegisterTests
+#if defined(_MSC_VER)
+#pragma warning ( disable : 4786 )
+#endif
+
+#include <iostream>
+#include "otbTestMain.h" 
+
+void RegisterTests()
+{
+REGISTER_TEST(otbLeeFilter);
+REGISTER_TEST(otbFrostFilter);
+}
