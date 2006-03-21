fd058829969e9095f86f1ce730c97e7669bcfd46
Caroline Ruffel <caroline.ruffel@c-s.fr>
2006-03-21 14:20:29 +0000

    nomsg

---
+++ Testing/Code/FeatureExtraction/otbTouziEdgeDetectorNew.cxx
@@ -0,0 +1,60 @@
+/*=========================================================================
+
+  Programme :   OTB (ORFEO ToolBox)
+  Auteurs   :   CS - C.Ruffel
+  Language  :   C++
+  Date      :   09 mars 2006
+  Version   :   
+  Role      :   Test du filtre de detection de contours (instanciation) 
+  $Id$
+
+=========================================================================*/
+#if defined(_MSC_VER)
+#pragma warning ( disable : 4786 )
+#endif
+
+#define MAIN
+
+
+#include "itkExceptionObject.h"
+#include "itkImage.h"
+#include <iostream>
+
+#include "otbTouziEdgeDetector.h"
+
+
+int otbTouziEdgeDetectorNew( int argc, char* argv[] )
+{
+  try 
+    {         
+        typedef unsigned char                                   InputPixelType;
+        typedef double		   	                        OutputPixelType;
+        const   unsigned int        	                        Dimension = 2;
+
+        typedef itk::Image< InputPixelType,  Dimension >        InputImageType;
+        typedef itk::Image< OutputPixelType, Dimension >        OutputImageType;
+
+        typedef otb::TouziEdgeDetector< InputImageType,OutputImageType >   FilterType;
+	
+	
+        FilterType::Pointer filtreTouzi = FilterType::New();
+        
+    } 
+  catch( itk::ExceptionObject & err ) 
+    { 
+    std::cout << "Exception itk::ExceptionObject levee !" << std::endl; 
+    std::cout << err << std::endl; 
+    return EXIT_FAILURE;
+    } 
+  catch( ... ) 
+    { 
+    std::cout << "Exception levee inconnue !" << std::endl; 
+    return EXIT_FAILURE;
+    } 
+  // Software Guide : EndCodeSnippet
+
+//#endif
+  return EXIT_SUCCESS;
+}
+
+
