a867a8be3ccf2cea0aa3af98485837dc942a4269
Thomas Feuvrier <thomas.feuvrier@c-s.fr>
2006-04-05 09:46:00 +0000

    nomsg

---
+++ Testing/Code/Visu/otbImageViewer.cxx
@@ -0,0 +1,66 @@
+/*=========================================================================
+
+  Programme :   OTB (ORFEO ToolBox)
+  Auteurs   :   CS - T.Feuvrier
+  Language  :   C++
+  Date      :   11 janvier 2005
+  Version   :   
+  Role      :   
+  $Id$
+
+=========================================================================*/
+#include <stdlib.h>
+#include "itkExceptionObject.h"
+
+#include "otbImageFileReader.h"
+#include "otbImageViewer.h"
+
+int main( int argc, char ** argv )
+{
+  try 
+    { 
+        const char * inputFilename  = argv[1];
+
+        typedef otb::ImageViewer<unsigned char,double>    ViewerType;
+        ViewerType::Pointer  lViewer = ViewerType::New();
+
+        typedef ViewerType::ImageType ImageType;
+        typedef otb::ImageFileReader< ImageType > VolumeReaderType;
+        VolumeReaderType::Pointer       lReader = VolumeReaderType::New();
+        lReader->SetFileName(inputFilename);
+        lReader->Update();
+        std::cout << " GetNumberOfComponentsPerPixel = "<<lReader->GetOutput()->GetNumberOfComponentsPerPixel()<<std::endl;
+
+
+//        lViewer->SetLabel( "Image" );
+        lViewer->SetImage( lReader->GetOutput() );  
+
+//        lViewer->SetGrayLevelChannel(1);
+        lViewer->SetRGBChannels(1,2,3);
+//        lViewer->SetRedChannel(3);
+//        lViewer->SetGreenChannel(2);
+//        lViewer->SetBlueChannel(1);
+//        lViewer->Update();
+        lViewer->Show();
+        
+        Fl::run();
+
+    } 
+
+  catch( itk::ExceptionObject & err ) 
+    { 
+    std::cout << "Exception itk::ExceptionObject levee !" << std::endl; 
+    std::cout << err << std::endl; 
+    return EXIT_FAILURE;
+    } 
+  catch( ... ) 
+    { 
+    std::cout << "Exception levee inconnue !" << std::endl; 
+    return EXIT_FAILURE;
+    } 
+
+
+  return EXIT_SUCCESS;
+}
+
+
