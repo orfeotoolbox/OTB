9437ebbb29053133761a3d5682b4e47b8a5230b5
Jordi Inglada <jordi.inglada@orfeo-toolbox.org>
2006-04-06 10:11:35 +0000

    Mise a jour du chapitre Installation

--- Examples/Installation/HelloWorld.cxx
+++ Examples/Installation/HelloWorld.cxx
@@ -1,27 +1,39 @@
-// Software Guide : BeginLatex
+//  Software Guide : BeginLatex
 //
-// Cet exemple permet d'afficher sur la sortie standard un petit message de 'Bonjour'
+//  The following code is an implementation of a small OTB
+//  program. It tests including header files and linking with OTB
+//  libraries.
 //
-// Software Guide : EndLatex 
+//  Software Guide : EndLatex 
+
 // Software Guide : BeginCodeSnippet
-#include "otbBonjour.h"
+#include "itkImage.h"
 #include <iostream>
-#include <iostream>
-#include <string>
 
 int main()
 {
-	otb::Bonjour lBonjour;
-	std::string lString = lBonjour.getMessage();
-	std::cout << "Message : "<<lString<<std::endl;
-	return 0;
+  typedef itk::Image< unsigned short, 2 > ImageType;
+
+  ImageType::Pointer image = ImageType::New();
+
+  std::cout << "OTB Hello World !" << std::endl;
+
+  return 0;
 }
 // Software Guide : EndCodeSnippet
 
 //  Software Guide : BeginLatex
 //
-//  Ce code permet d'afficher sur la sortie standard, le message enregistre dans la classe \code{otb::Bonjour}.
+//  This code instantiates an image whose pixels are represented with
+//  type \code{unsigned short}. The image is then constructed and assigned to a
+//  \doxygen{SmartPointer}. Although later in the text we will discuss
+//  \code{SmartPointer}'s in detail, for now think of it as a handle on an
+//  instance of an object (see section \ref{sec:SmartPointers} for more
+//  information).  The \doxygen{Image} class will be described in
+//  Section~\ref{sec:ImageSection}.
 //
 //  Software Guide : EndLatex 
+
+
 
 
