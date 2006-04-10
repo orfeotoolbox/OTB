9e6f4930fc88320dc87ba37aa82d2b9c8c2082fe
Caroline Ruffel <caroline.ruffel@c-s.fr>
2006-04-10 14:20:21 +0000

    nomsg

---
+++ Testing/Code/FeatureExtraction/otbLineCorrelationDetectorLinearDirection.cxx
@@ -0,0 +1,89 @@
+/*=========================================================================
+
+  Programme :   OTB (ORFEO ToolBox)
+  Auteurs   :   CS - C.Ruffel
+  Language  :   C++
+  Date      :   5 avril 2006
+  Version   :   
+  Role      :   Test du filtre de detection de lignes par correlation
+  $Id$
+
+=========================================================================*/
+#if defined(_MSC_VER)
+#pragma warning ( disable : 4786 )
+#endif
+
+#define MAIN
+
+
+#include "itkExceptionObject.h"
+#include "itkImage.h"
+#include "itkImageFileWriter.h"
+#include <iostream>
+
+#include "otbImageFileReader.h"
+#include "otbImageFileWriter.h"
+#include "otbLineCorrelationDetector.h"
+
+#include "itkLinearInterpolateImageFunction.h"
+
+int otbLineCorrelationDetectorLinearDirection( int argc, char* argv[] )
+{
+  try 
+    { 
+        const char * inputFilename  = argv[1];
+        const char * outputFilename = argv[2];
+
+	// Largeur de la ligne à detecter = 2*WidthLine+1
+        unsigned int  WidthLine((unsigned int)::atoi(argv[3]));
+        // Longueur de la ligne à detecter = 2*LengthLine+1
+        unsigned int  LengthLine((unsigned int)::atoi(argv[4]));
+        
+        typedef unsigned char                                   InputPixelType;
+        typedef double		   	                        OutputPixelType;
+        const   unsigned int        	                        Dimension = 2;
+
+        typedef itk::Image< InputPixelType,  Dimension >        InputImageType;
+        typedef itk::Image< OutputPixelType, Dimension >        OutputImageType;
+
+        typedef otb::ImageFileReader< InputImageType  >         ReaderType;
+        typedef otb::ImageFileWriter< OutputImageType >         WriterType;
+
+        typedef itk::LinearInterpolateImageFunction< InputImageType, double >	InterpolatorType;
+        typedef otb::LineCorrelationDetector< InputImageType, OutputImageType, InterpolatorType >   FilterType;
+	
+        FilterType::Pointer FilterLineCorrelation = FilterType::New();
+        
+	FilterLineCorrelation->SetWidthLine( WidthLine );
+	FilterLineCorrelation->SetLengthLine( LengthLine );
+	
+        ReaderType::Pointer reader = ReaderType::New();
+        WriterType::Pointer writer = WriterType::New();
+
+        reader->SetFileName( inputFilename  );
+        writer->SetFileName( outputFilename );
+        
+        FilterLineCorrelation->SetInput( reader->GetOutput() );
+        writer->SetInput( FilterLineCorrelation->GetOutputDirection() );
+        
+        writer->Update();
+
+    } 
+  catch( itk::ExceptionObject & err ) 
+    { 
+    std::cout << "Exception itk::ExceptionObject levee !" << std::endl; 
+    std::cout << err << std::endl; 
+    return EXIT_FAILURE;
+    } 
+  catch( ... ) 
+    { 
+    std::cout << "Exception levee inconnue !" << std::endl; 
+    return EXIT_FAILURE;
+    } 
+  // Software Guide : EndCodeSnippet
+
+//#endif
+  return EXIT_SUCCESS;
+}
+
+
---
+++ Testing/Code/FeatureExtraction/otbLineRatioDetectorLinearDirection.cxx
@@ -0,0 +1,89 @@
+/*=========================================================================
+
+  Programme :   OTB (ORFEO ToolBox)
+  Auteurs   :   CS - C.Ruffel
+  Language  :   C++
+  Date      :   5 avril 2006
+  Version   :   
+  Role      :   Test du filtre de detection de lignes (image de direction)
+  $Id$
+
+=========================================================================*/
+#if defined(_MSC_VER)
+#pragma warning ( disable : 4786 )
+#endif
+
+#define MAIN
+
+
+#include "itkExceptionObject.h"
+#include "itkImage.h"
+#include "itkImageFileWriter.h"
+#include <iostream>
+
+#include "otbImageFileReader.h"
+#include "otbImageFileWriter.h"
+#include "otbLineRatioDetector.h"
+
+#include "itkLinearInterpolateImageFunction.h"
+
+int otbLineRatioDetectorLinearDirection( int argc, char* argv[] )
+{
+  try 
+    { 
+        const char * inputFilename  = argv[1];
+        const char * outputFilename = argv[2];
+
+	// Width of the linear feature = 2*WidthLine+1
+        unsigned int  WidthLine((unsigned int)::atoi(argv[3]));
+        // Length of the linear feature = 2*LengthLine+1
+        unsigned int  LengthLine((unsigned int)::atoi(argv[4]));
+        
+        typedef unsigned char                                   InputPixelType;
+        typedef double		   	                        OutputPixelType;
+        const   unsigned int        	                        Dimension = 2;
+
+        typedef itk::Image< InputPixelType,  Dimension >        InputImageType;
+        typedef itk::Image< OutputPixelType, Dimension >        OutputImageType;
+
+        typedef otb::ImageFileReader< InputImageType  >         ReaderType;
+        typedef otb::ImageFileWriter< OutputImageType >         WriterType;
+
+        typedef itk::LinearInterpolateImageFunction< InputImageType, double >	InterpolatorType;
+        typedef otb::LineRatioDetector< InputImageType, OutputImageType, InterpolatorType >   FilterType;
+	
+        FilterType::Pointer FilterLineRatio = FilterType::New();
+        
+	FilterLineRatio->SetWidthLine( WidthLine );
+	FilterLineRatio->SetLengthLine( LengthLine );
+	
+        ReaderType::Pointer reader = ReaderType::New();
+        WriterType::Pointer writer = WriterType::New();
+
+        reader->SetFileName( inputFilename  );
+        writer->SetFileName( outputFilename );
+        
+        FilterLineRatio->SetInput( reader->GetOutput() );
+        writer->SetInput( FilterLineRatio->GetOutputDirection() );
+        
+        writer->Update();
+
+    } 
+  catch( itk::ExceptionObject & err ) 
+    { 
+    std::cout << "Exception itk::ExceptionObject levee !" << std::endl; 
+    std::cout << err << std::endl; 
+    return EXIT_FAILURE;
+    } 
+  catch( ... ) 
+    { 
+    std::cout << "Exception levee inconnue !" << std::endl; 
+    return EXIT_FAILURE;
+    } 
+  // Software Guide : EndCodeSnippet
+
+//#endif
+  return EXIT_SUCCESS;
+}
+
+
