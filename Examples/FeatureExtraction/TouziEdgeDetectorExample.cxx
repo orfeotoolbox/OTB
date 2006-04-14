3da3489f16267c5c9a46fda7e8946ca5e6ee2eca
Jordi Inglada <jordi.inglada@orfeo-toolbox.org>
2006-04-14 14:50:01 +0000

    Exemple line ratio detector

--- Examples/FeatureExtraction/TouziEdgeDetectorExample.cxx
+++ Examples/FeatureExtraction/TouziEdgeDetectorExample.cxx
@@ -25,7 +25,7 @@
 
 // Software Guide : BeginLatex
 //
-// This example illustrates the use of the \doxygen{TouziEdgeDetector}.
+// This example illustrates the use of the \doxygen{otb::TouziEdgeDetector}.
 // This filter belongs to the family of the fixed false alarm rate
 // edge detectors but it is apropriate for SAR images, where the
 // speckle noise is considered as multiplicative. By analogy with the
@@ -182,7 +182,7 @@ int main( int argc, char * argv[] )
   //  Software Guide : BeginLatex
   //  
   //  The image obtained with the reader is passed as input to the
-  //  \doxygen{TouziEdgeDetector}. The pipeline is built as follows.
+  //  \doxygen{otb::TouziEdgeDetector}. The pipeline is built as follows.
   //
   //  \index{otb::TouziEdgeDetector!SetInput()}
   //
@@ -256,14 +256,13 @@ int main( int argc, char * argv[] )
   // \includegraphics[width=0.25\textwidth]{amstTouziEdges.eps}
   // \includegraphics[width=0.25\textwidth]{amstTouziDirections.eps}
   // \itkcaption[Touzi Edge Detector Application]{Result of applying the
-  // \doxygen{TouziEdgeDetector} to a SAR image. From left to right :
+  // \doxygen{otb::TouziEdgeDetector} to a SAR image. From left to right :
   // original image, edge intensity and edge orientation.} 
   // \label{fig:TOUZI_FILTER}
   // \end{figure}
   //
   //  Software Guide : EndLatex 
 
-  //  Software Guide : EndLatex 
 
 
   return EXIT_SUCCESS;
