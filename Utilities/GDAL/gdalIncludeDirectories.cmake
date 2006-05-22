81cff856eb189d4e98c6cd00795175e9b2220f35
Thomas Feuvrier <thomas.feuvrier@c-s.fr>
2006-05-22 14:50:37 +0000

    nomsg

--- Utilities/GDAL/gdalIncludeDirectories.cmake
+++ Utilities/GDAL/gdalIncludeDirectories.cmake
@@ -6,7 +6,12 @@ SET(GDAL_INCLUDE_DIRS_BUILD_TREE
   ${OTB_SOURCE_DIR}/Utilities/GDAL
   ${OTB_SOURCE_DIR}/Utilities/GDAL/frmts
   ${OTB_SOURCE_DIR}/Utilities/GDAL/frmts/raw
+  ${OTB_SOURCE_DIR}/Utilities/GDAL/frmts/sdts
+  ${OTB_SOURCE_DIR}/Utilities/GDAL/frmts/iso8211
   ${OTB_SOURCE_DIR}/Utilities/GDAL/frmts/nitf
+  ${OTB_SOURCE_DIR}/Utilities/GDAL/frmts/zlib
+  ${OTB_SOURCE_DIR}/Utilities/GDAL/frmts/jpeg
+  ${OTB_SOURCE_DIR}/Utilities/GDAL/frmts/jpeg/libjpeg
   ${OTB_SOURCE_DIR}/Utilities/GDAL/frmts/gtiff
   ${OTB_SOURCE_DIR}/Utilities/GDAL/frmts/gtiff/libtiff
   ${OTB_SOURCE_DIR}/Utilities/GDAL/frmts/gtiff/libgeotiff
@@ -15,6 +20,7 @@ SET(GDAL_INCLUDE_DIRS_BUILD_TREE
   ${OTB_SOURCE_DIR}/Utilities/GDAL/gcore
   ${OTB_SOURCE_DIR}/Utilities/GDAL/ogr
   ${OTB_SOURCE_DIR}/Utilities/GDAL/ogr/ogrsf_frmts
+  ${OTB_SOURCE_DIR}/Utilities/GDAL/ogr/ogrsf_frmts/shape
   ${OTB_SOURCE_DIR}/Utilities/GDAL/port
   ${OTB_SOURCE_DIR}/Utilities/GDAL/alg
 )
