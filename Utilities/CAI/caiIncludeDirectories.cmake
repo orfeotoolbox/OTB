f009eec17a35a3a82a4c49394f47a10a14f24956
Thomas Feuvrier <thomas.feuvrier@c-s.fr>
2006-05-29 08:36:19 +0000

    MAJ Config CAI

--- Utilities/CAI/caiIncludeDirectories.cmake
+++ Utilities/CAI/caiIncludeDirectories.cmake
@@ -1,18 +1,16 @@
 # $Id$
 
-
 #-----------------------------------------------------------------------------
 # Include directories from the build tree.
-IF(WIN32)
+IF(UNIX OR CYGWIN OR MINGW)
+        SET(CAI_INCLUDE_DIRS_BUILD_TREE
+                ${OTB_SOURCE_DIR}/Utilities/CAI
+                ${OTB_SOURCE_DIR}/Utilities/CAI/inc
+        )
+ELSE(UNIX OR CYGWIN OR MINGW)
         SET(CAI_INCLUDE_DIRS_BUILD_TREE 
                 ${OTB_SOURCE_DIR}/Utilities/CAI/cai_dll
                 ${OTB_SOURCE_DIR}/Utilities/CAI/cai_dll/inc
                 ${OTB_SOURCE_DIR}/Utilities/CAI/cai_dll/inc/inc_unix2win
         )
-ELSE(WIN32)
-        SET(CAI_INCLUDE_DIRS_BUILD_TREE
-                ${OTB_SOURCE_DIR}/Utilities/CAI
-                ${OTB_SOURCE_DIR}/Utilities/CAI/inc
-        )
-ENDIF(WIN32)
-
+ENDIF(UNIX OR CYGWIN OR MINGW)
