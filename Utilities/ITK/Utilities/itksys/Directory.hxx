f9e67bebd521bec94dcdac3ed4e7a0accf1e5bbf
Romain Garrigues <romain.garrigues@c-s.fr>
2006-05-30 07:18:17 +0000

    Ajout du répertoire ITK dans Utilities

---
+++ Utilities/ITK/Utilities/itksys/Directory.hxx
@@ -0,0 +1,74 @@
+/*=========================================================================
+
+  Program:   KWSys - Kitware System Library
+  Module:    $RCSfile: Directory.hxx.in,v $
+
+  Copyright (c) Kitware, Inc., Insight Consortium.  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef itksys_Directory_hxx
+#define itksys_Directory_hxx
+
+#include <itksys/Configure.h>
+
+namespace itksys
+{
+
+class DirectoryInternals;
+
+/** \class Directory
+ * \brief Portable directory/filename traversal.
+ * 
+ * Directory provides a portable way of finding the names of the files
+ * in a system directory.
+ *
+ * Directory currently works with Windows and Unix operating systems.
+ */
+class itksys_EXPORT Directory 
+{
+public:
+  Directory();
+  ~Directory();
+  
+  /**
+   * Load the specified directory and load the names of the files
+   * in that directory. 0 is returned if the directory can not be 
+   * opened, 1 if it is opened.   
+   */
+  bool Load(const char*);
+  
+  /**
+   * Return the number of files in the current directory.
+   */
+  unsigned long GetNumberOfFiles() const;
+  
+  /**
+   * Return the file at the given index, the indexing is 0 based
+   */
+  const char* GetFile(unsigned long) const;
+
+  /**
+   * Return the path to Open'ed directory
+   */
+  const char* GetPath() const;
+
+protected:
+  /**
+   * Clear the internal structure. Used internally at beginning of Load(...) to clear
+   * the cache.
+   */
+  void Clear();
+
+private:
+  // Private implementation details.
+  DirectoryInternals* Internal;
+}; // End Class: Directory
+
+} // namespace itksys
+
+#endif
