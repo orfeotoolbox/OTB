6de4c7eb01544269bfa1120df0fcc0dd5336e1a4
Jordi Inglada <jordi.inglada@orfeo-toolbox.org>
2006-06-12 10:42:50 +0000

    Ajout de otbSVMSampleListModelEstimator

---
+++ Code/Learning/otbSVMSampleListModelEstimator.h
@@ -0,0 +1,130 @@
+/*=========================================================================
+
+  Program :   OTB (ORFEO ToolBox)
+  Authors   :   CNES - J. Inglada
+  Language  :   C++
+  Date      :   12 June 2006
+  Version   :   
+  Role      :   SVM SampleList Model Estimator
+  $Id$
+
+
+=========================================================================*/
+#ifndef _otbSVMSampleListModelEstimator_h
+#define _otbSVMSampleListModelEstimator_h
+
+#include "itkImageModelEstimatorBase.h"
+#include "itkImageRegionIterator.h"
+#include "otbSVMModel.h"
+#include "otbSVMModelEstimator.h"
+
+namespace otb
+{
+
+/** \class SVMSampleListModelEstimator \brief Class for SVM model
+ * estimation from SampleLists used for classification.
+ * 
+ *
+ * The basic functionality of the SVMSampleListModelEstimator is to
+ * generate the models used in SVM classification. It 
+ * requires one input poinset and a training pointset to be provided by the
+ * user.  This object supports data handling of multiband data. The
+ * object accepts the input image in vector format only, where each
+ * pixel is a vector and each element of the vector corresponds to an
+ * entry from 1 particular band of a multiband dataset. A single band
+ * data set is treated as a vector with a single element for every
+ * vector. The classified data is treated as a single band scalar
+ * data.
+ *
+ * EstimateModels() uses the libsvm library for SVM learning.
+ *
+ * \ingroup ClassificationFilters 
+ */
+template <class TInputSampleList, 
+          class TTrainingSampleList>
+class ITK_EXPORT SVMSampleListModelEstimator: 
+    public otb::SVMModelEstimator<typename TInputSampleList::ValueType, typename TTrainingSampleList::ValueType>
+{
+public:
+  /** Standard class typedefs. */
+  typedef SVMSampleListModelEstimator   Self;
+  typedef SVMModelEstimator<typename TInputSampleList::ValueType,
+			    typename TTrainingSampleList::ValueType>
+                            Superclass;
+
+  typedef itk::SmartPointer<Self>  Pointer;
+  typedef itk::SmartPointer<const Self>  ConstPointer;
+
+  /** Method for creation through the object factory. */
+  itkNewMacro(Self);
+
+  /** Run-time type information (and related methods). */
+  itkTypeMacro(SVMSampleListModelEstimator, otb::SVMModelEstimator);
+
+  /** Type definition for the input image. */
+    /** Type definitions for the training image. */
+  typedef typename TInputSampleList::Pointer   InputSampleListPointer;
+ 
+  /** Type definitions for the training image. */
+  typedef typename TTrainingSampleList::Pointer TrainingSampleListPointer;
+    
+  /** Type definition for the vector associated with
+   * input image pixel type. */     
+  typedef typename TInputSampleList::ValueType InputSampleListValueType;
+
+  /** Type definitions for the vector holding
+   * training image pixel type. */
+  typedef typename TTrainingSampleList::ValueType TrainingSampleListValueType;
+
+  /** Type definitions for the iterators for the input and training images. */
+  typedef typename
+  TInputSampleList::ConstIterator  InputSampleListIteratorType;
+  typedef typename
+  TTrainingSampleList::ConstIterator TrainingSampleListIteratorType;
+
+  /** Set the input image. */
+  itkSetObjectMacro(InputSampleList,TInputSampleList);
+  
+  /** Get the input image. */
+  itkGetObjectMacro(InputSampleList,TInputSampleList);
+
+
+  /** Set the training image. */
+  itkSetMacro(TrainingSampleList,TrainingSampleListPointer);
+
+  /** Get the training image. */
+  itkGetMacro(TrainingSampleList,TrainingSampleListPointer);
+
+
+
+protected: 
+  SVMSampleListModelEstimator();
+  ~SVMSampleListModelEstimator();
+  virtual void PrintSelf(std::ostream& os, itk::Indent indent) const;
+
+
+  virtual void BuildProblem();
+
+private:
+  SVMSampleListModelEstimator(const Self&); //purposely not implemented
+  void operator=(const Self&); //purposely not implemented
+
+
+
+  TrainingSampleListPointer  m_TrainingSampleList;
+  InputSampleListPointer  m_InputSampleList;
+
+
+
+}; // class SVMSampleListModelEstimator
+
+} // namespace otb
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "otbSVMSampleListModelEstimator.txx"
+#endif
+
+
+
+#endif
+
---
+++ Code/Learning/otbSVMSampleListModelEstimator.txx
@@ -0,0 +1,150 @@
+/*=========================================================================
+
+  Program :   OTB (ORFEO ToolBox)
+  Authors   :   CNES - J. Inglada
+  Language  :   C++
+  Date      :   4 May 2006
+  Version   :   
+  Role      :   SVM SampleList Model Estimator
+  $Id$
+
+
+=========================================================================*/
+#ifndef _otbSVMSampleListModelEstimator_txx
+#define _otbSVMSampleListModelEstimator_txx
+
+#include "otbSVMSampleListModelEstimator.h"
+#include "itkCommand.h"
+
+
+namespace otb
+{
+template<class TInputSampleList, 
+         class TTrainingSampleList>
+SVMSampleListModelEstimator<TInputSampleList, TTrainingSampleList>
+::SVMSampleListModelEstimator(void):  SVMModelEstimator<ITK_TYPENAME TInputSampleList::ValueType,
+    ITK_TYPENAME TTrainingSampleList::ValueType>()
+
+{
+
+
+}
+
+
+template<class TInputSampleList, 
+         class TTrainingSampleList>
+SVMSampleListModelEstimator<TInputSampleList, TTrainingSampleList>
+::~SVMSampleListModelEstimator(void)
+{
+}
+
+/*
+ * PrintSelf
+ */
+template<class TInputSampleList, 
+         class TTrainingSampleList>
+void
+SVMSampleListModelEstimator<TInputSampleList, TTrainingSampleList>
+::PrintSelf( std::ostream& os, itk::Indent indent ) const
+{  
+  // FIXME : print useful SVM information
+//   os << indent << "                   " << std::endl;
+//   os << indent << "Gaussian Models generated from the training data." << std::endl;
+//   os << indent << "TrainingSampleList: " ;
+//   os << m_TrainingSampleList.GetPointer() << std::endl;
+//   os << indent << "Results printed in the superclass " << std::endl;
+//   os << indent << "                   " << std::endl;
+
+  Superclass::PrintSelf(os,indent);
+
+}// end PrintSelf
+
+
+/**
+ * Generate data (start the model building process)
+ */
+
+
+template<class TInputSampleList, 
+         class TTrainingSampleList>
+void
+SVMSampleListModelEstimator<TInputSampleList,  TTrainingSampleList>
+::BuildProblem()
+{
+
+    //Do some error checking
+  InputSampleListPointer  inputSampleList = this->GetInputSampleList();
+  TrainingSampleListPointer  trainingSampleList = this->GetTrainingSampleList();
+
+
+  int  inputSampleListSize = inputSampleList->Size();
+
+  int trainingSampleListSize = trainingSampleList->Size();
+
+  // Check if size of the two inputs are same
+  if( inputSampleListSize != trainingSampleListSize ) throw itk::ExceptionObject(__FILE__, __LINE__,"Input pointset size is not the same as the training pointset size.",ITK_LOCATION); 
+  
+
+  // Declaration of the iterators on the input and training images
+
+
+  InputSampleListIteratorType inIt = inputSampleList->Begin();
+  TrainingSampleListIteratorType trIt = trainingSampleList->Begin();
+
+  InputSampleListIteratorType inEnd = inputSampleList->End();
+  TrainingSampleListIteratorType trEnd = trainingSampleList->End();
+
+
+  
+  // Erase the vector contents
+  this->m_Measures.resize(0);
+  this->m_Labels.resize(0);
+  
+
+  std::cout << " Input nb points " << inputSampleListSize <<std::endl;
+  std::cout << " Training nb points " << trainingSampleListSize <<std::endl;
+  
+
+  std::cout << " Before while " << std::endl;
+
+  while(inIt!=inEnd && trIt!=trEnd)
+    {
+
+    // If label != 0 
+
+    typename TTrainingSampleList::ValueType label =
+      trIt.GetMeasurementVector()[0];
+
+    this->m_Labels.push_back(label);
+
+    std::cout << " Label " << label << std::endl;
+
+    typename TInputSampleList::MeasurementVectorType value =
+      inIt.GetMeasurementVector();
+
+    typename Superclass::MeasurementVectorType v;
+    
+    typename TInputSampleList::MeasurementVectorType::ConstIterator pIt = value.Begin();
+    typename TInputSampleList::MeasurementVectorType::ConstIterator pEnd = value.End();
+
+    while(pIt!=pEnd)
+      {
+      v.push_back(*pIt);
+      ++pIt;
+      }
+    
+    this->m_Measures.push_back(v);
+    
+    ++inIt;
+    ++trIt;
+    }
+
+  std::cout << " Before prepare data " << std::endl;
+  this->PrepareData();
+  std::cout << " After prepare data " << std::endl;
+}
+
+
+
+}//End namespace OTB
+#endif
