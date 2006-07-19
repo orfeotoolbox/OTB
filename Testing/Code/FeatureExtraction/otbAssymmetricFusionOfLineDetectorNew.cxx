c2e144c74d7e9b9905df984d8c34dda83ac1502b
Thomas Feuvrier <thomas.feuvrier@c-s.fr>
2006-07-19 15:15:33 +0000

    Correction : suppression des parametres lus dans la ligne de commande, car inutile et plante sous Windows.

--- Testing/Code/FeatureExtraction/otbAssymmetricFusionOfLineDetectorNew.cxx
+++ Testing/Code/FeatureExtraction/otbAssymmetricFusionOfLineDetectorNew.cxx
@@ -37,14 +37,6 @@ int otbAssymmetricFusionOfLineDetectorNew( int argc, char * argv[] )
 {
   try 
     { 
-        const char * inputFilename  = argv[1];
-        const char * outputFilename = argv[2];
-        
-	// Width of the linear feature = 2*WidthLine+1
-        unsigned int  WidthLine((unsigned int)::atoi(argv[3]));
-        // Length of the linear feature = 2*LengthLine+1
-        unsigned int  LengthLine((unsigned int)::atoi(argv[4]));
-        
         typedef unsigned char                                   InputPixelType;
         typedef double		   	                        OutputPixelType;
         const   unsigned int        	                        Dimension = 2;
@@ -52,16 +44,11 @@ int otbAssymmetricFusionOfLineDetectorNew( int argc, char * argv[] )
         typedef itk::Image< InputPixelType,  Dimension >        InputImageType;
         typedef itk::Image< OutputPixelType, Dimension >        OutputImageType;
 
-        typedef otb::ImageFileReader< InputImageType  >         ReaderType;
-        typedef otb::ImageFileWriter< OutputImageType >         WriterType;
-
         typedef itk::LinearInterpolateImageFunction< InputImageType, double >	InterpolatorType;
 	
         typedef otb::AssymmetricFusionOfLineDetectorImageFilter< InputImageType, OutputImageType, InterpolatorType >   FilterType;
   
         FilterType::Pointer FilterAssSymSum = FilterType::New();
-        
-
 
     } 
   catch( itk::ExceptionObject & err ) 
