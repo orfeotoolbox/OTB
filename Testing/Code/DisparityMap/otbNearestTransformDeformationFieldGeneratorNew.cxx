27dcf550532d8e87dfdaab8708aa541da10d293c
Julien Michel <julien.michel@c-s.fr>
2007-04-10 09:32:22 +0000

    Ajout de l'estimation de la déformation par plus proche transformation locale.

---
+++ Testing/Code/DisparityMap/otbNearestTransformDeformationFieldGeneratorNew.cxx
@@ -0,0 +1,51 @@
+/*=========================================================================
+
+  Program:   ORFEO Toolbox
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
+
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#include "itkExceptionObject.h"
+#include "itkPointSet.h"
+#include "otbVectorImage.h"
+#include "otbNearestTransformDeformationFieldGenerator.h"
+
+int otbNearestTransformDeformationFieldGeneratorNew(int argc, char * argv[])
+{
+  try
+    {
+      const unsigned int Dimension = 2;
+      typedef double PixelType;
+      typedef otb::VectorImage<PixelType,Dimension> ImageType;
+      typedef ImageType::PointType PointType;
+      typedef itk::PointSet<PointType,Dimension> PointSetType;
+      typedef otb::NearestTransformDeformationFieldGenerator<PointSetType,ImageType> FilterType;
+
+      // Instantiating object
+      FilterType::Pointer filter = FilterType::New();
+    }
+
+  catch( itk::ExceptionObject & err ) 
+    { 
+    std::cout << "Exception itk::ExceptionObject thrown !" << std::endl; 
+    std::cout << err << std::endl; 
+    return EXIT_FAILURE;
+    } 
+
+  catch( ... ) 
+    { 
+    std::cout << "Unknown exception thrown !" << std::endl; 
+    return EXIT_FAILURE;
+    } 
+  return EXIT_SUCCESS;
+}
