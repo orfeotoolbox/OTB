0c90e702d338a2aee2a145fc923453353bb70f2f
Thomas Feuvrier <thomas.feuvrier@c-s.fr>
2007-08-24 15:02:34 +0000

    add aaa_com.c

---
+++ Utilities/otb6S/6SV1.0B/aaa_com.c
@@ -0,0 +1,16 @@
+
+/* OTB patches: replace "f2c.h" by "otb_6S_f2c.h" */
+/*#include "f2c.h"*/
+#include "otb_6S_f2c.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct {
+    doublereal rrl, ttl, ul, sl, clmp, clmp1, bi, bd, bqint;
+} aaa_;
+
+#ifdef __cplusplus
+}
+#endif
