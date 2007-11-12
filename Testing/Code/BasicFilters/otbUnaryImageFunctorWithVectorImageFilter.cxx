3686683e15faa738f466cbd654136f7ef69c7c7d
Cyrille Valladeau <cyrille.valladeau@c-s.fr>
2007-11-12 13:42:13 +0000

    Ajout de classes de Radiometrie : image->Luminance, image->Reflectance et luminance->reflectance. + création de la classe UnaryImageucntorWithVectorImage qui permet de travail avec des images vecteur et un functor prennant des InternalPixelType.

---
+++ Testing/Code/BasicFilters/otbUnaryImageFunctorWithVectorImageFilter.cxx
@@ -0,0 +1,71 @@
+/*=========================================================================
+
+  Program:   ORFEO Toolbox
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
+
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#include "itkExceptionObject.h"
+
+#include "otbUnaryImageFunctorWithVectorImageFilter.h"
+#include "otbVectorImage.h"
+#include "otbImage.h"
+#include "otbImageFileReader.h"
+#include "otbImageFileWriter.h"
+#include "itkCosImageFilter.h"
+
+int otbUnaryImageFunctorWithVectorImageFilter(int argc, char * argv[])
+{
+  try
+    {
+      const char * inputFileName  = argv[1];
+      const char * outputFileName = argv[2];
+
+      const unsigned int Dimension = 2;
+      typedef double PixelType;
+      typedef otb::VectorImage<PixelType,Dimension>                           InputImageType;
+      typedef InputImageType::InternalPixelType                               InternalPixelType;
+      typedef itk::Functor::Cos<InternalPixelType,InternalPixelType>          FunctorType;
+      typedef otb::VectorImage<PixelType,Dimension>                           OutputImageType;
+      typedef otb::ImageFileReader<InputImageType>                            ReaderType;
+      typedef otb::ImageFileWriter<OutputImageType>                           WriterType;
+      typedef otb::UnaryImageFunctorWithVectorImageFilter<InputImageType,
+	                                                  InputImageType,
+	                                                  FunctorType    >    UnaryImageFunctorWithVectorImageFilterType;
+      
+      // Instantiating object
+      UnaryImageFunctorWithVectorImageFilterType::Pointer filter = UnaryImageFunctorWithVectorImageFilterType::New();
+      ReaderType::Pointer reader  = ReaderType::New();
+      WriterType::Pointer writer = WriterType::New();
+
+      reader->SetFileName(inputFileName);
+      writer->SetFileName(outputFileName);
+      filter->SetInput(reader->GetOutput());
+      writer->SetInput(filter->GetOutput());
+      writer->Update();
+    }
+
+  catch( itk::ExceptionObject & err ) 
+    { 
+    std::cout << "Exception itk::ExceptionObject thrown !" << std::endl; 
+    std::cout << err << std::endl; 
+    return EXIT_FAILURE;
+    } 
+
+  catch( ... ) 
+    { 
+    std::cout << "Unknown exception thrown !" << std::endl; 
+    return EXIT_FAILURE;
+    } 
+  return EXIT_SUCCESS;
+}
