eb21d9af48df5242f899e2ff31e719f2474c72e6
Cyrille Valladeau <cyrille.valladeau@c-s.fr>
2007-11-12 13:46:42 +0000

    Ajout de classes de Radiometrie : image->Luminance, image->Reflectance et luminance->reflectance. + création de la classe UnaryImageucntorWithVectorImage qui permet de travail avec des images vecteur et un functor prennant des InternalPixelType.

---
+++ Code/BasicFilters/otbUnaryImageFunctorWithVectorImageFilter.h
@@ -0,0 +1,141 @@
+/*=========================================================================
+
+  Program:   ORFEO Toolbox
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
+
+  This code is from a Julien Radoux contribution.
+  
+  Some parts of this code are derived from ITK. See ITKCopyright.txt
+  for details.
+
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __otbUnaryImageFunctorWithVectorImageFilter_h
+#define __otbUnaryImageFunctorWithVectorImageFilter_h
+
+#include "itkImageToImageFilter.h"
+#include "otbVectorImage.h"
+#include "itkNumericTraits.h"
+#include "itkVariableLengthVector.h"
+#include "itkUnaryFunctorImageFilter.h"
+#include "itkInPlaceImageFilter.h"
+
+namespace otb
+{
+ 
+template <class TInputImage, class TOutputImage, class TFunction>
+class ITK_EXPORT UnaryImageFunctorWithVectorImageFilter : public itk::InPlaceImageFilter<TInputImage,TOutputImage> 
+ 
+{
+public:
+  /** Standard class typedefs. */
+  typedef UnaryImageFunctorWithVectorImageFilter             Self;
+  typedef itk::InPlaceImageFilter<TInputImage,TOutputImage>  Superclass;
+  typedef itk::SmartPointer<Self>                            Pointer;
+  typedef itk::SmartPointer<const Self>                      ConstPointer;
+
+  /** Method for creation through the object factory. */
+  itkNewMacro(Self);
+  
+
+  /** 	Extract input and output images dimensions.*/
+  itkStaticConstMacro( InputImageDimension, unsigned int, TInputImage::ImageDimension);
+  itkStaticConstMacro( OutputImageDimension, unsigned int, TOutputImage::ImageDimension);
+
+  /** "typedef" to simplify the variables definition and the declaration. */
+  typedef TInputImage         InputImageType;
+  typedef TOutputImage        OutputImageType;
+  typedef TFunction           FunctorType;
+  
+  /** Supported images definition. */
+  typedef typename InputImageType::PixelType                           InputPixelType;
+  typedef typename InputImageType::InternalPixelType                   InputInternalPixelType;
+  typedef typename InputImageType::RegionType                          InputImageRegionType;
+  typedef typename OutputImageType::PixelType                          OutputPixelType;
+  typedef typename OutputImageType::InternalPixelType                  OutputInternalPixelType;
+  typedef typename OutputImageType::RegionType                         OutputImageRegionType;
+
+  /** Image size "typedef" definition. */
+  typedef typename InputImageType::SizeType SizeType;
+
+
+  /** return class name. */
+  itkTypeMacro(UnaryImageFunctorWithVectorImageFilter, itk::InPlaceImageFilter);
+  
+
+
+   /** Get the functor object.  The functor is returned by reference. 
+    * (Functors do not have to derive from itk::LightObject, so they do 
+    * not necessarily have a reference count. So we cannot return a 
+    * SmartPointer.) */
+   FunctorType& GetFunctor() { return m_Functor; }; 
+   const FunctorType& GetFunctor() const { return m_Functor; }; 
+  
+   /** Set the functor object.  This replaces the current Functor with a 
+    * copy of the specified Functor. This allows the user to specify a 
+    * functor that has ivars set differently than the default functor.
+    * This method requires an operator!=() be defined on the functor 
+    * (or the compiler's default implementation of operator!=() being 
+    * appropriate). */
+   void SetFunctor(const FunctorType& functor) 
+     { 
+       if (m_Functor != functor) 
+ 	{ 
+ 	  m_Functor = functor; 
+ 	  this->Modified(); 
+ 	} 
+     } 
+   
+
+ protected:
+  UnaryImageFunctorWithVectorImageFilter();
+  virtual ~UnaryImageFunctorWithVectorImageFilter(){};
+
+  /** UnaryImageFunctorWithVectorImageFilter can produce an image which is a different
+   * resolution than its input image.  As such, UnaryImageFunctorWithVectorImageFilter
+   * needs to provide an implementation for
+   * GenerateOutputInformation() in order to inform the pipeline
+   * execution model.  The original documentation of this method is
+   * below.
+   *
+   * \sa ProcessObject::GenerateOutputInformaton()  */
+  virtual void GenerateOutputInformation();
+
+
+  /** UnaryImageFunctorWithVectorImageFilter can be implemented as a multithreaded filter.
+   * Therefore, this implementation provides a ThreadedGenerateData() routine
+   * which is called for each processing thread. The output image data is
+   * allocated automatically by the superclass prior to calling
+   * ThreadedGenerateData().  ThreadedGenerateData can only write to the
+   * portion of the output image specified by the parameter
+   * "outputRegionForThread"
+   *
+   * \sa ImageToImageFilter::ThreadedGenerateData(),
+   *     ImageToImageFilter::GenerateData()  */
+  void ThreadedGenerateData(const OutputImageRegionType &outputRegionForThread, int threadId);
+
+  void PrintSelf(std::ostream& os, itk::Indent indent) const;
+                        
+private:
+  /** Ponderation declaration*/
+  FunctorType m_Functor;
+
+};
+
+} // end namespace otb
+
+#ifndef OTB_MANUAL_INSTANTIATION
+#include "otbUnaryImageFunctorWithVectorImageFilter.txx"
+#endif
+
+#endif
