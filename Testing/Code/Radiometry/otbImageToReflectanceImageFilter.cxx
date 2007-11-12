a63ba5ed241d7691dd900efe897a18e1c7791414
Cyrille Valladeau <cyrille.valladeau@c-s.fr>
2007-11-12 13:50:47 +0000

    Ajout de classes de Radiometrie : image->Luminance, image->Reflectance et luminance->reflectance. + création de la classe UnaryImageucntorWithVectorImage qui permet de travail avec des images vecteur et un functor prennant des InternalPixelType.

---
+++ Testing/Code/Radiometry/otbImageToReflectanceImageFilter.cxx
@@ -0,0 +1,117 @@
+/*=========================================================================
+
+  Program:   ORFEO Toolbox
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
+
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#include "itkExceptionObject.h"
+
+#include "otbImageToReflectanceImageFilter.h"
+#include "otbVectorImage.h"
+#include "otbImageFileReader.h"
+#include "otbImageFileWriter.h"
+#include "itkVariableLengthVector.h"
+
+
+int otbImageToReflectanceImageFilter(int argc, char * argv[])
+{
+  try
+    {
+      const char * inputFileName  = argv[1];
+      const char * outputFileName = argv[2];
+      const double radius = static_cast<double>(atof(argv[3]));
+      double flux = 0.;
+      int day = 1;
+      int mounth = 1;
+
+      if (argc==17)
+	{
+	  flux = static_cast<double>(atof(argv[16]));
+	}
+      else
+	{
+	  day = atoi(argv[16]);
+	  mounth = atoi(argv[17]); 
+	}
+
+      const unsigned int Dimension = 2;
+      typedef double PixelType;
+      typedef otb::VectorImage<PixelType,Dimension> InputImageType;
+      typedef otb::VectorImage<PixelType,Dimension> OutputImageType;
+      typedef otb::ImageFileReader<InputImageType>  ReaderType;
+      typedef otb::ImageFileWriter<OutputImageType> WriterType;
+      typedef otb::ImageToReflectanceImageFilter<InputImageType,OutputImageType> ImageToReflectanceImageFilterType;
+      typedef ImageToReflectanceImageFilterType::VectorType VectorType; 
+
+      ReaderType::Pointer reader  = ReaderType::New();
+      WriterType::Pointer writer = WriterType::New();
+      reader->SetFileName(inputFileName);
+      writer->SetFileName(outputFileName);
+      reader->GenerateOutputInformation();
+
+      unsigned int nbOfComponent = reader->GetOutput()->GetNumberOfComponentsPerPixel();
+
+      VectorType alpha(nbOfComponent);
+      VectorType beta(nbOfComponent);
+      VectorType solarIllumination(nbOfComponent);
+      alpha.Fill(0);
+      beta.Fill(0);
+      solarIllumination.Fill(0);
+
+      for (unsigned int i=0; i<nbOfComponent; i++)
+	{
+	  alpha[i] = static_cast<double>(atof(argv[i+4]));
+	  beta[i] = static_cast<double>(atof(argv[i+8]));
+	  solarIllumination[i] = static_cast<double>(atof(argv[i+12]));
+	}
+    
+      
+      // Instantiating object
+      ImageToReflectanceImageFilterType::Pointer filter = ImageToReflectanceImageFilterType::New();
+
+      filter->SetAlpha(alpha);
+      filter->SetBeta(beta);
+      filter->SetZenithalSolarRadius(radius);
+      filter->SetSolarIllumination(solarIllumination);
+
+      if (argc==17)
+	{
+	  filter->SetFluxNormalizationCoefficient(flux);
+	}
+      else
+	{
+	  filter->SetDay(day);
+	  filter->SetMounth(mounth); 
+	}
+
+      filter->SetInput(reader->GetOutput());
+      writer->SetInput(filter->GetOutput());
+      writer->Update();
+
+    }
+
+  catch( itk::ExceptionObject & err ) 
+    { 
+    std::cout << "Exception itk::ExceptionObject thrown !" << std::endl; 
+    std::cout << err << std::endl; 
+    return EXIT_FAILURE;
+    } 
+
+  catch( ... ) 
+    { 
+    std::cout << "Unknown exception thrown !" << std::endl; 
+    return EXIT_FAILURE;
+    } 
+  return EXIT_SUCCESS;
+}
---
+++ Testing/Code/Radiometry/otbLuminanceToReflectanceImageFilter.cxx
@@ -0,0 +1,108 @@
+/*=========================================================================
+
+  Program:   ORFEO Toolbox
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
+
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#include "itkExceptionObject.h"
+
+#include "otbLuminanceToReflectanceImageFilter.h"
+#include "otbVectorImage.h"
+#include "otbImageFileReader.h"
+#include "otbImageFileWriter.h"
+#include "itkVariableLengthVector.h"
+
+
+int otbLuminanceToReflectanceImageFilter(int argc, char * argv[])
+{
+  try
+    {
+      const char * inputFileName  = argv[1];
+      const char * outputFileName = argv[2];
+      const double radius = static_cast<double>(atof(argv[3]));
+      double flux = 0.;
+      int day = 1;
+      int mounth = 1;
+
+      if (argc==9)
+	{
+	  flux = static_cast<double>(atof(argv[8]));
+	}
+      else
+	{
+	  day = atoi(argv[8]);
+	  mounth = atoi(argv[9]); 
+	}
+
+      const unsigned int Dimension = 2;
+      typedef double PixelType;
+      typedef otb::VectorImage<PixelType,Dimension> InputImageType;
+      typedef otb::VectorImage<PixelType,Dimension> OutputImageType;
+      typedef otb::ImageFileReader<InputImageType>  ReaderType;
+      typedef otb::ImageFileWriter<OutputImageType> WriterType;
+      typedef otb::LuminanceToReflectanceImageFilter<InputImageType,OutputImageType> LuminanceToReflectanceImageFilterType;
+      typedef LuminanceToReflectanceImageFilterType::VectorType VectorType; 
+
+      ReaderType::Pointer reader  = ReaderType::New();
+      WriterType::Pointer writer = WriterType::New();
+      reader->SetFileName(inputFileName);
+      writer->SetFileName(outputFileName);
+      reader->GenerateOutputInformation();
+ 
+      unsigned int nbOfComponent = reader->GetOutput()->GetNumberOfComponentsPerPixel();
+
+      VectorType solarIllumination(nbOfComponent);
+      solarIllumination.Fill(0);
+      solarIllumination[0] = static_cast<double>(atof(argv[4]));
+      solarIllumination[1] = static_cast<double>(atof(argv[5]));
+      solarIllumination[2] = static_cast<double>(atof(argv[6]));
+      solarIllumination[3] = static_cast<double>(atof(argv[7]));
+    
+ 
+      // Instantiating object
+      LuminanceToReflectanceImageFilterType::Pointer filter = LuminanceToReflectanceImageFilterType::New();
+
+      filter->SetZenithalSolarRadius(radius);
+      filter->SetSolarIllumination(solarIllumination);
+      if (argc==9)
+	{
+	  filter->SetFluxNormalizationCoefficient(flux);
+	}
+      else
+	{
+	  filter->SetDay(day);
+	  filter->SetMounth(mounth); 
+	}
+      
+
+      filter->SetInput(reader->GetOutput());
+      writer->SetInput(filter->GetOutput());
+      writer->Update();
+
+    }
+
+  catch( itk::ExceptionObject & err ) 
+    { 
+    std::cout << "Exception itk::ExceptionObject thrown !" << std::endl; 
+    std::cout << err << std::endl; 
+    return EXIT_FAILURE;
+    } 
+
+  catch( ... ) 
+    { 
+    std::cout << "Unknown exception thrown !" << std::endl; 
+    return EXIT_FAILURE;
+    } 
+  return EXIT_SUCCESS;
+}
