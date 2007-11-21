3869c76eaf46559e3660ffc0793682e40f88b276
Thomas Feuvrier <thomas.feuvrier@c-s.fr>
2007-11-21 15:41:47 +0000

    Création de la fonction main de 6S et création du main qui appelle cette fonction.

---
+++ Testing/Utilities/6SMainOtbWithCallFunctionTest.cxx
@@ -0,0 +1,17 @@
+// iostream is used for general output
+//
+
+/* main.f -- translated by f2c (version 19970805).
+   You must link the resulting object file with the libraries:
+	-lf2c -lm   (in that order)
+*/
+
+#include "otb_6S.h"
+#include "main_6s.h"
+
+
+/* OTB test main */
+int SIXSMainOtbWithCallFunctionTest(int argc, char* argv[])
+{
+   return (call6s_());
+}
