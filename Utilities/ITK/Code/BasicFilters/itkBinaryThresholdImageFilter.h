2002ce508445b2def4f0e2cce79a61d1924aab10
Emmanuel Christophe <emmanuel.christophe@orfeo-toolbox.org>
2007-11-29 16:24:39 +0000

    doxy

--- Utilities/ITK/Code/BasicFilters/itkBinaryThresholdImageFilter.h
+++ Utilities/ITK/Code/BasicFilters/itkBinaryThresholdImageFilter.h
@@ -36,8 +36,7 @@ namespace itk
  *
  * More precisely
  * \f[ Output(x_i) =
-      \begin{cases}
-        InsideValue & \text{if $LowerThreshold \leq x_i \leq UpperThreshold$}
+      \begin{cases} InsideValue & \text{if $LowerThreshold \leq x_i \leq UpperThreshold$}
         \\
         OutsideValue & \text{otherwise}
       \end{cases}
