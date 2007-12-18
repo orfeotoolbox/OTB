ff9ff2e68f9cc53493f05739700c72bc313df335
Cyrille Valladeau <cyrille.valladeau@c-s.fr>
2007-12-18 17:08:08 +0000

    Decoupage des exe de IO.

---
+++ Testing/Code/IO/otbIOTests12.cxx
@@ -0,0 +1,38 @@
+/*=========================================================================
+
+  Program:   ORFEO Toolbox
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
+
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+// this file defines the otbCommonTest for the test driver
+// and all it expects is that you have a function called RegisterTests
+#if defined(_MSC_VER)
+#pragma warning ( disable : 4786 )
+#endif
+
+#include <iostream>
+#include "otbTestMain.h" 
+
+
+void RegisterTests()
+{
+REGISTER_TEST(otbOSSIMImageMetaDataReaderTest);
+REGISTER_TEST(otbDEMHandlerNew);
+REGISTER_TEST(otbDEMHandlerTest);
+REGISTER_TEST(otbDEMToImageGeneratorNew);
+REGISTER_TEST(otbDEMToImageGeneratorTest);
+REGISTER_TEST(otbImageGeometryHandlerNew);
+REGISTER_TEST(otbOssimElevManagerTest);
+}
---
+++ Testing/Code/IO/otbIOTests9.cxx
@@ -0,0 +1,38 @@
+/*=========================================================================
+
+  Program:   ORFEO Toolbox
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
+
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+// this file defines the otbCommonTest for the test driver
+// and all it expects is that you have a function called RegisterTests
+#if defined(_MSC_VER)
+#pragma warning ( disable : 4786 )
+#endif
+
+#include <iostream>
+#include "otbTestMain.h" 
+
+
+void RegisterTests()
+{
+REGISTER_TEST(otbImageTest);
+REGISTER_TEST(otbVectorImageTest);
+REGISTER_TEST(otbStreamingImageFileWriterTest);
+REGISTER_TEST(otbStreamingImageFilterTest);
+REGISTER_TEST(otbVectorImageFileReaderWriterTest);
+REGISTER_TEST(otbStreamingShortImageFileWriterTest);
+REGISTER_TEST(otbStreamingImageFileWriterWithFilterTest);
+}
