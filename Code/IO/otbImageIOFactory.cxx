92b68880dbfbe5cde954def76619a2a14af3e02f
Thomas Feuvrier <thomas.feuvrier@c-s.fr>
2008-04-01 14:50:14 +0000

    Correction sur la façon de detecter si c'est un fichier JPEG2000: test l'extension du fichier.

--- Code/IO/otbImageIOFactory.cxx
+++ Code/IO/otbImageIOFactory.cxx
@@ -64,9 +64,7 @@ namespace otb
             itk::ObjectFactoryBase::RegisterFactory( LUMImageIOFactory::New() );			
                         
 	    // JPEG2000 : New format for OTB
-	    ;;
-            // itk::ObjectFactoryBase::RegisterFactory( JPEG2000ImageIOFactory::New() );
-	    ;;
+            itk::ObjectFactoryBase::RegisterFactory( JPEG2000ImageIOFactory::New() );
 	    
 	    // GDAL : New format for OTB
             itk::ObjectFactoryBase::RegisterFactory( GDALImageIOFactory::New() );            
