258de8c926502fc5c5f892283a07011026c65843
Mathieu Deltorre <mathieu.deltorre@c-s.fr>
2008-04-01 12:01:07 +0000

    Ajout test sur LabelizeImageFilter

---
+++ Testing/Code/BasicFilters/otbLabelizeImageFilterNew.cxx
@@ -0,0 +1,40 @@
+/*=========================================================================
+
+  Program:   ORFEO Toolbox
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
+
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#if defined(_MSC_VER)
+#pragma warning ( disable : 4786 )
+#endif
+
+#include "otbLabelizeImageFilter.h"
+#include "otbImage.h"
+
+int otbLabelizeImageFilterNew( int argc, char * argv[] )
+{
+  typedef unsigned char InputPixelType;
+  typedef unsigned char OutputPixelType;
+  const unsigned int Dimension = 2;
+  
+  typedef otb::Image<InputPixelType, Dimension> InputImageType;
+  typedef otb::Image<OutputPixelType, Dimension> OutputImageType;
+  
+  typedef otb::LabelizeImageFilter<InputImageType, OutputImageType> LabelizeFilterType;
+  typedef LabelizeFilterType::Pointer LabelizeFilterPointerType;
+  
+  LabelizeFilterPointerType filter = LabelizeFilterType::New();
+  
+  return EXIT_SUCCESS;
+}
