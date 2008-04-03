b794fe8b27751bc418443e8ffb6369253e32f569
Emmanuel Christophe <emmanuel.christophe@orfeo-toolbox.org>
2008-04-03 16:04:53 +0000

    Ajout Lambert2Etendu et PerBandImageFilter à l'appli d'orthorectif

--- Projections/otbObtainUTMZoneFromGeoPoint.cxx
+++ Projections/otbObtainUTMZoneFromGeoPoint.cxx
@@ -1,14 +1,21 @@
 /*=========================================================================
 
-  Programme :   OTB (ORFEO ToolBox)
-  Auteurs   :   CS - R. Garrigues
-  Language  :   C++
-  Date      :   18 Octobre 2007
-  Version   :   
-  Role      :   Ortho-Rectification Image Application
-  $Id$
+Program:   ORFEO Toolbox
+Language:  C++
+Date:      $Date$
+Version:   $Revision$
+
+
+Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+See OTBCopyright.txt for details.
+
+
+This software is distributed WITHOUT ANY WARRANTY; without even 
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+PURPOSE.  See the above copyright notices for more information.
 
 =========================================================================*/
+
  
 #if defined(_MSC_VER)
 #pragma warning ( disable : 4786 )
