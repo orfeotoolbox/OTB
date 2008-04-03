7279107451a75773e3c270f6495c83ac9390c9cd
Emmanuel Christophe <emmanuel.christophe@orfeo-toolbox.org>
2008-04-03 06:30:21 +0000

    Correction doxygen

--- Code/Common/otbMultiToMonoChannelExtractROI.h
+++ Code/Common/otbMultiToMonoChannelExtractROI.h
@@ -30,7 +30,7 @@ namespace otb
 /** \class MultiToMonoChannelExtractROI
  * \brief Extract a mono channel part of a multi-channel image.
  *
- * This filter either all channels or only those specified by the user.
+ * This filter extracts either all channels or only those specified by the user.
  * The SetChannel method allows to select the channels to process.
  * \note If nothing is specified, only the first channel is processed.
  * \note This class is templated over the pixel types of the input and output images.
