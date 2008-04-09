4eb98f59c628366be8a641e1a0381d3afde59bc9
Mathieu Deltorre <mathieu.deltorre@c-s.fr>
2008-04-09 14:07:41 +0000

    SIFT v2 creation image de sortie

--- Testing/Code/FeatureExtraction/otbImageToSIFTKeyPointSetFilter.cxx
+++ Testing/Code/FeatureExtraction/otbImageToSIFTKeyPointSetFilter.cxx
@@ -30,8 +30,10 @@ int otbImageToSIFTKeyPointSetFilter(int argc, char * argv[])
 {
   const char * infname = argv[1];
   const char * outfname = argv[2];
-  const unsigned int octaves = atoi(argv[3]);
-  const unsigned int scales = atoi(argv[4]);
+  const char * outputImageFilename = argv[3];
+
+  const unsigned int octaves = atoi(argv[4]);
+  const unsigned int scales = atoi(argv[5]);
   
   typedef float RealType;
   const unsigned int Dimension =2;
@@ -41,11 +43,17 @@ int otbImageToSIFTKeyPointSetFilter(int argc, char * argv[])
   typedef otb::ImageFileReader<ImageType> ReaderType;
   typedef itk::PointSet<RealVectorType,Dimension> PointSetType;
   typedef otb::ImageToSIFTKeyPointSetFilter<ImageType,PointSetType> ImageToSIFTKeyPointSetFilterType;
+
+  // PointSet iterator types
+  typedef PointSetType::PointsContainer PointsContainerType;
+  typedef PointsContainerType::Iterator PointsIteratorType;
+  typedef PointSetType::PointDataContainer PointDataContainerType;
+  typedef PointDataContainerType::Iterator PointDataIteratorType;
   
   // Instantiating object
   ReaderType::Pointer reader = ReaderType::New();
   ImageToSIFTKeyPointSetFilterType::Pointer filter = ImageToSIFTKeyPointSetFilterType::New();
-
+  
   reader->SetFileName(infname);
   // flou !!
   filter->SetInput(0,reader->GetOutput());
@@ -53,44 +61,49 @@ int otbImageToSIFTKeyPointSetFilter(int argc, char * argv[])
   filter->SetScalesNumber(scales);
   filter->Update();
   
-//   typedef otb::Image<unsigned char,2> UCharImageType;
-//   typedef otb::ImageFileWriter<UCharImageType> WriterType;
-    
-//   UCharImageType::Pointer outputImage = UCharImageType::New();
-//   outputImage->SetRegions(reader->GetOutput()->GetLargestPossibleRegion());
-//   outputImage->Allocate();
-//   outputImage->FillBuffer(0);
+  std::ofstream outfile(outfname);
+  outfile<<"Number of SIFT key: "<<filter->GetOutput()->GetNumberOfPoints()<<std::endl;
+  ImageType::OffsetType t = {{ 0, 1}};
+  ImageType::OffsetType b = {{ 0,-1}};
+  ImageType::OffsetType l = {{ 1, 0}};
+  ImageType::OffsetType r = {{-1, 0}};
   
-//   std::ofstream outfile(outfname);
-//   //std::ios::write);
-//   ImageType::IndexType index;
+  typedef otb::Image<unsigned char,2> UCharImageType;
+  typedef otb::ImageFileWriter<UCharImageType> WriterType;
   
-//   outfile<<"Number of SIFT key: "<<filter->GetOutput()->GetNumberOfPoints()<<std::endl;
+  UCharImageType::Pointer outputImage = UCharImageType::New();
+  outputImage->SetRegions(reader->GetOutput()->GetLargestPossibleRegion());
+  outputImage->Allocate();
+  outputImage->FillBuffer(0);
   
-//   if(filter->GetOutput()->GetNumberOfPoints()>0)
-//     {
-
-//       PointsIteratorType pIt = filter->GetOutput()->GetPoints()->Begin();
-//       PointDataIteratorType pdIt = filter->GetOutput()->GetPointData()->Begin();
-      
-//       while(pIt!=filter->GetOutput()->GetPoints()->End()
-// 	    && pdIt!=filter->GetOutput()->GetPointData()->End())
-// 	{
-// 	  reader->GetOutput()->TransformPhysicalPointToIndex(pIt.Value(),index);
+  if( filter->GetOutput()->GetNumberOfPoints()>0 )
+    {
+      PointsIteratorType pIt = filter->GetOutput()->GetPoints()->Begin();
+      PointDataIteratorType pdIt = filter->GetOutput()->GetPointData()->Begin();
+      while( pIt!=filter->GetOutput()->GetPoints()->End() &&
+	     pdIt!=filter->GetOutput()->GetPointData()->End() )
+ 	{
+	  ImageType::IndexType index;
+ 	  reader->GetOutput()->TransformPhysicalPointToIndex(pIt.Value(),index);
+	  outfile<<"Point: "<<pIt.Value()<<", Index: "<<index \
+		 << " Octave: " << pdIt.Value()[0] \
+		 << " Scale: " <<  pdIt.Value()[1] << std::endl;
 	  
-// 	  outputImage->SetPixel(index,255);
-	  
-// 	  outfile<<"Point "<<pIt.Value()<<", Index: "<<index<<", associated data: "<<pdIt.Value()<<std::endl;
-// 	  ++pIt;
-// 	  ++pdIt;
-// 	}
-//     }
-//   outfile.close();
+	  outputImage->SetPixel(index,255);
+	  outputImage->SetPixel(index+t,255);
+	  outputImage->SetPixel(index+b,255);
+	  outputImage->SetPixel(index+l,255);
+	  outputImage->SetPixel(index+r,255);
+ 	  ++pIt;
+ 	  ++pdIt;
+ 	}
+    }
+  outfile.close();
   
-//   WriterType::Pointer writer = WriterType::New();
-//   writer->SetInput(outputImage);
-//   writer->SetFileName("test.png");
-//   writer->Update();
+  WriterType::Pointer writer = WriterType::New();
+  writer->SetInput(outputImage);
+  writer->SetFileName(outputImageFilename);
+  writer->Update();
   
   return EXIT_SUCCESS;
 }
