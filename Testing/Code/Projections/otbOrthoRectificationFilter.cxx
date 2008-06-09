d3801588fdd99d0bd6a156ce8ade9b407dd184e1
Emmanuel Christophe <emmanuel.christophe@orfeo-toolbox.org>
2008-06-09 17:17:43 +0000

    Ajout du test d'ortho sur SPOT 5

--- Testing/Code/Projections/otbOrthoRectificationFilter.cxx
+++ Testing/Code/Projections/otbOrthoRectificationFilter.cxx
@@ -19,14 +19,6 @@
 #pragma warning ( disable : 4786 )
 #endif
 
-/*!
- *
- * PURPOSE:
- *
- * Application pour projeter une rï¿½gion d'une image en coordonnï¿½es gï¿½ographiques 
- * en utilisant un Interpolator+regionextractor et un Iterator. 
- * 
- */
 
 // iostream is used for general output
 #include <iostream>
@@ -48,64 +40,84 @@
 
 int otbOrthoRectificationFilter( int argc, char* argv[] )
 {
-        ossimInit::instance()->initialize(argc, argv);
+  ossimInit::instance()->initialize(argc, argv);
+  try 
+  { 
+    if(argc!=11)
+    {
+      std::cout << argv[0] <<" <input filename> <output filename> <origin easting> <origin northing> <x size> <y size> <x spacing> <y spacing> <UTM zone <UTM hemisphere>" 
+          << std::endl;
 
-        if(argc!=10)
-        {
-                std::cout << argv[0] <<" <input filename> <output filename> <latitude de l'origine> <longitude de l'origine> <taille_x> <taille_y> <NumberOfstreamDivisions>" 
-                << std::endl;
-
-                return EXIT_FAILURE;
-        }
+      return EXIT_FAILURE;
+    }
    
-        typedef otb::Image<double, 2>     ImageType;
-        typedef otb::ImageFileReader<ImageType>  ReaderType;
-        typedef otb::StreamingImageFileWriter<ImageType>  WriterType;
-        typedef otb::UtmInverseProjection UtmMapProjectionType ;
-        typedef otb::OrthoRectificationFilter<ImageType, ImageType, UtmMapProjectionType> OrthoRectifFilterType ;
+    typedef otb::Image<double, 2>     ImageType;
+    typedef otb::ImageFileReader<ImageType>  ReaderType;
+    typedef otb::StreamingImageFileWriter<ImageType>  WriterType;
+    typedef otb::UtmInverseProjection UtmMapProjectionType ;
+    typedef otb::OrthoRectificationFilter<ImageType, ImageType, UtmMapProjectionType> OrthoRectifFilterType ;
               
         //Allocate pointer
-        ReaderType::Pointer             reader = ReaderType::New();
-        WriterType::Pointer             writer = WriterType::New();
+    ReaderType::Pointer             reader = ReaderType::New();
+    WriterType::Pointer             writer = WriterType::New();
 
-        OrthoRectifFilterType::Pointer  orthoRectifFilter = OrthoRectifFilterType::New();
-        UtmMapProjectionType::Pointer   utmMapProjection = UtmMapProjectionType::New();
+    OrthoRectifFilterType::Pointer  orthoRectifFilter = OrthoRectifFilterType::New();
+    UtmMapProjectionType::Pointer   utmMapProjection = UtmMapProjectionType::New();
                                 
         // Set parameters ...
-        reader->SetFileName(argv[1]);
-        writer->SetFileName(argv[2]);
+    reader->SetFileName(argv[1]);
+    writer->SetFileName(argv[2]);
 
-	reader->GenerateOutputInformation();
-	std::cout<<reader->GetOutput()<<std::endl;
+    reader->GenerateOutputInformation();
+    std::cout<<reader->GetOutput()<<std::endl;
         
-        orthoRectifFilter->SetInput(reader->GetOutput());
+    orthoRectifFilter->SetInput(reader->GetOutput());
         
-        ImageType::IndexType start;
-        start[0]=0;
-        start[1]=0;
-        orthoRectifFilter->SetOutputStartIndex(start);
+    ImageType::IndexType start;
+    start[0]=0;
+    start[1]=0;
+    orthoRectifFilter->SetOutputStartIndex(start);
         
-        ImageType::SizeType size;
-        size[0]=atoi(argv[5]);      //Taille en X.
-        size[1]=atoi(argv[6]);            //Taille en Y.
-        orthoRectifFilter->SetSize(size);
+    ImageType::SizeType size;
+    size[0]=atoi(argv[5]);      // X size
+    size[1]=atoi(argv[6]);            //Y size
+    orthoRectifFilter->SetSize(size);
         
-        ImageType::SpacingType spacing;
-        spacing[0]=atof(argv[8]);
-        spacing[1]=atof(argv[9]);
-        orthoRectifFilter->SetOutputSpacing(spacing);
+    ImageType::SpacingType spacing;
+    spacing[0]=atof(argv[7]);
+    spacing[1]=atof(argv[8]);
+    orthoRectifFilter->SetOutputSpacing(spacing);
         
-        ImageType::PointType origin;
-        origin[0]=strtod(argv[3], NULL);         //Coord x en mètres de l'origine.
-        origin[1]=strtod(argv[4], NULL);         //Coord y en mètres de l'origine.
-        orthoRectifFilter->SetOutputOrigin(origin);
-        orthoRectifFilter->SetMapProjection(utmMapProjection);
+    ImageType::PointType origin;
+    origin[0]=strtod(argv[3], NULL);         //Origin easting 
+    origin[1]=strtod(argv[4], NULL);         //Origin northing
+    orthoRectifFilter->SetOutputOrigin(origin);
+        
+    utmMapProjection->SetZone(atoi(argv[9]));
+    utmMapProjection->SetHemisphere(argv[10][0]);
+    orthoRectifFilter->SetMapProjection(utmMapProjection);
 
-        writer->SetInput(orthoRectifFilter->GetOutput());
+    writer->SetInput(orthoRectifFilter->GetOutput());
                           
-        writer->SetTilingStreamDivisions();
-        writer->Update();
-
-        return EXIT_SUCCESS;
- }//Fin main()
+    writer->SetTilingStreamDivisions();
+    writer->Update();
+  } 
+  catch( itk::ExceptionObject & err ) 
+  { 
+    std::cout << "Exception itk::ExceptionObject levee !" << std::endl; 
+    std::cout << err << std::endl; 
+    return EXIT_FAILURE;
+  } 
+  catch( std::bad_alloc & err ) 
+  { 
+    std::cout << "Exception bad_alloc : "<<(char*)err.what()<< std::endl; 
+    return EXIT_FAILURE;
+  } 
+  catch( ... ) 
+  { 
+    std::cout << "Exception levee inconnue !" << std::endl; 
+    return EXIT_FAILURE;
+  } 
+  return EXIT_SUCCESS;
+}//Fin main()
 
--- Testing/Code/Projections/otbOrthoRectificationFilterWithDEM.cxx
+++ Testing/Code/Projections/otbOrthoRectificationFilterWithDEM.cxx
@@ -19,14 +19,6 @@
 #pragma warning ( disable : 4786 )
 #endif
 
-/*!
- *
- * PURPOSE:
- *
- * Application pour projeter une r�gion d'une image en coordonn�es g�ographiques 
- * en utilisant un Interpolator+regionextractor et un Iterator. 
- * 
- */
 
 // iostream is used for general output
 #include <iostream>
@@ -59,92 +51,92 @@
 int otbOrthoRectificationFilterWithDEM( int argc, char* argv[] )
 {
   try 
-    {        
+  {        
 
-        ossimInit::instance()->initialize(argc, argv);
+    ossimInit::instance()->initialize(argc, argv);
 
-        if(argc!=12)
-        {
-                std::cout << argv[0] <<" <input filename> <output filename>	<latitude de l'origine>	<longitude de l'origine> <taille_x>	<taille_y>";
-								std::cout << " <NumberOfstreamDivisions> <x_spacing> <y_spacing> <srtm directory> <DEM Image Filename>" << std::endl;
+    if(argc!=12)
+    {
+      std::cout << argv[0] <<" <input filename> <output filename>	<origin easting>	<origin northing> <x size> <y size>";
+      std::cout << "<x_spacing> <y_spacing> <UTM zone> <UTM hemisphere> <DEM directory>" << std::endl;
 							 
-							  return EXIT_FAILURE;
-        }
+      return EXIT_FAILURE;
+    }
    
    
-        typedef otb::Image<double, 2>     ImageType;
-        typedef otb::ImageFileReader<ImageType>  ReaderType;
-        typedef otb::StreamingImageFileWriter<ImageType>  WriterType;
+    typedef otb::Image<double, 2>     ImageType;
+    typedef otb::ImageFileReader<ImageType>  ReaderType;
+    typedef otb::StreamingImageFileWriter<ImageType>  WriterType;
        
-				typedef otb::UtmInverseProjection UtmMapProjectionType ;
-			  typedef otb::OrthoRectificationFilter<ImageType, ImageType, UtmMapProjectionType> OrthoRectifFilterType ;
+    typedef otb::UtmInverseProjection UtmMapProjectionType ;
+    typedef otb::OrthoRectificationFilter<ImageType, ImageType, UtmMapProjectionType> OrthoRectifFilterType ;
 				
         //Allocate pointer
-        ReaderType::Pointer     	reader=ReaderType::New();
-        WriterType::Pointer	    	writer=WriterType::New();
+    ReaderType::Pointer     	reader=ReaderType::New();
+    WriterType::Pointer	    	writer=WriterType::New();
 
-				OrthoRectifFilterType::Pointer     	orthoRectifFilter=OrthoRectifFilterType::New();
-				UtmMapProjectionType::Pointer utmMapProjection = UtmMapProjectionType::New();
+    OrthoRectifFilterType::Pointer     	orthoRectifFilter=OrthoRectifFilterType::New();
+    UtmMapProjectionType::Pointer utmMapProjection = UtmMapProjectionType::New();
 				
         // Set parameters ...
-        reader->SetFileName(argv[1]);
-        writer->SetFileName(argv[2]);
+    reader->SetFileName(argv[1]);
+    writer->SetFileName(argv[2]);
    
-				orthoRectifFilter->SetInput(reader->GetOutput());
+    orthoRectifFilter->SetInput(reader->GetOutput());
 				
-				ImageType::IndexType start;
-				start[0]=0;
-				start[1]=0;
-				orthoRectifFilter->SetOutputStartIndex(start);
+    ImageType::IndexType start;
+    start[0]=0;
+    start[1]=0;
+    orthoRectifFilter->SetOutputStartIndex(start);
 				
-				ImageType::SizeType size;
-				size[0]=atoi(argv[5]);      //Taille en X.
-  			size[1]=atoi(argv[6]);	    //Taille en Y.
-				orthoRectifFilter->SetSize(size);
+    ImageType::SizeType size;
+    size[0]=atoi(argv[5]);      //X size.
+    size[1]=atoi(argv[6]);	    //Y size
+    orthoRectifFilter->SetSize(size);
 				
-				ImageType::SpacingType spacing;
-  			spacing[0]=atof(argv[8]);
-  			spacing[1]=atof(argv[9]);
-				orthoRectifFilter->SetOutputSpacing(spacing);
+    ImageType::SpacingType spacing;
+    spacing[0]=atof(argv[7]);
+    spacing[1]=atof(argv[8]);
+    orthoRectifFilter->SetOutputSpacing(spacing);
 				
-				ImageType::PointType origin;
-				origin[0]=strtod(argv[3], NULL);         //longitude de l'origine.
-			  origin[1]=strtod(argv[4], NULL);         //latitude de l'origine.
-				orthoRectifFilter->SetOutputOrigin(origin);
+    ImageType::PointType origin;
+    origin[0]=strtod(argv[3], NULL);         //origin easting
+    origin[1]=strtod(argv[4], NULL);         //origin northing
+    orthoRectifFilter->SetOutputOrigin(origin);
 				
-				utmMapProjection->SetZone(31);
-				utmMapProjection->SetHemisphere('N');
-				orthoRectifFilter->SetMapProjection(utmMapProjection);
+    utmMapProjection->SetZone(atoi(argv[9]));
+    utmMapProjection->SetHemisphere(argv[10][0]);
+    orthoRectifFilter->SetMapProjection(utmMapProjection);
 				
-				std::string srtmDirectory(argv[10]);
-				orthoRectifFilter->SetDEMDirectory(srtmDirectory);
+    std::string srtmDirectory(argv[11]);
+    orthoRectifFilter->SetDEMDirectory(srtmDirectory);
 
-        writer->SetInput(orthoRectifFilter->GetOutput());
+    writer->SetInput(orthoRectifFilter->GetOutput());
 				
-				writer->SetTilingStreamDivisions();
+    writer->SetTilingStreamDivisions();
 
-        otbGenericMsgDebugMacro(<< "Update writer ..." ); 
-        writer->Update();
+    otbGenericMsgDebugMacro(<< "Update writer ..." ); 
+    writer->Update();
 
-    } 
+  } 
   catch( itk::ExceptionObject & err ) 
-    { 
+  { 
     std::cout << "Exception itk::ExceptionObject levee !" << std::endl; 
     std::cout << err << std::endl; 
     return EXIT_FAILURE;
-    } 
+  } 
   catch( std::bad_alloc & err ) 
-    { 
+  { 
     std::cout << "Exception bad_alloc : "<<(char*)err.what()<< std::endl; 
     return EXIT_FAILURE;
-    } 
+  } 
   catch( ... ) 
-    { 
+  { 
     std::cout << "Exception levee inconnue !" << std::endl; 
     return EXIT_FAILURE;
-    } 
+  } 
   return EXIT_SUCCESS;
 
- } //End main()
+} //End main()
 
 
