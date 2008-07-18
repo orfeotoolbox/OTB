f54306ddd0eacc540b43a32b33dc9b6e5f9b7b5d
Thomas Feuvrier <thomas.feuvrier@c-s.fr>
2008-07-18 15:20:14 +0000

    Pour MAC  glu.h est dans OpenGL #  ifdef __APPLE__ #    include <OpenGL/glu.h> #  else #    include <GL/glu.h> #  endif

--- Code/Visu/otbImageWidgetPolygonForm.h
+++ Code/Visu/otbImageWidgetPolygonForm.h
@@ -23,9 +23,9 @@
 #include "otbImageWidgetFormBase.h"
 #include "otbPolygon.h"
 #  ifdef __APPLE__
-#    include <OpenGL/gl.h>
+#    include <OpenGL/glu.h>
 #  else
-#    include <GL/gl.h>
+#    include <GL/glu.h>
 #  endif
 
 namespace otb
