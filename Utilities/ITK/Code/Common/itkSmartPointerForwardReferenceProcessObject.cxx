feeae1228dfd6d3414dde9827f4954f2fe9857a6
Thomas Feuvrier <thomas.feuvrier@c-s.fr>
2008-09-05 08:38:22 +0000

    Suppression de ITK en interne (opération avant mise à jour)

--- Utilities/ITK/Code/Common/itkSmartPointerForwardReferenceProcessObject.cxx
+++
@@ -1,9 +0,0 @@
-#include "itkSmartPointerForwardReference.h"
-#include "itkSmartPointerForwardReference.txx"
-#include "itkProcessObject.h"
-
-template class ITKCommon_EXPORT itk::SmartPointerForwardReference<itk::ProcessObject>;
-
-
-
-
