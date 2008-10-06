30798a1fd3cccd2ad026c311918ea3f4481a049d
Guillaume Borrut <guillaume.borrut@c-s.fr>
2008-10-06 16:25:44 +0200

    Missing files

---
+++ Testing/Code/IO/otbKMLVectorDataIOTestFileReader.cxx
@@ -0,0 +1,49 @@
+/*=========================================================================
+
+  Program:   ORFEO Toolbox
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
+
+
+  This software is distributed WITHOUT ANY WARRANTY; without even 
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+  PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#include "otbKMLVectorDataIO.h"
+#include "otbVectorData.h"
+#include <iostream>
+
+int otbKMLVectorDataIOTestFileReader(int argc, char* argv[])
+{
+  typedef otb::VectorData<> VectorDataType;
+  typedef otb::KMLVectorDataIO<VectorDataType> KMLVectorDataIOType;
+  typedef VectorDataType::Pointer VectorDataPointerType;
+  
+ 
+  KMLVectorDataIOType::Pointer object = KMLVectorDataIOType::New();
+  VectorDataPointerType data = VectorDataType::New();
+
+  bool lCanRead = object->CanReadFile(argv[1]);
+  
+// ??
+  object->SetFileName(argv[1]);
+  
+  std::cout<<"       FileName : "<< object->GetFileName() <<std::endl;
+
+  object->Read(data);  
+
+/*  if ( lCanRead == false)
+    {
+      std::cerr << "Erreur otb::KMLVectorDataIO : impossible to create the file "<<argv[1]<<"."<<std::endl;
+      return EXIT_FAILURE;
+    }
+  */
+  return EXIT_SUCCESS;
+}
