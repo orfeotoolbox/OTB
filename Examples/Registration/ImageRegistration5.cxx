04da3a59396b6272c6948b638e6ba632af743c64
Emmanuel Christophe <emmanuel.christophe@orfeo-toolbox.org>
2008-10-22 14:28:57 +0800

    ENH: replacing deprecated call to StartRegistration() method by Update()

--- Examples/Registration/ImageRegistration5.cxx
+++ Examples/Registration/ImageRegistration5.cxx
@@ -435,7 +435,7 @@ int main( int argc, char *argv[] )
 
   try 
     { 
-    registration->StartRegistration(); 
+    registration->Update(); 
     } 
   catch( itk::ExceptionObject & err ) 
     { 
