1a5836ed581adaf8573798232d94536d8b78f52b
Julien Michel <julien.michel@c-s.fr>
2008-11-18 18:17:01 +0100

    ENH: Replacing home made MeanShift implementation by EDISON's one

--- Code/BasicFilters/otbMeanShiftImageFilter.h
+++ Code/BasicFilters/otbMeanShiftImageFilter.h
@@ -18,75 +18,149 @@ PURPOSE.  See the above copyright notices for more information.
 #ifndef __otbMeanShiftImageFilter_h
 #define __otbMeanShiftImageFilter_h
 
-#include "otbMeanShiftImageFilterBase.h"
+#include "itkImageToImageFilter.h"
+#include "itkVariableLengthVector.h"
+#include "otbImage.h"
 
 namespace otb
 {
+  namespace MeanShift
+    {
+      /** \class ScalarBufferConverter
+       *  \brief Small utilities class used for buffer conversion with EDISON standard.
+       *   This class handles buffer conversion when pixel type is scalar.
+       **/
+      class ScalarBufferConverter
+	{
+	public:
+	  /**
+	   * Fill the pixel with the float array values at index, using nbComp values scaled by scale.
+	   */
+	  template <class TPixel> static inline void FloatArrayToPixel(const float * data, unsigned int index, TPixel & pixel, const unsigned int nbComp, double scale)
+	    {
+	      pixel = static_cast<TPixel>(scale * data[index]);
+	    }
+	  /**
+	   * Fill the float array data with the pixel values at index, using nbComp values scaled by scale.
+	   */
+	  template <class TPixel> static inline void PixelToFloatArray(float * data, unsigned int index, const TPixel & pixel, double scale)
+	    {
+	      data[index]=static_cast<float>(scale * pixel);
+	    }
+	};
+    }
+
   /** \class MeanShiftImageFilter
-   *    
-   * This is the implementation of the mean shift edge-preserving smoothing filter adapted to
-   * otb::Image or itk::Image. For the full documentation, please refer to the
-   * base class otb::MeanShiftImageFilterBase.
+   *   
+   * 
+   * Mean shift is a data clustering algorithm ofthen used in image processing and segmentation.
+   * For a given pixel, the mean shift will build a set of neighboring pixels within a given spatial 
+   * radius (can be set using SetSpatialRadius()) and a color range (can be set using SetRangeRadius()). 
+   * The spatial and color center of this set is then computed and the algorithm iterates with this new spatial
+   * and color center. 
    *
-   * \sa MeanShiftImageFilterBase
+   * Mean shift can be used for edge-preserving smoothing, or for clustering. The GetOutput() method will allow you to get the smoothed image, whereas the 
+   * GetClusteredOuptut() methods returns the clustered output.
+   *
+   * The MinimumRegionSize parameter allows you to prune small clustered regions.
+   *
+   * Please note that the filtering part is multi-threaded, while the clustering one is not (this is not really noticeable, because the clustering step is really faster
+   * than the filtering one).
+   *
+   * Please note that if both parts are streamable, only the filtering part will ensure you to get the same results than without streaming. In the clustering results, you 
+   * might find region split due to tiling.
+   * 
+   * This filter uses the Edison mean shift algorithm implementation. Please note that data whose precision is more that float are casted to float before processing.
+   * 
+   * The Scale parameter allows you to stretch the data dynamic 
+   *
+   * For more information about mean shift techniques, one might consider reading the following article:
+   * 
+   * D. Comaniciu, P. Meer, "Mean Shift: A Robust Approach Toward Feature Space Analysis," IEEE Transactions on Pattern Analysis and Machine Intelligence, vol. 24, no. 5, pp. 603-619, May, 2002
+   * D. Comaniciu, P. Meer, "Robust analysis of feature spaces: color image segmentation," cvpr, p. 750, 1997 IEEE Computer Society Conference on Computer Vision and Pattern Recognition (CVPR'97), 1997
+   * D. Comaniciu, P. Meer, "Mean Shift Analysis and Applications," iccv, p. 1197, Seventh International Conference on Computer Vision (ICCV'99) - Volume 2, 1999
+   *
+   * \sa MeanShiftImageFilter
+   * \sa MeanShiftVectorImageFilter
+   *
    * \ingroup ImageEnhancement
    * \ingroup Streamed
    * \ingroup Threaded
    */
-template <class TInputImage, class TOutputImage, class TClusterImage = Image<unsigned short, 2>, class TPrecision = double>
-class ITK_EXPORT MeanShiftImageFilter
-: public MeanShiftImageFilterBase<TInputImage,TOutputImage,TClusterImage,TPrecision>
-  {
-    public:
-    /** Standard class typedef */
-    typedef MeanShiftImageFilter                              Self;
-    typedef MeanShiftImageFilterBase<TInputImage,
-    TOutputImage,TClusterImage,TPrecision>                    Superclass;
-    typedef itk::SmartPointer<Self>                           Pointer;
-    typedef itk::SmartPointer<const Self>                     ConstPointer;
-   
-    // Precision pixel type 
-    typedef typename Superclass::InputPixelType     InputPixelType;
-    typedef typename Superclass::PrecisionPixelType PrecisionPixelType;
-    typedef typename Superclass::OutputPixelType    OutputPixelType;
-    
 
-    /** New and Type macros */
-    itkNewMacro(Self);
-    itkTypeMacro(MeanShiftImageFilter,MeanShiftImageFilterBase);
-    
-    protected:
-    /** Constructor */
-    MeanShiftImageFilter(){};
-    
-    /** destructor */
-    ~MeanShiftImageFilter(){};
-    
-    /**PrintSelf method */
-    virtual void PrintSelf(std::ostream& os, itk::Indent indent) const;
-    
-    /** Redefinition of the GetNumberOfComponentsPerPixel() adapted for Image */
-    inline virtual unsigned int GetNumberOfComponentsPerPixel();
-    
-    /** Redefinition of the InitValue() method adapted for Image
-     *  Please be aware that this function modifies the reference to value 
-     */
-    inline virtual void InitValue(PrecisionPixelType & value, const unsigned int& nbComponents);
-    
-    /** Redefinition of the SquaredNorm() method adapted for Image */
-    inline virtual double SquaredNorm(const PrecisionPixelType& value);
+  template <class TInputImage, class TOutputImage, class TBufferConverter = MeanShift::ScalarBufferConverter>
+    class ITK_EXPORT MeanShiftImageFilter
+    : public itk::ImageToImageFilter<TInputImage,TOutputImage>
+    {
+      public:
+      /** Standard class typedef */
+      typedef MeanShiftImageFilter                              Self;
+      typedef itk::ImageToImageFilter<TInputImage,TOutputImage> Superclass;
+      typedef itk::SmartPointer<Self>                           Pointer;
+      typedef itk::SmartPointer<const Self>                     ConstPointer;
 
-    /** Redefinition of CastInputPixelToPrecisionPixel() method adapted form Image */
-    inline virtual const PrecisionPixelType CastInputPixelToPrecisionPixel(const InputPixelType & pixel);
+      /** Type macro */
+      itkTypeMacro(MeanShiftImageFilter,ImageToImageFilter);
+      itkNewMacro(Self);
+    
+      /** Template parameters typedefs */
+      typedef TInputImage                                  InputImageType;
+      typedef typename InputImageType::Pointer             InputImagePointerType;
+      typedef typename InputImageType::PixelType           InputPixelType;
+      typedef typename InputImageType::PointType           PointType;
+      typedef TOutputImage                                 OutputImageType;
+      typedef typename OutputImageType::Pointer            OutputImagePointerType;
+      typedef typename OutputImageType::PixelType          OutputPixelType;
+      typedef typename OutputImageType::RegionType         RegionType;
 
-    /** Redefinition of CastPrecisionPixelToOutputPixel() method adapted form Image */
-    inline virtual const OutputPixelType CastPrecisionPixelToOutputPixel(const PrecisionPixelType & pixel);
+      /** Setters / Getters */
+      itkSetMacro(SpatialRadius,unsigned int);
+      itkGetMacro(SpatialRadius,unsigned int);
+      itkSetMacro(RangeRadius,double);
+      itkGetMacro(RangeRadius,double);
+      itkSetMacro(MinimumRegionSize,unsigned int);
+      itkGetMacro(MinimumRegionSize,unsigned int);
+      itkSetMacro(Scale,double);
+      itkGetMacro(Scale,double);
 
-    private:
-    MeanShiftImageFilter(const Self&); //purposely not implemented
-    void operator=(const Self&);             //purposely not implemented
-  };
+      /** Return the const output image direction */  
+      const OutputImageType * GetClusteredOutput() const;
+      /** Return the output image direction */  
+      OutputImageType * GetClusteredOutput();
+	
+      protected:
+      /** This filters use a neighborhood around the pixel, so it needs to redfine the 
+       * input requested region */
+      virtual void GenerateInputRequestedRegion();
+
+      /** Threaded generate data (handle the filtering part) */
+      virtual void ThreadedGenerateData(const RegionType& outputRegionForThread,int threadId);
+      /** After threaded generate data (handle the clustering part) */
+      virtual void AfterThreadedGenerateData();
+
+      /** Constructor */
+      MeanShiftImageFilter();
+      /** destructor */
+      ~MeanShiftImageFilter(){};
+
+      /**PrintSelf method */
+      virtual void PrintSelf(std::ostream& os, itk::Indent indent) const;
+
+      private:
+      MeanShiftImageFilter(const Self&); //purposely not implemented
+      void operator=(const Self&);             //purposely not implemented
+ 
+      /** Spatial radius for mean shift convergence */
+      unsigned int m_SpatialRadius;
+      /** Range radius for mean shift convergence */
+      double m_RangeRadius;
+      /** Minimum region size in pixels for clustering */
+      unsigned int m_MinimumRegionSize;
+      /** Data scale (used to stretch data range) */
+      double m_Scale;
+    };
 }// end namespace otb
+
 
 #ifndef OTB_MANUAL_INSTANTIATION
 #include "otbMeanShiftImageFilter.txx"
--- Code/BasicFilters/otbMeanShiftVectorImageFilter.h
+++ Code/BasicFilters/otbMeanShiftVectorImageFilter.h
@@ -18,78 +18,86 @@ PURPOSE.  See the above copyright notices for more information.
 #ifndef __otbMeanShiftVectorImageFilter_h
 #define __otbMeanShiftVectorImageFilter_h
 
-#include "otbMeanShiftImageFilterBase.h"
+#include "otbMeanShiftImageFilter.h"
 
 namespace otb
 {
-
+  namespace MeanShift
+    {
+      /** \class ScalarBufferConverter
+       *  \brief Small utilities class used for buffer conversion with EDISON standard.
+       *   This class handles buffer conversion when pixel type is scalar.
+       **/
+      class VectorBufferConverter
+	{
+	public:
+	  /**
+	   * Fill the pixel with the float array values at index, using nbComp values scaled by scale.
+	   */
+	  template <class TPixel> static inline void FloatArrayToPixel(const float * data, unsigned int index, TPixel & pixel, const unsigned int nbComp, double scale)
+	    {
+	      pixel.SetSize(nbComp);
+	      for(unsigned int i = 0; i<pixel.Size();++i)
+		{
+		  pixel[i]= static_cast<typename TPixel::ValueType> (scale * data[index+i]);
+		}
+	    }
+	  /**
+	   * Fill the float array data with the pixel values at index, using nbComp values scaled by scale.
+	   */
+	  template <class TPixel> static inline void PixelToFloatArray(float * data, unsigned int index, const TPixel & pixel,double scale)
+	    {
+	      for(unsigned int i = 0; i<pixel.Size();++i)
+		{
+		  data[index+i]=static_cast<float>(scale * pixel[i]);
+		}
+	    }
+	};
+    }
+  
   /** \class MeanShiftVectorImageFilter
    *    
-   * This is the implementation of the mean shift edge-preserving smoothing filter adapted to
+   * This is the implementation of the mean shift clustering and edge-preserving smoothing filter adapted to
    * otb::VectorImage or itk::VectorImage. For the full documentation, please refer to the
    * base class otb::MeanShiftImageFilterBase.
    *
-   * \sa MeanShiftImageFilterBase
+   * \sa MeanShiftImageFilter
    * \ingroup ImageEnhancement
    * \ingroup Streamed
    * \ingroup Threaded
    */
-  template <class TInputImage, class TOutputImage, class TClusterImage = Image<unsigned short, 2>, class TPrecision = double>
-    class ITK_EXPORT MeanShiftVectorImageFilter
-    : public MeanShiftImageFilterBase<TInputImage,TOutputImage,TClusterImage,itk::VariableLengthVector<TPrecision> >
+  template <class TInputImage, class TOutputImage>
+    class MeanShiftVectorImageFilter
+    : public MeanShiftImageFilter<TInputImage,TOutputImage,MeanShift::VectorBufferConverter>
     {
-      public:
+    public:
+
       /** Standard class typedef */
       typedef MeanShiftVectorImageFilter                        Self;
-      typedef MeanShiftImageFilterBase<TInputImage,TOutputImage,TClusterImage,
-      itk::VariableLengthVector<TPrecision> >                   Superclass;
+      typedef MeanShiftImageFilter<TInputImage,TOutputImage,
+	MeanShift::VectorBufferConverter>                       Superclass;
       typedef itk::SmartPointer<Self>                           Pointer;
       typedef itk::SmartPointer<const Self>                     ConstPointer;
 
-      /** New and Type macros */
+      /** Type macro */
+      itkTypeMacro(MeanShiftVectorImageFilter,MeanShiftImageFilter);
       itkNewMacro(Self);
-      itkTypeMacro(MeanShiftVectorImageFilter,MeanShiftImageFilterBase);
-    
-      /** Template parameters typedefs */
-      typedef typename Superclass::InputPixelType     InputPixelType;
-      typedef typename Superclass::PrecisionPixelType PrecisionPixelType;
-      typedef typename Superclass::OutputPixelType    OutputPixelType;
-      
-      protected:
+
+    protected:
       /** Constructor */
       MeanShiftVectorImageFilter(){};
-
       /** destructor */
       ~MeanShiftVectorImageFilter(){};
-
+      
       /**PrintSelf method */
-      virtual void PrintSelf(std::ostream& os, itk::Indent indent) const;
+      virtual void PrintSelf(std::ostream& os, itk::Indent indent) const
+	{
+	  Superclass::PrintSelf(os,indent);
+	}
 
-      /** Redefinition of the GetNumberOfComponentsPerPixel() adapted for VectorImages */
-      inline virtual unsigned int GetNumberOfComponentsPerPixel();
-
-      /** Redefinition of the InitValue() method adapted for vector images 
-       *  Please be aware that this function modifies the reference to value 
-       */
-      inline virtual void InitValue(PrecisionPixelType & value, const unsigned int& nbComponents);
-
-      /** Redefinition of the SquaredNorm() method adapted for vector images */
-      inline virtual double SquaredNorm(const PrecisionPixelType & value);
-
-      /** Redefinition of CastInputPixelToPrecisionPixel() method adapted form Image */
-      inline virtual const PrecisionPixelType CastInputPixelToPrecisionPixel(const InputPixelType & pixel);
-      
-      /** Redefinition of CastPrecisionPixelToOutputPixel() method adapted form Image */
-      inline virtual const OutputPixelType CastPrecisionPixelToOutputPixel(const PrecisionPixelType & pixel);
-      
       private:
       MeanShiftVectorImageFilter(const Self&); //purposely not implemented
       void operator=(const Self&);             //purposely not implemented
     };
 }// end namespace otb
-
-#ifndef OTB_MANUAL_INSTANTIATION
-#include "otbMeanShiftVectorImageFilter.txx"
-#endif
-
 #endif
