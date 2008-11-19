90a1bf7fecf695a7527b1a7fc5e03a2f4209a56c
Emmanuel Christophe <emmanuel.christophe@orfeo-toolbox.org>
2008-11-19 17:57:46 +0800

    DOC: adding doxygen comments

--- Code/BasicFilters/otbSimplifyPathFunctor.h
+++ Code/BasicFilters/otbSimplifyPathFunctor.h
@@ -24,9 +24,25 @@ namespace otb
 {
 
     /** \class SimplifyPathFunctor
-     *  \brief 
-     *
- 
+   *    \brief This filter performs a simplification of the input path.
+   * 
+   *  It reduces the number of vertices in each path, according to a tolerance criterion. It aims at 
+   *  removing aligned vertices while keeping sharp angular points.
+   *
+   *  In order to ensure the unicity of its output, each path is considered first from begin to end, then 
+   *  from begin to the first vertex before the end. At each step, the consistency of the path is checked : 
+   *  the equation of the line passing by the first and last vertices is computed. Then, for each
+   *  vertices between them, the euclidean distance to this line is computed. If for one vertex, this distance
+   *  is upper than the tolerance threshold, the path is considered to be inconsistent and no vertices can be removed.
+   *
+   *  If the path is considered consistent (which will occure at least with a 2 vertices path), 
+   * only the beginning and ending
+   *  vertices are kept and a new search iteration begin at its end.
+   *
+   *  This filter is part of the road extraction framework.
+   *
+   * \sa UnaryFunctorObjectListFilter
+    *
      *  \ingroup Functor
      */
   template <class TInput, class TOutput>
