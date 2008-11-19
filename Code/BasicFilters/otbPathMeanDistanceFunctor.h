bd75d12528fdcf85ea53890e1876c529101aaded
Emmanuel Christophe <emmanuel.christophe@orfeo-toolbox.org>
2008-11-19 15:04:27 +0800

    ENH: simplifying algorithm

--- Code/BasicFilters/otbPathMeanDistanceFunctor.h
+++ Code/BasicFilters/otbPathMeanDistanceFunctor.h
@@ -59,18 +59,30 @@ namespace otb
         typedef typename TInput1::ObjectType::VertexListType::ConstIterator VertexListConstIteratorType;
         typedef typename TInput1::ObjectType::VertexType VertexType;
         VertexListConstIteratorType beginIt = input->GetVertexList()->Begin();
-        VertexListConstIteratorType beforeTheEndIt = input->GetVertexList()->End();
-        --beforeTheEndIt;
-        while (beginIt != beforeTheEndIt)
+
+        VertexType v1 = beginIt.Value();
+        VertexType v2 = beginIt.Value();
+        ++beginIt;
+        while (beginIt != input->GetVertexList()->End())
         {
-          VertexType v1 = beginIt.Value();
-          ++beginIt;
-          VertexType v2 = beginIt.Value();
+          v1 = v2;
+          v2 = beginIt.Value();
           meanDistance+=vcl_sqrt(vcl_pow(v1[0]-v2[0],2)+vcl_pow(v1[1]-v2[1],2));
+          ++beginIt;
         }
 
         double nbVertices = static_cast<double>(input->GetVertexList()->Size());
-        meanDistance = meanDistance / (nbVertices-1);
+        if (nbVertices>1)
+        {
+          meanDistance = meanDistance / (nbVertices-1);
+        }
+        else
+        {
+          itkGenericExceptionMacro(<<"Object with only one vertex!" );
+        }
+        
+        std::cout << "Num vertex: " << nbVertices << std::endl;
+        std::cout << "Mean dist: " << meanDistance << std::endl;
       
         if ( meanDistance > m_Threshold)
         {
