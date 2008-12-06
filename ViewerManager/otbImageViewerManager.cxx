fc24d8e73753b37dfd95522ccd95c35416dae6e1
Emmanuel Christophe <emmanuel.christophe@orfeo-toolbox.org>
2008-12-06 23:12:19 +0800

    STYLE: removing trailing spaces

--- ViewerManager/otbImageViewerManager.cxx
+++ ViewerManager/otbImageViewerManager.cxx
@@ -10,8 +10,8 @@ Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
 See OTBCopyright.txt for details.
 
 
-This software is distributed WITHOUT ANY WARRANTY; without even 
-the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
 PURPOSE.  See the above copyright notices for more information.
 
 =========================================================================*/
@@ -19,15 +19,15 @@ PURPOSE.  See the above copyright notices for more information.
 
 int main(int argc, char* argv[])
 {
- try 
+ try
     {
   typedef double PixelType;
   typedef otb::ImageViewerManager<PixelType> ManagerType;
   ManagerType::Pointer manager = ManagerType::New();
   manager->Show();
   std::string strTest("--OTBTesting");
-  bool bFlRun(true);       
-        
+  bool bFlRun(true);
+
   for(int i = 1; i<argc;++i)
     {
       if( strTest.compare(argv[i])==0 )
@@ -36,28 +36,28 @@ int main(int argc, char* argv[])
         bFlRun=false;
       }
       else
-      {  
+      {
         manager->OpenImage(argv[i]);
         Fl::check();
-      } 
+      }
     }
   if( bFlRun==true) return Fl::run();
-    } 
-  catch( itk::ExceptionObject & err ) 
-    { 
-    std::cout << "Following otbException catch :" << std::endl; 
-    std::cout << err << std::endl; 
+    }
+  catch( itk::ExceptionObject & err )
+    {
+    std::cout << "Following otbException catch :" << std::endl;
+    std::cout << err << std::endl;
     return EXIT_FAILURE;
-    } 
-  catch( std::bad_alloc & err ) 
-    { 
-    std::cout << "Exception bad_alloc : "<<(char*)err.what()<< std::endl; 
+    }
+  catch( std::bad_alloc & err )
+    {
+    std::cout << "Exception bad_alloc : "<<(char*)err.what()<< std::endl;
     return EXIT_FAILURE;
-    } 
-  catch( ... ) 
-    { 
-    std::cout << "Unknown Exception found !" << std::endl; 
+    }
+  catch( ... )
+    {
+    std::cout << "Unknown Exception found !" << std::endl;
     return EXIT_FAILURE;
-    } 
+    }
   return EXIT_SUCCESS;
 }
