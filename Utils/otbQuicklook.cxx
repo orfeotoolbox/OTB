4188c226e650b0ffc4d7ebf654c8251a93805bff
Emmanuel Christophe <emmanuel.christophe@orfeo-toolbox.org>
2008-12-06 23:12:19 +0800

    STYLE: removing trailing spaces

--- Utils/otbQuicklook.cxx
+++ Utils/otbQuicklook.cxx
@@ -10,8 +10,8 @@ Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
 See OTBCopyright.txt for details.
 
 
-This software is distributed WITHOUT ANY WARRANTY; without even 
-the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
 PURPOSE.  See the above copyright notices for more information.
 
 =========================================================================*/
@@ -35,17 +35,17 @@ PURPOSE.  See the above copyright notices for more information.
 #include "otbStreamingImageFileWriter.h"
 #include "otbConfigure.h"
 
-int main(int argc, char* argv[] ) 
+int main(int argc, char* argv[] )
 {
 
-  try 
-    { 
+  try
+    {
 
         typedef int				InputPixelType;
         typedef unsigned char			OutputPixelType;
 
 
-        typedef otb::MultiChannelExtractROI< InputPixelType, 
+        typedef otb::MultiChannelExtractROI< InputPixelType,
                                              OutputPixelType >  ExtractROIFilterType;
 
 
@@ -53,18 +53,18 @@ int main(int argc, char* argv[] )
 
 
         // Parse command line parameters
-        typedef otb::CommandLineArgumentParser ParserType;	
+        typedef otb::CommandLineArgumentParser ParserType;
 	ParserType::Pointer parser = ParserType::New();
 
         parser->SetProgramDescription("Generates a subsampled version of an image");
         parser->AddInputImage();
         parser->AddOutputImage();
         parser->AddOptionNParams("--ChannelList", "channel list","-cl",false);
-        parser->AddOption("--ROIStartX","first point of ROI in x-direction ","-ROIx0",1,false);	
+        parser->AddOption("--ROIStartX","first point of ROI in x-direction ","-ROIx0",1,false);
         parser->AddOption("--ROIStartY","first point of ROI in y-direction ","-ROIy0",1,false);
-        parser->AddOption("--ROISizeX","size of ROI in x-direction ","-ROINx",1,false);	
+        parser->AddOption("--ROISizeX","size of ROI in x-direction ","-ROINx",1,false);
         parser->AddOption("--ROISizeY","size of ROI in y-direction ","-ROINy",1,false);
-	
+
         parser->AddOption("--SizeX","size of quicklook in x-direction ","-Nx",1,false);
         parser->AddOption("--SizeY","size of quicklook in y-direction ","-Ny",1,false);
 
@@ -72,19 +72,19 @@ int main(int argc, char* argv[] )
 
         typedef otb::CommandLineArgumentParseResult ParserResultType;
         ParserResultType::Pointer  parseResult = ParserResultType::New();
-  
+
   	try
 	   {
             parser->ParseCommandLine(argc,argv,parseResult);
  	   }
-  	catch( itk::ExceptionObject & err ) 
-    	   { 
-    	   std::string descriptionException = err.GetDescription();   
-    	   if(descriptionException.find("ParseCommandLine(): Version Parser") != std::string::npos) 
+  	catch( itk::ExceptionObject & err )
+    	   {
+    	   std::string descriptionException = err.GetDescription();
+    	   if(descriptionException.find("ParseCommandLine(): Version Parser") != std::string::npos)
 	   	{
 		return EXIT_SUCCESS;
 		}
-    	   if(descriptionException.find("ParseCommandLine(): Help Parser") != std::string::npos) 
+    	   if(descriptionException.find("ParseCommandLine(): Help Parser") != std::string::npos)
 	   	{
 		std::cout << "If --SamplingRatio parameter is selected, ";
 		std::cout << "SizeX and SizeY parameters  are not used " << std::endl;
@@ -93,12 +93,12 @@ int main(int argc, char* argv[] )
 		return EXIT_SUCCESS;
 		}
                 return EXIT_FAILURE;
-    	   } 
+    	   }
 
-        // Defining the IO filename 
+        // Defining the IO filename
         typedef otb::ImageFileReader< ExtractROIFilterType::InputImageType >    ReaderType;
         typedef otb::ImageFileWriter< ExtractROIFilterType::OutputImageType>    WriterType;
-       
+
         ReaderType::Pointer reader = ReaderType::New();
         WriterType::Pointer writer = WriterType::New();
 
@@ -110,13 +110,13 @@ int main(int argc, char* argv[] )
       	reader->GenerateOutputInformation();
 
       	unsigned long regionROI[2];
-	 
+
 	if( parseResult->IsOptionPresent("--ROIStartX") )
 	  extractROIFilter->SetStartX(parseResult->GetParameterULong("--ROIStartX"));
 
 	if( parseResult->IsOptionPresent("--ROIStartY") )
 	  extractROIFilter->SetStartY(parseResult->GetParameterULong("--ROIStartY"));
-	  
+
 		if( parseResult->IsOptionPresent("--ROISizeX") )
 		{
 	  		regionROI[0] = parseResult->GetParameterULong("--ROISizeX");
@@ -135,7 +135,7 @@ int main(int argc, char* argv[] )
 		{
 			regionROI[1] = reader->GetOutput()->GetLargestPossibleRegion().GetSize()[1];
 		}
- 
+
 	if( parseResult->IsOptionPresent("--ChannelList") )
 	  {
 	  int NumberOfChannel = parseResult->GetNumberOfParameters("--ChannelList");
@@ -151,19 +151,19 @@ int main(int argc, char* argv[] )
 	  {
 	  std::cout << " Size of ROI in x-direction and y -direction must be positive" << std::endl;
 	  return EXIT_SUCCESS;
-	  }	
-	
+	  }
+
     extractROIFilter->SetInput( reader->GetOutput() );
     //  extractROIFilter->Update();
     // extractROIFilter->GenerateOutputInformation();
-	
+
 	double ROISizeX =  static_cast<double>( regionROI[0] );
 	double ROISizeY =  static_cast<double>( regionROI[1] );
 
 	unsigned int Ratio = 2; //default value
 	double SamplingRatioX =1.0;
 	double SamplingRatioY =1.0;
-		
+
 	if( parseResult->IsOptionPresent("--SamplingRatio") )
 	  {
 	  Ratio = static_cast<unsigned int>(parseResult->GetParameterDouble("--SamplingRatio"));
@@ -188,7 +188,7 @@ int main(int argc, char* argv[] )
                         {
   		        	Ratio =  static_cast<unsigned int>(ROISizeX / parseResult->GetParameterDouble("--SizeX"));
                         }
-		
+
 	          	if( parseResult->IsOptionPresent("--SizeY") )
                         {
           			Ratio =  static_cast<unsigned int>(ROISizeY / parseResult->GetParameterDouble("--SizeY"));
@@ -198,12 +198,12 @@ int main(int argc, char* argv[] )
 	  	{
 		        std::cout << "Error in SizeX and/or SizeY : values must be greater than 1." << std::endl;
         		return EXIT_FAILURE;
-		}		  	
+		}
 	  }
-	 	
+
 
         // Setting the ShrinkImageFilter parameters
-/*        typedef itk::ShrinkImageFilter< ExtractROIFilterType::OutputImageType, 
+/*        typedef itk::ShrinkImageFilter< ExtractROIFilterType::OutputImageType,
                                              ExtractROIFilterType::OutputImageType >  ShrinkImageFilterType;*/
 		typedef otb::StreamingShrinkImageFilter<ExtractROIFilterType::OutputImageType,ExtractROIFilterType::OutputImageType> ShrinkImageFilterType;
 
@@ -215,25 +215,25 @@ int main(int argc, char* argv[] )
 		//ResamplingFilter->Update();
  	 	writer->SetInput(ResamplingFilter->GetOutput() );
 		writer->Update();
-		
 
-    } 
-  catch( itk::ExceptionObject & err ) 
-    { 
-    std::cout << "Following otbException catch :" << std::endl; 
-    std::cout << err << std::endl; 
+
+    }
+  catch( itk::ExceptionObject & err )
+    {
+    std::cout << "Following otbException catch :" << std::endl;
+    std::cout << err << std::endl;
     return EXIT_FAILURE;
-    } 
-  catch( std::bad_alloc & err ) 
-    { 
-    std::cout << "Exception bad_alloc : "<<(char*)err.what()<< std::endl; 
+    }
+  catch( std::bad_alloc & err )
+    {
+    std::cout << "Exception bad_alloc : "<<(char*)err.what()<< std::endl;
     return EXIT_FAILURE;
-    } 
-  catch( ... ) 
-    { 
-    std::cout << "Unknown Exception found !" << std::endl; 
+    }
+  catch( ... )
+    {
+    std::cout << "Unknown Exception found !" << std::endl;
     return EXIT_FAILURE;
-    } 
+    }
 
 
   return EXIT_SUCCESS;
