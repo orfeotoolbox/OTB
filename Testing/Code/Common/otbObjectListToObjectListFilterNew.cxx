5648d3040107198c8dabc8da695ac572967b0d09
Emmanuel Christophe <emmanuel.christophe@orfeo-toolbox.org>
2009-01-02 14:16:44 +0800

    TEST: adding instanciation test for ObjectListToObjectListFilter

---
+++ Testing/Code/Common/otbObjectListToObjectListFilterNew.cxx
@@ -0,0 +1,41 @@
+/*=========================================================================
+
+  Program:   ORFEO Toolbox
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
+
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#include "itkExceptionObject.h"
+#include "otbObjectListToObjectListFilter.h"
+#include "otbObjectList.h"
+#include <complex>
+#include "otbPolyLineParametricPathWithValue.h"
+#include "otbPolygon.h"
+
+int otbObjectListToObjectListFilterNew(int argc, char * argv[])
+{
+  typedef otb::PolyLineParametricPathWithValue<std::complex<int>,2> InputObjectType;
+  typedef otb::Polygon<unsigned char> OutputObjectType;
+
+  typedef otb::ObjectList<InputObjectType> InputListType;
+  typedef otb::ObjectList<OutputObjectType> OutputListType;
+
+  typedef otb::ObjectListToObjectListFilter<InputListType,OutputListType> ObjectListFilterType;
+
+  ObjectListFilterType::Pointer objectList = ObjectListFilterType::New();
+
+
+  return EXIT_SUCCESS;
+}
+
+
