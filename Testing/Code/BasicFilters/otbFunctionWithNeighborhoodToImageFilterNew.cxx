4c886a79c0c70d8d81dc68c64870d06bc75f2d61
Cyrille Valladeau <cyrille.valladeau@c-s.fr>
2009-01-30 10:43:05 +0100

    ENH : test error

--- Testing/Code/BasicFilters/otbFunctionWithNeighborhoodToImageFilterNew.cxx
+++ Testing/Code/BasicFilters/otbFunctionWithNeighborhoodToImageFilterNew.cxx
@@ -18,14 +18,14 @@
 #include "itkExceptionObject.h"
 #include "otbImage.h"
 #include "otbFunctionWithNeighborhoodToImageFilter.h"
-#include "itkVarianceImageFunction.h"
+#include "otbEnergyTextureImageFunction.h"
 
 int otbFunctionWithNeighborhoodToImageFilterNew(int argc, char * argv[])
 {
   const unsigned int Dimension = 2;
   typedef double PixelType;
   typedef otb::Image<PixelType,Dimension> ImageType;
-  typedef itk::VarianceImageFunction<ImageType> FunctionType;
+  typedef otb::EnergyTextureImageFunction<ImageType> FunctionType;
 
   typedef otb::FunctionWithNeighborhoodToImageFilter<ImageType, ImageType, FunctionType> FilterType;
 
