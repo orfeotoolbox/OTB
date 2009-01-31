3abf88dc92bcce6d7420efc39ed808e3668bd5d2
Emmanuel Christophe <emmanuel.christophe@orfeo-toolbox.org>
2009-01-31 19:26:06 +0800

    STYLE: formatting indentation

--- ViewerManager/otbImageViewerManager.cxx
+++ ViewerManager/otbImageViewerManager.cxx
@@ -19,18 +19,18 @@ PURPOSE.  See the above copyright notices for more information.
 
 int main(int argc, char* argv[])
 {
- try
-    {
-  typedef double PixelType;
-  typedef otb::ImageViewerManager<PixelType> ManagerType;
-  ManagerType::Pointer manager = ManagerType::New();
-  manager->Show();
-  std::string strTest("--OTBTesting");
-  bool bFlRun(true);
+  try
+  {
+    typedef double PixelType;
+    typedef otb::ImageViewerManager<PixelType> ManagerType;
+    ManagerType::Pointer manager = ManagerType::New();
+    manager->Show();
+    std::string strTest("--OTBTesting");
+    bool bFlRun(true);
 
-  for(int i = 1; i<argc;++i)
+    for (int i = 1; i<argc;++i)
     {
-      if( strTest.compare(argv[i])==0 )
+      if ( strTest.compare(argv[i])==0 )
       {
         std::cout << "--OTBTesting option. No FL::run() call !" << std::endl;
         bFlRun=false;
@@ -41,23 +41,23 @@ int main(int argc, char* argv[])
         Fl::check();
       }
     }
-  if( bFlRun==true) return Fl::run();
-    }
-  catch( itk::ExceptionObject & err )
-    {
+    if ( bFlRun==true) return Fl::run();
+  }
+  catch ( itk::ExceptionObject & err )
+  {
     std::cout << "Following otbException catch :" << std::endl;
     std::cout << err << std::endl;
     return EXIT_FAILURE;
-    }
-  catch( std::bad_alloc & err )
-    {
+  }
+  catch ( std::bad_alloc & err )
+  {
     std::cout << "Exception bad_alloc : "<<(char*)err.what()<< std::endl;
     return EXIT_FAILURE;
-    }
-  catch( ... )
-    {
+  }
+  catch ( ... )
+  {
     std::cout << "Unknown Exception found !" << std::endl;
     return EXIT_FAILURE;
-    }
+  }
   return EXIT_SUCCESS;
 }
