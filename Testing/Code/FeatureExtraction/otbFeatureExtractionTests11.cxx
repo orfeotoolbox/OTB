8ea81df12be8d44caa128503f11bd7fcb0e8d611
Cyrille Valladeau <cyrille.valladeau@c-s.fr>
2009-02-05 12:06:58 +0100

    MRG

--- Testing/Code/FeatureExtraction/otbFeatureExtractionTests11.cxx
+++ Testing/Code/FeatureExtraction/otbFeatureExtractionTests11.cxx
@@ -33,6 +33,7 @@ REGISTER_TEST(otbCloudEstimatorFilter);
 REGISTER_TEST(otbCloudDetectionFilterNew);
 REGISTER_TEST(otbCloudDetectionFilter);
 REGISTER_TEST(otbSimplifyManyPathListFilter);
+REGISTER_TEST(otbTextureFunctorBase);
 REGISTER_TEST(otbEnergyTextureFunctor);
 REGISTER_TEST(otbEntropyTextureFunctor);
 REGISTER_TEST(otbInverseDifferenceMomentTextureFunctor);
