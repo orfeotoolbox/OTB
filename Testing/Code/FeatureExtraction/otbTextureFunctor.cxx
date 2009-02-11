f5522e9e3cde2c2c4ae98c8f317c3c905914e80e
Cyrille Valladeau <cyrille.valladeau@c-s.fr>
2009-02-11 19:02:34 +0100

    ENH : remove old texture filter

--- Testing/Code/FeatureExtraction/otbTextureFunctor.cxx
+++ Testing/Code/FeatureExtraction/otbTextureFunctor.cxx
@@ -22,25 +22,8 @@
 #include "itkConstNeighborhoodIterator.h"
 #include "otbImageFileReader.h"
 #include "otbStreamingImageFileWriter.h"
+#include "otbTextureFunctors.h"
 
-// Functors list
-#include "otbEnergyTextureFunctor.h"
-#include "otbEntropyTextureFunctor.h"
-#include "otbInverseDifferenceMomentTextureFunctor.h"
-#include "otbAngularSecondMomentumTextureFunctor.h"
-#include "otbVarianceTextureFunctor.h"
-#include "otbCorrelationTextureFunctor.h"
-#include "otbContrastTextureFunctor.h"
-#include "otbSumAverageTextureFunctor.h"
-#include "otbDifferenceEntropyTextureFunctor.h"
-#include "otbSumEntropyTextureFunctor.h"
-#include "otbSumVarianceTextureFunctor.h"
-#include "otbDifferenceVarianceTextureFunctor.h"
-#include "otbInformationMeasureOfCorrelation1TextureFunctor.h"
-#include "otbInformationMeasureOfCorrelation2TextureFunctor.h"
-#include "otbClusterShadeTextureFunctor.h"
-#include "otbClusterProminenceTextureFunctor.h"
-#include "otbMeanTextureFunctor.h"
 
 template<class TInputImage, class TOutputImage, class TFunctor>
 int generic_TextureFunctor(int argc, char * argv[])
