909e304a18ffcc28dfccc63e658070e5901b19ee
Cyrille Valladeau <cyrille.valladeau@c-s.fr>
2009-02-11 19:02:02 +0100

    ENH : add functors gathered

---
+++ Code/FeatureExtraction/otbTextureFunctors.h
@@ -0,0 +1,41 @@
+/*=========================================================================
+
+  Program:   ORFEO Toolbox
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
+
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __otbTextureFunctors_h
+#define __otbTextureFunctors_h
+
+#include "otbEnergyTextureFunctor.h"
+#include "otbEntropyTextureFunctor.h"
+#include "otbInverseDifferenceMomentTextureFunctor.h"
+#include "otbAngularSecondMomentumTextureFunctor.h"
+#include "otbVarianceTextureFunctor.h"
+#include "otbCorrelationTextureFunctor.h"
+#include "otbContrastTextureFunctor.h"
+#include "otbSumAverageTextureFunctor.h"
+#include "otbDifferenceEntropyTextureFunctor.h"
+#include "otbSumEntropyTextureFunctor.h"
+#include "otbSumVarianceTextureFunctor.h"
+#include "otbDifferenceVarianceTextureFunctor.h"
+#include "otbInformationMeasureOfCorrelation1TextureFunctor.h"
+#include "otbInformationMeasureOfCorrelation2TextureFunctor.h"
+#include "otbClusterShadeTextureFunctor.h"
+#include "otbClusterProminenceTextureFunctor.h"
+#include "otbMeanTextureFunctor.h"
+
+
+#endif
+
