a1d7e2d82022e8f0284d6303fc4354d0ddaccb7c
Julien Michel <julien.michel@orfeo-toolbox.org>
2009-02-12 17:50:45 +0100

    TEST: (Visu Refactoring) Adding a base class for action handlers

---
+++ Testing/Code/VisuRefac/otbImageWidgetActionHandlerNew.cxx
@@ -0,0 +1,26 @@
+/*=========================================================================
+
+Program:   ORFEO Toolbox
+Language:  C++
+Date:      $Date$
+Version:   $Revision$
+
+
+Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+See OTBCopyright.txt for details.
+
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#include "otbImageWidgetActionHandler.h"
+
+int otbImageWidgetActionHandlerNew( int argc, char * argv[] )
+{
+  typedef otb::ImageWidgetActionHandler HandlerType;
+  HandlerType::Pointer handler = HandlerType::New();
+
+  return EXIT_SUCCESS;
+}
