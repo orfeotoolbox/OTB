bf6a2caf2241ce2f1754bcf77f222a7add6a1404
Cyrille Valladeau <cyrille.valladeau@c-s.fr>
2009-02-13 18:50:54 +0100

    WRG : unused variable

--- Testing/Code/FeatureExtraction/otbLineSegmentDetector.cxx
+++ Testing/Code/FeatureExtraction/otbLineSegmentDetector.cxx
@@ -48,18 +48,6 @@ int otbLineSegmentDetector( int argc, char * argv[] )
   DrawLineListType::Pointer drawLineFilter =   DrawLineListType::New();
   ReaderType::Pointer reader = ReaderType::New();
 
-
-  /** */
-  typedef otb::LineSpatialObjectList            LinesListType;
-  typedef LinesListType::LineType               LineType;
-  LinesListType::Pointer list = LinesListType::New();
-
-  LineType::PointListType             pointList;
-  LineType::LinePointType             pointBegin , pointEnd;
-  IndexType                           IndexBegin , IndexEnd;
-
-  /***/
-
   reader->SetFileName(infname);
   reader->GenerateOutputInformation();
   lsdFilter->SetInput(reader->GetOutput());
@@ -68,37 +56,6 @@ int otbLineSegmentDetector( int argc, char * argv[] )
   drawLineFilter->SetInputLineSpatialObjectList(lsdFilter->GetOutput());
 
   
-//   LinesListType::const_iterator it    = lsdFilter->GetOutput()->begin();
-//   LinesListType::const_iterator itEnd  = lsdFilter->GetOutput()->end();
-  
-//   while(it != itEnd)
-//     {
-//       LineType::PointListType & pointsList = (*it)->GetPoints();
-//       LineType::PointListType::const_iterator itPoints = pointsList.begin();
-      
-//       float x = (*itPoints).GetPosition()[0];
-//       float y = (*itPoints).GetPosition()[1];
-//       IndexBegin[0] = static_cast<int>(x) ; IndexBegin[1] = static_cast<int>(y);
-      
-//       itPoints++;
-
-//       float x1 = (*itPoints).GetPosition()[0];
-//       float y1 = (*itPoints).GetPosition()[1];
-//       IndexEnd[0]= static_cast<int>(x1) ; IndexEnd[1] = static_cast<int>(y1);
-
-//       LineIteratorFilter   itLine(reader->GetOutput(),IndexBegin,  IndexEnd);
-//       itLine.GoToBegin();
-//       while(!itLine.IsAtEnd())
-// 	{
-// 	  if(reader->GetOutput()->GetRequestedRegion().IsInside(itLine.GetIndex()))
-// 	    itLine.Set(255.);
-// 	  ++itLine;
-// 	}
-//       ++it;
-//     }
-
- 
-
   /** Write The Output Image*/
   WriterType::Pointer writer = WriterType::New();
   writer->SetFileName(outfname);
