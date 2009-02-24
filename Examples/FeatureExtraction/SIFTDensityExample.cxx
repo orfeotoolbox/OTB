cb1d3cf2e9a00b0afaee135f861600344b8db7e2
Jordi Inglada <jordi.inglada@orfeo-toolbox.org>
2009-02-24 15:59:53 +0100

    DOC: add comment to SIFT Density Example

--- Examples/FeatureExtraction/SIFTDensityExample.cxx
+++ Examples/FeatureExtraction/SIFTDensityExample.cxx
@@ -88,7 +88,7 @@ int main(int argc, char* argv[] )
   typedef itk::PointSet<RealVectorType,Dimension>   PointSetType;
   typedef otb::ImageToSIFTKeyPointSetFilter<ImageType, PointSetType>
                                                         DetectorType;
-    // Software Guide : EndCodeSnippet
+  // Software Guide : EndCodeSnippet
   // Software Guide : BeginLatex
   //
   // We can now define the filter which will compute the SIFT
@@ -100,33 +100,136 @@ int main(int argc, char* argv[] )
   // Software Guide : BeginCodeSnippet
   typedef otb::KeyPointDensityImageFilter<ImageType, ImageType, DetectorType>
                                                                    FilterType;
+  // Software Guide : EndCodeSnippet
+  // Software Guide : BeginLatex
+  //
+  // We can instantiate the reader and the writer as wella s the
+  // filter and the detector. The detector needs to be instantiated in
+  // order to set its parameters.
+  //
+  // Software Guide : EndLatex
+
+  // Software Guide : BeginCodeSnippet
 
 
-  /**Instancitation of an object*/
+  ReaderType::Pointer      reader = ReaderType::New();
+  WriterType::Pointer writer = WriterType::New();
   FilterType::Pointer    filter =     FilterType::New();
   DetectorType::Pointer  detector = DetectorType::New();
-  ReaderType::Pointer      reader = ReaderType::New();
+
+
+  // Software Guide : EndCodeSnippet
+  // Software Guide : BeginLatex
+  //
+  // We set the file names for the input and the output images.
+  //
+  // Software Guide : EndLatex
+
+  // Software Guide : BeginCodeSnippet
 
   reader->SetFileName(infname);
-  reader->GenerateOutputInformation();
+  writer->SetFileName(outfname);
 
-  filter->SetInput(reader->GetOutput());
-  detector = filter->GetDetector();
+
+  // Software Guide : EndCodeSnippet
+  // Software Guide : BeginLatex
+  //
+  // We set the parameters for the SIFT detector (the number of
+  // octaves and the number of scales per octave).
+  //
+  // Software Guide : EndLatex
+
+  // Software Guide : BeginCodeSnippet
+
+
   detector ->SetOctavesNumber(octaves);
   detector->SetScalesNumber(scales);
+
+  // Software Guide : EndCodeSnippet
+  // Software Guide : BeginLatex
+  //
+  // And we pass the detector to the filter and we set the radius for
+  // the density estimation.
+  //
+  // Software Guide : EndLatex
+
+  // Software Guide : BeginCodeSnippet
 
 
   filter->SetDetector(detector);
   filter->SetNeighborhoodRadius(radius);
 
-  /** Writing the densty Image*/
-  WriterType::Pointer writer = WriterType::New();
-  writer->SetFileName(outfname);
+  // Software Guide : EndCodeSnippet
+  // Software Guide : BeginLatex
+  //
+  // We plug the pipeline.
+  //
+  // Software Guide : EndLatex
+
+  // Software Guide : BeginCodeSnippet
+
+
+  filter->SetInput(reader->GetOutput());
   writer->SetInput(filter->GetOutput());
+
+  // Software Guide : EndCodeSnippet
+  // Software Guide : BeginLatex
+  //
+  // We trigger the execution by calling th \code{Update()} method on
+  // the writer, but before that we run the
+  // \code{GenerateOutputInformation()} on the reader so the filter
+  // gets the information about the image size (needed for the SIFT
+  // computation). 
+  //
+  // Software Guide : EndLatex
+
+  // Software Guide : BeginCodeSnippet
+
+  reader->GenerateOutputInformation();
   writer->Update();
+  // Software Guide : EndCodeSnippet
 
+  //  Software Guide : BeginLatex
+  // Figure~\ref{fig:SIFTDENSITY_FILTER} shows the result of applying
+  // the key point density filter to an image using the SIFT
+  // detector. The main difference with respect to figure
+  // \ref{fig:EDGEDENSITY_FILTER} is that for SIFTS, individual trees
+  // contribute to the density.
+  // \begin{figure}
+  // \center
+  // \includegraphics[width=0.25\textwidth]{suburb2.eps}
+  // \includegraphics[width=0.25\textwidth]{PrettySIFDensityOutput.eps}
+  // \itkcaption[SIFT Density Filter]{Result of applying the
+  // \doxygen{otb}{KeypointDensityImageFilter} to an image. From left
+  // to right : 
+  // original image, SIF density.}
+  // \label{fig:SIFTDENSITY_FILTER}
+  // \end{figure}
+  //
+  //  Software Guide : EndLatex
 
+  /************* Image for printing **************/
 
+  typedef otb::Image< unsigned char, 2 > OutputImageType;
+
+  typedef itk::RescaleIntensityImageFilter< ImageType, OutputImageType >
+    RescalerType;
+
+  RescalerType::Pointer rescaler = RescalerType::New();
+
+  rescaler->SetOutputMinimum(0);
+  rescaler->SetOutputMaximum(255);
+
+  rescaler->SetInput( filter->GetOutput() );
+
+  typedef otb::ImageFileWriter< OutputImageType > OutputWriterType;
+  OutputWriterType::Pointer outwriter = OutputWriterType::New();
+
+  outwriter->SetFileName( prettyfname );
+  outwriter->SetInput( rescaler->GetOutput() );
+  outwriter->Update();
+  
+ 
   return EXIT_SUCCESS;
 }
 
