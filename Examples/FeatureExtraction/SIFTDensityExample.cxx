9b39247f43f07da0945df9d9ba8216b4db1343a0
Jordi Inglada <jordi.inglada@orfeo-toolbox.org>
2009-02-24 15:40:25 +0100

    ENH: change template order for otbKeyPointDensityImageFilter

--- Examples/FeatureExtraction/SIFTDensityExample.cxx
+++ Examples/FeatureExtraction/SIFTDensityExample.cxx
@@ -98,7 +98,7 @@ int main(int argc, char* argv[] )
   // Software Guide : EndLatex
 
   // Software Guide : BeginCodeSnippet
-  typedef otb::KeyPointDensityImageFilter<ImageType, DetectorType, ImageType>
+  typedef otb::KeyPointDensityImageFilter<ImageType, ImageType, DetectorType>
                                                                    FilterType;
 
 
