7e10419c10235d5969a758820676c311153adfdd
Emmanuel Christophe <emmanuel.christophe@orfeo-toolbox.org>
2009-03-02 18:15:32 +0800

    TEST: add test for AmplitudePhaseToRGBFunctor

---
+++ Testing/Code/BasicFilters/otbAmplitudePhaseToRGBFunctor.cxx
@@ -0,0 +1,84 @@
+/*=========================================================================
+
+  Program:   ORFEO Toolbox
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
+
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#if defined(_MSC_VER)
+#pragma warning ( disable : 4786 )
+#endif
+
+#ifdef __BORLANDC__
+#define ITK_LEAN_AND_MEAN
+#endif
+
+
+
+#include "otbImage.h"
+#include "otbImageFileReader.h"
+#include "otbStreamingImageFileWriter.h"
+
+#include "itkBinaryFunctorImageFilter.h"
+#include "otbAmplitudePhaseToRGBFunctor.h"
+
+int otbAmplitudePhaseToRGBFunctor(int argc, char * argv[])
+{
+  typedef float PixelType;
+  typedef otb::Image<PixelType, 2> ImageType;
+
+  typedef std::complex<PixelType> ComplexPixelType;
+  typedef otb::Image<ComplexPixelType, 2> ComplexImageType;
+
+  typedef itk::RGBPixel<unsigned char> RGBPixelType;
+  typedef otb::Image<RGBPixelType, 2> RGBImageType;
+
+  typedef otb::ImageFileReader<ComplexImageType> ReaderType;
+  typedef otb::StreamingImageFileWriter<RGBImageType> WriterType;
+
+  ReaderType::Pointer reader = ReaderType::New();
+  WriterType::Pointer writer = WriterType::New();
+  reader->SetFileName(argv[1]);
+  writer->SetFileName(argv[2]);
+
+
+  typedef itk::ComplexToModulusImageFilter<ComplexImageType,ImageType> ModulusFilterType;
+  ModulusFilterType::Pointer modulusFilter = ModulusFilterType::New();
+  modulusFilter->SetInput(reader->GetOutput());
+
+
+  typedef itk::ComplexToPhaseImageFilter<ComplexImageType,ImageType> PhaseFilterType;
+  PhaseFilterType::Pointer phaseFilter = PhaseFilterType::New();
+  phaseFilter->SetInput(reader->GetOutput());
+
+
+  typedef otb::Functor::AmplitudePhaseToRGBFunctor<PixelType,PixelType,RGBPixelType>
+  ColorMapFunctorType;
+  typedef itk::BinaryFunctorImageFilter<ImageType,
+  RGBImageType, ColorMapFunctorType> ColorMapFilterType;
+  ColorMapFilterType::Pointer colormapper = ColorMapFilterType::New();
+  colormapper->GetFunctor().SetMaximum(150);
+  colormapper->GetFunctor().SetMinimum(70);
+
+
+  colormapper->SetInput1(modulus->GetOutput());
+  colormapper->SetInput2(phase->GetOutput());
+
+  writer->SetInput(colormapper->GetOutput());
+
+  writer->Update();
+
+
+  return EXIT_SUCCESS;
+}
