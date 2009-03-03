0957360a256725f21366ab827e530c98d42e75cc
Guillaume Borrut <guillaume.borrut@c-s.fr>
2009-03-03 11:46:41 +0100

    ENH: SetTextureStatus becomes SetFeatureStatus in otbSFSTexturesImageFilter.h

--- Code/FeatureExtraction/otbSFSTexturesImageFilter.h
+++ Code/FeatureExtraction/otbSFSTexturesImageFilter.h
@@ -165,7 +165,7 @@ public:
   typedef enum {LENGTH, WIDTH, PSI, WMEAN, RATIO, SD} FeatureType;
   void SetFeatureStatus(FeatureType id, bool isSelected )
     {
-      if ( id>this->GetTexturesStatus().size() || id == 0 )
+      if ( static_cast<unsigned int>(id) > this->GetTexturesStatus().size() || id == 0 )
 	  {
 	    itkExceptionMacro(<<"Invalid texture index "<<id<<", must be in [1;"<<this->GetTexturesStatus().size()<<"]");
 	  }
@@ -174,6 +174,7 @@ public:
 	    this->GetFunctor().SetTextureStatus( id-1, isSelected );
       }
     }
+
   std::vector<bool> GetTexturesStatus()
     {
       return this->GetFunctor().GetTexturesStatus();
@@ -184,7 +185,7 @@ public:
       unsigned int id;
       for (id=1;id<=6;id++)
       {
-        this->SetTextureStatus(id,false);
+        this->SetFeatureStatus(static_cast<FeatureType>(id),false);
       }
     }
 
