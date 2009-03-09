28e7e411fb871081bc12071b5eed12c8254e9829
Otmane Lahlou <otmane.lahlou@c-s.fr>
2009-03-09 14:42:46 +0100

    ENH : adding accessors in the header file, correct warnings

--- Code/Projections/otbGenericRSTransform.h
+++ Code/Projections/otbGenericRSTransform.h
@@ -37,95 +37,140 @@ PURPOSE.  See the above copyright notices for more information.
 namespace otb
 {
 
-/** \class GenericRSTransform
- *  \brief This is the class to handle generic remote sensing transform
- *
- * The transformation will transform the coordinate from any map projection or sensor
- * model to any map projection or sensor model.
- *
- *
- * \ingroup Projection
- *
- **/
+  /** \class GenericRSTransform
+   *  \brief This is the class to handle generic remote sensing transform
+   *
+   * The transformation will transform the coordinate from any map projection or sensor
+   * model to any map projection or sensor model.
+   *
+   *
+   * \ingroup Projection
+   *
+   **/
+  
+  template <class TScalarType = double,
+    unsigned int NInputDimensions=2,
+    unsigned int NOutputDimensions=2>
+    class ITK_EXPORT GenericRSTransform: public itk::Transform<TScalarType,       // Data type for scalars
+    NInputDimensions,  // Number of dimensions in the input space
+    NOutputDimensions> // Number of dimensions in the output space
+    {
+      public :
+      /** Standard class typedefs. */
+      typedef itk::Transform< TScalarType,
+      NInputDimensions,
+      NOutputDimensions >       Superclass;
+      typedef GenericRSTransform                      Self;
+      typedef itk::SmartPointer<Self>                   Pointer;
+      typedef itk::SmartPointer<const Self>             ConstPointer;
 
-template <class TScalarType = double,
-unsigned int NInputDimensions=2,
-unsigned int NOutputDimensions=2>
-class ITK_EXPORT GenericRSTransform: public itk::Transform<TScalarType,       // Data type for scalars
-      NInputDimensions,  // Number of dimensions in the input space
-      NOutputDimensions> // Number of dimensions in the output space
-{
-public :
-  /** Standard class typedefs. */
-  typedef itk::Transform< TScalarType,
-  NInputDimensions,
-  NOutputDimensions >       Superclass;
-  typedef GenericRSTransform                      Self;
-  typedef itk::SmartPointer<Self>                   Pointer;
-  typedef itk::SmartPointer<const Self>             ConstPointer;
+      typedef typename Superclass::ScalarType           ScalarType;
+      typedef ossimProjection                           OssimMapProjectionType;
+      typedef itk::Point<ScalarType,NInputDimensions >  InputPointType;
+      typedef itk::Point<ScalarType,NOutputDimensions > OutputPointType;
 
-  typedef typename Superclass::ScalarType           ScalarType;
-  typedef ossimProjection                           OssimMapProjectionType;
-  typedef itk::Point<ScalarType,NInputDimensions >  InputPointType;
-  typedef itk::Point<ScalarType,NOutputDimensions > OutputPointType;
+      typedef itk::Vector<double, 2> SpacingType;
+      typedef itk::Point<double, 2> OriginType;
 
-  typedef itk::Vector<double, 2> SpacingType;
-  typedef itk::Point<double, 2> OriginType;
+      typedef itk::Transform<double, 2, 2> GenericTransformType;
+      typedef typename GenericTransformType::Pointer GenericTransformPointerType;
+      typedef otb::CompositeTransform<GenericTransformType, GenericTransformType> TransformType;
+      
+      /** Method for creation through the object factory. */
+      itkNewMacro( Self );
 
-  typedef itk::Transform<double, 2, 2> GenericTransformType;
-  typedef typename GenericTransformType::Pointer GenericTransformPointerType;
-  typedef otb::CompositeTransform<GenericTransformType, GenericTransformType, double, 2, 2> TransformType;
+      /** Run-time type information (and related methods). */
+      itkTypeMacro( GenericRSTransform, itk::Transform );
 
-  /** Method for creation through the object factory. */
-  itkNewMacro( Self );
+      typedef InverseOrForwardTransformationEnum DirectionOfMappingEnumType;
 
-  /** Run-time type information (and related methods). */
-  itkTypeMacro( GenericRSTransform, itk::Transform );
+      itkStaticConstMacro(InputSpaceDimension, unsigned int, NInputDimensions);
+      itkStaticConstMacro(OutputSpaceDimension, unsigned int, NOutputDimensions);
+      itkStaticConstMacro(SpaceDimension, unsigned int, NInputDimensions);
+      itkStaticConstMacro(ParametersDimension, unsigned int,NInputDimensions*(NInputDimensions+1));
 
-  typedef InverseOrForwardTransformationEnum DirectionOfMappingEnumType;
+      /** Set/Get for input and output projections.  */
+      itkSetStringMacro(InputProjectionRef);
+      itkGetStringMacro(InputProjectionRef);
 
-  itkStaticConstMacro(InputSpaceDimension, unsigned int, NInputDimensions);
-  itkStaticConstMacro(OutputSpaceDimension, unsigned int, NOutputDimensions);
-  itkStaticConstMacro(SpaceDimension, unsigned int, NInputDimensions);
-  itkStaticConstMacro(ParametersDimension, unsigned int,NInputDimensions*(NInputDimensions+1));
+      itkSetStringMacro(OutputProjectionRef);
+      itkGetStringMacro(OutputProjectionRef);
 
-  virtual const TransformType* GetTransform() const;
+      itkSetStringMacro(DEMDirectory);
+      itkGetStringMacro(DEMDirectory);
 
+      /** Set/Get Dictionary*/
+      const itk::MetaDataDictionary& GetInputDictionary()
+      {  return this->m_InputDictionary;}
+      
+      void SetInputDictionary(const itk::MetaDataDictionary & dictionary)
+      { 
+	this->m_InputDictionary = dictionary;
+	this->Modified();
+      }
+      
+      const itk::MetaDataDictionary& GetOutputDictionary()
+      {  return this->m_OutputDictionary;}
+      
+      void SetOutputDictionary(const itk::MetaDataDictionary & dictionary)
+      { 
+	this->m_OutputDictionary = dictionary;
+	this->Modified();
+      }
+      
 
-  OutputPointType TransformPoint(const InputPointType &point) const;
+      
+      itkGetMacro(InputKeywordList,ImageKeywordlist);
+      void SetInputKeywordList(ImageKeywordlist kwl)
+      {
+	this->m_InputKeywordList = kwl;
+	this->Modified();
+      }
+      
+      itkGetMacro(OutputKeywordList,ImageKeywordlist);
+      void SetOutputKeywordList(ImageKeywordlist kwl)
+      {
+	this->m_OutputKeywordList = kwl;
+	this->Modified();
+      }
 
-  virtual bool InstanciateTransform();
+      /** Methods prototypes */
+      virtual const TransformType* GetTransform() const;
 
-protected:
-  GenericRSTransform();
-  virtual ~GenericRSTransform(){};
+      OutputPointType TransformPoint(const InputPointType &point) const;
 
-private :
-  GenericRSTransform(const Self&); //purposely not implemented
-  void operator=(const Self&); //purposely not implemented
+      virtual void  InstanciateTransform();
+      
+      
+      protected:
+      GenericRSTransform();
+      virtual ~GenericRSTransform(){};
 
-  ImageKeywordlist m_InputKeywordList;
-  ImageKeywordlist m_OutputKeywordList;
-//   const itk::MetaDataDictionary & m_InputDictionary;
-//   const itk::MetaDataDictionary & m_OutputDictionary;
-  const itk::MetaDataDictionary m_InputDictionary;
-  const itk::MetaDataDictionary m_OutputDictionary;
-  std::string m_InputProjectionRef;
-  std::string m_OutputProjectionRef;
-  std::string m_DEMDirectory;
+      private :
+      GenericRSTransform(const Self&); //purposely not implemented
+      void operator=(const Self&); //purposely not implemented
 
-  SpacingType         m_InputSpacing;
-  OriginType          m_InputOrigin;
-  SpacingType         m_OutputSpacing;
-  OriginType          m_OutputOrigin;
+      ImageKeywordlist m_InputKeywordList;
+      ImageKeywordlist m_OutputKeywordList;
 
+      itk::MetaDataDictionary   m_InputDictionary;
+      itk::MetaDataDictionary   m_OutputDictionary;
 
-  TransformType* m_Transform;
+      std::string m_InputProjectionRef;
+      std::string m_OutputProjectionRef;
+      std::string m_DEMDirectory;
 
-  GenericTransformPointerType m_InputTransform;
-  GenericTransformPointerType m_OutputTransform;
-  bool reinstanciateTransform;
-};
+      SpacingType         m_InputSpacing;
+      OriginType          m_InputOrigin;
+      SpacingType         m_OutputSpacing;
+      OriginType          m_OutputOrigin;
+
+      TransformType*      m_Transform;
+
+      GenericTransformPointerType m_InputTransform;
+      GenericTransformPointerType m_OutputTransform;
+      bool reinstanciateTransform;   
+    };
 
 
 
