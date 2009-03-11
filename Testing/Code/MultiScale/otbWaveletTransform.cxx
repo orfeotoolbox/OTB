82d5f084877bd0708063d4af870122835dbdc7fd
Thomas Feuvrier <thomas.feuvrier@c-s.fr>
2009-03-11 16:38:20 +0100

    COMP: replace char ** by char * [] in otbWaveletTransform.cxx testing file

--- Testing/Code/MultiScale/otbWaveletTransform.cxx
+++ Testing/Code/MultiScale/otbWaveletTransform.cxx
@@ -35,7 +35,7 @@
 #include "otbCommandLineArgumentParser.h"
 #include "otbCommandProgressUpdate.h"
 
-int otbWaveletTransform( int argc, char ** argv )
+int otbWaveletTransform( int argc, char * argv[] )
 {
   typedef otb::CommandLineArgumentParser ParserType;
   ParserType::Pointer parser = ParserType::New();
