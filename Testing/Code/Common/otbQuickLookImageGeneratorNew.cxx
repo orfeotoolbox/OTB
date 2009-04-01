cf1354faac7e341958203e37319cfe5ff4107d1b
Cyrille Valladeau <cyrille.valladeau@c-s.fr>
2009-04-01 09:43:33 +0200

    ENH : add quicklook generator + tests

---
+++ Testing/Code/Common/otbQuickLookImageGeneratorNew.cxx
@@ -0,0 +1,36 @@
+/*=========================================================================
+
+  Program:   ORFEO Toolbox
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
+
+
+  This software is distributed WITHOUT ANY WARRANTY; without even 
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+  PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#if defined(_MSC_VER)
+#pragma warning ( disable : 4786 )
+#endif
+
+#include "itkExceptionObject.h"
+#include "otbQuickLookImageGenerator.h"
+#include "otbVectorImage.h"
+
+
+int otbQuickLookImageGeneratorNew(int argc, char* argv[])
+{
+  typedef otb::VectorImage<double, 2>                        ImageType;
+  typedef otb::QuickLookImageGenerator<ImageType, ImageType> FilterType;
+
+  FilterType::Pointer filter = FilterType::New();
+
+  return EXIT_SUCCESS;
+}
