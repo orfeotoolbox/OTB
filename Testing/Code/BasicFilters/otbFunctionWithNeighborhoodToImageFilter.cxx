b90dddc44636a69a74a906aa412821650ddf7c04
Cyrille Valladeau <cyrille.valladeau@c-s.fr>
2009-04-23 11:30:54 +0200

    ENH : work on testing

--- Testing/Code/BasicFilters/otbFunctionWithNeighborhoodToImageFilter.cxx
+++ Testing/Code/BasicFilters/otbFunctionWithNeighborhoodToImageFilter.cxx
@@ -21,37 +21,61 @@
 #include "itkVarianceImageFunction.h"
 #include "otbImageFileReader.h"
 #include "otbStreamingImageFileWriter.h"
+#include "otbTextureFunctorBase.h"
+#include "otbTextureImageFunction.h"
+
+template <class TScalarInput, class TScalarOutput>
+class ITK_EXPORT FunctorTest : 
+public otb::Functor::TextureFunctorBase<TScalarInput, TScalarOutput>
+{
+public:
+  FunctorTest(){};
+  ~FunctorTest(){};
+
+  typedef itk::Neighborhood<TScalarInput, 2>    NeighborhoodType;
+  
+  virtual double ComputeOverSingleChannel(const NeighborhoodType &neigh, const NeighborhoodType &neighOff)
+  {
+    double out = neigh.GetCenterValue(); 
+    
+    return out;
+  }
+};
 
 int otbFunctionWithNeighborhoodToImageFilter(int argc, char * argv[])
 {
   const unsigned int Dimension = 2;
   typedef double PixelType;
-  typedef otb::Image<PixelType,Dimension> InputImageType;
-  typedef otb::Image<PixelType,Dimension> OutputImageType;
-  typedef otb::ImageFileReader<InputImageType> ReaderType;
+  typedef otb::Image<PixelType,Dimension>                InputImageType;
+  typedef InputImageType::SizeType                       SizeType;
+  typedef InputImageType::OffsetType                     OffsetType;
+  typedef otb::Image<PixelType,Dimension>                OutputImageType;
+  typedef otb::ImageFileReader<InputImageType>           ReaderType;
   typedef otb::StreamingImageFileWriter<OutputImageType> WriterType;
-  typedef itk::VarianceImageFunction<InputImageType> FunctionType;
+  typedef FunctorTest<PixelType, PixelType>                                                         FunctorType;
+  typedef otb::TextureImageFunction<InputImageType, FunctorType>                                    FunctionType;
   typedef otb::FunctionWithNeighborhoodToImageFilter<InputImageType, OutputImageType, FunctionType> FilterType;
 
   // Instantiating object
   FilterType::Pointer filter = FilterType::New();
   ReaderType::Pointer reader = ReaderType::New();
   WriterType::Pointer writer = WriterType::New();
+  
+  // jsut for accessors, the functor doesn't care
+  SizeType radius;
+  radius.Fill(2);
+  OffsetType offset;
+  offset.Fill(2);
 
   reader->SetFileName(argv[1]);
   writer->SetFileName(argv[2]);
   filter->SetInput( reader->GetOutput() );
-
-  FunctionType::Pointer function = FunctionType::New();
-  function->SetNeighborhoodRadius(atoi(argv[3]));
-  function->SetInputImage(reader->GetOutput());
-
-  filter->SetFunction(function);
-
+  filter->SetRadius(radius);
+  filter->SetOffset(offset);
 
   writer->SetInput(filter->GetOutput());
-  writer->SetNumberOfStreamDivisions(1);
+  //writer->SetNumberOfStreamDivisions(1);
   writer->Update();
-
+  
   return EXIT_SUCCESS;
 }
