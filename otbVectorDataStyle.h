5d3107955d0368d3badf2673436d85cc384268e4
Emmanuel Christophe <emmanuel.christophe@orfeo-toolbox.org>
2009-04-30 12:50:59 +0800

    ENH: separating style from the main code

---
+++ otbVectorDataStyle.h
@@ -0,0 +1,42 @@
+/*=========================================================================
+
+  Program:   ORFEO Toolbox
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
+
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#ifndef __otbVectorDataStyle_h
+#define __otbVectorDataStyle_h
+
+#include "itkLightObject.h"
+
+namespace otb
+{
+/** \class VectorDataStyle
+   *   \brief Render a vector data into an image
+   *
+ */
+
+
+class ITK_EXPORT VectorDataStyle : public itk::LightObject
+{
+  public:
+
+
+}; // end class
+} // end namespace otb
+
+
+
+#endif
