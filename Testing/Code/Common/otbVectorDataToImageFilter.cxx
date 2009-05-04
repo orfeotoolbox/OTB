7661fed47d4121eece294eb9ba1d1e1d1647c404
Emmanuel Christophe <emmanuel.christophe@orfeo-toolbox.org>
2009-05-04 17:18:54 +0800

    TEST: VectorDataToImageFilter test

--- Testing/Code/Common/otbVectorDataToImageFilter.cxx
+++ Testing/Code/Common/otbVectorDataToImageFilter.cxx
@@ -1,3 +1,21 @@
+/*=========================================================================
+
+  Program:   ORFEO Toolbox
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
+
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
 #include "otbVectorDataFileReader.h"
 #include "otbImageFileWriter.h"
 #include "otbVectorData.h"
@@ -14,7 +32,7 @@
 //  ./mapnikOTBClasses ~/OTB/trunk/OTB-Data/Input/ToulouseRoad-examples.shp output.png
 //  ./mapnikOTBClasses ~/OTB/trunk/OTB-Data/LargeInput/VECTOR/MidiPyrenees/roads.shp output.png
 
-int main(int argc, char * argv[])
+int otbVectorDataToImageFilter(int argc, char * argv[])
 {
 
   //Read the vector data
---
+++ Testing/Code/Common/otbVectorDataToImageFilterNew.cxx
@@ -0,0 +1,36 @@
+/*=========================================================================
+
+  Program:   ORFEO Toolbox
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
+
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+
+#include "itkRGBAPixel.h"
+#include "otbImage.h"
+#include "otbVectorData.h"
+#include "otbVectorDataToImageFilter.h"
+
+int otbVectorDataToImageFilterNew(int argc, char * argv[])
+{
+
+  typedef otb::VectorData<> VectorDataType;
+  typedef itk::RGBAPixel< unsigned char > PixelType;
+  typedef otb::Image<PixelType,2> ImageType;
+
+  typedef otb::VectorDataToImageFilter<VectorDataType, ImageType> VectorDataToImageFilterType;
+  VectorDataToImageFilterType::Pointer vectorDataRendering = VectorDataToImageFilterType::New();
+
+  return EXIT_SUCCESS;
+}
