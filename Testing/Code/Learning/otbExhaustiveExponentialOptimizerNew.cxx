8ffe381fd91ad5734a2ef79ff4ac3222d686017f
Julien Michel <julien.michel@orfeo-toolbox.org>
2009-05-18 11:36:24 +0200

    ENH: Adding exhaustive exponential optimizer

---
+++ Testing/Code/Learning/otbExhaustiveExponentialOptimizerNew.cxx
@@ -0,0 +1,35 @@
+/*=========================================================================
+
+  Program:   ORFEO Toolbox
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
+
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#if defined(_MSC_VER)
+#pragma warning ( disable : 4786 )
+#endif
+
+
+#include <iostream>
+
+#include "otbExhaustiveExponentialOptimizer.h"
+
+int otbExhaustiveExponentialOptimizerNew( int argc, char* argv[] )
+{
+  otb::ExhaustiveExponentialOptimizer::Pointer opt = otb::ExhaustiveExponentialOptimizer::New();
+
+  return EXIT_SUCCESS;
+}
+
+
