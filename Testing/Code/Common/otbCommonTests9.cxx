95d01589814125c769c37b95d415f5927f2b9296
Emmanuel Christophe <emmanuel.christophe@orfeo-toolbox.org>
2009-05-20 18:22:44 +0800

    TEST: add test otbVectorDataToImageFilterWorld

--- Testing/Code/Common/otbCommonTests9.cxx
+++ Testing/Code/Common/otbCommonTests9.cxx
@@ -29,4 +29,5 @@ void RegisterTests()
 REGISTER_TEST(otbVectorDataToImageFilterNew);
 REGISTER_TEST(otbVectorDataToImageFilter);
 REGISTER_TEST(otbVectorDataToImageFilterSensorModel);
+REGISTER_TEST(otbVectorDataToImageFilterWorld);
 }
