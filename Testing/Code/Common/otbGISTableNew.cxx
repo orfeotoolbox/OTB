48bf7b224a7027c20120d502afdaad9419f7ab04
Jordi Inglada <jordi.inglada@orfeo-toolbox.org>
2009-05-28 13:51:45 +0200

    ADD: forgotten add

---
+++ Testing/Code/Common/otbGISTableNew.cxx
@@ -0,0 +1,34 @@
+/*=========================================================================
+
+  Program:   ORFEO Toolbox
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
+
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#include "itkExceptionObject.h"
+#include "otbMacro.h"
+
+#include "otbGISTable.h"
+#include "otbPostGISConnectionImplementation.h"
+
+int otbGISTableNew(int argc, char * argv[])
+{
+  typedef otb::PostGISConnectionImplementation GISConnectionType;
+
+  typedef otb::GISTable<GISConnectionType, 2> GISTableType;
+
+  //Instantiation
+  GISTableType::Pointer data = GISTableType::New();
+
+  return EXIT_SUCCESS;
+}
