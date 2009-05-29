17fdf3fae56bae535090fc50b1375f70bf7b4678
Jordi Inglada <jordi.inglada@orfeo-toolbox.org>
2009-05-29 16:18:22 +0200

    ENH: an example of transactor

---
+++ Code/GISFilters/otbPostGISCreateTableTransactor.h
@@ -0,0 +1,85 @@
+/*=========================================================================
+
+  Program:   ORFEO Toolbox
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
+
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __itkPostGISCreateTableTransactor_h
+#define __itkPostGISCreateTableTransactor_h
+
+#include <pqxx/pqxx>
+#include <sstream>
+
+
+namespace otb
+{
+
+
+/** \class PostGISCreateTableTransactor
+ * \brief PQXX-based transactor for creating PostGIS tables 
+ *
+ * 
+ * 
+ * 
+ *
+ * \ingroup GISTransactors
+ */
+
+class ITK_EXPORT PostGISCreateTableTransactor :
+     public pqxx::transactor<pqxx::nontransaction>
+{
+  
+public:
+
+  typedef pqxx::result ResultType;
+
+  CreateTable() : pqxx::transactor<pqxx::nontransaction>("CreateTable") {}
+
+  void operator()(argument_type &T)
+  {
+    m_Result = T.exec("CREATE TABLE testtable (id serial PRIMARY KEY,genre text);");
+    m_Result = T.exec("SELECT AddGeometryColumn( 'testtable', 'geom', -1, 'GEOMETRY', 2 );");
+
+    
+  }
+
+  void on_commit()
+  {
+      std::cout << "\t Table is created \t"  << std::endl;
+
+  }
+
+  itkGetMacro(TableName, std::string);
+  itkSetMacro(TableName, std::string);
+
+  itkGetMacro(SRID, int);
+  itkSetMacro(SRID, int);
+
+  itkGetMacro(Dimension, unisgned short);
+  itkSetMacro(Dimension, unisgned short);
+
+  itkGetMacro(Result, ResultType);
+  
+protected:
+  ResultType m_Result;
+  std::string m_TableName;
+  int m_SRID;
+  unsigned short m_Dimension:
+};
+
+} // end namespace otb
+
+
+
+#endif
