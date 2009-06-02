a11824249de0c8e1f3d10134383bb51cb60c4164
Emmanuel Christophe <emmanuel.christophe@orfeo-toolbox.org>
2009-06-02 18:00:24 +0800

    ENH: simplify code

--- Code/Visualization/otbDirectRenderingFunction.h
+++ Code/Visualization/otbDirectRenderingFunction.h
@@ -119,28 +119,26 @@ public:
   {
     itk::OStringStream oss;
     OutputPixelType output = this->Evaluate(vpixel);
-
     for(unsigned int channel = 0; channel < vpixel.Size();++channel)
       {
+      oss<<"c= "<< channel << ", ";
       if(channel == m_RedChannelIndex)
         {
-        oss<<"c= "<<channel<<", R= " << std::setw(3) <<(int)output[0];
-        oss<<", v= "<<static_cast<typename itk::NumericTraits<PixelType>::PrintType>(vpixel[channel])<<std::endl;
+        oss <<"R= " << std::setw(3) <<(int)output[0]<< ", ";
         }
       else if(channel == m_GreenChannelIndex)
         {
-        oss<<"c= "<<channel<<", G= " << std::setw(3) <<(int)output[1];
-        oss<<", v= "<<static_cast<typename itk::NumericTraits<PixelType>::PrintType>(vpixel[channel])<<std::endl;
+        oss <<"G= " << std::setw(3) <<(int)output[1]<< ", ";
         }
       else if(channel == m_BlueChannelIndex)
         {
-        oss<<"c= "<<channel<<", B= " << std::setw(3) <<(int)output[2];
-        oss<<", v= "<<static_cast<typename itk::NumericTraits<PixelType>::PrintType>(vpixel[channel])<<std::endl;
+        oss <<"B= " << std::setw(3) <<(int)output[2]<< ", ";
         }
       else
         {
-        oss<<"c= "<<channel<<",         v= "<<static_cast<typename itk::NumericTraits<PixelType>::PrintType>(vpixel[channel])<<std::endl;
+        oss <<"       ";
         }
+        oss<<"v= "<<static_cast<typename itk::NumericTraits<PixelType>::PrintType>(vpixel[channel])<<std::endl;
       }
     return oss.str();
   }
