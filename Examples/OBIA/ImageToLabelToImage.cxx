f33a32ff65e772b20adb4d1b2005cbd5610a702c
Jordi Inglada <jordi.inglada@orfeo-toolbox.org>
2009-06-08 16:06:30 +0200

    DOC: added code for OBIA basic example

---
+++ Examples/OBIA/ImageToLabelToImage.cxx
@@ -0,0 +1,82 @@
+/*=========================================================================
+
+  Program:   ORFEO Toolbox
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
+
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+//  Software Guide : BeginLatex
+//
+//  
+//
+//  Software Guide : EndLatex
+
+// Software Guide : BeginCodeSnippet
+
+// Software Guide : EndCodeSnippet
+#include "otbImageFileReader.h"
+#include "otbImageFileWriter.h"
+
+#include "itkLabelObject.h"
+#include "itkLabelMap.h"
+#include "itkBinaryImageToLabelMapFilter.h"
+#include "itkLabelMapToLabelImageFilter.h"
+
+
+int main(int argc, char * argv[])
+{
+
+  if( argc != 6 )
+    {
+    std::cerr << "usage: " << argv[0] << " input output conn fg bg" << std::endl;
+    exit(1);
+    }
+
+  const int dim = 2;
+  
+  typedef itk::Image< unsigned char, dim > ImageType;
+
+  typedef itk::LabelObject< unsigned char, dim > LabelObjectType;
+  typedef itk::LabelMap< LabelObjectType > LabelMapType;
+  
+  typedef itk::ImageFileReader< ImageType > ReaderType;
+  ReaderType::Pointer reader = ReaderType::New();
+  reader->SetFileName( argv[1] );
+  
+  typedef itk::BinaryImageToLabelMapFilter< ImageType, LabelMapType> I2LType;
+  I2LType::Pointer i2l = I2LType::New();
+  i2l->SetInput( reader->GetOutput() );
+  i2l->SetFullyConnected( atoi(argv[3]) );
+  i2l->SetForegroundValue( atoi(argv[4]) );
+  i2l->SetBackgroundValue( atoi(argv[5]) );
+
+  typedef itk::LabelMapToLabelImageFilter< LabelMapType, ImageType> L2IType;
+  L2IType::Pointer l2i = L2IType::New();
+  l2i->SetInput( i2l->GetOutput() );
+
+  typedef itk::ImageFileWriter< ImageType > WriterType;
+  WriterType::Pointer writer = WriterType::New();
+  writer->SetInput( l2i->GetOutput() );
+  writer->SetFileName( argv[2] );
+  writer->Update();
+
+  return 0;
+}
+
+
+
+
+
+
+
