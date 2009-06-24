a4044d18024d7169de389ffdc8979ae5b2ecc9f7
Emmanuel Christophe <emmanuel.christophe@orfeo-toolbox.org>
2009-06-24 17:15:28 +0800

    TEST: add test for the AmplitudeFunctor

--- Testing/Code/Visualization/otbVisualizationTests1.cxx
+++ Testing/Code/Visualization/otbVisualizationTests1.cxx
@@ -37,6 +37,7 @@ void RegisterTests()
   REGISTER_TEST(otbRenderingImageFilterScalar);
   REGISTER_TEST(otbRenderingImageFilterVector);
   REGISTER_TEST(otbRenderingImageFilterVectorWithExpNegativeTransfer);
+  REGISTER_TEST(otbRenderingImageFilterAmplitude);
   REGISTER_TEST(otbImageLayerScalar);
   REGISTER_TEST(otbImageLayerVector);
   REGISTER_TEST(otbUniformAlphaBlendingFunctionNew);
