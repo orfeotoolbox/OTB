25bad70848f276e20a04ff932edd807cc8318d7b
Gregoire Mercier <gregoire.mercier@telecom-bretagne.eu>
2009-06-29 09:45:59 +0200

    TEST: SubsampleImageFilter (filter used in the wavelets)

---
+++ Testing/Code/BasicFilters/otbSubsampleImageFilterNew.cxx
@@ -0,0 +1,42 @@
+/*=========================================================================
+
+  Program:   ORFEO Toolbox
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
+
+  Copyright (c) Institut Telecom / Telecom Bretagne. All rights reserved. 
+  See ITCopyright.txt for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#if defined(_MSC_VER)
+#pragma warning ( disable : 4786 )
+#endif
+#include "otbImage.h"
+#include "otbImageFileReader.h"
+#include "otbImageFileWriter.h"
+
+#include "otbSubsampleImageFilter.h"
+
+int otbSubsampleImageFilterNew ( int argc, char * argv[] )
+{
+  const unsigned int Dimension = 2;
+  typedef double PixelType;
+  typedef otb::Image< PixelType, Dimension >  ImageType;
+  typedef otb::SubsampleImageFilter< ImageType, ImageType, otb::FORWARD > 
+    FilterType;
+
+  FilterType::Pointer filter = FilterType::New();
+
+  return EXIT_SUCCESS;
+}
+
