fa7e6d8c138b49173e282aadb7ba89db1f80875f
Cyrille Valladeau <cyrille.valladeau@c-s.fr>
2009-06-29 18:30:09 +0200

    ENH : PrintableImageFilter in progress

--- Code/BasicFilters/otbPrintableImageFilter.h
+++ Code/BasicFilters/otbPrintableImageFilter.h
@@ -22,77 +22,184 @@
 #ifndef __otbPrintableImageFilter_h
 #define __otbPrintableImageFilter_h
 
+#include "otbImage.h"
 #include "itkImageToImageFilter.h"
 #include "otbVectorRescaleIntensityImageFilter.h"
+#include "itkRescaleIntensityImageFilter.h"
+#include "itkRescaleIntensityImageFilter.h"
 #include "otbMultiChannelExtractROI.h"
+#include "otbMultiToMonoChannelExtractROI.h"
+#include "itkMultiplyImageFilter.h"
+#include "itkAddImageFilter.h"
+//#include "otbImageToVectorImageCastFilter.h"
+#include "otbImageList.h"
+#include "otbImageListToVectorImageFilter.h"
+#include "otbVectorRescaleIntensityImageFilter.h"
+#include "itkBinaryThresholdImageFilter.h"
+
+#include "itkInvertIntensityImageFilter.h"
 
 namespace otb
 {
 /**
-     * \class PrintableImageFilter
-     * \brief This class is a helper class to turn a vector image to a generic 8 bytes RGB image.
-     *
-     *  It is useful for publications for instance.
-     **/
-
-template <class TInputImage>
+ * \class PrintableImageFilter
+ * \brief This class is a helper class to turn a vector image to a generic 8 bytes RGB image.
+ *
+ *  It is useful for publications for instance.
+ **/
+  
+template <class TInputImage , class TMaskImage = otb::Image<unsigned char, 2> >
 class ITK_EXPORT PrintableImageFilter :
-      public itk::ImageToImageFilter<TInputImage, otb::VectorImage<unsigned char,2> >
+public itk::ImageToImageFilter<TInputImage, otb::VectorImage<unsigned char, 2> >
 {
-public:
+  public:
   typedef PrintableImageFilter                            Self;
   typedef itk::ImageToImageFilter
-  <TInputImage, otb::VectorImage<unsigned char,2> >     Superclass;
+  <TInputImage, otb::VectorImage<unsigned char,2> >       Superclass;
   typedef itk::SmartPointer<Self>                         Pointer;
   typedef itk::SmartPointer<const Self>                   ConstPointer;
-
+  
   typedef TInputImage                                     InputImageType;
+  //typedef typename InputImageType::Pointer                InputImagePointerType;
   typedef typename InputImageType::PixelType              InputPixelType;
-  typedef unsigned char                                   OutputPixelType;
-  typedef otb::VectorImage<OutputPixelType,2>             OutputImageType;
+  typedef typename InputImageType::InternalPixelType      InputInternalPixelType;
+  //typedef typename Image<InputInternalPixelType, 2>       SingleInputImageType;
+  typedef unsigned char                                   OutputInternalPixelType;
+  typedef VectorImage<OutputInternalPixelType,2>          OutputImageType;
+  typedef OutputImageType::PixelType                      OutputPixelType;
+  
+  typedef TMaskImage                                                        MaskImageType;
+  typedef typename MaskImageType::Pointer                                   MaskImagePointerType;
+  typedef typename MaskImageType::PixelType               MaskPixelType;   
 
-  typedef otb::VectorRescaleIntensityImageFilter
-  <InputImageType,OutputImageType>                    VectorRescalerType;
-  typedef otb::MultiChannelExtractROI
-  <OutputPixelType,OutputPixelType>                   ChannelExtractorType;
-  typedef typename ChannelExtractorType::ChannelsType     ChannelsType;
+  typedef VectorRescaleIntensityImageFilter<InputImageType,OutputImageType> VectorRescalerType;
+  //typedef itk::RescaleIntensityImageFilter<MaskImageType, MaskImageType>    MaskRescalerType;
+  
+  
+  //typedef otb::MultiChannelExtractROI<OutputInternalPixelType,
+  //OutputInternalPixelType>              ChannelExtractorType;
+  typedef otb::MultiChannelExtractROI<InputInternalPixelType,
+                                      InputInternalPixelType>              ChannelExtractorType;
+  typedef typename ChannelExtractorType::ChannelsType                       ChannelsType;
 
+
+  /** Filters */
+  //typedef itk::InvertIntensityImageFilter<MaskImageType,MaskImageType>       InvertIntensityImageFilterType;
+  typedef itk::BinaryThresholdImageFilter<MaskImageType,MaskImageType>      InvertIntensityImageFilterType;
+  typedef typename InvertIntensityImageFilterType::Pointer                   InvertIntensityImageFilterPointerType;
+  typedef typename itk::MultiplyImageFilter<OutputImageType, MaskImageType>  MultiplierType;
+  typedef typename MultiplierType::Pointer                                   MultiplierPointerType;
+  //typedef ImageToVectorImageCastFilter<MaskImageType, OutputImageType>       CasterFilterType;
+  //typedef typename CasterFilterType::Pointer                                 CasterFilterPointerType;
+  typedef otb::VectorRescaleIntensityImageFilter<OutputImageType,OutputImageType> RescalerFilterType;
+  typedef typename RescalerFilterType::Pointer                               RescalerFilterPointerType;
+  typedef typename itk::AddImageFilter<OutputImageType, OutputImageType>     AdderFilterType;
+  typedef typename AdderFilterType::Pointer                                  AdderFilterPointerType;
+  typedef ImageList<MaskImageType>                                           ImageListType;
+  typedef ImageListToVectorImageFilter<ImageListType, OutputImageType>       CasterFilterType;
+  typedef typename CasterFilterType::Pointer                                 CasterFilterPointerType;
+ 
 
   /** Method for creation through object factory */
   itkNewMacro(Self);
-
+  
   /** Run-time type information */
   itkTypeMacro(PrintableImageFilter,
                itk::ImageToImageFilter);
-
+  
   /** Display */
   void PrintSelf( std::ostream& os, itk::Indent indent ) const;
-
+  
   void SetChannel( unsigned int channel);
   const ChannelsType GetChannels(void) const;
-
+  
   otbSetObjectMemberMacro(Rescaler,AutomaticInputMinMaxComputation,bool);
   otbGetObjectMemberMacro(Rescaler,AutomaticInputMinMaxComputation,bool);
   otbSetObjectMemberMacro(Rescaler,InputMinimum,InputPixelType);
   otbGetObjectMemberMacro(Rescaler,InputMinimum,InputPixelType);
   otbSetObjectMemberMacro(Rescaler,InputMaximum,InputPixelType);
   otbGetObjectMemberMacro(Rescaler,InputMaximum,InputPixelType);
+  
+  
+  /**
+   * If set, only pixels within the mask will be classified.
+   * \param mask The input mask.
+   */
+  void SetInputMask(const MaskImageType * mask);
+  
+  /**
+   * Get the input mask.
+   * \return The mask.
+   */
+  MaskImageType * GetInputMask(void);
 
-protected:
+  itkSetMacro(UseMask, bool);
+  itkGetMacro(UseMask, bool);
+  
+  ChannelsType const GetChannelList()
+  {
+    return m_ChannelList;
+  }
+  void SetChannelList( ChannelsType chList )
+  {
+    if(chList.size() != 3)
+      {
+	itkExceptionMacro(<<"Invalid channel list, size is "<<chList.size()<<" instead of 3");
+      }
+    m_ChannelList = chList;
+    this->Modified();
+  }
+  
+  void SetObjectColor( OutputPixelType val )
+  {
+    if(val.GetSize() != 3)
+    {
+      itkExceptionMacro(<<"Invalid object color, size is "<<val.Size()<<" instead of 3");
+    }
+    m_ObjectColor = val;
+    this->Modified();
+  }
+  itkGetMacro(ObjectColor, OutputPixelType);
+  
+  itkSetMacro(BackgroundMaskValue, MaskPixelType);
+  itkGetMacro(BackgroundMaskValue, MaskPixelType); 
+  itkSetMacro(ForegroundMaskValue, MaskPixelType);
+  itkGetMacro(ForegroundMaskValue, MaskPixelType); 
+
+  protected:
 
   PrintableImageFilter();
 
+  void BeforeGenerateData();
   void GenerateData();
-
-private:
-
+  
+  private:
+  
   PrintableImageFilter(Self&);   // intentionally not implemented
   void operator=(const Self&);          // intentionally not implemented
-
-  typename VectorRescalerType::Pointer m_Rescaler;
+  
+  typename VectorRescalerType::Pointer   m_Rescaler;
   typename ChannelExtractorType::Pointer m_Extractor;
-};
+    
+  InvertIntensityImageFilterPointerType m_InvertIntensityImageFilter;
+  MultiplierPointerType                 m_Multiplier;
+  CasterFilterPointerType               m_MaskCaster;
+  RescalerFilterPointerType             m_MaskRescaler;
+  AdderFilterPointerType                m_Adder;
 
+  // Objects (of the mask) will be displayer with the choosen color.
+  OutputPixelType m_ObjectColor;
+  // Use mask
+  bool m_UseMask;
+  // Used channel for output Image
+  ChannelsType m_ChannelList;
+  // Foreground mask value
+  MaskPixelType m_ForegroundMaskValue;
+  // Background mask value
+  MaskPixelType m_BackgroundMaskValue;
+  
+};
+ 
 } // end namespace otb
 
 #ifndef OTB_MANUAL_INSTANTIATION
--- Code/BasicFilters/otbPrintableImageFilter.txx
+++ Code/BasicFilters/otbPrintableImageFilter.txx
@@ -23,72 +23,233 @@
 
 #include "otbPrintableImageFilter.h"
 
+#include "otbImageFileWriter.h"
 
 namespace otb
 {
 
-template <class TInputImage>
-PrintableImageFilter<TInputImage>
+template <class TInputImage, class TMaskImage>
+PrintableImageFilter<TInputImage, TMaskImage>
 ::PrintableImageFilter()
 {
 
   m_Rescaler = VectorRescalerType::New();
   m_Extractor = ChannelExtractorType::New();
+  
+  m_Rescaler->SetInput( m_Extractor->GetOutput() );
+  
+  m_MaskRescaler = RescalerFilterType::New();
+  m_InvertIntensityImageFilter = InvertIntensityImageFilterType::New();
+  m_Multiplier =  MultiplierType::New();
+  m_MaskCaster = CasterFilterType::New();
+  m_Adder = AdderFilterType::New();
+  
+  m_ChannelList = ChannelsType(3,2);
+  m_ChannelList[1] = 3;
+  m_ChannelList[2] = 4;
+  m_ObjectColor.SetSize( 3 );
+  m_ObjectColor.Fill(255);
 
-  m_Extractor->SetInput( m_Rescaler->GetOutput() );
-
+  m_ForegroundMaskValue = 1;
+  m_BackgroundMaskValue = 0;
 }
 
-template <class TInputImage>
+template <class TInputImage, class TMaskImage>
 void
-PrintableImageFilter<TInputImage>
+PrintableImageFilter<TInputImage, TMaskImage>
 ::SetChannel(unsigned int channel)
 {
+  m_ChannelList.push_back(channel);
   m_Extractor->SetChannel(channel);
   this->Modified();
 }
 
-template <class TInputImage>
-const typename PrintableImageFilter<TInputImage>::ChannelsType
-PrintableImageFilter<TInputImage>
+template <class TInputImage, class TMaskImage>
+const typename PrintableImageFilter<TInputImage,TMaskImage>::ChannelsType
+PrintableImageFilter<TInputImage, TMaskImage>
 ::GetChannels() const
 {
-  return m_Extractor->GetChannels();
+  return m_ChannelList;//m_Extractor->GetChannels();
+}
+
+template <class TInputImage, class TMaskImage>
+void
+PrintableImageFilter<TInputImage, TMaskImage>
+::SetInputMask(const MaskImageType * mask)
+{
+  this->itk::ProcessObject::SetNthInput(1,const_cast<MaskImageType *>(mask));
+  m_UseMask = true;
 }
 
 
-template <class TInputImage>
+template <class TInputImage, class TMaskImage>
+typename PrintableImageFilter<TInputImage,TMaskImage>::MaskImageType *
+PrintableImageFilter<TInputImage, TMaskImage>
+::GetInputMask()
+{
+  if (this->GetNumberOfInputs()<2)
+  {
+    return 0;
+  }
+  return static_cast<MaskImageType *>(this->itk::ProcessObject::GetInput(1));
+}
+
+
+template <class TInputImage, class TMaskImage>
 void
-PrintableImageFilter<TInputImage>
+PrintableImageFilter<TInputImage, TMaskImage>
+::BeforeGenerateData()
+{
+  if(m_Extractor->GetNbChannels() == 0 && m_ChannelList.size()>3)
+    {
+      itkExceptionMacro(<<"Invalid channel list (must be three channels instead of "<<m_ChannelList.size());
+    }
+  if(m_Extractor->GetNbChannels() == 0 && m_Extractor->GetNbChannels()>3)
+    {
+      itkExceptionMacro(<<"Invalid channel list (must be three channels instead of "<<m_Extractor->GetNbChannels());
+    }
+  if( m_ObjectColor.GetSize() != 3 )
+    {
+      itkExceptionMacro(<<"Invalid m_ObjectColor pixel size");
+    }
+  
+  if( m_UseMask == true )
+    {
+      MaskImagePointerType inputMaskPtr  = this->GetInputMask();
+      typename InputImageType::ConstPointer inputPtr = this->GetInput();
+ 
+      if( !inputMaskPtr )
+	{
+	  itkExceptionMacro(<<"No mask detected");
+	}
+      if( inputMaskPtr->GetLargestPossibleRegion().GetSize() !=  inputPtr->GetLargestPossibleRegion().GetSize())
+	{
+	  itkExceptionMacro(<<"Input size ("<<inputPtr->GetLargestPossibleRegion().GetSize()<<") and Mask size ("<<inputMaskPtr->GetLargestPossibleRegion().GetSize()<<") must be the same");
+	}
+    }
+}
+
+template <class TInputImage, class TMaskImage>
+void
+PrintableImageFilter<TInputImage, TMaskImage>
 ::GenerateData()
 {
+  this->BeforeGenerateData();
 
-  if (m_Extractor->GetNbChannels() == 0)
-  {
-    m_Extractor->SetChannel(2);
-    m_Extractor->SetChannel(3);
-    m_Extractor->SetChannel(4);
-  }
+  // let this loop to be compliant with previous version of the class where m_ChannelList didn't exist...
+  
+  if(m_Extractor->GetNbChannels() == 0)
+    {
+      for(unsigned int i=0; i<m_ChannelList.size(); i++)
+      m_Extractor->SetChannel( m_ChannelList[i]);
+    }
+  
+  
+  m_Extractor->SetInput(this->GetInput());
+  m_Extractor->UpdateOutputInformation();
 
   typename TInputImage::PixelType minimum,maximum;
-  minimum.SetSize(this->GetInput()->GetNumberOfComponentsPerPixel());
-  maximum.SetSize(this->GetInput()->GetNumberOfComponentsPerPixel());
+  minimum.SetSize(m_Extractor->GetNbChannels());
+  maximum.SetSize(m_Extractor->GetNbChannels());
   minimum.Fill(0);
   maximum.Fill(255);
 
-  m_Rescaler->SetInput(this->GetInput());
   m_Rescaler->SetOutputMinimum(minimum);
   m_Rescaler->SetOutputMaximum(maximum);
   m_Rescaler->SetClampThreshold(0.01);
 
-  m_Extractor->GraftOutput( this->GetOutput() );
-  m_Extractor->Update();
-  this->GraftOutput( m_Extractor->GetOutput() );
+  if( m_UseMask == false )
+    {
+      m_Rescaler->GraftOutput( this->GetOutput() );
+      m_Rescaler->Update();
+      this->GraftOutput( m_Rescaler->GetOutput() );
+    }
+  else
+    {
+      //m_InvertIntensityImageFilter->SetMaximum(m_ForegroundMaskValue);
+      m_InvertIntensityImageFilter->SetInput(this->GetInputMask());
+      if(m_ForegroundMaskValue>m_BackgroundMaskValue)
+	{
+	  m_InvertIntensityImageFilter->SetInsideValue(0);
+	  m_InvertIntensityImageFilter->SetOutsideValue(1);
+	}
+      else
+	{
+	  m_InvertIntensityImageFilter->SetInsideValue(1);
+	  m_InvertIntensityImageFilter->SetOutsideValue(0);
+	}
+      
+      MaskPixelType thresh = static_cast<MaskPixelType>(static_cast<double>(m_ForegroundMaskValue+m_BackgroundMaskValue)/2);
+      m_InvertIntensityImageFilter->SetLowerThreshold(thresh);
+      m_InvertIntensityImageFilter->SetUpperThreshold(itk::NumericTraits<MaskPixelType>::max());
+
+      typedef ImageFileWriter<MaskImageType> W44;
+      typename W44::Pointer w4 = W44::New();
+      w4->SetFileName("invert.tif");
+      w4->SetInput(m_InvertIntensityImageFilter->GetOutput());
+      w4->Update();
+      
+      
+      m_Multiplier->SetInput1( m_Rescaler->GetOutput());
+      m_Multiplier->SetInput2( m_InvertIntensityImageFilter->GetOutput());
+      m_Multiplier->UpdateOutputInformation();
+      
+      typedef ImageFileWriter<OutputImageType> W00;
+      W00::Pointer w00 = W00::New();
+      w00->SetFileName("multiplier.tif");
+      w00->SetInput(m_Multiplier->GetOutput());
+      w00->Update();
+
+      typename ImageListType::Pointer imList = ImageListType::New();
+      for(unsigned int i=0; i<m_Extractor->GetNbChannels(); i++)
+	imList->PushBack( this->GetInputMask());
+
+      m_MaskCaster->SetInput(imList);
+
+      typedef ImageFileWriter<OutputImageType> W1;
+      W1::Pointer w1 = W1::New();
+      w1->SetFileName("maskcaster.tif");
+      w1->SetInput(m_MaskCaster->GetOutput());
+      w1->Update();
+
+      OutputPixelType mini,maxi;
+      mini.SetSize(m_Extractor->GetNbChannels());
+      mini.Fill( std::min(m_BackgroundMaskValue, m_ForegroundMaskValue));
+      maxi = m_ObjectColor;
+      //maxi.SetSize(m_Extractor->GetNbChannels());
+      //maxi.Fill( 255 );
+      
+      m_MaskRescaler->SetOutputMinimum(mini);
+      m_MaskRescaler->SetOutputMaximum(maxi);
+      m_MaskRescaler->SetClampThreshold(1e-6);
+      m_MaskRescaler->SetInput(m_MaskCaster->GetOutput());
+
+      typedef ImageFileWriter<OutputImageType> W2;
+      W2::Pointer w2 = W2::New();
+      w2->SetFileName("maskrescaler.tif");
+      w2->SetInput(m_MaskRescaler->GetOutput());
+      w2->Update();
+
+      m_Adder->SetInput1( m_Multiplier->GetOutput()  );
+      m_Adder->SetInput2( m_MaskRescaler->GetOutput() );
+
+
+      m_Adder->GraftOutput( this->GetOutput() );
+ typedef ImageFileWriter<OutputImageType> W12;
+      W12::Pointer w12 = W12::New();
+      w12->SetFileName("adder.tif");
+      w12->SetInput(m_Adder->GetOutput());
+      w12->Update();
+      m_Adder->Update();
+std::cout<<"update3"<<std::endl;
+      this->GraftOutput( m_Adder->GetOutput() );
+    }
+  
 }
 
-template <class TInputImage>
+template <class TInputImage, class TMaskImage>
 void
-PrintableImageFilter<TInputImage>
+PrintableImageFilter<TInputImage, TMaskImage>
 ::PrintSelf( std::ostream& os, itk::Indent indent ) const
 {
   Superclass::PrintSelf(os,indent);
