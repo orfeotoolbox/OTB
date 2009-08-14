1891f46a5208b12c7a4f5ff523eefc0597dd07a0
Emmanuel Christophe <emmanuel.christophe@orfeo-toolbox.org>
2009-08-14 13:16:17 +0800

    BUG: fix ERS for direct use

--- Utilities/otbossimplugins/ossim/ossimErsSarModel.cpp
+++ Utilities/otbossimplugins/ossim/ossimErsSarModel.cpp
@@ -185,6 +185,13 @@ bool ossimErsSarModel::open(const ossimFilename& file)
         ossimNotify(ossimNotifyLevel_DEBUG)
         << "End reading Leader file" << std::endl;
       }
+
+      //To initialize the whole state, reusing saveState/loadState
+      //FIXME: This could be at the superclass level instead
+      ossimKeywordlist kwl;
+      saveState(kwl);
+      loadState(kwl);
+
     } // matches: if ( result=isErsLeader(file) == True )
 
   } // matches: if ( file.exists() )
@@ -198,11 +205,7 @@ bool ossimErsSarModel::open(const ossimFilename& file)
        << std::endl;
   }
 
-  //To initialize the whole state, reusing saveState/loadState
-  //FIXME: This could be at the superclass level instead
-  ossimKeywordlist kwl;
-  saveState(kwl);
-  loadState(kwl);
+
 
   return result;
 
