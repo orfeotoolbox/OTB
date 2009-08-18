8eaa8ef91772d0f1297f5b71a2ea0de1af364819
Thomas Feuvrier <thomas.feuvrier@c-s.fr>
2009-08-18 09:01:45 +0200

    WRG: Remove warning for svm library: unused parameter in function

--- Utilities/otbsvm/svm.h
+++ Utilities/otbsvm/svm.h
@@ -202,7 +202,7 @@ public:
   otbSetValueMacro(Double,double);
 */
 
-  virtual double operator()(const svm_node *x, const svm_node *y, const svm_parameter& param)const
+  virtual double operator()(const svm_node * /*x*/, const svm_node * /*y*/, const svm_parameter& /*param*/)const
     {
       itkGenericExceptionMacro(<<"Kernel functor not definied (Null)");
       return static_cast<double>(0.);
@@ -213,7 +213,7 @@ public:
   // index is the current value
   // isAtEnd to indicate that it's the last possible derivation
   // baseValue is the constant of the formula
-  virtual double derivative(const svm_node *x, const svm_node *y, const svm_parameter& param, int degree, int index, bool isAtEnd, double constValue)const
+  virtual double derivative(const svm_node * /*x*/, const svm_node */*y*/, const svm_parameter& /*param*/, int /*degree*/, int /*index*/, bool /*isAtEnd*/, double /*constValue*/)const
     {
       itkGenericExceptionMacro(<<"derivative method not definied (Null)");
       return 0.;
