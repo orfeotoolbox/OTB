95ff33d0e5c4a847a72de4784765882c3ecfa43a
Manuel Grizonnet <manuel.grizonnet@gmail.com>
2009-09-15 17:59:15 +0200

    TEST: add otbogrvectordataiocan*.cxx

---
+++ Testing/Code/IO/otbOGRVectorDataIOCanWrite.cxx
@@ -0,0 +1,36 @@
+/*=========================================================================
+
+  Program:   ORFEO Toolbox
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
+
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#include "otbOGRVectorDataIO.h"
+#include "otbVectorData.h"
+#include <iostream>
+
+int otbOGRVectorDataIOCanWrite(int argc, char* argv[])
+{
+  typedef otb::VectorData<> VectorDataType;
+  typedef otb::OGRVectorDataIO<VectorDataType> OGRVectorDataIOType;
+  OGRVectorDataIOType::Pointer object = OGRVectorDataIOType::New();
+  bool lCanWrite = object->CanWriteFile(argv[1]);
+  if ( lCanWrite == false)
+  {
+    std::cerr << "Erreur otb::OGRVectorDataIO : impossible to create the file "<<argv[1]<<"."<<std::endl;
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
+}
