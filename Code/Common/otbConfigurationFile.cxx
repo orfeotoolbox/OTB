dabd9008d9b3610ee1c6d1833d0bc5a32736dd73
Manuel Grizonnet <manuel.grizonnet@gmail.com>
2009-10-13 18:13:30 +0200

    ENH: add otbconfigurationfile.cxx in common with generic getparam and singleton

---
+++ Code/Common/otbConfigurationFile.cxx
@@ -0,0 +1,68 @@
+/*=========================================================================
+
+  Program:   ORFEO Toolbox
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
+
+  Some parts of this code are derived from ITK. See ITKCopyright.txt
+  for details.
+
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#include "otbConfigurationFile.h"
+
+namespace otb
+{
+/** Initialize the singleton */
+ConfigurationFile::Pointer ConfigurationFile::Instance = NULL;
+
+ConfigurationFile
+::ConfigurationFile()
+{
+  std::string OTBBinDir(OTB_CONFIG);
+  try
+  {
+    m_OTBConfig = new ConfigFile(OTBBinDir + "/otb.conf");
+  }
+  catch (ConfigFile::file_not_found& e)
+  {
+    itkExceptionMacro(<< "Error - File '" << e.filename << "' not found.");
+  }
+}  
+
+ConfigurationFile
+::~ConfigurationFile()
+{
+}
+
+ConfigurationFile::Pointer
+ConfigurationFile
+::GetInstance()
+{
+  if (!Instance)
+  {
+    Instance = Self::New();
+  }
+  return Instance;
+};
+
+
+void 
+ConfigurationFile
+::PrintSelf(std::ostream& os, itk::Indent indent) const 
+{
+  Superclass::PrintSelf(os, indent);
+  os << indent; 
+  os << (*m_OTBConfig);
+      
+}
+} // end namespace otb
