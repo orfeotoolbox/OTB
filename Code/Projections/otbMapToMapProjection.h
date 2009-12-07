fc7817aba3eb18f29b3c124b07c2064d6b62f219
Emmanuel Christophe <emmanuel.christophe@orfeo-toolbox.org>
2009-12-07 15:30:49 +0800

    DOC: mark class as deprecated

--- Code/Projections/otbMapToMapProjection.h
+++ Code/Projections/otbMapToMapProjection.h
@@ -30,12 +30,17 @@ namespace otb
 
 
 /** \class MapToMapProjection
-
-* \brief Class for switching from a Map Projection coordinates to other Map Projection coordinates.
+*
+* \brief <b>DEPRECATED</b>: Class for switching from a Map Projection coordinates to
+* other Map Projection coordinates.
+*
+* <b>DEPRECATED</b>: Use otb::GenericMapProjection instead
 * It converts MapProjection1 coordinates to MapProjection2 coordinates by using MapProjection methods.
 * It takes a point in input.
-* (X_1, Y_1) -> (lat, lon) -> (X_2, Y_2)
+* (X_1, Y_1) -> (lon, lat) -> (X_2, Y_2)
+*
 * \ingroup Transform
+* \sa GenericMapProjection
 */
 template <class TInputMapProjection,
 class TOutputMapProjection,
