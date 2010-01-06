93bdfab21bee1f86b85401cf6d43844d878498fd
Cyrille Valladeau <cyrille.valladeau@c-s.fr>
2010-01-06 10:09:12 +0100

    ENH : add test for TSX Brightness + create an extract for full image test

--- Testing/Code/Radiometry/otbTerraSarBrightnessImageFilterTest.cxx
+++ Testing/Code/Radiometry/otbTerraSarBrightnessImageFilterTest.cxx
@@ -19,6 +19,7 @@
 
 #include "otbTerraSarBrightnessImageFilter.h"
 #include "otbImage.h"
+#include "itkExtractImageFilter.h"
 #include "otbImageFileReader.h"
 #include "otbImageFileWriter.h"
 
@@ -27,30 +28,47 @@ int otbTerraSarBrightnessImageFilterTest(int argc, char * argv[])
 {
   const char * inputFileName  = argv[1];
   const char * outputFileName = argv[2];
+  bool useMetadata = true;
+  if(atoi(argv[3]) != 1)
+    useMetadata = true;
 
   typedef otb::Image<double, 2>                                    ImageType;
   typedef otb::ImageFileReader<ImageType>                          ReaderType;
   typedef otb::ImageFileWriter<ImageType>                          WriterType;
   typedef otb::TerraSarBrightnessImageFilter<ImageType, ImageType> FilterType;
+  typedef itk::ExtractImageFilter<ImageType, ImageType>            ExtractorType;
 
-
-  ReaderType::Pointer reader         = ReaderType::New();
-  WriterType::Pointer writer         = WriterType::New();
-
-  FilterType::Pointer filter         = FilterType::New();
+  ReaderType::Pointer    reader    = ReaderType::New();
+  WriterType::Pointer    writer    = WriterType::New();
+  FilterType::Pointer    filter    = FilterType::New();
+  ExtractorType::Pointer extractor = ExtractorType::New();
+  
 
   reader->SetFileName(inputFileName);
   writer->SetFileName(outputFileName);
-
   reader->UpdateOutputInformation();
-
-  //std::cout<<reader->GetOutput()->GetNumberOfComponentsPerPixel()<<std::endl;
-
-  if( atoi(argv[3]) == 1 )
-    filter->SetCalFactor( 10 );
-
   filter->SetInput(reader->GetOutput());
-  writer->SetInput(filter->GetOutput());
+
+  if( useMetadata )
+    {
+      // Generate an extract from the large input
+      ImageType::RegionType region;
+      ImageType::IndexType id;
+      id[0] = 50;   id[1] = 100;
+      ImageType::SizeType size;
+      size[0] = 150;   size[1] = 100;
+      region.SetIndex(id);
+      region.SetSize(size);
+      extractor->SetExtractionRegion(region);
+  
+      extractor->SetInput(filter->GetOutput());
+      writer->SetInput(extractor->GetOutput());
+    }
+  else
+    {
+      filter->SetCalFactor( 10 );
+      writer->SetInput(filter->GetOutput());
+    }
 
   writer->Update();
 
