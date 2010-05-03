10be754ad62549cca13dd2bd8ff3e6837e9dc14c
Emmanuel Christophe <emmanuel.christophe@orfeo-toolbox.org>
2010-05-03 21:37:53 +0800

    BUG: fix alternative to curl multi

--- Code/IO/otbCurlHelper.cxx
+++ Code/IO/otbCurlHelper.cxx
@@ -254,15 +254,13 @@ int CurlHelper::RetrieveFileMulti(const std::vector<std::string>& listURLs,
   // Cleanup multi handle
   curl_multi_cleanup(multiHandle);
 
-
-
 #else
   //fallback on non curl multi
   otbMsgDevMacro(<< "Curl multi is not available, fallback on standard");
   std::vector<std::string>::const_iterator url;
-  std::vector<FILE *>::const_iterator file;
+  std::vector<std::string>::const_iterator file;
   url = listURLs.begin();
-  file = listFiles.begin();
+  file = listFilename.begin();
   while ( (url != listURLs.end()) && (file != listFiles.end() ))
     {
     RetrieveFile(*url, *file);
@@ -274,6 +272,4 @@ int CurlHelper::RetrieveFileMulti(const std::vector<std::string>& listURLs,
 #endif
 }
 
-
 }
-
