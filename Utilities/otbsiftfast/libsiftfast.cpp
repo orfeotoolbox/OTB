76a6c5d28d702601ae4e42283ce59afbb77b3977
Julien Michel <julien.michel@orfeo-toolbox.org>
2010-06-04 10:12:34 +0200

    BUG: Applying patch to avoid libsiftfast compilation errors with gcc3.4.x

--- Utilities/otbsiftfast/libsiftfast.cpp
+++ Utilities/otbsiftfast/libsiftfast.cpp
@@ -908,7 +908,7 @@ Keypoint FindMaxMin(Image* imdiff, Image* imgaus, float fscale, Keypoint keypts,
 
     for( int index = 1; index < (int)nbScales+1; ++index) {
 
-#if !defined(_MSC_VER) && defined(__SSE__)
+#if !defined(OTB_DISABLE_FAST_FUNCTIONS) && !defined(_MSC_VER) && defined(__SSE__)
         GradOriImagesFast(imgaus[index],s_imgrad,s_imorient);
 #else
         GradOriImages(imgaus[index],s_imgrad,s_imorient);
@@ -1002,7 +1002,7 @@ void GradOriImages(Image image, Image imgrad, Image imorient)
     }
 }
 
-#if !defined(_MSC_VER) && defined(__SSE__)
+#if !defined(OTB_DISABLE_FAST_FUNCTIONS) && !defined(_MSC_VER) && defined(__SSE__)
 void GradOriImagesFast(Image image, Image imgrad, Image imorient)
 {
     DVSTARTPROFILE();
@@ -1703,7 +1703,7 @@ void DestroyAllResources()
     s_listKeypoints.clear();
 }
 
-#if !defined(_MSC_VER) && defined(__SSE__) && !defined(SIMDMATH_H) // copied from libsimdmath
+#if !defined(OTB_DISABLE_FAST_FUNCTIONS) && !defined(_MSC_VER) && defined(__SSE__) && !defined(SIMDMATH_H) // copied from libsimdmath
 
 #define DEF_CONST(a,b) static  const vec_float4 a = {b,b,b,b};
 #define DEI_CONST(a,b) static  const vec_int4   a = {b,b,b,b};
