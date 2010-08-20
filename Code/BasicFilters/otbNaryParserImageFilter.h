bd43a33f3aee0943edfd475a99a9ab7949170169
Julien Michel <julien.michel@orfeo-toolbox.org>
2010-08-20 15:48:03 +0200

    DOC: Typos

--- Code/BasicFilters/otbNaryParserImageFilter.h
+++ Code/BasicFilters/otbNaryParserImageFilter.h
@@ -34,22 +34,22 @@ namespace otb
  * \brief Performs a mathematical operation on the input images
  * according to the formula specified by the user.
  * 
- * This filter is based on the math parser library muParser. 
- * The built in function and operator list is available at:
+ * This filter is based on the mathematical parser library muParser.
+ * The built in functions and operators list is available at:
  * http://muparser.sourceforge.net/mup_features.html#idDef2
  * 
- * OTB specific additional functions : 
+ * OTB additional functions:
  * ndvi(r, niri)
  * 
- * OTB specific additional contants :
+ * OTB additional contants:
  * e - log2e - log10e - ln2 - ln10 - pi - euler
  *
  * In order to use this filter, at least one input image is to be
- * set. A associated variable name can be spécified or not by using
+ * set. An associated variable name can be specified or not by using
  * the corresponding SetNthInput method. For the nth input image, if
  * no associated variable name has been spefified, a default variable
  * name is given by concatenating the letter "b" (for band) and the
- * coresponding n.
+ * coresponding input index.
  * Next step is to set the expression according to the variable
  * names. For example, in the default case with three input images the
  * following expression is valid :
