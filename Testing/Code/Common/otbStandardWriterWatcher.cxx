1e77e32f50419c77dd0dedc0171eb88b2c55a5fc
Emmanuel Christophe <emmanuel.christophe@orfeo-toolbox.org>
2010-09-01 18:03:43 +0800

    TEST: increase coverage for WriterWatcher

--- Testing/Code/Common/otbStandardWriterWatcher.cxx
+++ Testing/Code/Common/otbStandardWriterWatcher.cxx
@@ -50,13 +50,14 @@ int otbStandardWriterWatcher(int argc, char * argv[])
   writer1->SetInput(gradient->GetOutput());
   writer1->SetFileName(outfname);
 
-  otb::StandardWriterWatcher watcher1(writer1, "Gradient (streaming)");
+  typedef otb::StandardWriterWatcher WatcherType;
+  WatcherType watcher1(writer1, "Gradient (streaming)");
 
-  otb::StandardWriterWatcher watcher3(writer1, gradient, "Gradient");
-
-  otb::StandardWriterWatcher watcher4(watcher1);
-
-  otb::StandardWriterWatcher watcher5 = watcher1;
+//  otb::StandardWriterWatcher watcher3(writer1, gradient, "Gradient");
+//
+//  otb::StandardWriterWatcher watcher4(watcher1);
+//
+//  otb::StandardWriterWatcher watcher5 = watcher1;
 
   writer1->Update();
 
@@ -66,6 +67,29 @@ int otbStandardWriterWatcher(int argc, char * argv[])
 
   otb::StandardWriterWatcher watcher2(writer2, "Gradient (non streaming)");
   writer2->Update();
+
+  // Test copy constructor.
+  WatcherType watcher3( watcher1 );
+  if ( watcher1.GetNameOfClass() != watcher3.GetNameOfClass()
+       || watcher1.GetProcess() != watcher3.GetProcess()
+       || watcher1.GetComment() != watcher3.GetComment() )
+    {
+    std::cout << "Copy constructor failed." << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  // Test default constructor.
+  WatcherType watcher4;
+
+  // Test assignment operator.
+  watcher4 = watcher3;
+  if ( watcher4.GetNameOfClass() != watcher3.GetNameOfClass()
+       || watcher4.GetProcess() != watcher3.GetProcess()
+       || watcher4.GetComment() != watcher3.GetComment() )
+    {
+    std::cout << "Operator= failed." << std::endl;
+    return EXIT_FAILURE;
+    }
 
   return EXIT_SUCCESS;
 }
