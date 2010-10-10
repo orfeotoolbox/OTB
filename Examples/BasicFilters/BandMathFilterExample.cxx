eddeca49aa779364be78fd7e5ed171be583c31f3
Emmanuel Christophe <emmanuel.christophe@orfeo-toolbox.org>
2010-10-10 08:19:55 +0800

    WRG

--- Examples/BasicFilters/BandMathFilterExample.cxx
+++ Examples/BasicFilters/BandMathFilterExample.cxx
@@ -28,13 +28,15 @@
 //
 //  This filter is based on the mathematical parser library muParser.
 //  The built in functions and operators list is available at:
-//  \url{http://muparser.sourceforge.net/mup_features.html} \\
+//  \url{http://muparser.sourceforge.net/mup_features.html}.
+//
 //  In order to use this filter, at least one input image is to be
 // set. An associated variable name can be specified or not by using
 // the corresponding SetNthInput method. For the nth input image, if
 // no associated variable name has been specified, a default variable
 // name is given by concatenating the letter "b" (for band) and the
-// corresponding input index. \\
+// corresponding input index.
+//
 // The next step is to set the expression according to the variable
 // names. For example, in the default case with three input images the
 // following expression is valid : "(b1+b2)*b3".
@@ -151,7 +153,8 @@ int main( int argc, char* argv[])
 //
 //  Now we can define the mathematical expression to perform on the layers (b1, b2,b3,b4).
 //  The filter takes advantage of the parsing capabilities of the muParser library and
-//  allows to set the expression as on a digital calculator. \\
+//  allows to set the expression as on a digital calculator.
+//
 //  The expression below returns 255 if the ratio $(NIR-RED)/(NIR+RED)$ is greater than 0.4 and 0 if not.
 //
 //  Software Guide : EndLatex
