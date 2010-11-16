7d525495d1ca1eae087e5f114da7a781da75a13f
Patrick Imbo <patrick.imbo@c-s.fr>
2010-11-16 16:18:45 +0100

    ENH: remove ImageFileReaderERS Test -> replace by ImageFileReaderWithComplexPixel

--- Testing/Code/IO/otbIOTests1.cxx
+++ Testing/Code/IO/otbIOTests1.cxx
@@ -26,7 +26,6 @@
 
 void RegisterTests()
 {
-  REGISTER_TEST(otbImageFileReaderERS);
   REGISTER_TEST(otbLUMImageIOTestCanRead);
   REGISTER_TEST(otbLUMImageIOTestCanWrite);
   REGISTER_TEST(otbMWImageIOTestCanRead);
