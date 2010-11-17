802fa2e1b89c61fea82ea14c88bfce9cb3108111
Cyrille Valladeau <cyrille.valladeau@c-s.fr>
2010-11-17 17:27:11 +0100

    Coorect test TorontoDb : write vectorimage

--- Testing/Code/Radiometry/otbTerraSarCalibrationImageComplexFilterTest.cxx
+++ Testing/Code/Radiometry/otbTerraSarCalibrationImageComplexFilterTest.cxx
@@ -20,9 +20,15 @@
 #include "otbTerraSarCalibrationImageFilter.h"
 #include "otbImage.h"
 #include "otbVectorImage.h"
+#include "otbVectorImage.h"
 #include "itkExtractImageFilter.h"
 #include "otbImageFileReader.h"
 #include "otbImageFileWriter.h"
+#include "itkComplexToRealImageFilter.h"
+#include "itkComplexToImaginaryImageFilter.h"
+#include "otbImageList.h"
+#include "otbImageListToVectorImageFilter.h"
+
 
 int otbTerraSarCalibrationImageComplexFilterTest(int argc, char * argv[])
 {
@@ -31,12 +37,20 @@ int otbTerraSarCalibrationImageComplexFilterTest(int argc, char * argv[])
   const bool   useFastCalibration = atoi(argv[3]);
   const bool   resultsInDbs = atoi(argv[4]);
 
-  typedef std::complex<double>                                      ComplexType;
-  typedef otb::Image<ComplexType, 2>                                ImageType;
-  typedef otb::ImageFileReader<ImageType>                           ReaderType;
-  typedef otb::ImageFileWriter<ImageType>                           WriterType;
-  typedef otb::TerraSarCalibrationImageFilter<ImageType, ImageType> FilterType;
-  typedef itk::ExtractImageFilter<ImageType, ImageType>             ExtractorType;
+  typedef std::complex<double>            ComplexType;
+  typedef otb::Image<ComplexType, 2>      ImageCplxType;
+  typedef otb::Image<double, 2>              ImageScalarType; 
+  typedef otb::VectorImage<double, 2>        OutputImageType;
+  typedef otb::ImageList<ImageScalarType> ImageListType;
+
+  typedef otb::ImageFileReader<ImageCplxType>                               ReaderType;
+  typedef otb::ImageFileWriter<OutputImageType>                             WriterType;
+  typedef otb::TerraSarCalibrationImageFilter<ImageCplxType, ImageCplxType> FilterType;
+  typedef itk::ExtractImageFilter<ImageCplxType, ImageCplxType>             ExtractorType;
+
+  typedef itk::ComplexToRealImageFilter<ImageCplxType, ImageScalarType>      RealExtractorType;
+  typedef itk::ComplexToImaginaryImageFilter<ImageCplxType, ImageScalarType> ImaginaryExtractorType;
+  typedef otb::ImageListToVectorImageFilter<ImageListType, OutputImageType>  ListToVectorImageFilterType;
 
   ReaderType::Pointer    reader = ReaderType::New();
   WriterType::Pointer    writer = WriterType::New();
@@ -52,26 +66,42 @@ int otbTerraSarCalibrationImageComplexFilterTest(int argc, char * argv[])
   filter->SetUseFastCalibration(useFastCalibration);
   filter->SetResultsInDecibels(resultsInDbs);
 
+  // The rest of the code is here to translate the image complexe into
+  // a VectorImage of int which each channel is the real and imagynary part
+  RealExtractorType::Pointer realExt = RealExtractorType::New();
+  ImaginaryExtractorType::Pointer imgExt = ImaginaryExtractorType::New();
+
   if (argc == 9)
     {
-    ImageType::RegionType region;
-    ImageType::IndexType  id;
+    ImageCplxType::RegionType region;
+    ImageCplxType::IndexType  id;
     id[0] = atoi(argv[5]);
     id[1] = atoi(argv[6]);
-    ImageType::SizeType size;
+    ImageCplxType::SizeType size;
     size[0] = atoi(argv[7]);
     size[1] = atoi(argv[8]);
     region.SetIndex(id);
     region.SetSize(size);
     extractor->SetExtractionRegion(region);
     extractor->SetInput(filter->GetOutput());
-    writer->SetInput(extractor->GetOutput());
+
+    realExt->SetInput(extractor->GetOutput());
+    imgExt->SetInput(extractor->GetOutput());
     }
   else
     {
-    writer->SetInput(filter->GetOutput());
+      realExt->SetInput(filter->GetOutput());
+      imgExt->SetInput(filter->GetOutput());
     }
-
+  
+  
+  ImageListType::Pointer imList = ImageListType::New();
+  imList->PushBack(realExt->GetOutput());
+  imList->PushBack(imgExt->GetOutput());
+  ListToVectorImageFilterType::Pointer listCaster = ListToVectorImageFilterType::New();
+  listCaster->SetInput(imList);
+  
+  writer->SetInput(listCaster->GetOutput());
   writer->Update();
 
   return EXIT_SUCCESS;
