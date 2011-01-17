c2b729729b10350ed60840f24f24226554a39337
Patrick Imbo <patrick.imbo@c-s.fr>
2011-01-17 11:42:46 +0100

    ENH: change SinclairToCovarianceImageFilter to SinclairImageFilter

---
+++ Testing/Code/SARPolarimetry/otbSinclairImageFilter.cxx
@@ -0,0 +1,112 @@
+/*=========================================================================
+
+  Program:   ORFEO Toolbox
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
+
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#if defined(_MSC_VER)
+#pragma warning ( disable : 4786 )
+#endif
+
+#include "itkExceptionObject.h"
+#include <iostream>
+
+#include "otbImage.h"
+#include "otbVectorImage.h"
+#include "otbImageFileReader.h"
+#include "otbImageFileWriter.h"
+#include "otbSinclairImageFilter.h"
+#include "otbSinclairToCovarianceFunctor.h"
+#include "otbSinclairToCoherencyFunctor.h"
+
+
+template<class TInputPixel, class TOutputPixel, class TFunction>
+int generic_SinclairImageFilter(int argc, char * argv[])
+{
+  const char * inputFilename1  = argv[1];
+  const char * inputFilename2  = argv[2];
+  const char * inputFilename3  = argv[3];
+
+  const char * outputFilename = argv[4];
+
+
+  typedef TInputPixel  InputPixelType;
+  typedef TOutputPixel OutputPixelType;
+  const unsigned int Dimension = 2;
+
+  typedef otb::Image<InputPixelType,  Dimension>       InputImageType;
+  typedef otb::VectorImage<OutputPixelType, Dimension> OutputImageType;
+
+  typedef otb::ImageFileReader<InputImageType>  ReaderType;
+  typedef otb::ImageFileWriter<OutputImageType> WriterType;
+
+  typedef otb::SinclairImageFilter<InputImageType, InputImageType,
+                                      InputImageType, InputImageType,
+                                      OutputImageType, TFunction > FilterType;
+
+  typename FilterType::Pointer filter = FilterType::New();
+
+  typename ReaderType::Pointer reader1 = ReaderType::New();
+  typename ReaderType::Pointer reader2 = ReaderType::New();
+  typename ReaderType::Pointer reader3 = ReaderType::New();
+  typename WriterType::Pointer writer = WriterType::New();
+
+  reader1->SetFileName(inputFilename1);
+  reader2->SetFileName(inputFilename2);
+  reader3->SetFileName(inputFilename3);
+  writer->SetFileName(outputFilename);
+
+  filter->SetInputHH(reader1->GetOutput());
+  filter->SetInputHV(reader2->GetOutput());
+  filter->SetInputVH(reader2->GetOutput());
+  filter->SetInputVV(reader3->GetOutput());
+  writer->SetInput(filter->GetOutput());
+
+  writer->Update();
+
+  return EXIT_SUCCESS;
+}
+
+int otbSinclairImageFilter(int argc, char * argv[])
+{
+  const unsigned int Dimension = 2;
+  typedef std::complex <double> InputPixelType;
+  typedef std::complex <double> OutputPixelType;
+
+  typedef otb::Image<InputPixelType,  Dimension>       InputImageType;
+  typedef otb::VectorImage<OutputPixelType, Dimension> OutputImageType;
+
+  std::string strArgv(argv[1]);
+  argc--;
+  argv++;
+  if (strArgv == "SinclairToCovariance")
+    return (generic_SinclairImageFilter<InputPixelType, OutputPixelType,
+                otb::Functor::SinclairToCovarianceFunctor<InputImageType::PixelType,
+                                    InputImageType::PixelType,
+                                    InputImageType::PixelType,
+                                    InputImageType::PixelType,
+                                    OutputImageType::PixelType> >
+                  (argc, argv));
+  else  if (strArgv == "SinclairToCoherency")
+    return (generic_SinclairImageFilter<InputPixelType, OutputPixelType,
+                otb::Functor::SinclairToCoherencyFunctor<InputImageType::PixelType,
+                                    InputImageType::PixelType,
+                                    InputImageType::PixelType,
+                                    InputImageType::PixelType,
+                                    OutputImageType::PixelType> >
+                  (argc, argv));
+  else return EXIT_FAILURE;
+
+  return EXIT_SUCCESS;
+}
