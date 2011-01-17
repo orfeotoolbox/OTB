27381df90202521119d0b571a539b9a2fafd331f
Patrick Imbo <patrick.imbo@c-s.fr>
2011-01-17 17:08:10 +0100

    ENH: write the MLCToCoherencyFunctor

--- Code/SARPolarimetry/otbMLCToCoherencyImageFilter.h
+++ Code/SARPolarimetry/otbMLCToCoherencyImageFilter.h
@@ -1,107 +1,110 @@
 /*=========================================================================
 
-  Program:   OTB
-  Module:    $otbMLCToCoherencyImageFilter.h$
+  Program:   ORFEO Toolbox
   Language:  C++
-  Date:      $Date: 24/10/2006 $
-  Version:   $Revision: 1.0 $
+  Date:      $Date$
+  Version:   $Revision$
 
- 
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
+
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
 
 =========================================================================*/
+
 #ifndef __MLCToCoherencyImageFilter_h
 #define __MLCToCoherencyImageFilter_h
 
-#include "itkImageToImageFilter.h"
-#include "itkImage.h"
-#include "itkNumericTraits.h"
-#include "itkImageRegionIterator.h"
-#include "itkImageRegionConstIterator.h"
-#include "itkImageRegionConstIteratorWithIndex.h"
-#include "itkVector.h"
-
-#include "otbImage.h"
+#include "itkUnaryFunctorImageFilter.h"
 
 namespace otb
  {
+
+namespace Functor {
+
+template< class TInput, class TOutput>
+class MLCToCoherencyFunctor
+{
+public:
+  typedef typename std::complex <double>           ComplexType;
+  typedef typename TOutput::ValueType              OutputValueType;
+
+  inline TOutput operator()( const TInput & Covariance ) const
+    {
+    TOutput result;
+    result.SetSize(m_NumberOfComponentsPerPixel);
+
+    ComplexType C11 =  static_cast<ComplexType>(Covariance[0]);
+    ComplexType C12 =  static_cast<ComplexType>(Covariance[1]);
+    ComplexType C13 =  static_cast<ComplexType>(Covariance[2]);
+    ComplexType C22 =  static_cast<ComplexType>(Covariance[3]);
+    ComplexType C23 =  static_cast<ComplexType>(Covariance[4]);
+    ComplexType C33 =  static_cast<ComplexType>(Covariance[5]);
+
+    ComplexType C21 =  vcl_conj(C12);
+    ComplexType C31 =  vcl_conj(C13);
+    ComplexType C32 =  vcl_conj(C23);
+
+    result[0] = static_cast<OutputValueType>( 0.5*(C11 + C13 + C31 + C33) );
+    result[1] = static_cast<OutputValueType>( 0.5*(C11 - C13 + C31 - C33) );
+    result[2] = static_cast<OutputValueType>( C12 + C32 );
+    result[3] = static_cast<OutputValueType>( 0.5*(C11 - C13 - C31 + C33) );
+    result[4] = static_cast<OutputValueType>( C12 - C32 );
+    result[5] = static_cast<OutputValueType>( 2.0 * C22 );
+
+    return result;
+    }
+
+   unsigned int GetOutputSize()
+   {
+     return m_NumberOfComponentsPerPixel;
+   }
+
+   /** Constructor */
+   MLCToCoherencyFunctor() : m_NumberOfComponentsPerPixel(6)  {}
+
+   /** Destructor */
+   ~MLCToCoherencyFunctor() {}
+
+private:
+    unsigned int m_NumberOfComponentsPerPixel;
+};
+}
+
+
 /** \class otbMLCToCoherencyImageFilter
  * \brief Compute the Coherency image (9 channels) from the MLC image (9 channels)
  */
-template <class TPixel>
-class  MLCToCoherencyImageFilter :
-   public itk::ImageToImageFilter< otb::Image<itk::Vector<TPixel,9>,2>, otb::Image<itk::Vector<TPixel,9>,2> >
+template <class TInputImage, class TOutputImage, class TFunction = Functor::MLCToCoherencyFunctor<
+    ITK_TYPENAME TInputImage::PixelType, ITK_TYPENAME TOutputImage::PixelType> >
+class ITK_EXPORT MLCToCoherencyImageFilter :
+   public itk::UnaryFunctorImageFilter<TInputImage,TOutputImage, TFunction>
 {
 public:
-  /** Convenient typedefs for simplifying declarations. */
-  typedef otb::Image<itk::Vector<TPixel,9>,2> InputImageType;
-  typedef otb::Image<itk::Vector<TPixel,9>,2> OutputImageType;
+   /** Standard class typedefs. */
+   typedef MLCToCoherencyImageFilter  Self;
+   typedef itk::UnaryFunctorImageFilter<TInputImage,TOutputImage, TFunction> Superclass;
+   typedef itk::SmartPointer<Self>        Pointer;
+   typedef itk::SmartPointer<const Self>  ConstPointer;
 
-  /** Extract dimension from input and output image. */
-  itkStaticConstMacro(InputImageDimension, unsigned int,
-                      InputImageType::ImageDimension);
-  itkStaticConstMacro(OutputImageDimension, unsigned int,
-                      OutputImageType::ImageDimension);
+   /** Method for creation through the object factory. */
+   itkNewMacro(Self);
 
-  
-  /** Standard class typedefs. */
-  typedef MLCToCoherencyImageFilter Self;
-  typedef itk::ImageToImageFilter< InputImageType, OutputImageType> Superclass;
-  typedef itk::SmartPointer<Self> Pointer;
-  typedef itk::SmartPointer<const Self>  ConstPointer;
+   /** Runtime information support. */
+   itkTypeMacro(MLCToCoherencyImageFilter,itk::UnaryFunctorImageFilter);
 
-  /** Method for creation through the object factory. */
-  itkNewMacro(Self);
-
-  /** Run-time type information (and related methods). */
-  itkTypeMacro(MLCToCoherencyImageFilter, ImageToImageFilter);
-  
-  /** Image typedef support. */
-  typedef typename InputImageType::PixelType InputPixelType;
-  typedef typename OutputImageType::PixelType OutputPixelType;
-  
-  typedef typename InputImageType::RegionType InputImageRegionType;
-  typedef typename OutputImageType::RegionType OutputImageRegionType;
-
-  typedef typename InputImageType::SizeType InputSizeType;
-  typedef typename InputImageType::IndexType IndexType ;
-
-  /** MLC type **/
-  typedef itk::Vector<float,9> MLCType;
-
-  /** Stokes type **/
-  typedef itk::Vector<float,9> CoherencyType;
-
-  
-  
-  /** MLCToCoherencyImageFilter needs a larger input requested region than
-   * the output requested region.  As such, MLCToCoherencyImageFilter needs
-   * to provide an implementation for GenerateInputRequestedRegion()
-   * in order to inform the pipeline execution model.
-   *
-   * \sa ImageToImageFilter::GenerateInputRequestedRegion() */
-  virtual void GenerateInputRequestedRegion() throw(itk::InvalidRequestedRegionError);
 
 protected:
-  MLCToCoherencyImageFilter();
+  MLCToCoherencyImageFilter() {}
   virtual ~MLCToCoherencyImageFilter() {}
-  void PrintSelf(std::ostream& os, itk::Indent indent) const;
-
-  /** MLCToCoherencyImageFilter can be implemented as a multithreaded filter.
-   * Therefore, this implementation provides a ThreadedGenerateData()
-   * routine which is called for each processing thread. The output
-   * image data is allocated automatically by the superclass prior to
-   * calling ThreadedGenerateData().  ThreadedGenerateData can only
-   * write to the portion of the output image specified by the
-   * parameter "outputRegionForThread"
-   *
-   * \sa ImageToImageFilter::ThreadedGenerateData(),
-   *     ImageToImageFilter::GenerateData() */
-  void ThreadedGenerateData(const OutputImageRegionType& outputRegionForThread,
-                            int threadId );
 
 private:
   MLCToCoherencyImageFilter(const Self&); //purposely not implemented
-  void operator=(const Self&); //purposely not implemented
+  void operator=(const Self&);            //purposely not implemented
 
  
  
@@ -109,9 +112,5 @@ private:
 };
   
 } // end namespace otb
-
-#ifndef ITK_MANUAL_INSTANTIATION
-#include "otbMLCToCoherencyImageFilter.txx"
-#endif
 
 #endif
