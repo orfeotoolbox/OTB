92a1c7b33186da3ab4b0fe6668eb7c5091dd04c4
Patrick Imbo <patrick.imbo@c-s.fr>
2011-01-17 18:10:20 +0100

    ENH: MLCToCoherencyDegreeImageFilter class inherite from an UnaryFunctorImageFilter

--- Code/SARPolarimetry/otbMLCToCoherencyDegreeImageFilter.h
+++ Code/SARPolarimetry/otbMLCToCoherencyDegreeImageFilter.h
@@ -1,111 +1,129 @@
 /*=========================================================================
 
-  Program:   OTB
-  Module:    $otbMLCToCoherencyDegreeImageFilter.h$
+  Program:   ORFEO Toolbox
   Language:  C++
-  Date:      $Date: 24/10/2006 $
-  Version:   $Revision: 1.0 $
+  Date:      $Date$
+  Version:   $Revision$
 
- 
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
+
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
 
 =========================================================================*/
+
 #ifndef __MLCToCoherencyDegreeImageFilter_h
 #define __MLCToCoherencyDegreeImageFilter_h
 
-#include "math.h"
-
-#include "itkImageToImageFilter.h"
-#include "itkImage.h"
-#include "itkNumericTraits.h"
-#include "itkImageRegionIterator.h"
-#include "itkImageRegionConstIterator.h"
-#include "itkImageRegionConstIteratorWithIndex.h"
-#include "itkVector.h"
-
-#include "otbImage.h"
+#include "itkUnaryFunctorImageFilter.h"
 
 namespace otb
  {
-/** \class otbMLCToCoherencyDegreeImageFilter
- * \brief Compute the Coherency image (9 channels) from the MLC image (9 channels)
+
+namespace Functor {
+
+/** \class otbMLCToCoherencyDegreeFunctor
+ * \brief Evaluate the Coherency Degree coefficient from from the MLC image
+ *
+ * *  Output value are:
+ *   channel #0 : \f$ 0.5 * (S_{hh}+S_{vv}.(S_{hh}+S_{vv})^{*} \f$
+ *   channel #1 : \f$ 0.5 * (S_{hh}+S_{vv}.(S_{hh}-S_{vv})^{*} \f$
+ *   channel #2 : \f$ (S_{hh}+S_{vv}.(S_{hv})^{*} \f$
+ *   channel #3 : \f$ 0.5 * (S_{hh}-S_{vv}.(S_{hh}-S_{vv})^{*} \f$
+ *   channel #4 : \f$ (S_{hh}-S_{vv}.(S_{hv})^{*}  \f$
+ *   channel #5 : \f$ 2.0*S_{hv}.S_{hv}^{*} \f$
+ *
  */
-template <class TPixel>
-class  MLCToCoherencyDegreeImageFilter :
-   public itk::ImageToImageFilter< otb::Image<itk::Vector<TPixel,9>,2>, otb::Image<itk::Vector<TPixel,3>,2> >
+template< class TInput, class TOutput>
+class MLCToCoherencyDegreeFunctor
 {
 public:
-  /** Convenient typedefs for simplifying declarations. */
-  typedef TPixel InputPixelType;
-  typedef otb::Image<itk::Vector<TPixel,9>,2> InputImageType;
-  typedef otb::Image<itk::Vector<TPixel,3>,2> OutputImageType;
+  typedef double                                   RealType;
+  typedef typename std::complex <double>           ComplexType;
+  typedef typename TOutput::ValueType              OutputValueType;
 
-  /** Extract dimension from input and output image. */
-  itkStaticConstMacro(InputImageDimension, unsigned int,
-                      InputImageType::ImageDimension);
-  itkStaticConstMacro(OutputImageDimension, unsigned int,
-                      OutputImageType::ImageDimension);
+  inline TOutput operator()( const TInput & Covariance ) const
+    {
+    TOutput result;
+    result.SetSize(m_NumberOfComponentsPerPixel);
+    result.Fill(0.0);
 
-  
-  /** Standard class typedefs. */
-  typedef MLCToCoherencyDegreeImageFilter Self;
-  typedef itk::ImageToImageFilter< InputImageType, OutputImageType> Superclass;
-  typedef itk::SmartPointer<Self> Pointer;
-  typedef itk::SmartPointer<const Self>  ConstPointer;
+    RealType C11 =  static_cast<RealType>(Covariance[0].real());
+    ComplexType C12 =  static_cast<ComplexType>(Covariance[1]);
+    ComplexType C13 =  static_cast<ComplexType>(Covariance[2]);
+    RealType C22 =  static_cast<RealType>(Covariance[3].real());
+    ComplexType C23 =  static_cast<ComplexType>(Covariance[4]);
+    RealType C33 =  static_cast<RealType>(Covariance[5].real());
 
-  /** Method for creation through the object factory. */
-  itkNewMacro(Self);
+    if ((C11 >0.00001) && (C33 > 0.0001))
+      {
+      result[0] = vcl_abs(C13) / vcl_sqrt(C11 *C33); // |<hh.vv*|/sqrt(<hh.hh*><vv.vv*>)
+      }
 
-  /** Run-time type information (and related methods). */
-  itkTypeMacro(MLCToCoherencyDegreeImageFilter, ImageToImageFilter);
-  
-  /** Image typedef support. */
-  typedef typename InputImageType::PixelType InputPixelType;
-  typedef typename OutputImageType::PixelType OutputPixelType;
-  
-  typedef typename InputImageType::RegionType InputImageRegionType;
-  typedef typename OutputImageType::RegionType OutputImageRegionType;
+    if ((C22 > 0.00001) && (C33 > 0.00001))
+      {
+      result[1] = vcl_abs(C23) / vcl_sqrt(C22 * C33);  // |<hv.vv*|/sqrt(<hv.hv*><vv.vv*>)
+      }
 
-  typedef typename InputImageType::SizeType InputSizeType;
-  typedef typename InputImageType::IndexType IndexType ;
+    if ((C11 > 0.00001) && (C22 > 0.00001) )
+      {
+      result[2] = vcl_abs(C12) / vcl_sqrt(C11 * C22);  // |<hh.hv*|/sqrt(<hh.hh*><hv.hv*>)
+      }
 
-  /** MLC type **/
-  typedef itk::Vector<float,9> MLCType;
+    return result;
+    }
 
-  /**CoherencyDegree type **/
-  typedef itk::Vector<TPixel,3> CoherencyDegreeType;
+   unsigned int GetOutputSize()
+   {
+     return m_NumberOfComponentsPerPixel;
+   }
 
- 
-  
-  
-  /** MLCToCoherencyDegreeImageFilter needs a larger input requested region than
-   * the output requested region.  As such, MLCToCoherencyDegreeImageFilter needs
-   * to provide an implementation for GenerateInputRequestedRegion()
-   * in order to inform the pipeline execution model.
-   *
-   * \sa ImageToImageFilter::GenerateInputRequestedRegion() */
-  virtual void GenerateInputRequestedRegion() throw(itk::InvalidRequestedRegionError);
+   /** Constructor */
+   MLCToCoherencyDegreeFunctor() : m_NumberOfComponentsPerPixel(3)  {}
+
+   /** Destructor */
+   ~MLCToCoherencyDegreeFunctor() {}
+
+private:
+    unsigned int m_NumberOfComponentsPerPixel;
+};
+}
+
+
+/** \class otbMLCToCoherencyDegreeImageFilter
+ * \brief Compute the Coherency Degree coefficient
+ * from the MLC image (6 complex channels)
+ */
+template <class TInputImage, class TOutputImage, class TFunction = Functor::MLCToCoherencyDegreeFunctor<
+    ITK_TYPENAME TInputImage::PixelType, ITK_TYPENAME TOutputImage::PixelType> >
+class ITK_EXPORT MLCToCoherencyDegreeImageFilter :
+   public itk::UnaryFunctorImageFilter<TInputImage,TOutputImage, TFunction>
+{
+public:
+   /** Standard class typedefs. */
+   typedef MLCToCoherencyDegreeImageFilter  Self;
+   typedef itk::UnaryFunctorImageFilter<TInputImage,TOutputImage, TFunction> Superclass;
+   typedef itk::SmartPointer<Self>        Pointer;
+   typedef itk::SmartPointer<const Self>  ConstPointer;
+
+   /** Method for creation through the object factory. */
+   itkNewMacro(Self);
+
+   /** Runtime information support. */
+   itkTypeMacro(MLCToCoherencyDegreeImageFilter,itk::UnaryFunctorImageFilter);
+
 
 protected:
-  MLCToCoherencyDegreeImageFilter();
+  MLCToCoherencyDegreeImageFilter() {}
   virtual ~MLCToCoherencyDegreeImageFilter() {}
-  void PrintSelf(std::ostream& os, itk::Indent indent) const;
-
-  /** MLCToCoherencyDegreeImageFilter can be implemented as a multithreaded filter.
-   * Therefore, this implementation provides a ThreadedGenerateData()
-   * routine which is called for each processing thread. The output
-   * image data is allocated automatically by the superclass prior to
-   * calling ThreadedGenerateData().  ThreadedGenerateData can only
-   * write to the portion of the output image specified by the
-   * parameter "outputRegionForThread"
-   *
-   * \sa ImageToImageFilter::ThreadedGenerateData(),
-   *     ImageToImageFilter::GenerateData() */
-  void ThreadedGenerateData(const OutputImageRegionType& outputRegionForThread,
-                            int threadId );
 
 private:
   MLCToCoherencyDegreeImageFilter(const Self&); //purposely not implemented
-  void operator=(const Self&); //purposely not implemented
+  void operator=(const Self&);                  //purposely not implemented
 
  
  
@@ -113,9 +131,5 @@ private:
 };
   
 } // end namespace otb
-
-#ifndef ITK_MANUAL_INSTANTIATION
-#include "otbMLCToCoherencyDegreeImageFilter.txx"
-#endif
 
 #endif
