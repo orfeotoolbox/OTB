d148251c2acb65dd801832674cc0f9e7d4db0338
Patrick Imbo <patrick.imbo@c-s.fr>
2011-01-18 10:14:52 +0100

    ADD: MLCToCircularCoherencyDegreeImageFilter test: New()

---
+++ Testing/Code/SARPolarimetry/otbMLCToCircularCoherencyDegreeImageFilterNew.cxx
@@ -0,0 +1,43 @@
+/*=========================================================================
+
+  Program:   ORFEO Toolbox
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
+
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#if defined(_MSC_VER)
+#pragma warning ( disable : 4786 )
+#endif
+
+#include "itkExceptionObject.h"
+#include <iostream>
+
+#include "otbVectorImage.h"
+#include "otbMLCToCircularCoherencyDegreeImageFilter.h"
+
+int otbMLCToCircularCoherencyDegreeImageFilterNew(int argc, char * argv[])
+{
+  const unsigned int Dimension = 2;
+
+  typedef std::complex<double>   PixelType;
+  typedef otb::VectorImage<PixelType, Dimension> ImageType;
+
+
+  typedef otb::MLCToCircularCoherencyDegreeImageFilter<ImageType, ImageType> FilterType;
+
+  FilterType::Pointer filter = FilterType::New();
+
+  std::cout << filter << std::endl;
+
+  return EXIT_SUCCESS;
+}
