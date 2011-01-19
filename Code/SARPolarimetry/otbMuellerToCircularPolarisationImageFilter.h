a0de6dbd91db401f66963884c46ae1d0f1385fac
Patrick Imbo <patrick.imbo@c-s.fr>
2011-01-19 16:07:10 +0100

    ENH: MuellerToCircularPolarisationImageFilter class inherite from an UnaryFunctorImageFilter

--- Code/SARPolarimetry/otbMuellerToCircularPolarisationImageFilter.h
+++ Code/SARPolarimetry/otbMuellerToCircularPolarisationImageFilter.h
@@ -1,123 +1,110 @@
 /*=========================================================================
 
-  Program:   OTB
-  Module:    $otbMuellerToCircularPolarisationImageFilter.h$
+  Program:   ORFEO Toolbox
   Language:  C++
-  Date:      $Date: 24/10/2006 $
-  Version:   $Revision: 1.0 $
+  Date:      $Date$
+  Version:   $Revision$
 
- 
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
+
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
 
 =========================================================================*/
+
 #ifndef __MuellerToCircularPolarisationImageFilter_h
 #define __MuellerToCircularPolarisationImageFilter_h
 
-#include "itkImageToImageFilter.h"
-#include "itkImage.h"
-#include "itkNumericTraits.h"
-#include "itkImageRegionIterator.h"
-#include "itkImageRegionConstIterator.h"
-#include "itkImageRegionConstIteratorWithIndex.h"
-#include "itkVector.h"
-
-#include "otbImage.h"
+#include "itkUnaryFunctorImageFilter.h"
 
 namespace otb
  {
-/** \class otbMuellerToCircularPolarisationImageFilter
- * \brief Compute the Circular Polarisation image (3 channels : LL, RR and LR) from the Mueller Matrix image (10 channels)
+
+namespace Functor {
+
+/** \class otbMuellerToCircularPolarisationFunctor
+ * \brief Evaluate the  Circular Polarisation image
+ * (3 channels : LL, RR and LR)  from the Mueller image
+ *
+ *
  */
-template <class TPixel>
-class  MuellerToCircularPolarisationImageFilter :
-   public itk::ImageToImageFilter< otb::Image<itk::Vector<TPixel,10>,2>, otb::Image<itk::Vector<TPixel,3>,2> >
+template< class TInput, class TOutput>
+class MuellerToCircularPolarisationFunctor
 {
 public:
-  /** Convenient typedefs for simplifying declarations. */
-  typedef TPixel InputPixelType;
-  typedef otb::Image<itk::Vector<TPixel,10>,2> InputImageType;//L'image de départ est de type Mueller
-  typedef otb::Image<itk::Vector<TPixel,3>,2> OutputImageType;// L'image d'arrivée contient les images LL, RR et LR
-  typedef otb::Image<itk::Vector<TPixel,10>,2> MuellerImageType;
+  typedef typename std::complex <double>           ComplexType;
+  typedef typename TOutput::ValueType              OutputValueType;
 
-  /** Extract dimension from input and output image. */
-  itkStaticConstMacro(InputImageDimension, unsigned int,
-                      InputImageType::ImageDimension);
-  itkStaticConstMacro(OutputImageDimension, unsigned int,
-                      OutputImageType::ImageDimension);
+  inline TOutput operator()( const TInput & Mueller ) const
+    {
+    TOutput result;
+    result.SetSize(m_NumberOfComponentsPerPixel);
 
-  
-  /** Standard class typedefs. */
-  typedef MuellerToCircularPolarisationImageFilter Self;
-  typedef itk::ImageToImageFilter< InputImageType, OutputImageType> Superclass;
-  typedef itk::SmartPointer<Self> Pointer;
-  typedef itk::SmartPointer<const Self>  ConstPointer;
+    RealType M11 =  static_cast<RealType>(Mueller[0]);
+    RealType M14 =  static_cast<RealType>(Mueller[3]);
+    RealType M41 =  static_cast<RealType>(Mueller[12]);
+    RealType M44 =  static_cast<RealType>(Mueller[15]);
 
-  /** Method for creation through the object factory. */
-  itkNewMacro(Self);
+    result[0] = static_cast<OutputValueType>( M11 + M14 + M41 + M44 ); // LL
+    result[1] = static_cast<OutputValueType>( M11 - M14 - M41 + M44 ); // RR
+    result[2] = static_cast<OutputValueType>( M11 -M44 );              // LR
 
-  /** Run-time type information (and related methods). */
-  itkTypeMacro(MuellerToCircularPolarisationImageFilter, ImageToImageFilter);
-  
-  /** Image typedef support. */
-  typedef typename InputImageType::PixelType InputPixelType;
-  typedef typename OutputImageType::PixelType OutputPixelType;
-  
-  typedef typename InputImageType::RegionType InputImageRegionType;
-  typedef typename OutputImageType::RegionType OutputImageRegionType;
+    return result;
+    }
 
-  typedef typename InputImageType::SizeType InputSizeType;
-  typedef typename InputImageType::IndexType IndexType ;
+   unsigned int GetOutputSize()
+   {
+     return m_NumberOfComponentsPerPixel;
+   }
 
-  /** MLC type **/
-  typedef itk::Vector<float,9> MLCType;
+   /** Constructor */
+   MuellerToCircularPolarisationFunctor() : m_NumberOfComponentsPerPixel(3)  {}
 
-  /** Coherency type **/
-  typedef itk::Vector<float,9> CoherencyType;
-  /** Mueller type **/
-  typedef itk::Vector<float,10> MuellerType; 
+   /** Destructor */
+   ~MuellerToCircularPolarisationFunctor() {}
 
-  /**CircularType **/
-  typedef itk::Vector<float, 3> CircularType;
-  
-  
-  /** MuellerToCircularPolarisationImageFilter needs a larger input requested region than
-   * the output requested region.  As such, MuellerToCircularPolarisationImageFilter needs
-   * to provide an implementation for GenerateInputRequestedRegion()
-   * in order to inform the pipeline execution model.
-   *
-   * \sa ImageToImageFilter::GenerateInputRequestedRegion() */
-  virtual void GenerateInputRequestedRegion() throw(itk::InvalidRequestedRegionError);
+private:
+    unsigned int m_NumberOfComponentsPerPixel;
+};
+}
+
+
+/** \class otbMuellerToCircularPolarisationImageFilter
+ * \brief Compute the circular polarisation image (3 channels : LL, RR and LR)
+ * from the Mueller image (16 real channels)
+ */
+template <class TInputImage, class TOutputImage, class TFunction = Functor::MuellerToCircularPolarisationFunctor<
+    ITK_TYPENAME TInputImage::PixelType, ITK_TYPENAME TOutputImage::PixelType> >
+class ITK_EXPORT MuellerToCircularPolarisationImageFilter :
+   public itk::UnaryFunctorImageFilter<TInputImage,TOutputImage, TFunction>
+{
+public:
+   /** Standard class typedefs. */
+   typedef MuellerToCircularPolarisationImageFilter  Self;
+   typedef itk::UnaryFunctorImageFilter<TInputImage,TOutputImage, TFunction> Superclass;
+   typedef itk::SmartPointer<Self>        Pointer;
+   typedef itk::SmartPointer<const Self>  ConstPointer;
+
+   /** Method for creation through the object factory. */
+   itkNewMacro(Self);
+
+   /** Runtime information support. */
+   itkTypeMacro(MuellerToCircularPolarisationImageFilter,itk::UnaryFunctorImageFilter);
+
 
 protected:
-  MuellerToCircularPolarisationImageFilter();
+   MuellerToCircularPolarisationImageFilter() {}
   virtual ~MuellerToCircularPolarisationImageFilter() {}
-  void PrintSelf(std::ostream& os, itk::Indent indent) const;
-
-  /** MuellerToCircularPolarisationImageFilter can be implemented as a multithreaded filter.
-   * Therefore, this implementation provides a ThreadedGenerateData()
-   * routine which is called for each processing thread. The output
-   * image data is allocated automatically by the superclass prior to
-   * calling ThreadedGenerateData().  ThreadedGenerateData can only
-   * write to the portion of the output image specified by the
-   * parameter "outputRegionForThread"
-   *
-   * \sa ImageToImageFilter::ThreadedGenerateData(),
-   *     ImageToImageFilter::GenerateData() */
-  void ThreadedGenerateData(const OutputImageRegionType& outputRegionForThread,
-                            int threadId );
 
 private:
   MuellerToCircularPolarisationImageFilter(const Self&); //purposely not implemented
-  void operator=(const Self&); //purposely not implemented
-
- 
- 
-
+  void operator=(const Self&);                           //purposely not implemented
 };
   
 } // end namespace otb
-
-#ifndef ITK_MANUAL_INSTANTIATION
-#include "otbMuellerToCircularPolarisationImageFilter.txx"
-#endif
 
 #endif
