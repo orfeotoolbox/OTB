cff85ebd8baadcd9c5e06e4e5b9e9ce9afb9656e
Patrick Imbo <patrick.imbo@c-s.fr>
2011-01-20 10:34:49 +0100

    ENH: MuellerToPolarisationDegreeAndPowerImageFilter class inherite from an UnaryFunctorImageFilter

--- Code/SARPolarimetry/otbMuellerToPolarisationDegreeAndPowerImageFilter.h
+++ Code/SARPolarimetry/otbMuellerToPolarisationDegreeAndPowerImageFilter.h
@@ -1,130 +1,108 @@
 /*=========================================================================
 
-  Program:   OTB
-  Module:    $otbMuellerToPolarisationDegreeAndPowerImageFilter.h$
+  Program:   ORFEO Toolbox
   Language:  C++
-  Date:      $Date: 24/10/2006 $
-  Version:   $Revision: 1.0 $
+  Date:      $Date$
+  Version:   $Revision$
 
- 
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
+
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
 
 =========================================================================*/
+
 #ifndef __MuellerToPolarisationDegreeAndPowerImageFilter_h
 #define __MuellerToPolarisationDegreeAndPowerImageFilter_h
 
-#include "math.h"
-
-#include "itkImageToImageFilter.h"
-#include "itkImage.h"
-#include "itkNumericTraits.h"
-#include "itkImageRegionIterator.h"
-#include "itkImageRegionConstIterator.h"
-#include "itkImageRegionConstIteratorWithIndex.h"
-#include "itkVector.h"
-
-#include "otbImage.h"
-
-/** Déclaration des constantes */
-
-#ifndef PI
-#define PI 3.14159265358979323846
-#endif
+#include "itkUnaryFunctorImageFilter.h"
 
 namespace otb
  {
-/** \class otbMuellerToPolarisationDegreeAndPowerImageFilter
- * \brief Compute the Circular Polarisation image (3 channels : LL, RR and LR) from the Mueller Matrix image (10 channels)
+
+namespace Functor {
+
+/** \class otbMuellerToPolarisationDegreeAndPowerFunctor
+ * \brief Evaluate the  min and max polarisation degree and min and max power
+ *   from the Mueller image
+ *
  */
-template <class TPixel>
-class  MuellerToPolarisationDegreeAndPowerImageFilter :
-   public itk::ImageToImageFilter< otb::Image<itk::Vector<TPixel,10>,2>, otb::Image<itk::Vector<TPixel,4>,2> >
+template< class TInput, class TOutput>
+class MuellerToPolarisationDegreeAndPowerFunctor
 {
 public:
-  /** Convenient typedefs for simplifying declarations. */
-  typedef TPixel InputPixelType;
-  typedef otb::Image<itk::Vector<TPixel,10>,2> InputImageType;//L'image de départ est de type Mueller
-  typedef otb::Image<itk::Vector<TPixel,4>,2> OutputImageType;// L'image d'arrivée contient les Pmin, Pmax, deg_pol_min, deg_pol_max
-  typedef otb::Image<itk::Vector<TPixel,10>,2> MuellerImageType;
+  typedef double                                    RealType;
+  typedef typename TOutput::ValueType               OutputValueType;
 
-  /** Extract dimension from input and output image. */
-  itkStaticConstMacro(InputImageDimension, unsigned int,
-                      InputImageType::ImageDimension);
-  itkStaticConstMacro(OutputImageDimension, unsigned int,
-                      OutputImageType::ImageDimension);
+  inline TOutput operator()( const TInput & Mueller ) const
+    {
+    TOutput result;
+    result.SetSize(m_NumberOfComponentsPerPixel);
 
-  
-  /** Standard class typedefs. */
-  typedef MuellerToPolarisationDegreeAndPowerImageFilter Self;
-  typedef itk::ImageToImageFilter< InputImageType, OutputImageType> Superclass;
-  typedef itk::SmartPointer<Self> Pointer;
-  typedef itk::SmartPointer<const Self>  ConstPointer;
 
-  /** Method for creation through the object factory. */
-  itkNewMacro(Self);
+    return result;
+    }
 
-  /** Run-time type information (and related methods). */
-  itkTypeMacro(MuellerToPolarisationDegreeAndPowerImageFilter, ImageToImageFilter);
-  
-  /** Image typedef support. */
-  typedef typename InputImageType::PixelType InputPixelType;
-  typedef typename OutputImageType::PixelType OutputPixelType;
-  
-  typedef typename InputImageType::RegionType InputImageRegionType;
-  typedef typename OutputImageType::RegionType OutputImageRegionType;
+   unsigned int GetOutputSize()
+   {
+     return m_NumberOfComponentsPerPixel;
+   }
 
-  typedef typename InputImageType::SizeType InputSizeType;
-  typedef typename InputImageType::IndexType IndexType ;
+   /** Constructor */
+   MuellerToPolarisationDegreeAndPowerFunctor() : m_NumberOfComponentsPerPixel(4),
+       m_PowerMin(100000.0), m_PowerMax(-100000.0),
+       m_PolarisationDegreeMin( 100000.0),
+       m_PolarisationDegreeMax(-100000.0) {}
 
- 
-  /** Mueller type **/
-  typedef itk::Vector<float,10> MuellerType; 
+   /** Destructor */
+   ~MuellerToPolarisationDegreeAndPowerFunctor() {}
 
-  /**CircularType **/
-  typedef itk::Vector<float, 4> DegreeAndPowerType; //va contenir Pmin, Pmax, deg_pol_min, deg_pol_max dans cet ordre
+private:
+    unsigned int m_NumberOfComponentsPerPixel;
+    RealType     m_PowerMin;
+    RealType     m_PowerMax;
+    RealType     m_PolarisationDegreeMin;
+    RealType     m_PolarisationDegreeMax;
+};
+}
 
-  /**StokesVectorType **/
-  typedef itk::Vector<float, 4> StokesVectorType; //Vecteur de stokes définissant une polarisation
-  
-  
-  /** MuellerToPolarisationDegreeAndPowerImageFilter needs a larger input requested region than
-   * the output requested region.  As such, MuellerToPolarisationDegreeAndPowerImageFilter needs
-   * to provide an implementation for GenerateInputRequestedRegion()
-   * in order to inform the pipeline execution model.
-   *
-   * \sa ImageToImageFilter::GenerateInputRequestedRegion() */
-  virtual void GenerateInputRequestedRegion() throw(itk::InvalidRequestedRegionError);
+
+/** \class otbMuellerToPolarisationDegreeAndPowerImageFilter
+ * \brief Compute the circular polarisation image (3 channels : LL, RR and LR)
+ * from the Mueller image (16 real channels)
+ */
+template <class TInputImage, class TOutputImage, class TFunction = Functor::MuellerToPolarisationDegreeAndPowerFunctor<
+    ITK_TYPENAME TInputImage::PixelType, ITK_TYPENAME TOutputImage::PixelType> >
+class ITK_EXPORT MuellerToPolarisationDegreeAndPowerImageFilter :
+   public itk::UnaryFunctorImageFilter<TInputImage,TOutputImage, TFunction>
+{
+public:
+   /** Standard class typedefs. */
+   typedef MuellerToPolarisationDegreeAndPowerImageFilter  Self;
+   typedef itk::UnaryFunctorImageFilter<TInputImage,TOutputImage, TFunction> Superclass;
+   typedef itk::SmartPointer<Self>        Pointer;
+   typedef itk::SmartPointer<const Self>  ConstPointer;
+
+   /** Method for creation through the object factory. */
+   itkNewMacro(Self);
+
+   /** Runtime information support. */
+   itkTypeMacro(MuellerToPolarisationDegreeAndPowerImageFilter,itk::UnaryFunctorImageFilter);
+
 
 protected:
-  MuellerToPolarisationDegreeAndPowerImageFilter();
+   MuellerToPolarisationDegreeAndPowerImageFilter() {}
   virtual ~MuellerToPolarisationDegreeAndPowerImageFilter() {}
-  void PrintSelf(std::ostream& os, itk::Indent indent) const;
-
-  /** MuellerToPolarisationDegreeAndPowerImageFilter can be implemented as a multithreaded filter.
-   * Therefore, this implementation provides a ThreadedGenerateData()
-   * routine which is called for each processing thread. The output
-   * image data is allocated automatically by the superclass prior to
-   * calling ThreadedGenerateData().  ThreadedGenerateData can only
-   * write to the portion of the output image specified by the
-   * parameter "outputRegionForThread"
-   *
-   * \sa ImageToImageFilter::ThreadedGenerateData(),
-   *     ImageToImageFilter::GenerateData() */
-  void ThreadedGenerateData(const OutputImageRegionType& outputRegionForThread,
-                            int threadId );
 
 private:
   MuellerToPolarisationDegreeAndPowerImageFilter(const Self&); //purposely not implemented
-  void operator=(const Self&); //purposely not implemented
-
- 
- 
-
+  void operator=(const Self&);                                 //purposely not implemented
 };
   
 } // end namespace otb
-
-#ifndef ITK_MANUAL_INSTANTIATION
-#include "otbMuellerToPolarisationDegreeAndPowerImageFilter.txx"
-#endif
 
 #endif
