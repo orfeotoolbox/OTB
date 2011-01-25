3b83a1a753440ba9ec00daf109863d9fe0795e69
Patrick Imbo <patrick.imbo@c-s.fr>
2011-01-25 10:54:10 +0100

    TEST: output value of MLCToCircularCoherencyDegreeImageFilter is real type

--- Testing/Code/SARPolarimetry/otbMLCToCircularCoherencyDegreeImageFilter.cxx
+++ Testing/Code/SARPolarimetry/otbMLCToCircularCoherencyDegreeImageFilter.cxx
@@ -46,6 +46,7 @@ int otbMLCToCircularCoherencyDegreeImageFilter(int argc, char * argv[])
   
   typedef otb::Image<InputPixelType,  Dimension>       InputImageType;
   typedef otb::VectorImage<InputPixelType, Dimension>  ImageType;
+  typedef otb::VectorImage<PixelType, Dimension>       RealImageType;
   typedef otb::Functor::SinclairToCovarianceFunctor<
                       InputImageType::PixelType,
                       InputImageType::PixelType,
@@ -57,11 +58,11 @@ int otbMLCToCircularCoherencyDegreeImageFilter(int argc, char * argv[])
                       InputImageType, InputImageType,
                       ImageType, FunctionType >  SinclairToCovarianceFilterType;
 
-  typedef otb::MLCToCircularCoherencyDegreeImageFilter<ImageType,ImageType> FilterType;
+  typedef otb::MLCToCircularCoherencyDegreeImageFilter<ImageType,RealImageType> FilterType;
 
 
   typedef otb::ImageFileReader<InputImageType>  ReaderType;
-  typedef otb::ImageFileWriter<ImageType> WriterType;
+  typedef otb::ImageFileWriter<RealImageType> WriterType;
 
   ReaderType::Pointer reader1 = ReaderType::New();
   ReaderType::Pointer reader2 = ReaderType::New();
