704e2f5c8e32caa55ccf71117dfe30a7fc0d6f16
Otmane Lahlou <otmane.lahlou@c-s.fr>
2011-02-03 17:23:33 +0100

    ENH : change the copyright to the otb one and add name of the classes that should be override in the exception description

--- Code/Common/otbTransform.h
+++ Code/Common/otbTransform.h
@@ -1,20 +1,20 @@
 /*=========================================================================
- *
- *  Copyright Insight Software Consortium
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0.txt
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *
- *=========================================================================*/
+
+  Program:   ORFEO Toolbox
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
+
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
 #ifndef __otbTransform_h
 #define __otbTransform_h
 
@@ -117,7 +117,7 @@ public:
    * \sa SetParametersByValue
    */
     virtual void SetParameters( const ParametersType & ) 
-    { itkExceptionMacro( << "Subclasses should override this method" ) }
+    { itkExceptionMacro( << "Subclasses should override this method (SetParameters)" ) }
   
   /** Set the transformation parameters and update internal transformation.
    * This method forces the transform to copy the parameters.  The
@@ -137,12 +137,12 @@ public:
 
   /** Set the fixed parameters and update internal transformation. */
   virtual void SetFixedParameters( const ParametersType & ) 
-    { itkExceptionMacro( << "Subclasses should override this method" ) }
+    { itkExceptionMacro( << "Subclasses should override this method (SetFixedParameters)" ) }
 
   /** Get the Fixed Parameters. */
   virtual const ParametersType& GetFixedParameters(void) const
     {
-    itkExceptionMacro( << "Subclasses should override this method" );
+    itkExceptionMacro( << "Subclasses should override this method (GetFixedParameters)" );
     // Next line is needed to avoid errors due to: 
     // "function must return a value".
     return this->m_FixedParameters;
@@ -177,7 +177,7 @@ public:
    * */
   virtual const JacobianType & GetJacobian(const InputPointType  &) const
     {
-    itkExceptionMacro( << "Subclass should override this method" );
+    itkExceptionMacro( << "Subclass should override this method (GetJacobian)" );
     // Next line is needed to avoid errors due to: 
     // "function must return a value" .
     return this->m_Jacobian;
