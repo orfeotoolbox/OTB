11dda78ccd816bda1bdcf32bca5808c852928ffe
Julien Michel <julien.michel@orfeo-toolbox.org>
2011-02-09 12:04:22 +0100

    ENH: Adding a class to represent masses of belief in Dempster-Shafer theory

---
+++ Code/Fuzzy/otbMassOfBelief.h
@@ -0,0 +1,160 @@
+/*=========================================================================
+
+  Program:   ORFEO Toolbox
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
+
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __otbMassOfBelief_h
+#define __otbMassOfBelief_h
+
+#include "itkDataObject.h"
+#include "itkObjectFactory.h"
+#include "itkNumericTraits.h"
+
+#include <set>
+#include <map>
+#include <ostream>
+
+namespace otb
+{
+/** \class MassOfBelief
+ *  \brief This class represent a mass of belief function
+ *
+ *  This class represent a Mass of Belief function to be used in
+ *  Dempster-Shafer theory.
+ * 
+ *  Template parameter TLabel is the type of the elements in the
+ *  universe set, while TMass is the type of the mass. TLabel can be
+ *  any type implementing ordering operator, while TMass should be a
+ *  floating point type.
+ * 
+ *  An element of the power-set is represented by the sub-type
+ *  LabelSetType, which is a std::set<TLabel>.
+ *
+ *  A mass can be associated with a given element from the power set
+ *  by calling the SetMass method. It is not necessary to associate
+ *  masses with each element of the power set: mass is supposed to be
+ *  null otherwise declared.
+ * 
+ *  A mass associated with an element from the power-set can be
+ *  removed using the RemoveMass method, and all masses can be cleared
+ *  using the Clear() function.
+ *
+ *  The mass associated with a given element from the power-set can be
+ *  retrieved using the GetMass method. If no mass has been associated
+ *  with this element, null is returned.
+ *
+ *  The Normalize() method allows to normalize all masses so that they
+ *  sum to 1. The EstimateUncertainty() method allows to associate to
+ *  the uncertain set (i.e. the universe set) a mass so that all
+ *  masses sum to 1.
+ * 
+ *  The GetUniverse() method allows to retrieve the set of all symbols
+ *  (i.e. the universal set), while the GetSupport() method allows to
+ *  retrieve the set of all elements of the power-set for which a mass
+ *  has been associated. This method returns a LabelSetOfSetType,
+ *  which is a std::set<std::set<TLabel> >.
+ *
+ *  The mass function can be optionnaly initialized from a given
+ *  universe set by building the power-set and associating a null mass
+ *  with every elements of this power-set. This allows to ensure that
+ *  two mass functions will share the exact same universe and
+ *  power-set, but is not mandatory, since mass is supposed to be null
+ *  for any set if not stated otherwise.
+ */
+template <class TLabel = unsigned short, class TMass = double>
+class ITK_EXPORT MassOfBelief : public itk::DataObject
+{
+public:
+  /** Standard class typedefs */
+  typedef MassOfBelief                  Self;
+  typedef itk::DataObject               Superclass;
+  typedef itk::SmartPointer<Self>       Pointer;
+  typedef itk::SmartPointer<const Self> ConstPointer;
+  
+  /** Method for creation through the object factory. */
+  itkNewMacro(Self);
+  
+  /** Run-time type information (and related methods). */
+  itkTypeMacro(MassOfBelief, DataObject);
+
+  typedef TLabel                          LabelType;
+  typedef TMass                           MassType;
+  typedef std::set<LabelType>             LabelSetType;
+  typedef std::map<LabelSetType,MassType> MassMapType;
+  typedef std::set<LabelSetType>          LabelSetOfSetType;
+
+  /** Register a mass associated with an element of the power-set */
+  void SetMass(const LabelSetType & labelSet, const MassType & mass);
+  
+  /** Retrieve the mass associated with the given element of the power
+   *  set */
+  MassType GetMass(const LabelSetType & labelSet) const;
+
+  /** Remove mass from the support */
+  void RemoveMass(const LabelSetType & labelSet);
+  
+  /** Retrieve the support of the mass of belief (i.e. a vector of
+   * element of the power set for which the mass of belief is not
+   * null) */
+  LabelSetOfSetType GetSupport() const;
+
+  /** Get the set of universe symbols for this mass function */
+  LabelSetType GetUniverse() const;
+
+  /** Normalize mass set sot that the sum of masses equals 1 */
+  void Normalize();
+
+  /** Associate to uncertainty a mass such that the sum of masses
+   * equals 1 */
+  void EstimateUncertainty();
+
+  /** Clear all masses */
+  void Clear();
+
+  /** Initialize with all elements from the power set with null
+   *  masses. Please note that this is not mandatory and only allows
+   *  to ensure that two mass functions share the same universe, even
+   *  if most of masses are null.
+   */
+  void InitializePowerSetMasses(const LabelSetType & universe);
+
+protected:
+  /** Constructor */
+  MassOfBelief() {}
+  
+  /** Desctructor */
+  ~MassOfBelief() {}
+  
+  /** PrintSelf method */
+  void PrintSelf(std::ostream& os, itk::Indent indent) const;
+
+private:
+  /** The masses map */
+  MassMapType  m_MassesMap;
+};
+
+} // end namespace otb
+
+/** Define the << operator for label sets */
+template <class TLabel> 
+std::ostream & 
+operator<<(std::ostream & out,
+           const std::set<TLabel> & labelSet);
+
+#ifndef OTB_MANUAL_INSTANTIATION
+#include "otbMassOfBelief.txx"
+#endif
+
+#endif
