08b537bb68b4b98e99cb156219a1b575b652aa28
Manuel Grizonnet <manuel.grizonnet@orfeo-toolbox.org>
2011-02-14 15:27:32 +0100

    STY:kwstyle

--- Examples/IO/HDFReaderExample.cxx
+++ Examples/IO/HDFReaderExample.cxx
@@ -24,14 +24,19 @@
 // Software Guide : BeginLatex
 //
 // This example describes how to read a MODIS image(Moderate-resolution Imaging Spectroradiometer)
-// .This scientific instrument was launched into Earth orbit by NASA. The MODIS image format is HDF (Hierarchical Data Format). This
+// .This scientific instrument was launched into Earth orbit by NASA.
+// The MODIS image format is HDF (Hierarchical Data Format). This
 // format is widely use to store remote sensing images.
 // HDF format is designed to store and organize large amounts of numerical data
 // including multidimensional arrays, raster images, and tables.
-// A specific feature of this format is that it works with container structures which can hold datasets and other groups. It implies that
-// access to these structures in OTB presents some adaptations compare to other image formats.
-// OTB is using GDAL to support HDF. HDF4 and HDF5 supports in Gdal is not activated by default. You need to download the HDF run-time
-// libraries and compile Gdal by adding the support of these formats. You can find more informations here : \url{http://trac.osgeo.org/gdal/wiki/HDF}
+// A specific feature of this format is that it works with container
+// structures which can hold datasets and other groups. It implies that
+// access to these structures in OTB presents some adaptations compare
+// to other image formats.
+// OTB is using GDAL to support HDF. HDF4 and HDF5 supports in Gdal is
+// not activated by default. You need to download the HDF run-time
+// libraries and compile Gdal by adding the support of these formats. You can
+// find more informations here : \url{http://trac.osgeo.org/gdal/wiki/HDF}
 //
 //
 // The first step toward the use of these filters is to include the proper header files.
@@ -56,11 +61,11 @@ int main(int argc, char * argv[])
 // Software Guide : EndLatex
 
 // Software Guide : BeginCodeSnippet
-  typedef unsigned char InputPixelType;
-  const unsigned int Dimension = 2;
+  typedef unsigned char                                InputPixelType;
+  const unsigned int Dimension                         = 2;
   typedef otb::VectorImage<InputPixelType,  Dimension> InputImageType;
-  typedef otb::ImageFileReader<InputImageType> HDFReaderType;
-  typedef otb::ImageFileWriter<InputImageType> HDFWriterType;
+  typedef otb::ImageFileReader<InputImageType>         HDFReaderType;
+  typedef otb::ImageFileWriter<InputImageType>         HDFWriterType;
 
   HDFReaderType::Pointer reader = HDFReaderType::New();
 // Software Guide : EndCodeSnippet
@@ -162,4 +167,3 @@ int main(int argc, char * argv[])
   //  Software Guide : EndLatex
   return EXIT_SUCCESS;
 }
-
