29bdd6b143700fbfc81e2f6041d10cf2b11634f3
Julien Malik <julien.malik@c-s.fr>
2011-02-16 10:07:00 +0100

    DOC: add copyright

--- Code/Hyperspectral/otbVcaImageFilter.h
+++ Code/Hyperspectral/otbVcaImageFilter.h
@@ -1,3 +1,23 @@
+/*=========================================================================
+
+  Program:   ORFEO Toolbox
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
+
+  Some parts of this code are derived from ITK. See ITKCopyright.txt
+  for details.
+
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANT2ABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
 #ifndef __otbVCAImageFilter_h
 #define __otbVCAImageFilter_h
 
