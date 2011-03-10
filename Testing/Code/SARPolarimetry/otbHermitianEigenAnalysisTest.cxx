5f288a7896b87513dfc1b20d8961ad18f088ff2c
Cyrille Valladeau <cyrille.valladeau@c-s.fr>
2011-03-10 18:22:04 +0100

    TEST : improve Hermitian test

--- Testing/Code/SARPolarimetry/otbHermitianEigenAnalysisTest.cxx
+++ Testing/Code/SARPolarimetry/otbHermitianEigenAnalysisTest.cxx
@@ -25,16 +25,95 @@
 #include "otbImage.h"
 #include "otbVectorImage.h"
 #include "otbHermitianEigenAnalysis.h"
+#include "itkVariableLengthVector.h"
+
 
 int otbHermitianEigenAnalysisTest(int argc, char * argv[])
 {
-  typedef vnl_matrix<double> MatrixType;
-  typedef std::vector<double> VectorType;
+  typedef itk::Vector<float, 9> MatrixType;
+  typedef itk::Vector<float, 3> EigenvalueType;
+  typedef itk::Vector<itk::Vector<float, 6>, 3> EigenMatrixType;
 
+  typedef otb::HermitianEigenAnalysis<MatrixType, EigenvalueType, EigenMatrixType> FilterType;
 
-  typedef otb::HermitianEigenAnalysis<MatrixType, VectorType> FilterType;
+  EigenMatrixType resEigVal;
+  itk::Vector<float, 6> temp;
+  temp[0] = 0.162793;
+  temp[1] = -0.432753;
+  temp[2] = 0.766443;
+  temp[3] = 1.08418e-18;
+  temp[4] = 0.338266;
+  temp[5] = 0.290456;
+  resEigVal[0] = temp;
+  temp[0] = 0.420547;
+  temp[1] = -0.406821;
+  temp[2] = -0.541437;
+  temp[3] = 0.112727;
+  temp[4] = 0.58546;
+  temp[5] = -0.0949371;
+  resEigVal[1] = temp;
+  temp[0] = 0.664613;
+  temp[1] = -0.0463679;
+  temp[2] = 0.206196;
+  temp[3] = -0.253351;
+  temp[4] = -0.361684;
+  temp[5] =  -0.564465;
+  resEigVal[2] = temp;
 
+  EigenvalueType resVect;
+  resVect[0] = 8.22892;
+  resVect[1] = -1.08457;
+  resVect[2] = -5.64434;
+  
   FilterType filter;
+
+  MatrixType mat;
+  mat.Fill(0.);
+
+  float val = 0;
+  for(unsigned int i=0; i<mat.Size(); i++)
+    {
+      mat[i] = val;
+      val += 0.5;
+    }
+  std::cout<<"Matrix: "<<std::endl;
+  std::cout<<mat<<std::endl;
+
+  EigenvalueType vect;
+  EigenMatrixType eigVal;
+  // Only method used in SARPolarimetry...
+  filter.ComputeEigenValuesAndVectors( mat, vect, eigVal);
+
+  if( (vcl_abs(resVect[0] - vect[0]) > 1e-5) || (vcl_abs(resVect[1] - vect[1]) > 1e-5) || (vcl_abs(resVect[2] - vect[2]) > 1e-5) )
+  {
+    std::cout<<"Test gives :"<<std::endl;
+    std::cout<<"Values: "<<vect<<std::endl;
+    std::cout<<"Wanted results are :"<<std::endl;
+    std::cout<<"Values: "<<resVect<<std::endl;
+
+    return EXIT_FAILURE;
+  }
+
+  bool isOK = true;
+  for(unsigned int i=0; i<6; i++)
+    {
+      for(unsigned int j=0; j<3; j++)
+        {
+          if(vcl_abs(eigVal[j][i] - resEigVal[j][i]) > 1e-5)
+            {
+              isOK == false;
+            }
+        }
+    }
+  if( isOK == false )
+  {
+    std::cout<<"Test gives :"<<std::endl;
+    std::cout<<"Vectors: "<<eigVal<<std::endl;
+    std::cout<<"Wanted results are :"<<std::endl;
+    std::cout<<"Vectors: "<<resEigVal<<std::endl;
+   
+    return EXIT_FAILURE;
+  }
 
   return EXIT_SUCCESS;
 }
