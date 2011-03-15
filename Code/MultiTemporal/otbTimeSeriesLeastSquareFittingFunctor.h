794d40f625a4656449daffd9308c07da99c3cc48
Jordi Inglada <jordi.inglada@cesbio.cnes.fr>
2011-03-15 11:03:52 +0100

    ENH: Add possibility to use weights for each measure

--- Code/MultiTemporal/otbTimeSeriesLeastSquareFittingFunctor.h
+++ Code/MultiTemporal/otbTimeSeriesLeastSquareFittingFunctor.h
@@ -26,10 +26,24 @@ namespace Functor
 /** \class otbTimeSeriesLeastSquareFittingFunctor \brief: This functor
    *  implements a least squares fitting of a time profile. The time
    *  series as well as the date series are supposed to accept the []
-   *  syntax to get the values and the Size() method to get their length.
+   *  syntax to get the values and the Size() method to get their
+   *  length. The fitting is performed using a time function which is
+   *  just a weighted sum of basis functions : \f$ f(t) = c_0 *
+   *  \phi_0(t) + c_1 * \phi_1(t) + c_1 * \phi_1(t) + \cdots \f$ Using
+   *  a matrix notation, this can be written as follows : 
+   *  \f$ F = \Phi c \f$ 
+   *  \f$ F = ( f(t_i) ) \f$ 
+   *  \f$ c = ( c_j ) \f$ 
+   *  \f$ \Phi_{ij} = ( \phi_j(t_i) ) \f$ 
+   *  If information about the error
+   *  of each measure is available, it can be provided using a weight
+   *  series \f$\sigma_i\f$ (the higher the error, the higher the \f$ \sigma_i \f$. The problem then becomes:
+   *  \f$ b = A c \f$
+   *  \f$ b = (\frac{ f(t_i) }{\sigma_i}) \f$
+   *  \f$ A_{ij} = \frac{\Phi_{ij}}{\sigma_i} \f$
  *
  */
-template <class TSeriesType, class TTimeFunction, class TDateType = TSeriesType>
+template <class TSeriesType, class TTimeFunction, class TDateType = TSeriesType, class TWeightType = TSeriesType>
 class TimeSeriesLeastSquareFittingFunctor
 {
 public:
@@ -45,7 +59,10 @@ public:
 
   inline TSeriesType operator ()(const TSeriesType& series)
   {
+    this->EstimateTimeFunction(series);
     TSeriesType outSeries;
+    for(unsigned int i=0; i<m_DoySeries.Size(); ++i)
+      outSeries[i] = m_TimeFunction.GetValue( m_DoySeries[i] );
     return outSeries;
   }
 
@@ -55,9 +72,21 @@ public:
       m_DoySeries[i] = doy[i];
   }
 
+  inline void SetWeights(const TWeightType& weights)
+  {
+    for(unsigned int i=0; i<weights.Size(); ++i)
+      m_WeightSeries[i] = weights[i];
+  }
+
+
   inline CoefficientsType GetCoefficients() const
   {
     return m_TimeFunction.GetCoefficients();
+  }
+
+  inline void EstimateTimeFunction(const TSeriesType& series)
+  {
+
   }
 
 
@@ -65,6 +94,7 @@ private:
   /// 
   TTimeFunction m_TimeFunction;
   TDateType m_DoySeries;
+  TWeightType m_WeightSeries;
 };
 }
 }
