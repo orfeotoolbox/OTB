fda7d29101cf42af671ae8ed958e7bf8eb84efb4
Jordi Inglada <jordi.inglada@cesbio.cnes.fr>
2011-03-15 15:37:52 +0100

    ENH: implementation of the moving window Savitzky-Golay interpolation

--- Code/MultiTemporal/otbSavitzkyGolayInterpolationFunctor.h
+++ Code/MultiTemporal/otbSavitzkyGolayInterpolationFunctor.h
@@ -18,6 +18,8 @@
 #ifndef __otbSavitzkyGolayInterpolationFunctor_h
 #define __otbSavitzkyGolayInterpolationFunctor_h
 
+#include "otbTimeSeries.h"
+#include "otbTimeSeriesLeastSquareFittingFunctor.h"
 
 namespace otb
 {
@@ -28,6 +30,21 @@ class SavitzkyGolayInterpolationFunctor
 {
 public:
 
+  typedef typename TSeries::ValueType ValueType;
+  typedef typename TDates::ValueType DateType;
+  typedef typename TWeight::ValueType WeightType;
+  static const unsigned int Degree = 2;
+  typedef double CoefficientPrecisionType;
+  typedef otb::PolynomialTimeSeries< Degree, CoefficientPrecisionType > TSFunctionType;
+  static const unsigned int nbDates = TSeries::Dimension;
+  static const unsigned int InterpolatedLength = 2*Radius+1;
+
+  typedef itk::FixedArray< ValueType, InterpolatedLength> InterpolatedSeriesType;
+  typedef itk::FixedArray< DateType, InterpolatedLength> InterpolatedDatesType;
+  typedef itk::FixedArray< WeightType, InterpolatedLength> InterpolatedWeightType;
+
+  typedef otb::Functor::TimeSeriesLeastSquareFittingFunctor<InterpolatedSeriesType, 
+            TSFunctionType, InterpolatedDatesType, InterpolatedWeightType> TLSFunctorType;
 
   /// Constructor
   SavitzkyGolayInterpolationFunctor()
@@ -51,7 +68,39 @@ public:
 
   inline TSeries operator ()(const TSeries& series)
   {
+    TSeries outSeries;
 
+    unsigned int firstSample = Radius;
+    unsigned int lastSample = nbDates-Radius-1;
+
+    for(unsigned int i=0; i<firstSample; ++i)
+      outSeries[i] = series[i];
+    for(unsigned int i=lastSample+1; i<nbDates; ++i)
+      outSeries[i] = series[i];
+
+    for(unsigned int i=firstSample; i<=lastSample; ++i)
+      {
+
+      InterpolatedSeriesType tmpInSeries;
+      InterpolatedDatesType tmpDates;
+      InterpolatedWeightType tmpWeights;
+
+      for(int j=-Radius; j<=Radius; ++j)
+        {
+        tmpInSeries[j+Radius] = series[i+j];
+        tmpDates[j+Radius] = m_DoySeries[i+j];
+        tmpWeights[j+Radius] = m_WeightSeries[i+j];
+        }
+      
+
+      TLSFunctorType f;
+      f.SetDates( tmpDates );
+      f.SetWeights( tmpWeights);
+      InterpolatedSeriesType tmpOutSeries = f(tmpInSeries); 
+      outSeries[i] = tmpOutSeries[Radius];
+      }
+
+    return outSeries;
   }
 
 private:
