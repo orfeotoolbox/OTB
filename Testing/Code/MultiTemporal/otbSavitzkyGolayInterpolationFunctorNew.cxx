7e3f011279432feadacba330b754ea499f3a5a2c
Jordi Inglada <jordi.inglada@cesbio.cnes.fr>
2011-03-15 14:34:26 +0100

    TEST: add test for Savitzky-Golay interpolation functor

--- Testing/Code/MultiTemporal/otbSavitzkyGolayInterpolationFunctorNew.cxx
+++ Testing/Code/MultiTemporal/otbSavitzkyGolayInterpolationFunctorNew.cxx
@@ -15,19 +15,26 @@
   PURPOSE.  See the above copyright notices for more information.
 
 =========================================================================*/
-
-// this file defines the otbCommonTest for the test driver
-// and all it expects is that you have a function called RegisterTests
 #if defined(_MSC_VER)
 #pragma warning ( disable : 4786 )
 #endif
 
-#include "otbTestMain.h"
+#include "itkFixedArray.h"
+#include "otbSavitzkyGolayInterpolationFunctorFunctor.h"
+#include "otbTimeSeries.h"
 
-void RegisterTests()
+int otbSavitzkyGolayInterpolationFunctorFunctorNew(int argc, char* argv[])
 {
-  REGISTER_TEST(otbPolynomialTimeSeriesTest);
-  REGISTER_TEST(otbTimeSeriesLeastSquareFittingFunctorNew);
-  REGISTER_TEST(otbTimeSeriesLeastSquareFittingFunctorTest);
-  REGISTER_TEST(otbSavitzkyGolayInterpolationFunctorNew);
+
+  typedef float PixelType;
+  typedef unsigned int DoYType;
+  typedef itk::FixedArray< PixelType > SeriesType;
+  typedef itk::FixedArray< DoYType > DatesType;
+  const unsigned int Radius = 2;
+  
+  typedef otb::Functor::SavitzkyGolayInterpolationFunctorFunctor<Radius, SeriesType, DatesType> FunctorType;
+
+  FunctorType f;
+  
+  return EXIT_SUCCESS;
 }
