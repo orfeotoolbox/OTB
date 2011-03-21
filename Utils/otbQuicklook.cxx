27ada17409e76c5a7f2c2365b85318e83091b41c
Julien Malik <julien.malik@c-s.fr>
2011-03-21 11:33:04 +0100

    ENH: update otbQuicklook application (avoid the extract ROI if not necessary)

--- Utils/otbQuicklook.cxx
+++ Utils/otbQuicklook.cxx
@@ -26,7 +26,7 @@
 #include "otbStreamingShrinkImageFilter.h"
 #include "otbStreamingImageFileWriter.h"
 #include "otbConfigure.h"
-#include "otbStandardWriterWatcher.h"
+#include "otbStandardFilterWatcher.h"
 
 namespace otb
 {
@@ -37,7 +37,7 @@ int Quicklook::Describe(ApplicationDescriptor* descriptor)
   descriptor->SetDescription("Generates a subsampled version of an extract of an image defined by ROIStart and ROISize.\n This extract is subsampled using the ration OR the output image Size");
   descriptor->AddInputImage();
   descriptor->AddOutputImage();
-  descriptor->AddOptionNParams("ChannelList", "channel list [1..N], separate with \" \"","cl",false,ApplicationDescriptor::StringList);
+  descriptor->AddOptionNParams("ChannelList", "channel list [1..N], separate with spaces","cl",false,ApplicationDescriptor::StringList);
   descriptor->AddOption("ROIStartX","first point of ROI in x-direction ","ROIx0",1,false,ApplicationDescriptor::Integer);
   descriptor->AddOption("ROIStartY","first point of ROI in y-direction ","ROIy0",1,false,ApplicationDescriptor::Integer);
   descriptor->AddOption("ROISizeX","size of ROI in x-direction ","ROINx",1,false,ApplicationDescriptor::Integer);
@@ -55,9 +55,10 @@ int Quicklook::Execute(otb::ApplicationOptionsResult* parseResult)
 {
   try
     {
-    typedef double InputPixelType;
-    typedef double OutputPixelType;
+    typedef float InputPixelType;
+    typedef float OutputPixelType;
     typedef otb::MultiChannelExtractROI< InputPixelType,OutputPixelType >   ExtractROIFilterType;
+    typedef ExtractROIFilterType::InputImageType InputImageType;
 
     // Defining the IO filename
     typedef otb::ImageFileReader< ExtractROIFilterType::InputImageType >    ReaderType;
@@ -67,65 +68,84 @@ int Quicklook::Execute(otb::ApplicationOptionsResult* parseResult)
     ReaderType::Pointer           reader           = ReaderType::New();
     WriterType::Pointer           writer           = WriterType::New();
 
-    reader->SetFileName( parseResult->GetInputImage().c_str()  );
-    writer->SetFileName( parseResult->GetOutputImage().c_str() );
-
-    // Setting the ROIFilter parameters
-    //Determine if using streaming
+    reader->SetFileName( parseResult->GetInputImage() );
     reader->GenerateOutputInformation();
 
-    unsigned long regionROI[2];
+    InputImageType::Pointer readerImage = reader->GetOutput();
 
-    if ( parseResult->IsOptionPresent("ROIStartX") )
-      extractROIFilter->SetStartX(parseResult->GetParameterULong("ROIStartX"));
+    // The image on which the quicklook will be generated
+    // Will eventually be the extractROIFilter output
+    InputImageType::Pointer image = readerImage;
 
-    if ( parseResult->IsOptionPresent("ROIStartY") )
-      extractROIFilter->SetStartY(parseResult->GetParameterULong("ROIStartY"));
+    writer->SetFileName( parseResult->GetOutputImage() );
 
-    if ( parseResult->IsOptionPresent("ROISizeX") )
-      {
-      regionROI[0] = parseResult->GetParameterULong("ROISizeX");
-      extractROIFilter->SetSizeX(regionROI[0]);
-      }
-    else
-      {
-      regionROI[0] = reader->GetOutput()->GetLargestPossibleRegion().GetSize()[0];
-      }
-    if ( parseResult->IsOptionPresent("ROISizeY") )
-      {
-      regionROI[1] = parseResult->GetParameterULong("ROISizeY");
-      extractROIFilter->SetSizeY(regionROI[1]);
-      }
-    else
-      {
-      regionROI[1] = reader->GetOutput()->GetLargestPossibleRegion().GetSize()[1];
-      }
+    double ROISizeX = image->GetLargestPossibleRegion().GetSize()[0];
+    double ROISizeY = image->GetLargestPossibleRegion().GetSize()[1];
 
-    if ( parseResult->IsOptionPresent("ChannelList") )
+    if (parseResult->IsOptionPresent("ROIStartX") || parseResult->IsOptionPresent("ROIStartY")
+        || parseResult->IsOptionPresent("ROISizeX") || parseResult->IsOptionPresent("ROISizeY")
+        || parseResult->IsOptionPresent("ChannelList"))
       {
-      int NumberOfChannel = parseResult->GetNumberOfParameters("ChannelList");
-      for (int i = 0 ; i < NumberOfChannel ; i++)
+      unsigned long regionROI[2];
+
+      if (parseResult->IsOptionPresent("ROIStartX"))
         {
-        unsigned int ChannelNumber = parseResult->GetParameterUInt("ChannelList",i);
-        extractROIFilter->SetChannel(ChannelNumber);
+        extractROIFilter->SetStartX(parseResult->GetParameterULong("ROIStartX"));
+
         }
-      }
+      if (parseResult->IsOptionPresent("ROIStartY"))
+        {
+        extractROIFilter->SetStartY(parseResult->GetParameterULong("ROIStartY"));
+        }
 
-    if (    (extractROIFilter->GetSizeX() < 0) ||
-            (extractROIFilter->GetSizeY() < 0)   )
-      {
-      std::cout << " Size of ROI in x-direction and y -direction must be positive" << std::endl;
-      return EXIT_SUCCESS;
-      }
+      if (parseResult->IsOptionPresent("ROISizeX"))
+        {
+        regionROI[0] = parseResult->GetParameterULong("ROISizeX");
+        extractROIFilter->SetSizeX(regionROI[0]);
+        }
+      else
+        {
+        regionROI[0] = reader->GetOutput()->GetLargestPossibleRegion().GetSize()[0];
+        }
 
-    extractROIFilter->SetInput( reader->GetOutput() );
+      if (parseResult->IsOptionPresent("ROISizeY"))
+        {
+        regionROI[1] = parseResult->GetParameterULong("ROISizeY");
+        extractROIFilter->SetSizeY(regionROI[1]);
+        }
+      else
+        {
+        regionROI[1] = reader->GetOutput()->GetLargestPossibleRegion().GetSize()[1];
+        }
+
+      ROISizeX = regionROI[0];
+      ROISizeY = regionROI[1];
+
+      if (parseResult->IsOptionPresent("ChannelList"))
+        {
+        int NumberOfChannel = parseResult->GetNumberOfParameters("ChannelList");
+        for (int i = 0; i < NumberOfChannel; i++)
+          {
+          unsigned int ChannelNumber = parseResult->GetParameterUInt("ChannelList", i);
+          extractROIFilter->SetChannel(ChannelNumber);
+          }
+        }
+
+      if ((extractROIFilter->GetSizeX() < 0) || (extractROIFilter->GetSizeY() < 0))
+        {
+        std::cout << " Size of ROI in x-direction and y -direction must be positive" << std::endl;
+        return EXIT_FAILURE;
+        }
+
+      extractROIFilter->SetInput(readerImage);
+
+      image = extractROIFilter->GetOutput();
+      }
  
-    double ROISizeX =  static_cast<double>( regionROI[0] );
-    double ROISizeY =  static_cast<double>( regionROI[1] );
 
     unsigned int Ratio = 2; //default value
-    double SamplingRatioX =1.0;
-    double SamplingRatioY =1.0;
+    double SamplingRatioX = 1.0;
+    double SamplingRatioY = 1.0;
 
     if ( parseResult->IsOptionPresent("SamplingRatio") )
       {
@@ -168,15 +188,17 @@ int Quicklook::Execute(otb::ApplicationOptionsResult* parseResult)
 
     typedef otb::StreamingShrinkImageFilter<ExtractROIFilterType::OutputImageType,ExtractROIFilterType::OutputImageType> ShrinkImageFilterType;
 
-    ShrinkImageFilterType::Pointer ResamplingFilter = ShrinkImageFilterType::New();
-    ResamplingFilter->SetInput( extractROIFilter->GetOutput() );
-    ResamplingFilter->SetShrinkFactor( Ratio );
+    ShrinkImageFilterType::Pointer resamplingFilter = ShrinkImageFilterType::New();
+    resamplingFilter->SetInput( image );
+    resamplingFilter->SetShrinkFactor( Ratio );
 
-    otb::StandardWriterWatcher watcher (ResamplingFilter->GetStreamer(), ResamplingFilter->GetFilter(), "Quicklook geenration");
-    ResamplingFilter->Update();
-    otbGenericMsgDebugMacro(<<ResamplingFilter->GetOutput()->GetLargestPossibleRegion());
+    otbMsgDevMacro(<< "ShrinkFactor : " << Ratio )
 
-    writer->SetInput(ResamplingFilter->GetOutput() );
+    otb::StandardFilterWatcher watcher (resamplingFilter->GetStreamer(), "Quicklook generation");
+    resamplingFilter->Update();
+    otbGenericMsgDebugMacro(<<resamplingFilter->GetOutput()->GetLargestPossibleRegion());
+
+    writer->SetInput(resamplingFilter->GetOutput() );
     writer->Update();
     
     }
