8e013ff92e3baeef233ac6b55ffe5606d8f8fd60
Julien Michel <julien.michel@orfeo-toolbox.org>
2011-03-28 17:06:39 +0200

    ENH: Adding a factory for int parameters

---
+++ Code/Wrappers/QtWidget/otbWrapperQtWidgetIntParameterFactory.cxx
@@ -0,0 +1,63 @@
+/*=========================================================================
+
+  Program:   ORFEO Toolbox
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
+
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#include "otbWrapperQtWidgetIntParameterFactory.h"
+#include "otbWrapperNumericalParameter.h"
+
+
+namespace otb
+{
+
+namespace Wrapper
+{
+
+QtWidgetIntParameterFactory::QtWidgetIntParameterFactory()
+{}
+
+QtWidgetIntParameterFactory::~QtWidgetIntParameterFactory()
+{}
+
+static QWidget * QtWidgetIntParameterFactory::CreateQtWidget(Parameter * param)
+{
+  // Try to cast to int parameter
+  IntParameter * intParam = dynamic_cast<IntParameter *>(param);
+
+  // Check if dynamic cast succeeds
+  if(param.IsNull())
+    {
+    return 0;
+    }
+
+  // Set up input text edit
+  QHBoxLayout *hLayout = new QHBoxLayout;
+  hLayout->setSpacing(0);
+  hLayout->setContentsMargins(0,0,0,0);
+  
+  QSpinBox* input = new QSpinBox;
+  input->setRange(intParam->GetMinimumValue(), intParam->GetMaximumValue());
+  input->setToolTip(intParam->GetDescription().c_str());
+  
+  QString optionID(intParam->GetName().c_str());
+
+  hLayout->addWidget(input);
+  hLayout->addStretch();
+
+  QGroupBox *paramHGroup = new QGroupBox;
+  paramHGroup->setLayout(hLayout);
+  return paramHGroup;
+}
