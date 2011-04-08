6ec70dd921170a15489285742cd2bcbe49873664
Cyrille Valladeau <cyrille.valladeau@c-s.fr>
2011-04-08 10:16:54 +0200

    ENH: correct troubles in commit

--- Code/IO/otbGDALImageIO.cxx
+++ Code/IO/otbGDALImageIO.cxx
@@ -1458,9 +1458,8 @@ std::string GDALImageIO::FilenameToGdalDriverShortName(std::string name)
     gdalDriverShortName="JPEG";
   else if ( extension == "pix" )
     gdalDriverShortName="PCIDSK";
-  // Sun raster format
-  else if ( extension == "ras" || extension == "sum" )
-    gdalDriverShortName="ENVI";
+  else if ( extension == "ras" )
+    gdalDriverShortName="GTiff";
   else
     gdalDriverShortName = "NOT-FOUND";
 
@@ -1475,15 +1474,7 @@ std::string GDALImageIO::GetGdalWriteImageFileName(std::string& gdalDriverShortN
   // Suppress hdr extension for ENVI format
   if (gdalDriverShortName == "ENVI")
     {
-      std::string extension;
-      // Get extension in lowercase
-      extension = otb::System::SetToLower( System::GetExtension(filename));
-      if(  extension != "ras" && extension != "sum" )
-        {
-          std::cout<<filename<<std::endl;
-          gdalFileName = System::GetRootName(filename);
-          std::cout<<gdalFileName<<std::endl;
-        }
+    gdalFileName = System::GetRootName(filename);
     }
   return gdalFileName;
 }
