4fd619085ccf34a26bab131c86ea7a305bc390b3
Mickael Savinaud <mickael.savinaud@c-s.fr>
2011-04-11 16:46:45 +0200

    TEST: all test about compare ascii with with space should be work

--- Testing/Code/TestSystem/otbCompareAsciiTestsEpsilon3_WhiteSpace.cxx
+++ Testing/Code/TestSystem/otbCompareAsciiTestsEpsilon3_WhiteSpace.cxx
@@ -16,95 +16,14 @@
 
 =========================================================================*/
 #include <iostream>
-#include <fstream>
 
 #include "itkExceptionObject.h"
 #include "otbMacro.h"
 
 int otbCompareAsciiTestsEpsilon3_WhiteSpace(int argc, char * argv[])
 {
-  std::cout << "ASCII COMPARISON TEST :" << std::endl;
+  std::cout << "ASCII COMPARISON TEST: it will use directly the baseline and input files in OTB-Data "
+      "to avoid problem with otb-bot." << std::endl;
 
-  std::cout << "nb of differences should be equal to 30 (for more details see the test function)" << std::endl;
-
-  /*std::cout << "L1 expected = true and output = true" << std::endl;
-  std::cout << "L2 expected = true and output = true" << std::endl;
-  std::cout << "L3 expected = false and output = false" << std::endl;
-  std::cout << "L4 expected = true and output = true" << std::endl;
-  std::cout << "L5 expected = true and output = true" << std::endl;
-  std::cout << "L6 expected = false and output = false" << std::endl;
-  std::cout << "L7 expected = true and output = true" << std::endl;
-  std::cout << "L8 expected = true and output = true" << std::endl;
-  std::cout << "L9 expected = false and output = false" << std::endl;
-  std::cout << "L10 expected = true and output = true" << std::endl;
-  std::cout << "L11 expected = true and output = true" << std::endl;
-  std::cout << "L12 expected = false and output = false" << std::endl;
-
-  // A
-  std::cout << "L13 expected = true and output = true" << std::endl;
-  std::cout << "L14 expected = true and output = true" << std::endl;
-  std::cout << "L15 expected = false and output = false" << std::endl;
-
-  // B
-  std::cout << "L16 expected = true and output = true" << std::endl;
-  std::cout << "L17 expected = true and output = true" << std::endl;
-  std::cout << "L18 expected = false and output = false" << std::endl;
-
-  // C
-  std::cout << "L19 expected = true and output = true" << std::endl;
-  std::cout << "L20 expected = true and output = true" << std::endl;
-  std::cout << "L21 expected = false and output = false" << std::endl;
-
-  // D
-  std::cout << "L22 expected = true and output = true" << std::endl;
-  std::cout << "L23 expected = true and output = true" << std::endl;
-  std::cout << "L24 expected = false and output = false" << std::endl;
-
-  // E
-  std::cout << "L25 expected = true and output = true" << std::endl;
-  std::cout << "L26 expected = true and output = true" << std::endl;
-  std::cout << "L27 expected = false and output = false" << std::endl;
-
-  // F
-  std::cout << "L29: expected = true and output = true" << std::endl;
-  std::cout << "L30: expected = true and output = true" << std::endl;
-  std::cout << "L31: expected = false and output = false" << std::endl;
-  std::cout << "L32: expected = false and output = false" << std::endl;
-  std::cout << "L33: expected = false and output = false" << std::endl;
-  std::cout << "L34: expected = false and output = false" << std::endl;
-  std::cout << "L35: expected = false and output = false" << std::endl;
-
-  // G
-  std::cout << "L37: expected = true and output = true" << std::endl;
-  std::cout << "L38: expected = true and output = true" << std::endl;
-  std::cout << "L39: expected = false and output = false" << std::endl;
-  std::cout << "L40: expected = false and output = false" << std::endl;
-  std::cout << "L41: expected = false and output = false" << std::endl;
-  std::cout << "L42: expected = false and output = false" << std::endl;
-  std::cout << "L43: expected = false and output = false" << std::endl;
-
-  // H
-  std::cout << "L45: expected = true and output = true" << std::endl;
-  std::cout << "L46: expected = true and output = true" << std::endl;
-  std::cout << "L44: expected = false and output = false" << std::endl;
-  std::cout << "L48: expected = false and output = false" << std::endl;
-  std::cout << "L49: expected = false and output = false" << std::endl;
-  std::cout << "L50: expected = false and output = false" << std::endl;
-  std::cout << "L51: expected = false and output = false" << std::endl;
-
-  // I
-  std::cout << "L53: expected = true and output = true" << std::endl;
-  std::cout << "L54: expected = true and output = true" << std::endl;
-  std::cout << "L55: expected = false and output = false" << std::endl;
-  std::cout << "L56: expected = false and output = false" << std::endl;
-  std::cout << "L57: expected = false and output = false" << std::endl;
-  std::cout << "L58: expected = false and output = false" << std::endl;
-  std::cout << "L59: expected = false and output = false" << std::endl;
-
-  // J
-  std::cout << "L61: expected = false and output = false" << std::endl;
-  std::cout << "L62: expected = false and output = false" << std::endl;
-  std::cout << "L63: expected = false and output = false" << std::endl;
-*/
   return EXIT_SUCCESS;
 }
