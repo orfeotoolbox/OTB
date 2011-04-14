0b7bbe34afc3dba5fd40d492e1399e64f1cd03e4
Aurelien Bricier <aurelien.bricier@c-s.fr>
2011-04-14 11:24:53 +0200

    COMP: solved build error incriminating deprecated class tests

--- Testing/Code/BasicFilters/otbBasicFiltersTests10.cxx
+++ Testing/Code/BasicFilters/otbBasicFiltersTests10.cxx
@@ -28,12 +28,6 @@
 
 void RegisterTests()
 {
-#if defined(OTB_USE_DEPRECATED)
-  REGISTER_TEST(otbScalarImageTextureFunctorNew);
-  REGISTER_TEST(otbScalarImageTextureFunctor);
-  REGISTER_TEST(otbScalarVectorImageTextureFunctorNew);
-  REGISTER_TEST(otbScalarVectorImageTextureFunctor);
-#endif
   REGISTER_TEST(otbGaborFilterGeneratorNew);
   REGISTER_TEST(otbGaborFilterGenerator);
 #if defined(USE_FFTWD)
