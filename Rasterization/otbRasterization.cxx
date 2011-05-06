b264a4e67ae47964a16ff6e0847d978e90dfccdf
Jonathan Guinet <jonathan.guinet@c-s.fr>
2011-05-06 14:21:26 +0200

    ENH Rasterization application input modification : output projection ref is set by SRID or by using a reference image.

--- Rasterization/otbRasterization.cxx
+++ Rasterization/otbRasterization.cxx
@@ -37,10 +37,14 @@
 //Rasterization
 #include "otbVectorDataToImageFilter.h"
 
+#include "otbGeoInformationConversion.h"
+
 //Misc
 #include "otbRemoteSensingRegion.h"
 #include "otbStandardWriterWatcher.h"
 #include "otbPipelineMemoryPrintCalculator.h"
+
+
 
 namespace otb
 {
@@ -53,22 +57,22 @@ int Rasterization::Describe(ApplicationDescriptor* descriptor)
                         "in",  1, true, ApplicationDescriptor::FileName);
   descriptor->AddOption("OutputImage", "An output image containing the rasterized vector data",
                         "out", 1, true, ApplicationDescriptor::OutputImage);
-  descriptor->AddOption("SizeX", "OutputSize[0]",
-                        "szx", 1, true, ApplicationDescriptor::Real);
-  descriptor->AddOption("SizeY", "OutputSize[1]",
-                        "szy", 1, true, ApplicationDescriptor::Real);
-
-  // Optional
-  descriptor->AddOption("OriginX", "OutputOrigin[0] (optional)",
+  descriptor->AddOption("InputImage", "A reference image for extraction region and projection information (optional)",
+                          "im", 1, false, ApplicationDescriptor::InputImage);
+  descriptor->AddOption("SizeX", "OutputSize[0] (useless if support image is given)",
+                        "szx", 1, false, ApplicationDescriptor::Real);
+  descriptor->AddOption("SizeY", "OutputSize[1] (useless if support image is given)",
+                        "szy", 1, false, ApplicationDescriptor::Real);
+  descriptor->AddOption("RSID", "Projection System RSID number (RSID 4326 for WGS84 32631 for UTM31N)  (useless if support image is given)",
+                         "epsg",  1, false, ApplicationDescriptor::Integer);
+  descriptor->AddOption("OriginX", "OutputOrigin[0] (optional if support image is given )",
                         "orx", 1, false, ApplicationDescriptor::Real);
-  descriptor->AddOption("OriginY", "OutputOrigin[1] (optional)",
+  descriptor->AddOption("OriginY", "OutputOrigin[1] (optional, useless if support image is given)",
                         "ory", 1, false, ApplicationDescriptor::Real);
-  descriptor->AddOption("SpacingX", "OutputSpacing[0] (optional)",
+  descriptor->AddOption("SpacingX", "OutputSpacing[0] (optional, useless if support image is given )",
                         "spx", 1, false, ApplicationDescriptor::Real);
-  descriptor->AddOption("SpacingY", "OutputSpacing[1] (optional)",
+  descriptor->AddOption("SpacingY", "OutputSpacing[1] (optional, useless if support image is given)",
                         "spy", 1, false, ApplicationDescriptor::Real);
-  descriptor->AddOption("ProjRef", "Projection (optional)",
-                        "pr",  1, false, ApplicationDescriptor::String);
   descriptor->AddOption("AvailableMemory", "Set the maximum of available memory for the pipeline execution in mega bytes (optional, 256 by default)",
                         "ram", 1, false, otb::ApplicationDescriptor::Integer);
   
@@ -79,142 +83,212 @@ int Rasterization::Execute(otb::ApplicationOptionsResult* parseResult)
 {
   // Images
   //typedef itk::RGBAPixel<unsigned char>                   PixelType;
-  typedef unsigned char                                   PixelType;
-  typedef otb::Image<PixelType, 2>                        ImageType;
-  typedef ImageType::PointType                            PointType;
-  typedef ImageType::SizeType                             SizeType;
-  typedef ImageType::SpacingType                          SpacingType;
-  typedef ImageType::IndexType                            IndexType;
-  typedef otb::StreamingImageFileWriter<ImageType>        WriterType;
+  typedef unsigned char PixelType;
+  typedef otb::Image<PixelType, 2> ImageType;
+  typedef ImageType::PointType PointType;
+  typedef ImageType::SizeType SizeType;
+  typedef ImageType::SpacingType SpacingType;
+  typedef ImageType::IndexType IndexType;
+  typedef otb::StreamingImageFileWriter<ImageType> WriterType;
 
   // VectorData
-  typedef otb::VectorData<>                               VectorDataType;
-  typedef VectorDataType::DataNodeType                    DataNodeType;
-  typedef VectorDataType::DataTreeType                    DataTreeType;
-  typedef DataNodeType::PointType                         PointType;
-  typedef otb::VectorDataFileReader<VectorDataType>       VectorDataReaderType;
-  typedef otb::VectorDataFileWriter<VectorDataType>       VectorDataWriterType;
-  typedef VectorDataProjectionFilter<
-    VectorDataType, VectorDataType>                        VectorDataProjectionFilterType;
-  typedef VectorDataExtractROI<VectorDataType>            VectorDataExtractROIType;
-  typedef VectorDataProperties<VectorDataType>            VectorDataPropertiesType;
+  typedef otb::VectorData<> VectorDataType;
+  typedef VectorDataType::DataNodeType DataNodeType;
+  typedef VectorDataType::DataTreeType DataTreeType;
+  typedef DataNodeType::PointType PointType;
+  typedef otb::VectorDataFileReader<VectorDataType> VectorDataReaderType;
+  typedef otb::VectorDataFileWriter<VectorDataType> VectorDataWriterType;
+  typedef VectorDataProjectionFilter<VectorDataType, VectorDataType> VectorDataProjectionFilterType;
+  typedef VectorDataExtractROI<VectorDataType> VectorDataExtractROIType;
+  typedef VectorDataProperties<VectorDataType> VectorDataPropertiesType;
+
+  // Image
+
+  typedef otb::ImageFileReader<ImageType> ImageReaderType;
 
   // Rasterization
-  typedef otb::VectorDataToImageFilter<VectorDataType,
-    ImageType>                                      VectorDataToImageFilterType;
+  typedef otb::VectorDataToImageFilter<VectorDataType, ImageType> VectorDataToImageFilterType;
 
   // Misc
-  typedef otb::RemoteSensingRegion<double>                RemoteSensingRegionType;
-  typedef RemoteSensingRegionType::SizeType               SizePhyType;
-  typedef otb::PipelineMemoryPrintCalculator              MemoryCalculatorType;
-  
+  typedef otb::RemoteSensingRegion<double> RemoteSensingRegionType;
+  typedef RemoteSensingRegionType::SizeType SizePhyType;
+  typedef otb::PipelineMemoryPrintCalculator MemoryCalculatorType;
+
+  ImageReaderType::Pointer imReader;
+  ImageType::Pointer referenceImage;
+
   // Reading the VectorData
   std::string vdFilename = parseResult->GetParameterString("InputVData");
-  std::cout<<"Processing vector data : "<<vdFilename<<std::endl;
+  std::cout << "Processing vector data : " << vdFilename << std::endl;
   VectorDataReaderType::Pointer vdReader = VectorDataReaderType::New();
   vdReader->SetFileName(vdFilename);
   vdReader->Update();
 
+  // region information
+  SizeType size;
+  PointType origin;
+  SpacingType spacing;
+  SizePhyType sizePhy;
+
+  // reading projection information
+  // two choice :
+  std::string outputProjectionRef;
+  // a reference image is given as input
+  if (parseResult->IsOptionPresent("InputImage"))
+    {
+    if (parseResult->IsOptionPresent("SizeX") || parseResult->IsOptionPresent("SizeY")
+        || parseResult->IsOptionPresent("OriginX") || parseResult->IsOptionPresent("OriginY")
+        || parseResult->IsOptionPresent("SpacingX") || parseResult->IsOptionPresent("SpacingY")
+        || parseResult->IsOptionPresent("RSID"))
+      {
+      std::cerr << "Input entry problem " << std::endl
+          << " reference image can't be used with other projection parameters. " << std::endl;
+      return EXIT_FAILURE;
+      }
+
+    std::string imFilename = parseResult->GetParameterString("InputImage");
+
+    imReader = ImageReaderType::New();
+    imReader->SetFileName(imFilename);
+    imReader->Update();
+
+    referenceImage = imReader->GetOutput();
+    outputProjectionRef = referenceImage->GetProjectionRef();
+
+    size[0] = referenceImage->GetLargestPossibleRegion().GetSize(0);
+    size[1] = referenceImage->GetLargestPossibleRegion().GetSize(1);
+
+    origin = referenceImage->GetOrigin();
+
+    spacing = referenceImage->GetSpacing();
+
+    sizePhy[0] = size[0] * spacing[0];
+    sizePhy[1] = size[1] * spacing[1];
+
+    }
+
+  else
+    if (parseResult->IsOptionPresent("SizeX") && parseResult->IsOptionPresent("SizeY"))
+      {
+
+      if (parseResult->IsOptionPresent("RSID"))
+        {
+        unsigned int RSID = parseResult->GetParameterInt("RSID");
+        outputProjectionRef = otb::GeoInformationConversion::ToWKT(RSID);
+        }
+      else
+        {
+        outputProjectionRef = vdReader->GetOutput()->GetProjectionRef();
+        }
+
+      VectorDataPropertiesType::Pointer vdProperties = VectorDataPropertiesType::New();
+      vdProperties->SetVectorDataObject(vdReader->GetOutput());
+      vdProperties->ComputeBoundingRegion();
+
+      size[0] = parseResult->GetParameterDouble("SizeX");
+      size[1] = parseResult->GetParameterDouble("SizeY");
+
+      if (parseResult->IsOptionPresent("OriginX") && parseResult->IsOptionPresent("OriginY"))
+        {
+        origin[0] = parseResult->GetParameterDouble("OriginX");
+        origin[1] = parseResult->GetParameterDouble("OriginY");
+        }
+      else
+        {
+        origin = vdProperties->GetBoundingRegion().GetIndex();
+        }
+
+      if (parseResult->IsOptionPresent("SpacingX") && parseResult->IsOptionPresent("SpacingY"))
+        {
+        spacing[0] = parseResult->GetParameterDouble("SpacingX");
+        spacing[1] = parseResult->GetParameterDouble("SpacingY");
+        }
+      else
+        {
+        spacing[0] = vdProperties->GetBoundingRegion().GetSize()[0] / size[0];
+        spacing[1] = vdProperties->GetBoundingRegion().GetSize()[1] / size[1];
+        }
+
+      sizePhy[0] = size[0] * spacing[0];
+      sizePhy[1] = size[1] * spacing[1];
+
+      }
+    else
+      {
+      std::cerr << "Input entry problem " << std::endl << " if no reference image given, region size is needed "
+          << std::endl;
+      return EXIT_FAILURE;
+      }
+
   // Reprojecting the VectorData
-  std::string projectionRef;
+
   VectorDataProjectionFilterType::Pointer vproj = VectorDataProjectionFilterType::New();
   vproj->SetInput(vdReader->GetOutput());
-  if(parseResult->IsOptionPresent("ProjRef"))
-    {
-    projectionRef = parseResult->GetParameterString("ProjRef");
-    }
-  else
-    {
-    projectionRef = vdReader->GetOutput()->GetProjectionRef();
-    }
-  vproj->SetOutputProjectionRef(projectionRef);
 
-  // Converting the VectorData
-  VectorDataPropertiesType::Pointer vdProperties = VectorDataPropertiesType::New();
-  vdProperties->SetVectorDataObject(vdReader->GetOutput());
-  vdProperties->ComputeBoundingRegion();
+  //
+  vproj->SetInputProjectionRef(vdReader->GetOutput()->GetProjectionRef());
 
-  SizeType size;
-  size[0] = parseResult->GetParameterDouble("SizeX");
-  size[1] = parseResult->GetParameterDouble("SizeY");
-
-  PointType origin;
-  if(parseResult->IsOptionPresent("OriginX") && parseResult->IsOptionPresent("OriginY"))
+  vproj->SetOutputProjectionRef(outputProjectionRef);
+  if (parseResult->IsOptionPresent("InputImage"))
     {
-    origin[0] = parseResult->GetParameterDouble("OriginX");
-    origin[1] = parseResult->GetParameterDouble("OriginY");
-    }
-  else
-    {
-    origin = vdProperties->GetBoundingRegion().GetIndex();
-    }
-  
-  SpacingType spacing;
-  if(parseResult->IsOptionPresent("SpacingX") && parseResult->IsOptionPresent("SpacingY"))
-    {
-    spacing[0] = parseResult->GetParameterDouble("SpacingX");
-    spacing[1] = parseResult->GetParameterDouble("SpacingY");
-    }
-  else
-    {
-    spacing[0] = vdProperties->GetBoundingRegion().GetSize()[0]/size[0];
-    spacing[1] = vdProperties->GetBoundingRegion().GetSize()[1]/size[1];
+    vproj->SetOutputKeywordList(referenceImage->GetImageKeywordlist());
     }
 
-  
-  SizePhyType               sizePhy;
-  sizePhy[0] = size[0] * spacing[0];
-  sizePhy[1] = size[1] * spacing[1];
-
-  RemoteSensingRegionType   region;
+  RemoteSensingRegionType region;
   region.SetSize(sizePhy);
   region.SetOrigin(origin);
-  region.SetRegionProjection(projectionRef);
- 
+  region.SetRegionProjection(outputProjectionRef);
+  if (parseResult->IsOptionPresent("InputImage"))
+    {
+    region.SetKeywordList(referenceImage->GetImageKeywordlist());
+    }
   VectorDataExtractROIType::Pointer vdextract = VectorDataExtractROIType::New();
   vdextract->SetRegion(region);
-  vdextract->SetInput(vproj->GetOutput());
 
+  vdextract->SetInput(vproj->GetOutput());
   VectorDataToImageFilterType::Pointer vectorDataRendering = VectorDataToImageFilterType::New();
   vectorDataRendering->SetInput(vdextract->GetOutput());
   vectorDataRendering->SetSize(size);
   vectorDataRendering->SetOrigin(origin);
   vectorDataRendering->SetSpacing(spacing);
-  vectorDataRendering->SetVectorDataProjectionWKT(projectionRef);
+  vectorDataRendering->SetVectorDataProjectionWKT(outputProjectionRef);
   vectorDataRendering->SetRenderingStyleType(VectorDataToImageFilterType::Binary);
 
+  ImageType::Pointer outputImage = vectorDataRendering->GetOutput();
+  if (parseResult->IsOptionPresent("InputImage"))
+    {
+    outputImage->SetMetaDataDictionary(referenceImage->GetMetaDataDictionary());
+    }
   // Instantiate the writer
   std::string oFilename = parseResult->GetParameterString("OutputImage");
-  
+
   WriterType::Pointer oWriter = WriterType::New();
   oWriter->SetFileName(oFilename);
-  oWriter->SetInput(vectorDataRendering->GetOutput());
-  
+  oWriter->SetInput(outputImage);
 
   //Instantiate the pipeline memory print estimator
   MemoryCalculatorType::Pointer calculator = MemoryCalculatorType::New();
-  const double byteToMegabyte = 1./vcl_pow(2.0, 20);
+  const double byteToMegabyte = 1. / vcl_pow(2.0, 20);
 
   if (parseResult->IsOptionPresent("AvailableMemory"))
     {
-    long long int memory = static_cast <long long int> (parseResult->GetParameterUInt("AvailableMemory"));
+    long long int memory = static_cast<long long int> (parseResult->GetParameterUInt("AvailableMemory"));
     calculator->SetAvailableMemory(memory / byteToMegabyte);
     }
   else
     {
     calculator->SetAvailableMemory(256 * byteToMegabyte);
     }
-  
+
   calculator->SetDataToWrite(vectorDataRendering->GetOutput());
   calculator->Compute();
-      
-  oWriter->SetTilingStreamDivisions(calculator->GetOptimalNumberOfStreamDivisions());
-      
-  otbMsgDevMacro(<< "Guess the pipeline memory print " << calculator->GetMemoryPrint()*byteToMegabyte << " Mo");
-  otbMsgDevMacro(<< "Number of stream divisions : " << calculator->GetOptimalNumberOfStreamDivisions());
 
-  otb::StandardWriterWatcher watcher(oWriter,"Rasterization");
-  
+  oWriter->SetTilingStreamDivisions(calculator->GetOptimalNumberOfStreamDivisions());
+
+  otbMsgDevMacro(<< "Guess the pipeline memory print " << calculator->GetMemoryPrint()*byteToMegabyte << " Mo"); otbMsgDevMacro(<< "Number of stream divisions : " << calculator->GetOptimalNumberOfStreamDivisions());
+
+  otb::StandardWriterWatcher watcher(oWriter, "Rasterization");
+
   oWriter->Update();
 
   return EXIT_SUCCESS;
