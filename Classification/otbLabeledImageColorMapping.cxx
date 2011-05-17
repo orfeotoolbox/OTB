df7a9bf08cc6b32f16707e28211d9ed13fcaa0ce
Julien Malik <julien.malik@c-s.fr>
2011-05-17 15:07:12 +0200

    STYLE: fix indentation

--- Classification/otbLabeledImageColorMapping.cxx
+++ Classification/otbLabeledImageColorMapping.cxx
@@ -1,20 +1,20 @@
 /*=========================================================================
 
-  Program:   ORFEO Toolbox
-  Language:  C++
-  Date:      $Date$
-  Version:   $Revision$
+ Program:   ORFEO Toolbox
+ Language:  C++
+ Date:      $Date$
+ Version:   $Revision$
 
 
-  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
-  See OTBCopyright.txt for details.
+ Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+ See OTBCopyright.txt for details.
 
 
-     This software is distributed WITHOUT ANY WARRANTY; without even
-     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-     PURPOSE.  See the above copyright notices for more information.
+ This software is distributed WITHOUT ANY WARRANTY; without even
+ the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ PURPOSE.  See the above copyright notices for more information.
 
-=========================================================================*/
+ =========================================================================*/
 #include "otbLabeledImageColorMapping.h"
 
 #include <fstream>
@@ -36,30 +36,32 @@ int LabeledImageColorMapping::Describe(ApplicationDescriptor* descriptor)
   descriptor->SetDescription("Replace labels of a classification map with user defined 8-bits RGB colors. Unknown label are mapped to black by default.");
   descriptor->AddInputImage();
   descriptor->AddOutputImage();
-  descriptor->AddOption("ColorTable", "An ascii file containing the color table with one color per line (for instance line 1 255 0 0 means that all pixel with label 1 will be replaced by RGB  color 255 0 0). Lines begining with a # are ignored.",
+  descriptor->AddOption("ColorTable",
+                        "An ascii file containing the color table with one color per line (for instance line 1 255 0 0 means that all pixel with label 1 will be replaced by RGB  color 255 0 0). Lines begining with a # are ignored.",
                         "ct", 1, true, ApplicationDescriptor::FileName);
-  descriptor->AddOption("AvailableMemory","Set the maximum of available memory for the pipeline execution in mega bytes (optional, 256 by default)","ram", 1, false, otb::ApplicationDescriptor::Integer);
+  descriptor->AddOption("AvailableMemory",
+                        "Set the maximum of available memory for the pipeline execution in mega bytes (optional, 256 by default)",
+                        "ram", 1, false, otb::ApplicationDescriptor::Integer);
 
   return EXIT_SUCCESS;
 }
 
-
 int LabeledImageColorMapping::Execute(otb::ApplicationOptionsResult* parseResult)
 {
   // Pixel type
-  typedef unsigned char  PixelType;
+  typedef unsigned char PixelType;
   typedef unsigned short LabelType;
 
-  typedef otb::VectorImage<PixelType, 2>                  VectorImageType;
-  typedef VectorImageType::PixelType                     VectorPixelType;
-  typedef otb::Image<LabelType, 2>                        LabelImageType;
-  typedef otb::ImageFileReader<LabelImageType>           ReaderType;
+  typedef otb::VectorImage<PixelType, 2> VectorImageType;
+  typedef VectorImageType::PixelType VectorPixelType;
+  typedef otb::Image<LabelType, 2> LabelImageType;
+  typedef otb::ImageFileReader<LabelImageType> ReaderType;
   typedef otb::StreamingImageFileWriter<VectorImageType> WriterType;
   typedef otb::ChangeLabelImageFilter<LabelImageType, VectorImageType> ChangeLabelFilterType;
 
   ReaderType::Pointer reader = ReaderType::New();
   reader->SetFileName(parseResult->GetInputImage().c_str());
-  
+
   ChangeLabelFilterType::Pointer mapper = ChangeLabelFilterType::New();
   mapper->SetInput(reader->GetOutput());
   mapper->SetNumberOfComponentsPerPixel(3);
@@ -73,23 +75,22 @@ int LabeledImageColorMapping::Execute(otb::ApplicationOptionsResult* parseResult
   const char * colormap = parseResult->GetParameterString("ColorTable").c_str();
 
   ifs.open(colormap);
-  
-  if(!ifs)
+
+  if (!ifs)
     {
-    std::cerr<<"Can not read file "<<colormap<<std::endl;
+    std::cerr << "Can not read file " << colormap << std::endl;
     return EXIT_FAILURE;
     }
-  
-  std::cout<<"Parsing color map file "<<colormap<<"."<<std::endl;
 
+  std::cout << "Parsing color map file " << colormap << "." << std::endl;
 
-  while(!ifs.eof())
+  while (!ifs.eof())
     {
     std::string line;
     std::getline(ifs, line);
 
     // Avoid commented lines or too short ones
-    if(line[0]!='#' && line.size()> 0)
+    if (line[0] != '#' && line.size() > 0)
       {
       // retrieve the label
       std::string::size_type pos = line.find_first_of(" ", 0);
@@ -98,37 +99,35 @@ int LabeledImageColorMapping::Execute(otb::ApplicationOptionsResult* parseResult
       // Retrieve the color
       VectorPixelType color(3);
       color.Fill(0);
-      for(unsigned int i = 0; i < 3; ++i)
-       {
-       std::string::size_type nextpos = line.find_first_of(" ", pos);
+      for (unsigned int i = 0; i < 3; ++i)
+        {
+        std::string::size_type nextpos = line.find_first_of(" ", pos);
         int value = atoi(line.substr(pos, nextpos).c_str());
-        if(value < 0 || value > 255)
-          std::cerr<<"WARNING: color value outside 8-bits range (<0 or >255). Value will be clamped."<<std::endl;
-       color[i]=static_cast<PixelType>(value);
-       pos = nextpos+1;
-       nextpos = line.find_first_of(" ", pos);
-       }
-      std::cout<<"Adding color mapping "<<clabel<<" -> ["<<(int)color[0]<<" "<<(int)color[1]<<" "<<(int)color[2]<<" ]"<<std::endl;
+        if (value < 0 || value > 255) std::cerr
+            << "WARNING: color value outside 8-bits range (<0 or >255). Value will be clamped." << std::endl;
+        color[i] = static_cast<PixelType> (value);
+        pos = nextpos + 1;
+        nextpos = line.find_first_of(" ", pos);
+        }
+      std::cout << "Adding color mapping " << clabel << " -> [" << (int) color[0] << " " << (int) color[1] << " "
+          << (int) color[2] << " ]" << std::endl;
       mapper->SetChange(clabel, color);
       }
     }
   ifs.close();
 
-unsigned int ram = 256;
-if (parseResult->IsOptionPresent("AvailableMemory"))
-  {
-  ram = parseResult->GetParameterUInt("AvailableMemory");
-  }
-writer->SetAutomaticTiledStreaming(ram);
+  unsigned int ram = 256;
+  if (parseResult->IsOptionPresent("AvailableMemory"))
+    {
+    ram = parseResult->GetParameterUInt("AvailableMemory");
+    }
+  writer->SetAutomaticTiledStreaming(ram);
 
-otb::StandardWriterWatcher watcher(writer, mapper,"Color mapping");
+  otb::StandardWriterWatcher watcher(writer, mapper, "Color mapping");
 
+  writer->Update();
 
-writer->Update();
-  
-
-
-return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
 
 }
