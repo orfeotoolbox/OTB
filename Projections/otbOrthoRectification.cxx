9e141bc5531f45488a6f4c2658efa45f710f7d9c
Julien Michel <julien.michel@orfeo-toolbox.org>
2011-06-06 18:39:09 +0200

    ENH: Adding a new option to allow ortho-rectification from EPSG code

--- Projections/otbOrthoRectification.cxx
+++ Projections/otbOrthoRectification.cxx
@@ -259,7 +259,7 @@ int OrthoRectification::Describe(ApplicationDescriptor* descriptor)
   descriptor->AddOption("OutputSpacing","Spacing resolution in meters on X/Y Axis","spacing", 2, false, otb::ApplicationDescriptor::Real);
   descriptor->AddOption("DEMDirectory","Directory where to find the DEM tiles","dem", 1, false, otb::ApplicationDescriptor::DirectoryName);
   descriptor->AddOptionNParams("MapProjectionType",
-                               "Type (UTM/LAMBERT/LAMBERT2/LAMBERT93/SINUS/ECKERT4/TRANSMERCATOR/MOLLWEID) and parameters of map projection used",
+                               "Type (UTM/LAMBERT/LAMBERT2/LAMBERT93/SINUS/ECKERT4/TRANSMERCATOR/MOLLWEID/EPSG) and parameters of map projection used. If EPSG is used, their is only one parameter corresponding to the EPSG code (see http://spatialreference.org).",
                                "mapProj", false, otb::ApplicationDescriptor::String);
   descriptor->AddOption("RPC","Activate RPC sensor model estimation. Parameter is the number of control points per axis.","rpc", 1, false, otb::ApplicationDescriptor::Integer);
   descriptor->AddOption("LocMapSpacing","Generate a coarser deformation field with the given spacing.","lmSpacing", 1, false, otb::ApplicationDescriptor::Real);
@@ -269,6 +269,219 @@ int OrthoRectification::Describe(ApplicationDescriptor* descriptor)
 
   return EXIT_SUCCESS;
 }
+
+int epsg_main(otb::ApplicationOptionsResult* parseResult)
+{
+  try
+    {
+    typedef otb::VectorImage<unsigned short int, 2>   ImageType;
+    typedef otb::ImageFileReader<ImageType>           ReaderType;
+    typedef otb::StreamingImageFileWriter<ImageType>  WriterType;
+
+    typedef otb::GenericRSResampleImageFilter< ImageType, ImageType > OrthorectifFilterType;
+
+    typedef itk::LinearInterpolateImageFunction<ImageType, double>          LinearInterpolationType;
+    typedef itk::NearestNeighborInterpolateImageFunction<ImageType, double> NearestNeighborInterpolationType;
+    typedef otb::BCOInterpolateImageFunction<ImageType>                     BCOInterpolationType;
+
+    typedef otb::PipelineMemoryPrintCalculator           MemoryCalculatorType;
+    typedef itk::ExtractImageFilter<ImageType, ImageType> ExtractFilterType;
+
+    // Read input image information
+    ReaderType::Pointer reader=ReaderType::New();
+    reader->SetFileName(parseResult->GetInputImage().c_str());
+    reader->GenerateOutputInformation();
+    ImageType::SizeType lsize = reader->GetOutput()->GetLargestPossibleRegion().GetSize();
+
+    // Orthorectification filter
+    OrthorectifFilterType::Pointer orthoFilter = OrthorectifFilterType::New();
+    orthoFilter->SetInput(reader->GetOutput());
+
+    // If activated, generate RPC model
+    if(parseResult->IsOptionPresent("RPC"))
+      {
+      orthoFilter->EstimateInputRpcModelOn();
+      orthoFilter->SetInputRpcGridSize(parseResult->GetParameterUInt("RPC"));
+      }
+
+    std::string outputProjectionRef = otb::GeoInformationConversion::ToWKT(parseResult->GetParameterInt("MapProjectionType",1));
+
+    otbMsgDevMacro(<<"Output projection ref: "<<outputProjectionRef);
+
+    // Compute the output parameters
+    typedef otb::ImageToGenericRSOutputParameters<ImageType> OutputParametersEstimatorType;
+    OutputParametersEstimatorType::Pointer genericRSEstimator = OutputParametersEstimatorType::New();
+    genericRSEstimator->SetInput(reader->GetOutput());
+    genericRSEstimator->SetOutputProjectionRef(outputProjectionRef);
+
+    // Set up output image informations
+
+    ImageType::SpacingType spacing;
+    if(parseResult->IsOptionPresent("OutputSpacing"))
+      {
+      spacing[0]= parseResult->GetParameterDouble("OutputSpacing", 0);
+      spacing[1]= parseResult->GetParameterDouble("OutputSpacing", 1);
+      }
+    else
+      {
+      typedef otb::ImageMetadataInterfaceBase ImageMetadataInterfaceType;
+      ImageMetadataInterfaceType::Pointer metadataInterface = ImageMetadataInterfaceFactory::CreateIMI(
+          reader->GetOutput()->GetMetaDataDictionary());
+      double isotropicSpacing = std::max(metadataInterface->GetXPixelSpacing(), metadataInterface->GetYPixelSpacing());
+
+      spacing[0] =  isotropicSpacing;
+      spacing[1] = -isotropicSpacing;
+      }
+    genericRSEstimator->ForceSpacingTo(spacing);
+
+    otbMsgDevMacro(<< "Output image spacing: " << spacing);
+
+    genericRSEstimator->Compute();
+    // set the start index
+    ImageType::IndexType start;
+    start[0]=0;
+    start[1]=0;
+
+    //Update now the orthoFilter with output image parameters
+    ImageType::PixelType defaultValue;
+    itk::PixelBuilder<ImageType::PixelType>::Zero(defaultValue,
+                                                  reader->GetOutput()->GetNumberOfComponentsPerPixel());
+
+    orthoFilter->SetInput(reader->GetOutput());
+
+    ImageType::PointType origin;
+    if(parseResult->IsOptionPresent("UpperLeft"))
+      {
+      origin[0]= parseResult->GetParameterDouble("UpperLeft", 0);
+      origin[1]= parseResult->GetParameterDouble("UpperLeft", 1);
+      }
+    else
+      {
+      origin = genericRSEstimator->GetOutputOrigin();
+      }
+
+    otbMsgDevMacro(<< "Output image origin: " << origin);
+
+    ImageType::SizeType size;
+    if(parseResult->IsOptionPresent("OutputSize"))
+      {
+      size[0]= parseResult->GetParameterDouble("OutputSize", 0);
+      size[1]= parseResult->GetParameterDouble("OutputSize", 1);
+      genericRSEstimator->ForceSizeTo(size);
+      }
+    else
+      {
+      size = genericRSEstimator->GetOutputSize();
+      }
+
+    otbMsgDevMacro(<< "Output image size: " << size);
+
+    orthoFilter->SetOutputOrigin(origin);
+    orthoFilter->SetOutputSpacing(genericRSEstimator->GetOutputSpacing());
+    orthoFilter->SetOutputSize(size);
+    orthoFilter->SetOutputStartIndex(start);
+    orthoFilter->SetEdgePaddingValue(defaultValue);
+
+    // Generate deformation field
+    if(parseResult->IsOptionPresent("DEMDirectory"))
+      {
+      orthoFilter->SetDEMDirectory(parseResult->GetParameterString("DEMDirectory", 0));
+      }
+
+    // Set the output map projection
+    orthoFilter->SetOutputProjectionRef(outputProjectionRef);
+
+    if (parseResult->IsOptionPresent("LocMapSpacing"))
+      {
+      double defScalarSpacing = parseResult->GetParameterFloat("LocMapSpacing");
+      ImageType::SpacingType defSpacing;
+      defSpacing[0] = defScalarSpacing;
+      defSpacing[1] = -defScalarSpacing;
+
+      orthoFilter->SetDeformationFieldSpacing(defSpacing);
+      }
+    else
+      {
+      // By default, generate a 10 times coarser deformation field
+      ImageType::SpacingType defSpacing;
+      defSpacing[0] = 10*spacing[0];
+      defSpacing[1] = 10*spacing[1];
+      orthoFilter->SetDeformationFieldSpacing(defSpacing);
+      }
+
+    // Set the interpolator type
+    if (parseResult->IsOptionPresent("InterpolatorType"))
+      {
+      int nbInterpParams = parseResult->GetNumberOfParameters("InterpolatorType") - 1;
+      std::string typeInterpolator = parseResult->GetParameterString("InterpolatorType");
+
+      if (typeInterpolator == "BCO")
+        {
+        BCOInterpolationType::Pointer interpolator = BCOInterpolationType::New();
+        if( nbInterpParams == 1)
+          interpolator->SetRadius(parseResult->GetParameterUInt("InterpolatorType", 1));
+        orthoFilter->SetInterpolator(interpolator);
+        }
+      else if (typeInterpolator == "NEARESTNEIGHBOR" && nbInterpParams == 0)
+        {
+        NearestNeighborInterpolationType::Pointer interpolator = NearestNeighborInterpolationType::New();
+        orthoFilter->SetInterpolator(interpolator);
+        }
+      else if (typeInterpolator == "LINEAR" && nbInterpParams == 0)
+        {
+        LinearInterpolationType::Pointer interpolator = LinearInterpolationType::New();
+        orthoFilter->SetInterpolator(interpolator);
+        }
+      else
+        {
+        itkGenericExceptionMacro(<< "Interpolator type not recognized, choose one with (parameters) : BCO(0/1), NEARESTNEIGHBOR(0), LINEAR(0)");
+        }
+      }
+    else
+      {
+      // Default is BCO
+      BCOInterpolationType::Pointer interpolator = BCOInterpolationType::New();
+      orthoFilter->SetInterpolator(interpolator);
+      }
+
+    //Update informations from orthorectification filter
+    orthoFilter->GetOutput()->UpdateOutputInformation();
+
+    //Instantiate the writer
+    WriterType::Pointer writer = WriterType::New();
+    writer->SetFileName(parseResult->GetOutputImage());
+    writer->SetInput(orthoFilter->GetOutput());
+
+    unsigned int ram = 256;
+    if (parseResult->IsOptionPresent("AvailableMemory"))
+      {
+      ram = parseResult->GetParameterUInt("AvailableMemory");
+      }
+    writer->SetAutomaticTiledStreaming(ram);
+
+    otb::StandardWriterWatcher watcher(writer, orthoFilter,"Orthorectification");
+    writer->Update();
+  }
+  catch ( itk::ExceptionObject & err )
+  {
+    std::cout << "Exception itk::ExceptionObject raised !" << std::endl;
+    std::cout << err << std::endl;
+    return EXIT_FAILURE;
+  }
+  catch ( std::bad_alloc & err )
+  {
+    std::cout << "Exception bad_alloc : "<<(char*)err.what()<< std::endl;
+    return EXIT_FAILURE;
+  }
+  catch ( ... )
+  {
+    std::cout << "Unknown exception raised !" << std::endl;
+    return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
+}
+
+
 
 int OrthoRectification::Execute(otb::ApplicationOptionsResult* parseResult)
 {
@@ -359,9 +572,14 @@ int OrthoRectification::Execute(otb::ApplicationOptionsResult* parseResult)
 
         return generic_main<MollweidProjectionType>(parseResult, mollweidProjection);
         }
+       else if ((typeMap == "EPSG")&&(nbParams==1))
+        {
+        return epsg_main(parseResult);
+        }
+
       else
         {
-        itkGenericExceptionMacro(<< "TypeMap not recognized, choose one with (parameters) : UTM(2), LAMBERT(4), LAMBERT2(0), LAMBERT93(0), SINUS(2), ECKERT4(2), TRANSMERCATOR(3), MOLLWEID(2)");
+        itkGenericExceptionMacro(<< "TypeMap not recognized, choose one with (parameters) : UTM(2), LAMBERT(4), LAMBERT2(0), LAMBERT93(0), SINUS(2), ECKERT4(2), TRANSMERCATOR(3), MOLLWEID(2), EPSG(1)");
         }
       }
     }
