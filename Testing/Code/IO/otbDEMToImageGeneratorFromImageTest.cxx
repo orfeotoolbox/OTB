5f5a1a9896b4f1cdcc46cd8671040b8b7c3ade78
Mickael Savinaud <mickael.savinaud@c-s.fr>
2011-06-07 16:41:51 +0200

    TEST: add test about otbDEMToImageGenerator from an image

--- Testing/Code/IO/otbDEMToImageGeneratorFromImageTest.cxx
+++ Testing/Code/IO/otbDEMToImageGeneratorFromImageTest.cxx
@@ -27,28 +27,29 @@
 #include "otbMultiToMonoChannelExtractROI.h"
 #include "otbExtractROI.h"
 
-#include "otbStandardFilterWatcher.h"
-
-
 int otbDEMToImageGeneratorFromImageTest(int argc, char * argv[])
 {
-  if (argc < 9)
+  std::cout << argc << std::endl;
+  if ( (argc != 9) && (argc != 4) )
     {
     std::cout << argv[0] <<
-    " input filename, folder path, output filename 1, output filename 2, X Output Origin, Y Output Origin, X Output Size, Y Output Size"
-              << std::endl;
+    " input filename, folder path, output filename 1, "
+    " (option) output filename 2,"
+    " (option) X Output Origin, (option) Y Output Origin,"
+    " (option) X Output Size, (option) Y Output Size." << std::endl;
     return EXIT_FAILURE;
+    }
+
+  bool useExtractFilter = false;
+  if (argc == 9)
+    {
+    useExtractFilter = true;
     }
 
   char * inputName = argv[1];
   char * folderPath = argv[2];
   char * outputName1 = argv[3];
-  char * outputName2= argv[4];
 
-  std::cout << argv[5] << ","
-            << argv[6] << ","
-            << argv[7] << ","
-            << argv[8] << "," << std::endl;
 
   const unsigned int Dimension = 2;
   typedef double                                   PixelType;
@@ -66,8 +67,8 @@ int otbDEMToImageGeneratorFromImageTest(int argc, char * argv[])
   ReaderType::Pointer              reader = ReaderType::New();
   DEMToImageGeneratorType::Pointer generatorFilter1 = DEMToImageGeneratorType::New();
   DEMToImageGeneratorType::Pointer generatorFilter2 = DEMToImageGeneratorType::New();
-  ExtractVFilterType::Pointer       extract1 = ExtractVFilterType::New();
-  ExtractFilterType::Pointer      extract2 = ExtractFilterType::New();
+  ExtractVFilterType::Pointer      extract1 = ExtractVFilterType::New();
+  ExtractFilterType::Pointer       extract2 = ExtractFilterType::New();
 
   WriterType::Pointer              writer1 = WriterType::New();
   WriterType::Pointer              writer2 = WriterType::New();
@@ -78,19 +79,28 @@ int otbDEMToImageGeneratorFromImageTest(int argc, char * argv[])
   reader->GenerateOutputInformation();
 
   // First pipeline:
-  extract1->SetInput(reader->GetOutput());
-  extract1->SetSizeX(atoi(argv[7]));
-  extract1->SetSizeY(atoi(argv[8]));
-  extract1->SetStartX(atoi(argv[5]));
-  extract1->SetStartY(atoi(argv[6]));
-  extract1->SetChannel(1);
 
-  extract1->UpdateOutputInformation();
+ if (useExtractFilter)
+   {
+    extract1->SetInput(reader->GetOutput());
+    extract1->SetSizeX(atoi(argv[7]));
+    extract1->SetSizeY(atoi(argv[8]));
+    extract1->SetStartX(atoi(argv[5]));
+    extract1->SetStartY(atoi(argv[6]));
+    extract1->SetChannel(1);
+
+    extract1->UpdateOutputInformation();
+    }
 
   generatorFilter1->SetDEMDirectoryPath(folderPath);
-  generatorFilter1->SetOutputParametersFromImage(extract1->GetOutput());
-
-  otb::StandardFilterWatcher watcher1(generatorFilter1, "DEM to image generator");
+  if (useExtractFilter)
+    {
+    generatorFilter1->SetOutputParametersFromImage(extract1->GetOutput());
+    }
+  else
+    {
+    generatorFilter1->SetOutputParametersFromImage(reader->GetOutput());
+    }
 
   writer1->SetFileName(outputName1);
   writer1->SetInput(generatorFilter1->GetOutput());
@@ -98,22 +108,24 @@ int otbDEMToImageGeneratorFromImageTest(int argc, char * argv[])
   writer1->Update();
 
   // Second pipeline:
-  generatorFilter2->SetDEMDirectoryPath(folderPath);
-  generatorFilter2->SetOutputParametersFromImage(reader->GetOutput());
-  generatorFilter2->InstanciateTransform();
+  if (useExtractFilter)
+    {
+    char * outputName2 = argv[4];
+    generatorFilter2->SetDEMDirectoryPath(folderPath);
+    generatorFilter2->SetOutputParametersFromImage(reader->GetOutput());
+    generatorFilter2->InstanciateTransform();
 
-  otb::StandardFilterWatcher watcher2(generatorFilter2, "DEM to image generator");
+    extract2->SetInput(generatorFilter2->GetOutput());
+    extract2->SetSizeX(atoi(argv[7]));
+    extract2->SetSizeY(atoi(argv[8]));
+    extract2->SetStartX(atoi(argv[5]));
+    extract2->SetStartY(atoi(argv[6]));
 
-  extract2->SetInput(generatorFilter2->GetOutput());
-  extract2->SetSizeX(atoi(argv[7]));
-  extract2->SetSizeY(atoi(argv[8]));
-  extract2->SetStartX(atoi(argv[5]));
-  extract2->SetStartY(atoi(argv[6]));
+    writer2->SetFileName(outputName2);
+    writer2->SetInput(extract2->GetOutput());
 
-  writer2->SetFileName(outputName2);
-  writer2->SetInput(extract2->GetOutput());
-
-  writer2->Update();
+    writer2->Update();
+    }
 
   return EXIT_SUCCESS;
 }
