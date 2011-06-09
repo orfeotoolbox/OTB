f97065e6fb09fec40595db3b7c0ef20206fe9539
Otmane Lahlou <otmane.lahlou@c-s.fr>
2011-06-09 19:19:59 +0200

    ENH : Address #bug 318 : Add option -print to be able to see the OSMKey/Value       present in the OSM xml downloaded

--- Utils/otbOSMDownloader.cxx
+++ Utils/otbOSMDownloader.cxx
@@ -38,20 +38,47 @@ int OSMDownloader::Describe(ApplicationDescriptor* descriptor)
   descriptor->AddOption("InputImage", "Support to estimate the models on",
                         "in", 1, true, ApplicationDescriptor::InputImage);
   descriptor->AddOption("OSMKey", "OSM key (highway, building...)",
-                        "key", 1, true, ApplicationDescriptor::String);
+                        "key", 1, false, ApplicationDescriptor::String);
   descriptor->AddOption("OSMValue", "OSM Value (motorway, footway...)",
                         "val", 1, false, ApplicationDescriptor::String);
   descriptor->AddOption("DEMDirectory", "DEM directory",
                         "dem", 1, false, ApplicationDescriptor::DirectoryName);
   descriptor->AddOption("Output", "OutputVectorData",
                         "out", 1, false, ApplicationDescriptor::FileName);
+  
+  std::ostringstream oss;
+  oss << "Print the key/value classes available for the bounding box of the input image "<<std::endl;
+  oss << "\t\t\t\t  ** If not used : Note that the options OSMKey (-key) and Output (-out) become mandatory";
+  descriptor->AddOption("PrintClasses", oss.str().c_str(), "print", 0, false, ApplicationDescriptor::Boolean);
+  
   return EXIT_SUCCESS;
 }
 
-
 int OSMDownloader::Execute(otb::ApplicationOptionsResult* parseResult)
 {
+  // CASE:  when the -print option is not required and the User
+  // does not set the option OSMKey or the option Output or does not
+  // set both of them
+  if (!parseResult->IsOptionPresent("PrintClasses"))
+    {
+    if( !parseResult->IsOptionPresent("Output") && !parseResult->IsOptionPresent("OSMKey"))
+      {
+      itkExceptionMacro(<<"Missing Options : Output (-out) and OSMKey (-key)");
+      }
+
+    if(!parseResult->IsOptionPresent("OSMKey") )
+      {
+      itkExceptionMacro(<<"Missing Option : OSMKey (-key)");
+      }
+    
+    if( !parseResult->IsOptionPresent("Output"))
+      {
+      itkExceptionMacro(<<"Missing Option : Output (-out)");
+      }    
+    }
+
   typedef otb::OSMDataToVectorDataGenerator           VectorDataProviderType;
+  typedef VectorDataProviderType::KeyMapType          KeyMapType;
   typedef VectorDataProviderType::VectorDataType      VectorDataType;
   typedef VectorDataType::ValuePrecisionType          PrecisionType;
   typedef VectorDataType::PrecisionType               CoordRepType;
@@ -63,7 +90,6 @@ int OSMDownloader::Execute(otb::ApplicationOptionsResult* parseResult)
                                                       EnvelopeFilterType;
   typedef otb::VectorDataProperties<VectorDataType>   VectorDataPropertiesType;
   typedef otb::VectorDataFileWriter<VectorDataType>   VectorDataWriterType;
-
 
   //Instantiate
   ImageReaderType::Pointer                 imgReader = ImageReaderType::New();
@@ -78,7 +104,7 @@ int OSMDownloader::Execute(otb::ApplicationOptionsResult* parseResult)
 
   //Generate the envelope
   envelopeFilter->SetInput(imgReader->GetOutput()); //->Output in WGS84
-  if (parseResult->IsOptionPresent("CriterionFormula"))
+  if (parseResult->IsOptionPresent("DEMDirectory"))
     {
     envelopeFilter->SetDEMDirectory(parseResult->GetParameterString("DEMDirectory"));
     }
@@ -94,13 +120,6 @@ int OSMDownloader::Execute(otb::ApplicationOptionsResult* parseResult)
   east  = vdProperties->GetBoundingRegion().GetIndex()[0]
            + vdProperties->GetBoundingRegion().GetSize()[0];
   west  = vdProperties->GetBoundingRegion().GetIndex()[0];
-
-  /*
-  std::cout << "north : " << north << std::endl;
-  std::cout << "south : " << south << std::endl;
-  std::cout << "east  : " << east  << std::endl;
-  std::cout << "west  : " << west  << std::endl;
-  */
 
   vdOSMGenerator->SetNorth(north);
   vdOSMGenerator->SetSouth(south);
@@ -118,17 +137,41 @@ int OSMDownloader::Execute(otb::ApplicationOptionsResult* parseResult)
     return EXIT_FAILURE;
   }
 
-  std::cout << vdOSMGenerator->GetVectorDataByName(parseResult->GetParameterString("OSMKey"))->Size()-3
-      << " vector elements retrieved" << std::endl;
+  // If the user wants to print the Key/Values present in the XML file
+  // downloaded  :
+  if (parseResult->IsOptionPresent("PrintClasses"))
+    {
+    // Print the classes
+    KeyMapType  keymap = vdOSMGenerator->GetKeysMap();
 
+    KeyMapType::iterator  it = keymap.begin();
+
+    while(it != keymap.end())
+      {
+      std::cout << " Key : "<< (*it).first<< " value : ";
+      for(unsigned int i = 0; i < (*it).second.size(); i++)
+        std::cout<< (*it).second[i]<< " ";
+      std::cout<< std::endl;
+      ++it;
+      }
+    return EXIT_SUCCESS;
+    }
+
+  // Get the VectorData By name
   if (parseResult->IsOptionPresent("OSMValue"))
     {
     vdWriter->SetInput(vdOSMGenerator->GetVectorDataByName(parseResult->GetParameterString("OSMKey"),
                                                            parseResult->GetParameterString("OSMValue")));
+
+    std::cout << vdOSMGenerator->GetVectorDataByName(parseResult->GetParameterString("OSMKey"),
+                                                     parseResult->GetParameterString("OSMValue"))->Size()-3
+              << " elements retrieved" << std::endl;
     }
   else
     {
     vdWriter->SetInput(vdOSMGenerator->GetVectorDataByName(parseResult->GetParameterString("OSMKey")));
+    std::cout << vdOSMGenerator->GetVectorDataByName(parseResult->GetParameterString("OSMKey"))->Size()-3
+              << " elements retrieved" << std::endl;
     }
 
   vdWriter->SetFileName(parseResult->GetParameterString("Output"));
@@ -136,5 +179,4 @@ int OSMDownloader::Execute(otb::ApplicationOptionsResult* parseResult)
 
   return EXIT_SUCCESS;
 }
-
 }
