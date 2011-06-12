1d2b2852d68d3c799dd5015bfdc6ef142dc9ee0a
Julien Malik <julien.malik@c-s.fr>
2011-06-12 10:25:00 +0200

    ENH: improve build system

---
+++ CMake/WrapperMacros.cmake
@@ -0,0 +1,17 @@
+include(ParseArguments)
+
+macro(OTB_CREATE_APPLICATION)
+  PARSE_ARGUMENTS(
+      APPLICATION
+      "NAME;SOURCES;INCLUDE_DIRS;LINK_LIBRARIES"
+      ""
+      ${ARGN})
+
+   set( APPLICATION_TARGET_NAME otbapp_${APPLICATION_NAME} )
+
+   include_directories(${CMAKE_SOURCE_DIR}/Code/Core)
+   include_directories(${APPLICATION_INCLUDE_DIRS})
+   add_library(${APPLICATION_TARGET_NAME} MODULE ${APPLICATION_SOURCES})
+   target_link_libraries(${APPLICATION_TARGET_NAME} OTBWrapperCore ${APPLICATION_LINK_LIBRARIES})
+   set_target_properties(${APPLICATION_TARGET_NAME} PROPERTIES PREFIX "")
+endmacro(OTB_CREATE_APPLICATION)
