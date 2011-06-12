42a30bb33696bc531b2b25103f9568fd56aa2d9e
Julien Malik <julien.malik@c-s.fr>
2011-06-12 09:32:50 +0200

    ENH: put Smoothing in a single file

--- Example/Smoothing/otbSmoothing.cxx
+++ Example/Smoothing/otbSmoothing.cxx
@@ -15,7 +15,7 @@
  PURPOSE.  See the above copyright notices for more information.
 
  =========================================================================*/
-#include "otbSmoothing.h"
+#include "otbWrapperApplication.h"
 #include "otbWrapperApplicationFactory.h"
 #include "otbWrapperNumericalParameter.h"
 
@@ -41,109 +41,129 @@ enum
 
 typedef otb::Image<VectorImageType::InternalPixelType, 2>  ImageType;
 
-
-Smoothing::Smoothing()
+class Smoothing : public Application
 {
-  this->SetName("Smoothing");
-  this->SetDescription("Apply a smoothing filter to an image");
-}
+public:
+  /** Standard class typedefs. */
+  typedef Smoothing                      Self;
+  typedef Application                   Superclass;
+  typedef itk::SmartPointer<Self>       Pointer;
+  typedef itk::SmartPointer<const Self> ConstPointer;
 
-Smoothing::~Smoothing()
-{
-}
+  /** Standard macro */
+  itkNewMacro(Self);
 
-void Smoothing::DoCreateParameters()
-{
-  AddParameter(ParameterType_InputImage,  "in",   "Input Image");
-  AddParameter(ParameterType_OutputImage, "out",  "Output Image");
+  itkTypeMacro(Self, otb::Application);
 
-  AddParameter(ParameterType_Choice,      "type", "Smoothing Type");
+protected:
+  Smoothing()
+  {
+    this->SetName("Smoothing");
+    this->SetDescription("Apply a smoothing filter to an image");
+  }
 
-  AddChoice("type.mean",     "Mean");
-  AddParameter(ParameterType_Radius, "type.mean.radius", "Radius");
-  SetParameterInt("type.mean.radius", 2);
+  virtual ~Smoothing()
+  {
+    std::cout << "~Smoothing" << std::endl;
+  }
 
-  AddChoice("type.gaussian", "Gaussian");
-  AddParameter(ParameterType_Radius, "type.gaussian.radius", "Radius");
-  SetParameterInt("type.gaussian.radius", 2);
+  void DoCreateParameters()
+  {
+    AddParameter(ParameterType_InputImage,  "in",   "Input Image");
+    AddParameter(ParameterType_OutputImage, "out",  "Output Image");
 
-  AddChoice("type.anidif",   "Anisotropic Diffusion");
-  AddParameter(ParameterType_Float,  "type.anidif.timestep", "Time Step");
-  AddParameter(ParameterType_Int,  "type.anidif.nbiter", "Nb Iterations");
-  SetParameterFloat("type.anidif.timestep",   0.125);
-  SetParameterInt("type.anidif.nbiter",     10);
+    AddParameter(ParameterType_Choice,      "type", "Smoothing Type");
 
-  SetParameterInt("type", 2);
-}
+    AddChoice("type.mean",     "Mean");
+    AddParameter(ParameterType_Radius, "type.mean.radius", "Radius");
+    SetParameterInt("type.mean.radius", 2);
 
-void Smoothing::DoUpdateParameters()
-{
-  // Nothing to do here : all parameters are independent
-}
+    AddChoice("type.gaussian", "Gaussian");
+    AddParameter(ParameterType_Radius, "type.gaussian.radius", "Radius");
+    SetParameterInt("type.gaussian.radius", 2);
 
-void Smoothing::DoExecute()
-{
-  VectorImageType::Pointer inImage = this->GetParameterImage("in");
+    AddChoice("type.anidif",   "Anisotropic Diffusion");
+    AddParameter(ParameterType_Float,  "type.anidif.timestep", "Time Step");
+    AddParameter(ParameterType_Int,  "type.anidif.nbiter", "Nb Iterations");
+    SetParameterFloat("type.anidif.timestep",   0.125);
+    SetParameterInt("type.anidif.nbiter",     10);
 
-  switch (this->GetParameterInt("type"))
-    {
-    case Smoothing_Mean:
+    SetParameterInt("type", 2);
+  }
+
+  void DoUpdateParameters()
+  {
+    // Nothing to do here : all parameters are independent
+  }
+
+  void DoExecute()
+  {
+    VectorImageType::Pointer inImage = this->GetParameterImage("in");
+
+    switch (this->GetParameterInt("type"))
       {
-      typedef itk::MeanImageFilter<ImageType, ImageType>         MeanFilterType;
-      typedef otb::PerBandVectorImageFilter<VectorImageType, VectorImageType, MeanFilterType>
-        PerBandMeanFilterType;
+      case Smoothing_Mean:
+        {
+        typedef itk::MeanImageFilter<ImageType, ImageType>         MeanFilterType;
+        typedef otb::PerBandVectorImageFilter<VectorImageType, VectorImageType, MeanFilterType>
+          PerBandMeanFilterType;
 
-      PerBandMeanFilterType::Pointer perBand
-        = PerBandMeanFilterType::New();
-      perBand->SetInput(inImage);
+        PerBandMeanFilterType::Pointer perBand
+          = PerBandMeanFilterType::New();
+        perBand->SetInput(inImage);
 
-      MeanFilterType::InputSizeType radius;
-      radius.Fill( this->GetParameterInt("type.mean.radius") );
-      perBand->GetFilter()->SetRadius(radius);
-      ref = perBand;
-      this->SetParameterOutputImage("out", perBand->GetOutput());
+        MeanFilterType::InputSizeType radius;
+        radius.Fill( this->GetParameterInt("type.mean.radius") );
+        perBand->GetFilter()->SetRadius(radius);
+        m_FilterRef = perBand;
+        this->SetParameterOutputImage("out", perBand->GetOutput());
+        }
+        break;
+      case Smoothing_Gaussian:
+        {
+        typedef itk::DiscreteGaussianImageFilter<ImageType, ImageType>  DiscreteGaussianFilterType;
+        typedef otb::PerBandVectorImageFilter<VectorImageType, VectorImageType, DiscreteGaussianFilterType>
+          PerBandDiscreteGaussianFilterType;
+
+        PerBandDiscreteGaussianFilterType::Pointer perBand
+          = PerBandDiscreteGaussianFilterType::New();
+        perBand->SetInput(inImage);
+
+        int radius = this->GetParameterInt("type.gaussian.radius");
+        double variance = static_cast<double>(radius) * radius;
+        perBand->GetFilter()->SetVariance(variance);
+        m_FilterRef = perBand;
+        this->SetParameterOutputImage("out", perBand->GetOutput());
+        }
+        break;
+      case Smoothing_Anisotropic:
+        {
+        typedef itk::GradientAnisotropicDiffusionImageFilter<ImageType, ImageType>  GradientAnisotropicDiffusionFilterType;
+        typedef otb::PerBandVectorImageFilter<VectorImageType, VectorImageType, GradientAnisotropicDiffusionFilterType>
+          PerBandGradientAnisotropicDiffusionFilterType;
+
+        PerBandGradientAnisotropicDiffusionFilterType::Pointer perBand
+          = PerBandGradientAnisotropicDiffusionFilterType::New();
+        perBand->SetInput(inImage);
+
+        int aniDifNbIter = this->GetParameterInt("type.anidif.nbiter");
+        perBand->GetFilter()->SetNumberOfIterations(static_cast<unsigned int>(aniDifNbIter));
+
+        float aniDifTimeStep = this->GetParameterFloat("type.anidif.timestep");
+        perBand->GetFilter()->SetTimeStep(static_cast<double>(aniDifTimeStep));
+        // perBand->GetFilter()->SetConductanceParameter()
+        perBand->UpdateOutputInformation();
+        m_FilterRef = perBand;
+        this->SetParameterOutputImage("out", perBand->GetOutput());
+        }
+        break;
       }
-      break;
-    case Smoothing_Gaussian:
-      {
-      typedef itk::DiscreteGaussianImageFilter<ImageType, ImageType>  DiscreteGaussianFilterType;
-      typedef otb::PerBandVectorImageFilter<VectorImageType, VectorImageType, DiscreteGaussianFilterType>
-        PerBandDiscreteGaussianFilterType;
+  }
+private:
+  itk::LightObject::Pointer m_FilterRef;
+};
 
-      PerBandDiscreteGaussianFilterType::Pointer perBand
-        = PerBandDiscreteGaussianFilterType::New();
-      perBand->SetInput(inImage);
 
-      int radius = this->GetParameterInt("type.gaussian.radius");
-      double variance = static_cast<double>(radius) * radius;
-      perBand->GetFilter()->SetVariance(variance);
-      ref = perBand;
-      this->SetParameterOutputImage("out", perBand->GetOutput());
-      }
-      break;
-    case Smoothing_Anisotropic:
-      {
-      typedef itk::GradientAnisotropicDiffusionImageFilter<ImageType, ImageType>  GradientAnisotropicDiffusionFilterType;
-      typedef otb::PerBandVectorImageFilter<VectorImageType, VectorImageType, GradientAnisotropicDiffusionFilterType>
-        PerBandGradientAnisotropicDiffusionFilterType;
-
-      PerBandGradientAnisotropicDiffusionFilterType::Pointer perBand
-        = PerBandGradientAnisotropicDiffusionFilterType::New();
-      perBand->SetInput(inImage);
-
-      int aniDifNbIter = this->GetParameterInt("type.anidif.nbiter");
-      perBand->GetFilter()->SetNumberOfIterations(static_cast<unsigned int>(aniDifNbIter));
-
-      float aniDifTimeStep = this->GetParameterFloat("type.anidif.timestep");
-      perBand->GetFilter()->SetTimeStep(static_cast<double>(aniDifTimeStep));
-      // perBand->GetFilter()->SetConductanceParameter()
-      perBand->UpdateOutputInformation();
-      ref = perBand;
-      this->SetParameterOutputImage("out", perBand->GetOutput());
-      }
-      break;
-    }
-}
 
 }
 }
