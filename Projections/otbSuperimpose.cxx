76cd4502841f5deab953e999bc86bbd108614842
Julien Malik <julien.malik@c-s.fr>
2011-06-14 16:17:14 +0200

    ENH: add possibility to set DEM from the configuration file

--- Projections/otbSuperimpose.cxx
+++ Projections/otbSuperimpose.cxx
@@ -74,10 +74,17 @@ int Superimpose::Execute(otb::ApplicationOptionsResult* parseResult)
     InterpolatorType::Pointer interpolator = InterpolatorType::New();
     resampler->SetInterpolator(interpolator);
     
-    // Add DEM if any
+    // Configure DEM directory
     if(parseResult->IsOptionPresent("DEMDirectory"))
       {
       resampler->SetDEMDirectory(parseResult->GetParameterString("DEMDirectory", 0));
+      }
+    else
+      {
+      if ( otb::ConfigurationFile::GetInstance()->IsValid() )
+        {
+        resampler->SetDEMDirectory(otb::ConfigurationFile::GetInstance()->GetDEMDirectory());
+        }
       }
     
     // Set up output image informations
