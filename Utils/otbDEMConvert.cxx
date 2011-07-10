9c67107ab44ab94d89eca475ae10e186d9fd3615
Emmanuel Christophe <emmanuel.christophe@orfeo-toolbox.org>
2011-07-09 22:52:55 -0700

    ENH: use the DEMConvert code in adapter

--- Utils/otbDEMConvert.cxx
+++ Utils/otbDEMConvert.cxx
@@ -22,22 +22,7 @@
 #include "otbVectorImage.h"
 #include "otbImageFileReader.h"
 #include "otbStreamingImageFileWriter.h"
-
-// OSSIM include
-#include <ossim/base/ossimKeywordNames.h>
-#include <ossim/base/ossimStdOutProgress.h>
-#include <ossim/base/ossimFilename.h>
-#include <ossim/base/ossimKeywordlist.h>
-#include <ossim/imaging/ossimJpegWriter.h>
-#include <ossim/imaging/ossimImageHandler.h>
-#include <ossim/imaging/ossimImageSource.h>
-#include <ossim/imaging/ossimImageHandlerRegistry.h>
-#include <ossim/imaging/ossimImageWriterFactoryRegistry.h>
-#include <ossim/imaging/ossimImageWriterFactory.h>
-#include <ossim/imaging/ossimImageFileWriter.h>
-#include <ossim/imaging/ossimCacheTileSource.h>
-#include <ossim/imaging/ossimBandSelector.h>
-#include <ossim/imaging/ossimCibCadrgTileSource.h>
+#include "otbDEMConvertAdapter.h"
 
 namespace otb
 {
@@ -45,7 +30,7 @@ namespace otb
 template<typename TPixelType>
 int generic_convert_to_tif(otb::ApplicationOptionsResult* parseResult, std::string tempFilename)
 {
-  typedef otb::VectorImage<TPixelType>              ImageType;
+  typedef otb::VectorImage<TPixelType>             ImageType;
   typedef otb::ImageFileReader<ImageType>          ReaderType;
   typedef otb::StreamingImageFileWriter<ImageType> WriterType;
 
@@ -55,7 +40,7 @@ int generic_convert_to_tif(otb::ApplicationOptionsResult* parseResult, std::stri
   reader->SetFileName(parseResult->GetInputImage().c_str());
   writer->SetFileName(tempFilename);
 
-  writer->SetInput(reader->GetOutput());
+  writer->SetInput(reader->GetOutput());\
   writer->Update();
 
   return EXIT_SUCCESS;
@@ -71,6 +56,10 @@ int DEMConvert::Describe(ApplicationDescriptor* descriptor)
   descriptor->AddOption("KeepTif", "Keep the temporary generate tif file.","ktif", 0, false, ApplicationDescriptor::Boolean);
   
   return EXIT_SUCCESS;
+}
+
+int temporaryMethod(std::string tempFilename, std::string output)
+{
 }
 
 
@@ -140,93 +129,13 @@ int DEMConvert::Execute(otb::ApplicationOptionsResult* parseResult)
     }
   else
     {
-    itkExceptionMacro("This appication doesn't support image pixel type " << componentTypeInfo);
+    itkExceptionMacro("This application doesn't support image pixel type " << componentTypeInfo);
     return EXIT_FAILURE;
     }
+  std::string output = parseResult->GetParameterString("OutputPath")+".ras";
 
-
-  // Keyword list to initialize image writers with.
-  ossimKeywordlist kwl;
-  const char* PREFIX = "imagewriter.";
-  // Define the output file type
-  const ossimString output_type("general_raster_bsq_envi");
-  kwl.add(PREFIX, ossimKeywordNames::TYPE_KW, output_type.c_str(), true);
-    
-  // Get an image handler for the input file.
-  ossimRefPtr<ossimImageHandler> ih = ossimImageHandlerRegistry::instance()->open(ossimString(tempFilename));
-   
-  // Initialize the
-  if (ih->getErrorStatus() == ossimErrorCodes::OSSIM_ERROR)
-    {
-    itkExceptionMacro("Error reading image:  " << input_file << "Exiting application...");
-    return EXIT_FAILURE;
-    }
-   
-  ih->initialize();
-   
-  ossimRefPtr<ossimImageSource> source = ih.get();
-  ossimRefPtr<ossimBandSelector> bs = 0;
-   
-   
-  // Get the image rectangle for the rrLevel selected.
-  ossimIrect output_rect;
-  output_rect = source->getBoundingRect(0);
-
-  ossimRefPtr<ossimImageFileWriter> writer =
-    ossimImageWriterFactoryRegistry::instance()->createWriter(kwl, PREFIX);
-
-  writer->connectMyInputTo(0, source.get());
-  writer->open(ossimFilename(parseResult->GetParameterString("OutputPath"))+".ras");
-   
-  // Add a listener to get percent complete.
-  ossimStdOutProgress prog(0, true);
-  writer->addListener(&prog);
-
-  if (writer->getErrorStatus() == ossimErrorCodes::OSSIM_OK)
-    {
-    if( (ih->getOutputScalarType() != OSSIM_UCHAR) &&
-        (PTR_CAST(ossimJpegWriter, writer.get()) ) )
-      {
-      writer->setScaleToEightBitFlag(true);
-      }
-
-    ossimRefPtr<ossimCacheTileSource> cache = new ossimCacheTileSource;
-    ossimIpt tileWidthHeight(ih->getImageTileWidth(),
-                             ih->getImageTileHeight());
-    // only use the cache if its stripped
-    if(static_cast<ossim_uint32>(tileWidthHeight.x) ==
-       ih->getBoundingRect().width())
-      {
-      cache->connectMyInputTo(0, source.get());
-      cache->setTileSize(tileWidthHeight);
-      writer->connectMyInputTo(0, cache.get());
-      }
-    else
-      {
-      writer->connectMyInputTo(0, source.get());
-      }
-    writer->initialize();
-    writer->setAreaOfInterest(output_rect); // Set the output rectangle.
-      
-    try
-      {
-      writer->execute();
-      }
-    catch(std::exception& e)
-      {
-      std::cerr << "std::exception  thrown:" << std::endl;
-      std::cerr << e.what() <<  std::endl;
-      itkExceptionMacro("Error occurs writing the ouput image...");
-      return EXIT_FAILURE;
-      }
-      
-    }
-  else
-    {
-    itkExceptionMacro("Error detected in the image writer...");
-    return EXIT_FAILURE;
-    }
-
+  DEMConvertAdapter::Pointer DEMConverter = DEMConvertAdapter::New();
+  return DEMConverter->Convert(tempFilename, output);
 
   if ( parseResult->IsOptionPresent("KeepTif") == false)
     {
