820b7943a9c2ea03940610c42dd17e9b36d71a6e
Jonathan Guinet <jonathan.guinet@c-s.fr>
2011-07-12 16:23:18 +0200

    ENH testing repository refactoring, RSR Simulation test

---
+++ Testing/Code/Simulation/otbReduceSpectralResponse.cxx
@@ -0,0 +1,77 @@
+/*=========================================================================
+
+  Program:   ORFEO Toolbox
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
+
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#include "itkExceptionObject.h"
+#include "otbMacro.h"
+
+#include "otbSpectralResponse.h"
+#include "otbSatelliteRSR.h"
+#include "otbReduceSpectralResponse.h"
+
+int otbReduceSpectralResponse(int argc, char * argv[])
+{
+  if ( argc!=4 )
+  {
+    std::cout << argv[0] << std::endl << "\t" << "<Spectral_response_filename>"<< "\t" << "<RSR_filename>"<< "\t" << "<Nb total satelite band>"<< std::endl ;
+    return EXIT_FAILURE;
+  }
+  
+  typedef otb::SpectralResponse< double,double>  ResponseType;
+  typedef ResponseType::Pointer  ResponsePointerType;
+  
+  typedef otb::SatelliteRSR< double,double>  SatRSRType;
+  typedef SatRSRType::Pointer  SatRSRPointerType;
+  
+  typedef otb::ReduceSpectralResponse < ResponseType,SatRSRType>  ReduceResponseType;
+  typedef ReduceResponseType::Pointer  ReduceResponseTypePointerType;
+  
+  const std::string spectreFile(argv[1]);
+  const std::string RSRfile(argv[2]);
+  unsigned int nbBand = atoi(argv[3]);
+  
+  //Instantiation
+  ResponsePointerType  mySpectralResponse=ResponseType::New();
+  /** Load the spectral response file*/
+  mySpectralResponse->Load(spectreFile, 100.0);
+  
+  SatRSRPointerType  myRSR=SatRSRType::New();
+  /** Set the satelite number of bands */
+  myRSR->SetNbBands(nbBand);
+  /** Load the satelite response file*/
+  myRSR->Load(RSRfile);
+  
+  ReduceResponseTypePointerType  myReduceResponse=ReduceResponseType::New();
+  //Instantiation
+  //ResponsePointerType  myResponse=ResponseType::New();
+  /** Load the satelite response in the simulator */
+  myReduceResponse->SetInputSatRSR(myRSR);
+  /** Load the spectral response of the object in the simulator*/
+  myReduceResponse->SetInputSpectralResponse(mySpectralResponse);
+  //Load file into vector
+  
+  /** Print the input spectral response*/
+  //std::cout << mySpectralResponse << std::endl;
+  //itk::Indent ind;
+  /** Print the input Satelite RSR*/
+  //std::cout << myRSR << std::endl;
+  //myResponse->PrintSelf(std::cout, ind);
+  /** Compute the RSR reduce spectral response*/
+  myReduceResponse->CalculateResponse();
+  /** Print the Reduce SR*/
+  std::cout << myReduceResponse << std::endl;
+  return EXIT_SUCCESS;
+}
---
+++ Testing/Code/Simulation/otbSatelliteRSR.cxx
@@ -0,0 +1,53 @@
+/*=========================================================================
+
+  Program:   ORFEO Toolbox
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
+
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#include "itkExceptionObject.h"
+#include "otbMacro.h"
+
+#include "otbSatelliteRSR.h"
+
+int otbSatelliteRSR(int argc, char * argv[])
+{
+  typedef otb::SatelliteRSR< double,double>  ResponseType;
+  typedef ResponseType::Pointer  ResponsePointerType;
+  
+  if ( argc!=5 )
+  {
+    std::cout << argv[0] << std::endl << "\t" << "<RSR_filename>"<< "\t" << "<Nb total band>"<< "\t" << "<lambda>"<< "\t" << "<band>"<< std::endl ;
+    return EXIT_FAILURE;
+  }
+  //Instantiation
+  ResponsePointerType  myResponse=ResponseType::New();
+  
+  myResponse->SetNbBands(atoi(argv[2]));
+  //Load file into vector
+  const std::string file(argv[1]);
+  myResponse->Load(file);
+  //itk::Indent ind;
+  unsigned int NumBand = atoi(argv[4]);
+  double lambda = atof(argv[3]);
+
+  std::cout << myResponse <<std::endl;
+  std::cout << "RSR("<<lambda<<","<< NumBand <<")= " << (*myResponse)(lambda,NumBand) << std::endl;
+//   for(unsigned int i=0;i<myResponse->GetRSR()[NumBand]->GetResponse().size();i++)
+//   {
+//      std::cout << "RSR("<<myResponse->GetRSR()[NumBand]->GetResponse()[i]->first<<","<< NumBand <<")= " << myResponse->GetRSR()[NumBand]->GetResponse()[i]->second << std::endl;
+//   }
+  
+  
+  return EXIT_SUCCESS;
+}
