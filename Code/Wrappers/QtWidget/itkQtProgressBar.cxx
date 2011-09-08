2c2cb84e12a18e6630fca66c1d615d3c4d0c3f4e
Otmane Lahlou <otmane.lahlou@c-s.fr>
2011-09-08 17:03:29 +0200

    ENH : Add class from ITKApp to manage progressEvents of ProcessObject filters

---
+++ Code/Wrappers/QtWidget/itkQtProgressBar.cxx
@@ -0,0 +1,87 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkQtProgressBar.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#include "itkProcessObject.h"
+#include "itkQtProgressBar.h"
+
+
+namespace itk {
+
+
+/** Constructor */
+QtProgressBar::QtProgressBar( QWidget *parent):QProgressBar(parent)
+{
+  m_RedrawCommand = RedrawCommandType::New();
+  m_RedrawCommand->SetCallbackFunction( this, &QtProgressBar::ProcessEvent );
+  m_RedrawCommand->SetCallbackFunction( this, &QtProgressBar::ConstProcessEvent );
+
+  this->setMaximum( 100 );
+  this->reset();
+}
+
+
+  
+/** Get Command */
+QtProgressBar::RedrawCommandType * 
+QtProgressBar::GetRedrawCommand( void ) const
+{
+  return m_RedrawCommand.GetPointer();
+}
+
+/** Manage a Progress event */
+void 
+QtProgressBar::ProcessEvent( itk::Object * caller, 
+                           const itk::EventObject & event )
+{
+  if( typeid( itk::ProgressEvent )   ==  typeid( event ) )
+    {
+    ::itk::ProcessObject::Pointer  process = 
+             dynamic_cast< itk::ProcessObject *>( caller );
+
+    const int value = static_cast<int>( 
+                        process->GetProgress() * this->maximum() );
+
+    emit SetValueChanged( value );
+    }
+}
+
+void 
+QtProgressBar::ConstProcessEvent( const itk::Object * caller, 
+                                const itk::EventObject & event )
+{
+  if( typeid( itk::ProgressEvent )   ==  typeid( event ) ) 
+    {
+    itk::ProcessObject::ConstPointer  process = 
+              dynamic_cast< const itk::ProcessObject *>( caller );
+
+    const int value = static_cast<int>( 
+                        process->GetProgress() * this->maximum() );
+
+    emit SetValueChanged( value );
+    }
+}
+
+/** Manage a Progress event */
+void 
+QtProgressBar::Observe( itk::Object *caller )
+{
+  caller->AddObserver(  itk::ProgressEvent(), m_RedrawCommand.GetPointer() );
+}
+
+} // end namespace fltk
+
+
