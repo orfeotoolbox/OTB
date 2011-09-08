8e2f3ee1012ed59caa7a24a1680627d8d4f862f9
Otmane Lahlou <otmane.lahlou@c-s.fr>
2011-09-08 18:08:14 +0200

    ENH : Use the virtual writer to be able to get the progressEvents

--- Example/otbRescale.cxx
+++ Example/otbRescale.cxx
@@ -53,7 +53,7 @@ private:
     SetDescription("Rescale the image between two given values.");
     m_RescaleFilter = RescaleImageFilterType::New();
     m_MinMaxFilter = MinMaxFilterType::New();
-    this->AddInternalProcess( m_MinMaxFilter, "Min/Max computing" );
+    this->AddInternalProcess( m_MinMaxFilter->GetStreamer(), "Min/Max computing" );
   }
 
   virtual ~Rescale()
