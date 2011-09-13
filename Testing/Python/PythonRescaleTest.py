b1b800c3bbe3d48b7a19cdecbe29cbc4f0e20398
Julien Malik <julien.malik@c-s.fr>
2011-09-13 15:19:35 +0200

    TEST: make Rescale test pass

--- Testing/Python/PythonRescaleTest.py
+++ Testing/Python/PythonRescaleTest.py
@@ -4,20 +4,13 @@
 #  Example on the use of the Rescale 
 #
 from sys import argv
-import PythonApplication
-#import otbApplication as otb
+import otbApplication as otb
 
-app = PythonApplication.PythonApplicationClass("Rescale")
-
-print app.m_App.GetParametersKeys()
-app.SetParameterString("in", argv[1])
-
+app = otb.Registry.CreateApplication("Rescale")
 
 app.SetParameterString("in", argv[1])
-
 app.SetParameterFloat("outmin", 1)
 app.SetParameterFloat("outmax", 100)
 app.SetParameterString("out", argv[2] + ".tif")
+
 app.ExecuteAndWriteOutput()
-  
-print dir(app)
