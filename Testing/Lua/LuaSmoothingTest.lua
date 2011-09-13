f96a3051928e4749f6a639879b32d7d73f4ae767
Julien Malik <julien.malik@c-s.fr>
2011-09-13 16:06:17 +0200

    ENH: remove Lua tests (not working, not needed)

--- Testing/Lua/LuaSmoothingTest.lua
+++
@@ -1,9 +0,0 @@
-print ( "Lua test begin" )
-
-r = otbApplication.Registry
-apps = r:GetAvailableApplications()
-
-print ( "otbApplication.Registry:GetAvailableApplications OK" )
-print ( "apps" )
-print ( apps )
-os.exit ( 0 )
