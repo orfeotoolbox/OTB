ccd63b77f89a6df90db65915f2ffef22a82b877b
Julien Malik <julien.malik@c-s.fr>
2011-09-14 18:35:43 +0200

    ENH: add ReadImageInfo application, outputs info through the ITK log system

---
+++ Applications/Util/otbReadImageInfo.cxx
@@ -0,0 +1,79 @@
+/*=========================================================================
+
+ Program:   ORFEO Toolbox
+ Language:  C++
+ Date:      $Date$
+ Version:   $Revision$
+
+
+ Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+ See OTBCopyright.txt for details.
+
+
+ This software is distributed WITHOUT ANY WARRANTY; without even
+ the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ PURPOSE.  See the above copyright notices for more information.
+
+ =========================================================================*/
+#include "otbWrapperApplication.h"
+#include "otbWrapperApplicationFactory.h"
+
+namespace otb
+{
+namespace Wrapper
+{
+
+class ReadImageInfo : public Application
+{
+public:
+  /** Standard class typedefs. */
+  typedef ReadImageInfo                      Self;
+  typedef Application                   Superclass;
+  typedef itk::SmartPointer<Self>       Pointer;
+  typedef itk::SmartPointer<const Self> ConstPointer;
+
+  /** Standard macro */
+  itkNewMacro(Self);
+
+  itkTypeMacro(ReadImageInfo, otb::Application);
+
+private:
+  ReadImageInfo()
+  {
+    SetName("ReadImageInfo");
+    SetDescription("Get information about the image");
+  }
+
+  virtual ~ReadImageInfo()
+  {
+  }
+
+  void DoCreateParameters()
+  {
+    AddParameter(ParameterType_InputImage,  "in",   "Input Image");
+  }
+
+  void DoUpdateParameters()
+  {
+    // Nothing to do here : all parameters are independent
+  }
+
+  void DoExecute()
+  {
+    FloatVectorImageType::Pointer inImage = GetParameterImage("in");
+
+    std::ostringstream oss;
+    oss << std::setprecision(15);
+    oss << inImage;
+    oss << std::endl;
+
+    this->GetLogger()->Info( oss.str() );
+  }
+
+};
+
+}
+}
+
+OTB_APPLICATION_EXPORT(otb::Wrapper::ReadImageInfo)
+
--- Testing/Applications/otbWrapperExampleTests.cxx
+++ Testing/Applications/otbWrapperExampleTests.cxx
@@ -28,5 +28,6 @@ void RegisterTests()
 {
   REGISTER_TEST(otbSmoothingTest);
   REGISTER_TEST(otbRescaleTest);
+  REGISTER_TEST(otbReadImageInfoTest);
 }
 
