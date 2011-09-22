dcf0e96686113d1e818313b83b873ff5bca1517c
Julien Malik <julien.malik@c-s.fr>
2011-09-22 15:33:19 +0200

    TEST: fix problem with quotes in EXECUTE_PROCESS

--- Testing/Fa/StripOTBData.cmake
+++ Testing/Fa/StripOTBData.cmake
@@ -5,7 +5,7 @@ IF(NOT Mercurial_HG_EXECUTABLE)
   MESSAGE(FATAL_ERROR "mercurial executable not found")
 ENDIF()
 
-EXECUTE_PROCESS(COMMAND ${Mercurial_HG_EXECUTABLE} --config 'extensions.mq=' strip 47f468703345
+EXECUTE_PROCESS(COMMAND ${Mercurial_HG_EXECUTABLE} --config extensions.mq= strip 47f468703345
                 WORKING_DIRECTORY ${OTB_DATA_ROOT}
                 TIMEOUT 1500
                 RESULT_VARIABLE result )
