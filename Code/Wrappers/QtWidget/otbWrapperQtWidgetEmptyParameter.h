2237d654a7fc6a020a0caef342a692bb73c56241
Otmane Lahlou <otmane.lahlou@c-s.fr>
2011-09-27 16:23:18 +0200

    ENH: wrong definition

--- Code/Wrappers/QtWidget/otbWrapperQtWidgetEmptyParameter.h
+++ Code/Wrappers/QtWidget/otbWrapperQtWidgetEmptyParameter.h
@@ -15,8 +15,8 @@
      PURPOSE.  See the above copyright notices for more information.
 
 =========================================================================*/
-#ifndef __otbWrapperQtEmptyWidgetFactory_h
-#define __otbWrapperQtEmptyWidgetFactory_h
+#ifndef __otbWrapperQtWidgetEmptyParameter_h
+#define __otbWrapperQtWidgetEmptyParameter_h
 
 #include <QtGui>
 #include "otbWrapperEmptyParameter.h"
