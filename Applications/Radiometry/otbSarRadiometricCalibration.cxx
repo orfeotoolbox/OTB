18db26648be863fb5fa27ac24123c2cd589b3418
Otmane Lahlou <otmane.lahlou@c-s.fr>
2011-10-21 16:53:56 +0200

    ENH: add SarRadiometricCalibration application

---
+++ Applications/Radiometry/otbSarRadiometricCalibration.cxx
@@ -0,0 +1,90 @@
+/*=========================================================================
+
+ Program:   ORFEO Toolbox
+ Language:  C++
+ Date:      $Date$
+ Version:   $Revision$
+
+
+ Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+ See OTBCopyright.txt for details.
+
+
+ This software is distributed WITHOUT ANY WARRANTY; without even
+ the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ PURPOSE.  See the above copyright notices for more information.
+
+ =========================================================================*/
+#include "otbWrapperApplication.h"
+#include "otbWrapperApplicationFactory.h"
+
+#include "otbSarRadiometricCalibrationToImageFilter.h"
+
+namespace otb
+{
+namespace Wrapper
+{
+
+class SarRadiometricCalibration : public Application
+{
+public:
+  /** Standard class typedefs. */
+  typedef SarRadiometricCalibration     Self;
+  typedef Application                   Superclass;
+  typedef itk::SmartPointer<Self>       Pointer;
+  typedef itk::SmartPointer<const Self> ConstPointer;
+
+  /** Standard macro */
+  itkNewMacro(Self);
+
+  itkTypeMacro(SarRadiometricCalibration, otb::Application);
+
+  typedef otb::SarRadiometricCalibrationToImageFilter<ComplexFloatImageType,
+                                                      ComplexFloatImageType>     CalibrationFilterType;
+
+private:
+  SarRadiometricCalibration()
+  {
+    SetName("SarRadiometricCalibration");
+    SetDescription("Perform SAR calibration on input complex images");
+  }
+
+  virtual ~SarRadiometricCalibration()
+  {
+  }
+
+  void DoCreateParameters()
+  {
+    AddParameter(ParameterType_ComplexInputImage,  "cin", "Input Complex Image");
+    AddParameter(ParameterType_ComplexOutputImage,  "out", "Output Image");
+    AddParameter(ParameterType_Empty, "noise", "Disable Noise");
+    MandatoryOff("noise");
+  }
+
+  void DoUpdateParameters()
+  { }
+
+  void DoExecute()
+  {
+    // Get the input complex image
+    ComplexFloatImageType*  floatComplexImage = GetParameterComplexFloatImage("cin");
+
+    // Set the filer input
+    m_CalibrationFilter = CalibrationFilterType::New();
+    m_CalibrationFilter->SetInput(floatComplexImage);
+
+    if (IsParameterEnabled("noise"))
+      {
+      m_CalibrationFilter->SetEnableNoise(false);
+      }
+
+    // Set the output image
+    SetParameterComplexOutputImage("out", m_CalibrationFilter->GetOutput());
+  }
+
+  CalibrationFilterType::Pointer   m_CalibrationFilter;
+};
+}
+}
+
+OTB_APPLICATION_EXPORT(otb::Wrapper::SarRadiometricCalibration)
