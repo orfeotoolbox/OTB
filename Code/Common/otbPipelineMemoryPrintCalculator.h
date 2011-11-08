5372ef000968c394377f94a6c0e94e78f5107cc9
Otmane Lahlou <otmane.lahlou@c-s.fr>
2011-11-08 18:43:49 +0100

    ENH: replace long long int by itk type itk::uint64_t

--- Code/Common/otbPipelineMemoryPrintCalculator.h
+++ Code/Common/otbPipelineMemoryPrintCalculator.h
@@ -20,6 +20,7 @@
 
 #include "itkProcessObject.h"
 #include "itkDataObject.h"
+#include "itkIntTypes.h"
 #include <set>
 
 namespace otb
@@ -73,7 +74,7 @@ public:
   typedef ProcessObjectType::Pointer          ProcessObjectPointerType;
   typedef itk::DataObject                     DataObjectType;
   typedef DataObjectType::Pointer             DataObjectPointerType;
-  typedef long long int                       MemoryPrintType;
+  typedef itk::uint64_t                       MemoryPrintType;
   typedef std::set<const ProcessObjectType *> ProcessObjectPointerSetType;
 
   /** Run-time type information (and related methods). */
