cf051ed36cb4c07de804b64172ad8b209e4cfa07
Julien Malik <julien.malik@c-s.fr>
2011-11-23 13:35:42 +0100

    COMP: allow importing symbols from ossimplugin DLL

--- Utilities/otbossimplugins/ossimPluginConstants.h
+++ Utilities/otbossimplugins/ossimPluginConstants.h
@@ -18,11 +18,25 @@
  * DLL IMPORT/EXORT SECTION
  */
 #if defined(OSSIM_STATIC)
-# define OSSIM_PLUGINS_DLL
+#  define OSSIMPLUGINSEXPORT
+#  define OSSIMPLUGINSIMPORT
+#  define OSSIMPLUGINSDLLEXPORT
+#  define OSSIM_PLUGINS_DLL
 #elif defined(__MINGW32__) || defined(__CYGWIN__) || defined(_MSC_VER) || defined(__VISUALC__) || defined(__BORLANDC__) || defined(__WATCOMC__)
-#  define OSSIM_PLUGINS_DLL __declspec(dllexport)
-#else
-# define OSSIM_PLUGINS_DLL
-#endif
+#  define OSSIMPLUGINSEXPORT __declspec(dllexport)
+#  define OSSIMPLUGINSIMPORT __declspec(dllimport)
+#  ifdef OSSIMPLUGINSMAKINGDLL
+#    define OSSIMPLUGINSDLLEXPORT OSSIMEXPORT
+#    define OSSIM_PLUGINS_DLL       OSSIMEXPORT
+#  else
+#    define OSSIMPLUGINSDLLEXPORT OSSIMIMPORT
+#    define OSSIM_PLUGINS_DLL      OSSIMIMPORT
+#  endif
+#else /* not #if defined(_MSC_VER) */
+#  define OSSIMPLUGINSEXPORT
+#  define OSSIMPLUGINSIMPORT
+#  define OSSIMPLUGINSDLLEXPORT
+#  define OSSIM_PLUGINS_DLL
+#endif /* #if defined(_MSC_VER) */
 
 #endif /* #ifndef ossimPluginConstants_HEADER */
