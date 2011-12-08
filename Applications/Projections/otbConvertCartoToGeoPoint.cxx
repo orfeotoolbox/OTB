5bfc52abbfcf76a3e3347f224b023a56c2f3ab36
Otmane Lahlou <otmane.lahlou@c-s.fr>
2011-12-08 17:39:39 +0100

    ENH: use the MapProjectionParameter handler class in the CartoToGeo application & use the GenericRSTransform for points projection

--- Applications/Projections/otbConvertCartoToGeoPoint.cxx
+++ Applications/Projections/otbConvertCartoToGeoPoint.cxx
@@ -21,26 +21,16 @@
 #include <iostream>
 #include <iomanip>
 
-#include "otbMapProjections.h"
 #include "otbWrapperNumericalParameter.h"
+#include "otbWrapperMapProjectionParametersHandler.h"
+
+#include "otbImageToGenericRSOutputParameters.h"
+#include "otbGenericRSTransform.h"
 
 namespace otb
 {
 namespace Wrapper
 {
-
-enum
-{
-  UTM,
-  LAMBERT,
-  LAMBERT2,
-  LAMBERT93,
-  SINUS,
-  ECKERT4,
-  TRANSMERCATOR,
-  MOLLWEID,
-  SVY21
-};
 
 class ConvertCartoToGeoPoint : public Application
 {
@@ -55,6 +45,8 @@ public:
   itkNewMacro(Self);
 
   itkTypeMacro(ConvertCartoToGeoPoint, otb::Application);
+
+  typedef otb::GenericRSTransform<>        TransformType;
   
 private:
   void DoInit()
@@ -79,59 +71,8 @@ private:
     AddParameter(ParameterType_Float, "carto.y", "Y cartographic coordinates");
     SetParameterDescription("carto.y", "Y cartographic coordinates in the specified projection.");
 
-    AddParameter(ParameterType_Choice, "mapproj", "Map projection type");
-    SetParameterDescription("mapproj", "Type of projection used for the conversion. Possible values are: utm, lambert, lambert2, lambert93, sinus, eckert4, transmercator, mollweid and svy21.");
-
-    AddChoice("mapproj.utm", "utm");
-    AddParameter(ParameterType_Int, "mapproj.utm.zone", "zone");
-    SetParameterDescription( "mapproj.utm.zone", "UTM zone of the point.");
-    SetMinimumParameterIntValue("mapproj.utm.zone", 1);
-    SetParameterInt( "mapproj.utm.zone", 1);
-    AddParameter(ParameterType_Empty, "mapproj.utm.hemisphere", "Is in north hemisphere");
-    SetParameterDescription( "mapproj.utm.hemisphere", "Is the point is in the north hemisphere or not.");
-    EnableParameter( "mapproj.utm.hemisphere");
-
-    AddChoice("mapproj.lambert", "lambert");
-    AddParameter(ParameterType_Float, "mapproj.lambert.parallel1degree", "First parallele degree");
-    SetParameterDescription( "mapproj.lambert.parallel1degree", "First Lambert parallele degree.");
-    AddParameter(ParameterType_Float, "mapproj.lambert.parallel2degree", "Second parallele degree");
-    SetParameterDescription( "mapproj.lambert.parallel2degree", "Second Lambert parallele degree.");
-    AddParameter(ParameterType_Float, "mapproj.lambert.falseeasting", "False easting");
-    SetParameterDescription( "mapproj.lambert.falseeasting", "Lambert false easting value.");
-    AddParameter(ParameterType_Float, "mapproj.lambert.falsenorthing", "False easting");
-    SetParameterDescription( "mapproj.lambert.falsenorthing", "Lambert false easting value.");
-
-    AddChoice("mapproj.lambert", "lambert3");
-
-    AddChoice("mapproj.lambert", "lambert93");
-
-    AddChoice("mapproj.sinus", "sinus");
-    AddParameter(ParameterType_Float, "mapproj.sinus.falseeasting", "False easting");
-    SetParameterDescription( "mapproj.sinus.falseeasting", "Sinus false easting value.");
-    AddParameter(ParameterType_Float, "mapproj.sinus.falsenorthing", "False easting");
-    SetParameterDescription( "mapproj.sinus.falsenorthing", "Sinus false easting value.");
-
-    AddChoice("mapproj.eckert4", "eckert4");
-    AddParameter(ParameterType_Float, "mapproj.eckert4.falseeasting", "False easting");
-    SetParameterDescription( "mapproj.eckert4.falseeasting", "Eckert4 false easting value.");
-    AddParameter(ParameterType_Float, "mapproj.eckert4.falsenorthing", "False easting");
-    SetParameterDescription( "mapproj.eckert4.falsenorthing", "Eckert4 false easting value.");
-
-    AddChoice("mapproj.transmercator", "transmercator");
-    AddParameter(ParameterType_Float, "mapproj.transmercator.falseeasting", "False easting");
-    SetParameterDescription( "mapproj.transmercator.falseeasting", " Transmercator false easting value.");
-    AddParameter(ParameterType_Float, "mapproj.transmercator.falsenorthing", "False easting");
-    SetParameterDescription( "mapproj.transmercator.falsenorthing", " Transmercator false easting value.");
-    AddParameter(ParameterType_Float, "mapproj.transmercator.scale", "Scale factor");
-    SetParameterDescription( "mapproj.transmercator.falsenorthing", " Transmercator scale factor value.");
-
-    AddChoice("mapproj.mollweid", "mollweid");
-    AddParameter(ParameterType_Float, "mapproj.mollweid.falseeasting", "False easting");
-    SetParameterDescription( "mapproj.mollweid.falseeasting", "Mollweid false easting value.");
-    AddParameter(ParameterType_Float, "mapproj.mollweid.falsenorthing", "False easting");
-    SetParameterDescription( "mapproj.mollweid.falsenorthing", "Mollweid false easting value.");
-
-    AddChoice("mapproj.svy21", "svy21");
+    // Add the MapProjectionParameters
+    MapProjectionParametersHandler::AddMapProjectionParameters(this, "mapproj");
     
     AddParameter(ParameterType_Float, "long", "Output long");
     SetParameterDescription("long", "Point longitude coordinates.");
@@ -153,19 +94,26 @@ private:
   {
   }
 
-
-  template<typename TMapProjection>
-  void Generic_DoExecute( TMapProjection* mapProjection )
+  void DoExecute()
   {
-    typedef TMapProjection MapProjectionType;
-    
-    typename MapProjectionType::InputPointType cartoPoint;
-    typename MapProjectionType::OutputPointType geoPoint;
+    // Get the projectionRef 
+    std::string inputProjRef = MapProjectionParametersHandler::GetProjectionRefFromChoice(this, "mapproj");
 
-    cartoPoint[0] = this->GetParameterFloat("carto.x");
-    cartoPoint[1] = this->GetParameterFloat("carto.y");
+    // Instanciate a GenericRSTranform 
+    // Input : coordiante system picked up by the user
+    // Output : WGS84 correponding to epsg code 4326
+    TransformType::Pointer  transform = TransformType::New();
+    transform->SetInputProjectionRef(inputProjRef);
+    transform->SetOutputProjectionRef(otb::GeoInformationConversion::ToWKT(4326));
+    transform->InstanciateTransform();
 
-    geoPoint = mapProjection->TransformPoint(cartoPoint);
+    TransformType::InputPointType   cartoPoint;
+    TransformType::OutputPointType  geoPoint;
+
+    cartoPoint[0] = GetParameterFloat("carto.x");
+    cartoPoint[1] = GetParameterFloat("carto.y");
+
+    geoPoint = transform->TransformPoint(cartoPoint);
 
     otbAppLogINFO( << std::setprecision(10) << "Cartographic Point  (x , y)  : (" << cartoPoint[0] << ", " << cartoPoint[1] << ")" );
     otbAppLogINFO( << std::setprecision(10) << "Geographic   Point (Long, Lat) : (" << geoPoint[0] << ", " <<  geoPoint[1] << ")" );
@@ -173,117 +121,7 @@ private:
 
     SetParameterFloat( "long", geoPoint[0] );
     SetParameterFloat( "lat", geoPoint[1] );
-
   }
-  
-  void DoExecute()
-  {
-    const int typeMap = this->GetParameterInt("mapproj");
-    
-    switch( typeMap )
-      {
-      case UTM:
-      {
-      typedef otb::UtmInverseProjection UtmProjectionType;
-      UtmProjectionType::Pointer utmProjection = UtmProjectionType::New();
-      
-      utmProjection->SetZone(this->GetParameterInt("mapproj.utm.zone"));
-
-      if( this->IsParameterEnabled("mapproj.utm.hemisphere"))
-        utmProjection->SetHemisphere('N');
-      else
-        utmProjection->SetHemisphere('S');
-      
-      this->Generic_DoExecute<UtmProjectionType>(utmProjection);
-      break;
-      }
-      case LAMBERT:
-      {
-      typedef otb::LambertConformalConicInverseProjection LambertProjectionType;
-      LambertProjectionType::Pointer lambertProjection = LambertProjectionType::New();
-      
-      lambertProjection->SetParameters(this->GetParameterFloat( "mapproj.lambert.parallel1degree" ),
-                                       this->GetParameterFloat( "mapproj.lambert.parallel2degree" ),
-                                       this->GetParameterFloat( "mapproj.lambert.falseeasting" ),
-                                       this->GetParameterFloat( "mapproj.lambert.falsenorthing" ));
-      
-      this->Generic_DoExecute<LambertProjectionType>(lambertProjection);
-      break;
-      }
-      case LAMBERT2:
-      {
-      typedef otb::Lambert2EtenduInverseProjection Lambert2ProjectionType;
-      Lambert2ProjectionType::Pointer lambert2Projection = Lambert2ProjectionType::New();
-      
-      this->Generic_DoExecute<Lambert2ProjectionType>(lambert2Projection);
-      break;
-      }
-      case LAMBERT93:
-      {
-      typedef otb::Lambert93InverseProjection Lambert93ProjectionType;
-      Lambert93ProjectionType::Pointer lambert93Projection = Lambert93ProjectionType::New();
-      
-      this->Generic_DoExecute<Lambert93ProjectionType>(lambert93Projection);
-      break;
-      }
-      case SINUS:
-      {
-      typedef otb::SinusoidalInverseProjection SinusoidalProjectionType;
-      SinusoidalProjectionType::Pointer sinusoidalProjection = SinusoidalProjectionType::New();
-      
-      sinusoidalProjection->SetParameters(  this->GetParameterFloat( "mapproj.sinus.falseeasting" ),
-                                            this->GetParameterFloat( "mapproj.sinus.falsenorthing" ) );
-      
-      this->Generic_DoExecute<SinusoidalProjectionType>(sinusoidalProjection);
-      break;
-      }
-      case ECKERT4:
-      {
-      typedef otb::Eckert4InverseProjection Eckert4ProjectionType;
-      Eckert4ProjectionType::Pointer eckert4Projection = Eckert4ProjectionType::New();
-      
-      eckert4Projection->SetParameters(this->GetParameterFloat( "mapproj.eckert4.falseeasting" ),
-                                       this->GetParameterFloat( "mapproj.eckert4.falsenorthing" ));
-
-      this->Generic_DoExecute<Eckert4ProjectionType>(eckert4Projection);
-      }
-     case TRANSMERCATOR:
-      {
-      typedef otb::TransMercatorInverseProjection TransMercatorProjectionType;
-      TransMercatorProjectionType::Pointer transMercatorProjection = TransMercatorProjectionType::New();
-
-      transMercatorProjection->SetParameters(this->GetParameterFloat( "mapproj.transmercator.falseeasting" ),
-                                             this->GetParameterFloat( "mapproj.transmercator.falsenorthing" ),
-                                             this->GetParameterFloat( "mapproj.transmercator.scale" ) );
-
-      this->Generic_DoExecute<TransMercatorProjectionType>(transMercatorProjection);
-      break;
-      }
-      case MOLLWEID:
-      {
-        typedef otb::MollweidInverseProjection MollweidProjectionType;
-        MollweidProjectionType::Pointer mollweidProjection = MollweidProjectionType::New();
-
-        mollweidProjection->SetParameters(this->GetParameterFloat( "mapproj.mollweid.falseeasting" ),
-                                          this->GetParameterFloat( "mapproj.mollweid.falsenorthing" ) );
-
-        this->Generic_DoExecute<MollweidProjectionType>(mollweidProjection);
-        break;
-      }
-      case SVY21:
-      {
-      typedef otb::SVY21InverseProjection SVY21ProjectionType;
-      SVY21ProjectionType::Pointer svy21Projection = SVY21ProjectionType::New();
-      
-      this->Generic_DoExecute<SVY21ProjectionType>(svy21Projection);
-      break;
-      }
-      default:
-      {
-      otbAppLogFATAL( << "Unknow map projection "<<typeMap<<".");
-      }
-      }
-}
 };
 
 }
