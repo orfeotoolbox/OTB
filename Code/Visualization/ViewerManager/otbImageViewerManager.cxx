7b7ede3ec5d9b7de89fddc266488449da25ae955
Cyrille Valladeau <cyrille.valladeau@c-s.fr>
2011-12-20 16:27:20 +0100

    ENH: VixerManager : supress dead code

--- Code/Visualization/ViewerManager/otbImageViewerManager.cxx
+++ Code/Visualization/ViewerManager/otbImageViewerManager.cxx
@@ -18,7 +18,7 @@ See OTBCopyright.txt for details.
 #include "otbImageViewerManagerViewGUI.h"
 #include "otbImageViewerManagerModel.h"
 #include "otbImageViewerManagerController.h"
-#include "otbCommandLineArgumentParser.h"
+
 #include "otbMsgReporter.h"
 
 #include "otbI18n.h"
@@ -26,37 +26,8 @@ See OTBCopyright.txt for details.
 int main(int argc, char* argv[])
 {
 
-    otbI18nMacro();
-
-  // Parse command line parameters
-//   typedef otb::CommandLineArgumentParser ParserType;
-//   ParserType::Pointer parser = ParserType::New();
-
-//   //parser->AddInputImage(false); //Optionnal parameter
-//   parser->AddOptionNParams("--InputImage","Input Images","-in", false);
-
-//   typedef otb::CommandLineArgumentParseResult ParserResultType;
-//   ParserResultType::Pointer  parseResult = ParserResultType::New();
-
-//   try
-//   {
-//     parser->ParseCommandLine(argc, argv, parseResult);
-//   }
-//   catch ( itk::ExceptionObject & err )
-//   {
-//     std::string descriptionException = err.GetDescription();
-//     if (descriptionException.find("ParseCommandLine(): Help Parser") != std::string::npos)
-//     {
-//       std::cout << "WARNING : output file pixels are converted in 'unsigned char'" << std::endl;
-//       return EXIT_SUCCESS;
-//     }
-//     if (descriptionException.find("ParseCommandLine(): Version Parser") != std::string::npos)
-//     {
-//       return EXIT_SUCCESS;
-//     }
-//     return EXIT_FAILURE;
-//   }
-
+  otbI18nMacro();
+  
   typedef otb::ImageViewerManagerController ControllerType;
   typedef otb::ImageViewerManagerViewGUI ViewType;
 
@@ -64,22 +35,13 @@ int main(int argc, char* argv[])
   ViewType::Pointer view = ViewType::New();
   controller->SetView(view);
   view->SetImageViewerManagerController(controller);
-  //otb::MsgReporter::GetInstance()->SetTitle("Image Viewer Manager application");
 
-  //
   view->Show();
   for (int i = 1; i<argc; ++i)
     {
       view->OpenImage(argv[i]);
     }
 
-//   if ( parseResult->IsOptionInputImagePresent() )
-//     {
-//       for(int i = 0; i<parseResult->GetNumberOfParameters("--InputImage"); i++ )
-//        {
-//          view->OpenImage(parseResult->GetParameterString("--InputImage", i).c_str());
-//        }
-//     }
   Fl::check();
 
   otbGenericMsgDebugMacro(<<"Running GUI ...");
