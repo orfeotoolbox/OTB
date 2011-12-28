d1684c6cc4ea4afb10ce1692431a8cf927949dcc
Emmanuel Christophe <emmanuel.christophe@orfeo-toolbox.org>
2011-12-28 08:48:26 +0800

    ENH: remove useless methods

--- Utilities/otbossimplugins/ossim/otb/Arithmetic.h
+++ Utilities/otbossimplugins/ossim/otb/Arithmetic.h
@@ -37,77 +37,7 @@ namespace ossimplugins
       return (x >= 0.0)  ?  (double)((long)(x + 0.5))  :  (double)((long)(x - 0.5));
    }
 
-/*-------------------------------------------------------------------------*/
-/*          Greatest Common Divisor                                    */
-/*-------------------------------------------------------------------------*/
-
-   long pgcd ( long n , long p )
-   {
-      long k ;
-      long n1 , p1 ;
-      long m0 , m1 , m ;
-
-      n1 = labs(n) ;
-      p1 = labs(p) ;
-      if (n1 > p1)
-      {
-         m0 = n1 ;
-         m1 = p1 ;
-      }
-      else
-      {
-         m0 = p1 ;
-         m1 = n1 ;
-      }
-      if (m1 != 0)
-      {
-         while (m1 != 0)
-         {
-            m = m0 - (m0/m1) * m1 ;
-            m0 = m1 ;
-            m1 = m ;
-         }
-         k = m0 ;
-      }
-      else k = 1 ;
-
-      return k ;
-   }
-
-/*-------------------------------------------------------------------------*/
-/*          Least Common Multiple                                     */
-/*-------------------------------------------------------------------------*/
-
-   long ppcm ( long n , long p )
-   {
-      long k ;
-      long n1 , p1 ;
-      long m0 , m1 , m ;
-
-      n1 = labs(n) ;
-      p1 = labs(p) ;
-      if (n1>p1)
-      {
-         m0 = n1 ;
-         m1 = p1 ;
-      }
-      else
-      {
-         m0 = p1 ;
-         m1 = n1 ;
-      }
-      if (m1 != 0)
-      {
-         m = m0 ;
-         while ((m % m1) != 0) m += m0 ;
-         k = m ;
-      }
-      else k = 0 ;
-
-      return k ;
-   }
    
 }  // End: namespace ossimplugins
 
 #endif
-
