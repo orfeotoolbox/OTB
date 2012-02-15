3fbfbe0a8588d08c96e0d9b8af33cb3285403d50
Patrick Imbo <patrick.imbo@c-s.fr>
2012-02-15 10:09:27 +0100

    ENH: adding Origin/Spacing to otbRationalTransform class

--- Code/Projections/otbRationalTransform.h
+++ Code/Projections/otbRationalTransform.h
@@ -58,13 +58,16 @@ public:
   typedef itk::Point<ScalarType, Dimension>         InputPointType;
   typedef itk::Point<ScalarType, Dimension>         OutputPointType;
 
+  typedef itk::Vector<double, 2> SpacingType;
+  typedef itk::Point<double, 2>  OriginType;
+
   typedef typename Superclass::InverseTransformBasePointer InverseTransformBasePointer;
 
   /** Method for creation through the object factory. */
   itkNewMacro(Self);
 
   /** Run-time type information (and related methods). */
-  itkTypeMacro(RationalTransform, itk::Transform);
+  itkTypeMacro(RationalTransform, Transform);
 
   itkStaticConstMacro(SpaceDimension, unsigned int, Dimension);
 
@@ -80,6 +83,26 @@ public:
   /** Get the denominator degree */
   itkGetMacro(DenominatorDegree, unsigned int);
 
+  /** Set the origin of the vector data.
+  * \sa GetOrigin() */
+  itkSetMacro(InputOrigin, OriginType);
+  itkGetConstReferenceMacro(InputOrigin, OriginType);
+
+  /** Set the spacing (size of a pixel) of the vector data.
+    * \sa GetSpacing() */
+  itkSetMacro(InputSpacing, SpacingType);
+  itkGetConstReferenceMacro(InputSpacing, SpacingType);
+
+  /** Set the origin of the vector data.
+  * \sa GetOrigin() */
+  itkSetMacro(OutputOrigin, OriginType);
+  itkGetConstReferenceMacro(OutputOrigin, OriginType);
+
+  /** Set the spacing (size of a pixel) of the vector data.
+  * \sa GetSpacing() */
+  itkSetMacro(OutputSpacing, SpacingType);
+  itkGetConstReferenceMacro(OutputSpacing, SpacingType);
+
 
   /** The transform point method */
   virtual OutputPointType TransformPoint(const InputPointType& point) const
@@ -92,7 +115,7 @@ public:
       }
       }
 
-    // Build output
+    InputPointType  inputPoint = point;
     OutputPointType outputPoint;
 
     unsigned int dimensionStride = (m_DenominatorDegree+1)+(m_NumeratorDegree+1);
@@ -100,28 +123,36 @@ public:
     // Compute RPC transform
     for(unsigned int dim = 0; dim < SpaceDimension; ++dim)
       {
-      // Initialize numerator and denominator
+      // 1) Apply input origin/spacing
+      inputPoint[dim] = inputPoint[dim] * m_InputSpacing[dim] + m_InputOrigin[dim];
+
+      // 2 )Transform point with :
+
+      // 2-1) Initialize numerator and denominator
       TScalarType num   = itk::NumericTraits<TScalarType>::Zero;
       TScalarType denom = itk::NumericTraits<TScalarType>::Zero;
       TScalarType currentPower = 1.;
       
-      // Compute numerator
+      // 2-2) Compute numerator
       for(unsigned int numDegree = 0; numDegree <= m_NumeratorDegree; ++numDegree)
         {
         num+=this->m_Parameters[dim*dimensionStride+numDegree]*currentPower;
-        currentPower*=point[dim];
+        currentPower*=inputPoint[dim];
         }
       
-      // Compute denominator
+      // 2-3) Compute denominator
       currentPower = 1.;
       for(unsigned int denomDegree = 0; denomDegree <= m_DenominatorDegree; ++denomDegree)
         {
         denom+=this->m_Parameters[dim*dimensionStride+m_NumeratorDegree+denomDegree+1]*currentPower;
-        currentPower*=point[dim];
+        currentPower*=inputPoint[dim];
         }
 
-      // Finally, fill the output
+      // 2-4) Fill the output
       outputPoint[dim]=num/denom;
+
+      // 3) Apply output origin/spacing
+      outputPoint[dim] = (outputPoint[dim] - m_OutputOrigin[dim]) / m_OutputSpacing[dim];
       }
 
     // Return the output point
@@ -152,15 +183,29 @@ protected:
     {
     this->m_Parameters.SetSize(this->GetNumberOfParameters());
     this->m_Parameters.Fill(0);
-    this->m_Parameters[1] = 1.;
-    this->m_Parameters[4] = 1.;
+    this->m_Parameters[ 0] = 1.;
+    this->m_Parameters[ 4] = 1.;
+    this->m_Parameters[ 8] = 1.;
+    this->m_Parameters[12] = 1.;
+    m_InputSpacing.Fill(1);
+    m_InputOrigin.Fill(0);
+    m_OutputSpacing.Fill(1);
+    m_OutputOrigin.Fill(0);
+
     }
 
   virtual ~RationalTransform() {}
 
   void PrintSelf(std::ostream& os, itk::Indent indent) const
   {
-    Superclass::PrintSelf(os, indent);
+    Superclass::PrintSelf(os, indent); 
+    os << indent << "Numerator Degree : " << m_NumeratorDegree << std::endl;
+    os << indent << "Denominator Degree : " << m_DenominatorDegree << std::endl;
+    os << indent << "InputSpacing : " << m_InputSpacing << std::endl;
+    os << indent << "InputOrigin : " << m_InputOrigin << std::endl;
+    os << indent << "InputSpacing : " << m_OutputSpacing << std::endl;
+    os << indent << "OutputOrigin : " << m_OutputOrigin << std::endl;
+
   }
 
 private:
@@ -172,6 +217,11 @@ private:
 
   // Degree of denominator
   unsigned int m_DenominatorDegree;
+
+  SpacingType m_InputSpacing;
+  OriginType  m_InputOrigin;
+  SpacingType m_OutputSpacing;
+  OriginType  m_OutputOrigin;
 };
 
 } // namespace otb
