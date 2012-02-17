fc6045a5236db91687b68dd39fa3e228fc0787f2
Julien Michel <julien.michel@orfeo-toolbox.org>
2012-02-17 11:33:15 +0100

    ENH: End of block-matching application

--- Applications/DisparityMap/otbHorizontalPixelWiseBlockMatching.cxx
+++ Applications/DisparityMap/otbHorizontalPixelWiseBlockMatching.cxx
@@ -18,6 +18,11 @@
 #include "otbWrapperApplication.h"
 #include "otbWrapperApplicationFactory.h"
 
+#include "otbHorizontalPixelWiseBlockMatchingImageFilter.h"
+#include "otbVarianceImageFilter.h"
+#include "otbBandMathImageFilter.h"
+#include "otbImageList.h"
+#include "otbImageListToVectorImageFilter.h"
 
 namespace otb
 {
@@ -33,6 +38,32 @@ public:
   typedef itk::SmartPointer<Self>       Pointer;
   typedef itk::SmartPointer<const Self> ConstPointer;
 
+  typedef otb::Functor::SSDBlockMatching<FloatImageType,FloatImageType> SSDBlockMatchingFunctorType;
+  typedef otb::Functor::NCCBlockMatching<FloatImageType,FloatImageType> NCCBlockMatchingFunctorType;
+
+  typedef otb::HorizontalPixelWiseBlockMatchingImageFilter<FloatImageType,
+                                                           FloatImageType,
+                                                           FloatImageType,
+                                                           FloatImageType,
+                                                           SSDBlockMatchingFunctorType> SSDBlockMatchingFilterType;
+
+  typedef otb::HorizontalPixelWiseBlockMatchingImageFilter<FloatImageType,
+                                                           FloatImageType,
+                                                           FloatImageType,
+                                                           FloatImageType,
+                                                           NCCBlockMatchingFunctorType> NCCBlockMatchingFilterType;
+
+  typedef otb::VarianceImageFilter<FloatImageType,FloatImageType> VarianceFilterType;
+
+
+  typedef otb::BandMathImageFilter<FloatImageType>         BandMathFilterType;
+
+  typedef otb::ImageList<FloatImageType>                   ImageListType;
+
+  typedef otb::ImageListToVectorImageFilter<ImageListType,
+                                            FloatVectorImageType> ImageListToVectorImageFilterType;
+
+
   /** Standard macro */
   itkNewMacro(Self);
 
@@ -41,6 +72,17 @@ public:
   /** Filters typedef */
 
 private:
+  HorizontalPixelWiseBlockMatching()
+  {
+    // Initialize filters
+    m_SSDBlockMatcher = SSDBlockMatchingFilterType::New();
+    m_NCCBlockMatcher = NCCBlockMatchingFilterType::New();
+    m_VarianceFilter  = VarianceFilterType::New();
+    m_BandMathFilter  = BandMathFilterType::New();
+    m_OutputImageList = ImageListType::New();
+    m_ImageListFilter = ImageListToVectorImageFilterType::New();
+  }
+
   void DoInit()
   {
     SetName("HorizontalPixelWiseBlockMatching");
@@ -66,6 +108,10 @@ private:
     AddParameter(ParameterType_OutputImage, "io.out", "The output disparity map");
     SetParameterDescription("io.out","An image containing the estimated disparity as well as the metric values if the option is used");
 
+    AddParameter(ParameterType_OutputImage, "io.outmask", "The output mask corresponding to all criterions");
+    SetParameterDescription("io.outmask","A mask image corresponding to all citerions (see masking parameters). Only required if variance threshold or nodata criterions are set.");
+    DisableParameter("io.outmask");
+
     AddParameter(ParameterType_Empty,"io.outmetric","Output optimal metric values as well");
     SetParameterDescription("io.outmetric","If used, the output image will have a second component with metric optimal values");
 
@@ -79,10 +125,14 @@ private:
     AddParameter(ParameterType_Float,"mask.nodata","Discard pixels with no-data value");
     SetParameterDescription("mask.in","This parameter allows to discard pixels whose value is equal to the user-defined no-data value.");
     MandatoryOff("mask.nodata");
+    SetDefaultParameterFloat("mask.nodata",0.);
+    DisableParameter("mask.nodata");
     
     AddParameter(ParameterType_Float,"mask.variancet","Discard pixels with low local variance");
     SetParameterDescription("mask.variancet","This parameter allows to discard pixels whose local variance is too small (the size of the neighborhood is given by the radius parameter)");
     MandatoryOff("mask.variancet");
+    SetDefaultParameterFloat("mask.variancet",100.);
+    DisableParameter("mask.variancet");
 
     AddParameter(ParameterType_Group,"bm","Block matching parameters");
     SetParameterDescription("bm","This group of parameters allow to tune the block-matching behaviour");
@@ -97,6 +147,7 @@ private:
     AddParameter(ParameterType_Int,"bm.radius","Radius of blocks");
     SetParameterDescription("bm.radius","The radius (in pixels) of blocks in Block-Matching");
     SetDefaultParameterInt("bm.radius",3);
+    SetMinimumParameterIntValue("bm.radius",1);
 
     AddParameter(ParameterType_Int,"bm.mind","Minimum disparity");
     SetParameterDescription("bm.mind","Minimum disparity to explore (can be negative)");
@@ -112,13 +163,157 @@ private:
 
   void DoUpdateParameters()
   {
-    // Nothing to do
+    if(IsParameterEnabled("mask.variancet") || IsParameterEnabled("mask.nodata"))
+      {
+      EnableParameter("io.outmask");
+      }
+    else
+      {
+      DisableParameter("io.outmask");
+      }
   }
 
   void DoExecute()
   {
+    FloatImageType::Pointer leftImage = GetParameterFloatImage("io.inleft");
+    FloatImageType::Pointer rightImage = GetParameterFloatImage("io.inright");
+    FloatImageType::Pointer leftmask;
+
+    unsigned int radius  = GetParameterInt("bm.radius");
+    int          mindisp = GetParameterInt("bm.mind");
+    int          maxdisp = GetParameterInt("bm.maxd");
     
-  }  
+    itk::OStringStream bandMathExpression;
+    bandMathExpression<<"if(";
+    
+    unsigned int inputId = 0;
+    bool masking = false;
+
+    // Handle input mask if present
+    if(IsParameterEnabled("mask.in"))
+      {
+      leftmask = GetParameterFloatImage("mask.in");
+      m_BandMathFilter->SetNthInput(inputId,leftmask,"inmask");
+      masking = true;
+      ++inputId;
+      bandMathExpression<<"inmask > 0";
+      }
+    
+    // Handle variance threshold if present
+    if(IsParameterEnabled("mask.variancet"))
+      {
+      if(masking)
+        {
+        bandMathExpression<<" and ";
+        }
+
+      m_VarianceFilter->SetInput(leftImage);
+      VarianceFilterType::InputSizeType vradius;
+      vradius.Fill(radius);
+      m_VarianceFilter->SetRadius(vradius);
+
+      m_BandMathFilter->SetNthInput(inputId,m_VarianceFilter->GetOutput(),"variance");
+      bandMathExpression<<"variance > "<<GetParameterFloat("mask.variancet");
+      ++inputId;
+      masking = true;
+      }
+
+    // Handle nodata field if present
+    if(IsParameterEnabled("mask.nodata"))
+      {
+      if(masking)
+        {
+        bandMathExpression<<" and ";
+        }
+      m_BandMathFilter->SetNthInput(inputId,leftImage,"leftimage");
+      bandMathExpression<<"leftimage != "<<GetParameterFloat("mask.nodata");
+      masking = true;
+      }
+
+    bandMathExpression<<",255,0)";
+
+    if(masking)
+      {
+      GetLogger()->Info("Masking criterion: " + bandMathExpression.str());
+      m_BandMathFilter->SetExpression(bandMathExpression.str());
+      }
+      
+    FloatImageType * dispImage;
+    FloatImageType * metricImage;
+
+    // SSD case
+    if(GetParameterInt("bm.metric") == 0)
+      {
+      m_SSDBlockMatcher->SetLeftInput(leftImage);
+      m_SSDBlockMatcher->SetRightInput(rightImage);
+      m_SSDBlockMatcher->SetRadius(radius);
+      m_SSDBlockMatcher->SetMinimumDisparity(mindisp);
+      m_SSDBlockMatcher->SetMaximumDisparity(maxdisp);
+
+      if(masking)
+        {
+        m_SSDBlockMatcher->SetMaskInput(m_BandMathFilter->GetOutput());
+        }
+
+      dispImage = m_SSDBlockMatcher->GetDisparityOutput();
+      metricImage = m_SSDBlockMatcher->GetMetricOutput();
+      }
+    // NCC case
+    else
+      {
+      m_NCCBlockMatcher->SetLeftInput(leftImage);
+      m_NCCBlockMatcher->SetRightInput(rightImage);
+      m_NCCBlockMatcher->SetRadius(radius);
+      m_NCCBlockMatcher->SetMinimumDisparity(mindisp);
+      m_NCCBlockMatcher->SetMaximumDisparity(maxdisp);
+
+      if(masking)
+        {
+        m_SSDBlockMatcher->SetMaskInput(m_BandMathFilter->GetOutput());
+        }
+
+      dispImage = m_NCCBlockMatcher->GetDisparityOutput();
+      metricImage = m_NCCBlockMatcher->GetMetricOutput();
+      }
+    
+
+    m_OutputImageList->Clear();
+
+    m_OutputImageList->PushBack(dispImage);
+
+    if(IsParameterEnabled("io.outmetric"))
+      {
+      m_OutputImageList->PushBack(metricImage);
+      }
+    
+    m_ImageListFilter->SetInput(m_OutputImageList);
+    
+    SetParameterOutputImage("io.out",m_ImageListFilter->GetOutput());
+
+    if(IsParameterEnabled("io.outmask"))
+      {
+      SetParameterOutputImage("io.outmask",m_BandMathFilter->GetOutput());
+      }
+  }
+
+  // SSD Block matching functor
+  SSDBlockMatchingFilterType::Pointer m_SSDBlockMatcher;
+  
+  // NCC Block matching functor
+  NCCBlockMatchingFilterType::Pointer m_NCCBlockMatcher;
+  
+  // Variance filter
+  VarianceFilterType::Pointer         m_VarianceFilter;
+  
+  // Band-math filter
+  BandMathFilterType::Pointer         m_BandMathFilter;
+
+  // The Image list
+  ImageListType::Pointer              m_OutputImageList;
+
+  // Image list to VectorImage filter
+  ImageListToVectorImageFilterType::Pointer m_ImageListFilter;
+
 };
 
 }
