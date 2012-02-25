61c4183b42a4d8d5a93c5ca2935d868f3b2405b9
Emmanuel Christophe <emmanuel.christophe@orfeo-toolbox.org>
2012-02-25 10:15:03 -0800

    COMP: detect compatibility with mapnik2.

--- CMake/ImportMapnik.cmake
+++ CMake/ImportMapnik.cmake
@@ -21,17 +21,17 @@ IF(OTB_USE_MAPNIK)
 		# when linking with mapnik, need to link with libicuuc (at least since 1.42 release)
         FIND_PATH(ICUUC_INCLUDE_DIR unicode/unistr.h)
         MARK_AS_ADVANCED(ICUUC_INCLUDE_DIR)
-		
+
         FIND_LIBRARY(ICUUC_LIBRARY icuuc)
         MARK_AS_ADVANCED(ICUUC_LIBRARY)
-        
+
         # ltdl (libtool dl)
         FIND_PATH(LTDL_INCLUDE_DIR ltdl.h)
         MARK_AS_ADVANCED(LTDL_INCLUDE_DIR)
-		
+
         FIND_LIBRARY(LTDL_LIBRARY ltdl)
         MARK_AS_ADVANCED(LTDL_LIBRARY)
-       
+
         IF (NOT MAPNIK_INCLUDE_DIR)
                 MESSAGE(FATAL_ERROR
                         "Cannot find MAPNIK include directory. Please set MAPNIK_INCLUDE_DIR or set OTB_USE_MAPNIK OFF.")
@@ -45,7 +45,7 @@ IF(OTB_USE_MAPNIK)
                 MESSAGE(FATAL_ERROR
                         "Cannot find FREETYPE2 include directory. Please set FREETYPE2_INCLUDE_DIR or set OTB_USE_MAPNIK OFF.")
         ENDIF (NOT FREETYPE2_INCLUDE_DIR)
-        
+
         IF (NOT ICUUC_INCLUDE_DIR)
                 MESSAGE(FATAL_ERROR
                         "Cannot find ICUUC_INCLUDE_DIR include directory. Please set ICUUC_INCLUDE_DIR or set OTB_USE_MAPNIK OFF.")
@@ -54,7 +54,7 @@ IF(OTB_USE_MAPNIK)
                 MESSAGE(FATAL_ERROR
                         "Cannot find ICUUC library, needed by MAPNIK. Please set ICUUC_LIBRARY or set OTB_USE_MAPNIK OFF.")
         ENDIF (NOT ICUUC_LIBRARY)
-        
+
         IF (NOT LTDL_INCLUDE_DIR)
                 MESSAGE(FATAL_ERROR
                         "Cannot find LTDL_INCLUDE_DIR include directory. Please set LTDL_INCLUDE_DIR or set OTB_USE_MAPNIK OFF.")
@@ -64,11 +64,25 @@ IF(OTB_USE_MAPNIK)
                         "Cannot find ICUUC library, needed by MAPNIK. Please set ICUUC_LIBRARY or set OTB_USE_MAPNIK OFF.")
         ENDIF (NOT LTDL_LIBRARY)
 
+
+        # Validating the config
+        TRY_COMPILE(MAPNIK_SUPPORTS_NEW_INTERFACE
+          ${CMAKE_CURRENT_BINARY_DIR}/CMake
+          ${CMAKE_CURRENT_SOURCE_DIR}/CMake/otbTestCompileMapnik2.cxx
+          CMAKE_FLAGS "-DINCLUDE_DIRECTORIES:PATH=${MAPNIK_INCLUDE_DIR};${ICUUC_INCLUDE_DIR};${LTDL_INCLUDE_DIR};${FREETYPE2_INCLUDE_DIR}" "-DLINK_LIBRARIES:STRING=${MAPNIK_LIBRARY}"
+          OUTPUT_VARIABLE OUTPUT)
+        IF(MAPNIK_SUPPORTS_NEW_INTERFACE)
+          MESSAGE(STATUS "  Testing if Mapnik2     -- yes")
+        ELSE(MAPNIK_SUPPORTS_NEW_INTERFACE)
+          MESSAGE(STATUS "  Testing if Mapnik2     -- no")
+          MESSAGE(FATAL_ERROR "Does not support mapnik2 interface: ${OUTPUT}")
+        ENDIF(MAPNIK_SUPPORTS_NEW_INTERFACE)
+
         # Add compiler option
         ADD_DEFINITIONS(-DOTB_USE_MAPNIK)
 
         INCLUDE_DIRECTORIES(${MAPNIK_INCLUDE_DIR} ${ICUUC_INCLUDE_DIR} ${LTDL_INCLUDE_DIR} ${FREETYPE2_INCLUDE_DIR})
-        
+
         MESSAGE(STATUS "  Enabling Mapnik support")
         MESSAGE(STATUS "  Mapnik includes : ${MAPNIK_INCLUDE_DIR}")
         MESSAGE(STATUS "  Mapnik library  : ${MAPNIK_LIBRARY}")
@@ -76,9 +90,9 @@ IF(OTB_USE_MAPNIK)
         MESSAGE(STATUS "  ICU library  : ${ICUUC_LIBRARY}")
         MESSAGE(STATUS "  LTDL includes : ${LTDL_INCLUDE_DIR}")
         MESSAGE(STATUS "  LTDL library  : ${LTDL_LIBRARY}")
-        
+
 ELSE(OTB_USE_MAPNIK)
 
     MESSAGE(STATUS "  Disabling Mapnik support")
-    
+
 ENDIF(OTB_USE_MAPNIK)
---
+++ CMake/otbTestCompileMapnik2.cxx
@@ -0,0 +1,13 @@
+#include <mapnik/feature.hpp>
+#include <mapnik/geometry.hpp>
+#include <mapnik/raster.hpp>
+
+int main(int argc, char* argv[])
+{
+ typedef mapnik::vertex<double, 2>  vertex2d;
+ typedef mapnik::geometry<vertex2d>  geom;
+ typedef boost::shared_ptr<mapnik::raster> raster_ptr;
+ typedef mapnik::feature<geom, raster_ptr> Feature;
+ typedef boost::shared_ptr<Feature>        feature_ptr;
+ feature_ptr mfeature = feature_ptr(new Feature(1));
+}
