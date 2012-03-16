0157ae10846ba174c2f0745f3a2b7ab7240f3263
Julien Malik <julien.malik@c-s.fr>
2012-03-16 23:20:07 +0100

    ENH: more cmake-ish FindOssim

--- CMake/FindOssim.cmake
+++ CMake/FindOssim.cmake
@@ -1,9 +1,9 @@
 # - Find Ossim
 # Find the native Ossim includes and library
 #
-#   OSSIM_FOUND       - True if Ossim found.
-#   OSSIM_INCLUDE_DIR - where to find tinyxml.h, etc.
-#   OSSIM_LIBRARIES   - List of libraries when using Ossim.
+#   OSSIM_FOUND        - True if Ossim found.
+#   OSSIM_INCLUDE_DIRS - where to find tinyxml.h, etc.
+#   OSSIM_LIBRARIES    - List of libraries when using Ossim.
 #
 
 IF( OSSIM_INCLUDE_DIR )
@@ -25,3 +25,12 @@ INCLUDE( FindPackageHandleStandardArgs )
 FIND_PACKAGE_HANDLE_STANDARD_ARGS( Ossim DEFAULT_MSG OSSIM_INCLUDE_DIR OSSIM_LIBRARY )
 
 MARK_AS_ADVANCED( OSSIM_INCLUDE_DIR OSSIM_LIBRARY )
+
+IF(OSSIM_FOUND)
+  SET(OSSIM_INCLUDE_DIRS ${OSSIM_INCLUDE_DIR})
+  SET(OSSIM_LIBRARIES ${OSSIM_LIBRARY})
+ELSE(OSSIM_FOUND)
+  SET(OSSIM_INCLUDE_DIRS)
+  SET(OSSIM_LIBRARIES)
+ENDIF(OSSIM_FOUND)
+
