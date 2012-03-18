42c9b119d0d24ca8804025798c65379c6b352348
Julien Malik <julien.malik@c-s.fr>
2012-03-18 21:12:45 +0100

    ENH: improve ImportCurl, avoid multiple find_package

--- CMake/ImportCurl.cmake
+++ CMake/ImportCurl.cmake
@@ -8,31 +8,18 @@ ENDIF(CURL_FOUND)
 
 MARK_AS_ADVANCED(OTB_USE_CURL)
 IF(OTB_USE_CURL)
-
-        FIND_PATH(CURL_INCLUDE_DIR NAMES curl/curl.h PATHS ${OSGEO4W_INCLUDE})
-        MARK_AS_ADVANCED(CURL_INCLUDE_DIR)
-        IF (NOT CURL_INCLUDE_DIR)
-                MESSAGE(FATAL_ERROR
-                        "Cannot find CURL include directory. Please set CURL_INCLUDE_DIR or SET OTB_USE_CURL OFF.")
-        ENDIF (NOT CURL_INCLUDE_DIR)
-
-        FIND_LIBRARY(CURL_LIBRARY NAMES curl libcurl_imp PATHS ${OSGEO4W_LIB} )
-        MARK_AS_ADVANCED(CURL_LIBRARY)
-        IF (NOT CURL_LIBRARY)
-                MESSAGE(FATAL_ERROR
-                        "Cannot find CURL library. Please set CURL_LIBRARY or SET OTB_USE_CURL OFF.")
-        ENDIF (NOT CURL_LIBRARY)
-
-        TRY_COMPILE(OTB_CURL_MULTI_AVAILABLE
+    IF(CURL_FOUND)
+      MESSAGE(STATUS "Using CURL external version")
+      TRY_COMPILE(OTB_CURL_MULTI_AVAILABLE
                     ${CMAKE_CURRENT_BINARY_DIR}/CMake
                     ${CMAKE_CURRENT_SOURCE_DIR}/CMake/TestCurlMulti.cxx
-                    CMAKE_FLAGS "-DINCLUDE_DIRECTORIES:PATH=${CURL_INCLUDE_DIR}" "-DLINK_LIBRARIES:STRING=${CURL_LIBRARY}"
+                    CMAKE_FLAGS "-DINCLUDE_DIRECTORIES:PATH=${CURL_INCLUDE_DIRS}" "-DLINK_LIBRARIES:STRING=${CURL_LIBRARIES}"
                     OUTPUT_VARIABLE OUTPUT)
-         
-        MESSAGE(STATUS "  Enabling Curl support")
-        MESSAGE(STATUS "  Curl includes : ${CURL_INCLUDE_DIR}")
-        MESSAGE(STATUS "  Curl library  : ${CURL_LIBRARY}")
-
+    ELSE(CURL_FOUND)
+      MESSAGE(FATAL_ERROR "CURL required but not found. "
+                          "Turn OTB_USE_CURL to OFF to disable CURL support")
+    ENDIF(CURL_FOUND)
 ELSE(OTB_USE_CURL)    
         MESSAGE(STATUS "  Disabling Curl support")
 ENDIF(OTB_USE_CURL)
+
