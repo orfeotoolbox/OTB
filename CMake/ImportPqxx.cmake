4d62f136c6a0f84bb9a49961c14227b54c7c7a8e
Julien Malik <julien.malik@c-s.fr>
2012-03-18 10:22:12 +0100

    STYLE

--- CMake/ImportPqxx.cmake
+++ CMake/ImportPqxx.cmake
@@ -5,14 +5,14 @@ MARK_AS_ADVANCED(OTB_USE_PQXX)
 
 IF(OTB_USE_PQXX)
   FIND_PACKAGE(Pqxx)
-    IF(PQXX_FOUND)
-      MESSAGE(STATUS "Activating Pqxx support")
-      ADD_DEFINITIONS(-DOTB_USE_PQXX)
-    ELSE(PQXX_FOUND)
-      # Generate an error if no external Pqxx is available
-      MESSAGE(FATAL_ERROR "Pqxx required but not found. "
-                          "Turn OTB_USE_PQXX to OFF")
-    ENDIF(PQXX_FOUND)
+  IF(PQXX_FOUND)
+    MESSAGE(STATUS "Activating Pqxx support")
+    ADD_DEFINITIONS(-DOTB_USE_PQXX)
+  ELSE(PQXX_FOUND)
+    # Generate an error if no external Pqxx is available
+    MESSAGE(FATAL_ERROR "Pqxx required but not found. "
+                        "Turn OTB_USE_PQXX to OFF")
+  ENDIF(PQXX_FOUND)
 ELSE(OTB_USE_PQXX)
   MESSAGE(STATUS "Disabling Pqxx support")
 ENDIF(OTB_USE_PQXX)
