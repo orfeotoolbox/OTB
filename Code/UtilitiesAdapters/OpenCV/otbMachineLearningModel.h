8434777488c961060ad6a3f0283fe3ffde29e8bd
Julien Michel <julien.michel@orfeo-toolbox.org>
2012-03-23 12:06:59 +0100

    ENH: Adding sample list interface

--- Code/UtilitiesAdapters/OpenCV/otbMachineLearningModel.h
+++ Code/UtilitiesAdapters/OpenCV/otbMachineLearningModel.h
@@ -19,20 +19,33 @@
 #define __otbMachineLearningModel_h
 
 #include "itkLightObject.h"
+#include "itkVariableLengthVector.h"
+#inlcude "itkFixedArray.h"
+#include "itkListSample.h"
 
 namespace otb
 {
-template <class TInputValue, class TOutputValue>
+template <class TInputValue, class TTargetValue>
 class ITK_EXPORT MachineLearningModel 
   : public itk::LightObject
 {
 public:
   /** Standard class typedefs. */
-  typedef SVMModel                      Self;
-  typedef itk::DataObject               Superclass;
-  typedef itk::SmartPointer<Self>       Pointer;
-  typedef itk::SmartPointer<const Self> ConstPointer;
+  typedef SVMModel                                      Self;
+  typedef itk::DataObject                               Superclass;
+  typedef itk::SmartPointer<Self>                       Pointer;
+  typedef itk::SmartPointer<const Self>                 ConstPointer;
 
+  // Input related typedefs
+  typedef TInputValue                                   InputValueType;
+  typedef itk::VariableLengthVector<InputValueType>     InputSampleType;
+  typedef itk::Statistics::ListSample<InputSampleType>  InputListSampleType;
+
+  // Target related typedefs
+  typedef TTargetValue                                  TargetValueType;
+  typedef itk::FixedArray<TargetValueType,1>            TargetSampleType;
+  typedef itk::Statistics::ListSample<TargetSampleType> TargetListSampleType;
+  
   /** Run-time type information (and related methods). */
   itkNewMacro(Self);
   itkTypeMacro(MachineLearningModel, itk::DataObject);
@@ -46,8 +59,16 @@ public:
   /** Save the model to file */
   virtual void Save() = 0;
 
-/** Load the model from file */
+  /** Load the model from file */
   virtual void Load() = 0;
+
+  /** Input accessors */
+  itkSetObjectMacro(InputListSample,InputListSampleType);
+  itkGetObjectMacro(InputListSample,InputListSampleType);
+
+  /** Target accessors */
+  itkSetObjectMacro(TargetListSample,TargetListSampleType);
+  itkGetObjectMacro(TargetListSample,TargetListSampleType);
   
 protected:
   /** Constructor */
@@ -58,6 +79,12 @@ protected:
 
   /** PrintSelf method */
   void PrintSelf(std::ostream& os, itk::Indent indent) const;
+
+  /** Input list sample */
+  typename InputListSampleType::Pointer m_InputListSample;
+
+  /** Target list sample */
+  typename TargetListSampleType::Pointer m_TargetListSample;
 
 private:
   MachineLearningModel(const Self &); //purposely not implemented
