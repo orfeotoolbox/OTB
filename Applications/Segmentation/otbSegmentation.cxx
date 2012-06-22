a36765041f3a540fbbc308a4b8c0a1458a68386a
Julien Malik <julien.malik@c-s.fr>
2012-06-22 18:43:10 +0200

    ENH: use the right mode for opening

--- Applications/Segmentation/otbSegmentation.cxx
+++ Applications/Segmentation/otbSegmentation.cxx
@@ -451,7 +451,7 @@ private:
       if(GetParameterString("mode.vector.outmode") == "ovw")
         {
         // Create the datasource
-        ogrDS = otb::ogr::DataSource::New(dataSourceName, otb::ogr::DataSource::Modes::Update_LayerOverwrite);
+        ogrDS = otb::ogr::DataSource::New(dataSourceName, otb::ogr::DataSource::Modes::Overwrite);
 
         // and create the layer since we are in overwrite mode, the
         // datasource is blank
