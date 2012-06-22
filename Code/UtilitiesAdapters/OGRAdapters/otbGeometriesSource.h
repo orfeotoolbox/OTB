e0ebd0a908a3e2a1621a84c4e2a580ec343aef03
Jonathan Guinet <jonathan.guinet@c-s.fr>
2012-06-22 15:24:24 +0200

    ENH: wrong itkTypeMacro in GeometriesSource.

--- Code/UtilitiesAdapters/OGRAdapters/otbGeometriesSource.h
+++ Code/UtilitiesAdapters/OGRAdapters/otbGeometriesSource.h
@@ -71,7 +71,7 @@ public:
   itkNewMacro(Self);
 
   /** Run-time type information (and related methods). */
-  itkTypeMacro(VectorDataSource, itk::ProcessObject);
+  itkTypeMacro(GeometriesSource, itk::ProcessObject);
   //@}
 
   /**\name GeometriesSet Output property. */
