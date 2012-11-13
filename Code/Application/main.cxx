ec2d0fd3758b0eb0a6634ab3021b593b7b4052a2
Stephane Albert <stephane.albert@c-s.fr>
2012-11-13 12:37:41 +0100

    ENH: Set up CMake skeleton files and added main skeleton.

---
+++ Code/Application/main.cxx
@@ -0,0 +1,53 @@
+/*=========================================================================
+
+  Program:   ORFEO Toolbox
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
+
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+//#include "ConfigureMonteverdi.h"
+
+//
+// System includes (sorted by alphabetic order)
+#include <cstdlib>
+
+//
+// Qt includes (sorted by alphabetic order)
+#include <QApplication>
+
+//
+// OTB includes (sorted by alphabetic order)
+
+//
+// Monteverdi includes (sorted by alphabetic order)
+
+
+//
+// Main functions definitions.
+//
+
+//
+// MAIN
+//
+int
+main( int argc, char* argv[] )
+{
+  QApplication application( argc, argv );
+
+  return application.exec();
+}
+
+//
+// Main function implementation.
+//
