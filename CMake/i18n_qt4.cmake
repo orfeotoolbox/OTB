538919aa8749b7c42eb3021f7d8c0ed7579dc66c
Stephane Albert <stephane.albert@c-s.fr>
2012-11-30 12:22:09 +0100

    COMP: Fixed listings of source files to translate. WARNING: Do not compile with -DMERGE_TS:BOOL=ON (compilation problem which is being fixed).

--- CMake/i18n_qt4.cmake
+++ CMake/i18n_qt4.cmake
@@ -1,6 +1,17 @@
 #
+# Declare and initialize variable to be present in parent scope.
+# set( Monteverdi2_I18N_SOURCE_FILES
+#   ""
+#   CACHE INTERNAL "" FORCE
+#   )
+#
 # Set locale human-readable translation files.
-macro( add_to_qt4_i18n_sources SOURCES)
-  # message( STATUS "Added I18N sources:'${ARGV}'" )
-  set( i18n_SOURCES ${i18n_SOURCES} ${ARGV} )
+macro( add_to_qt4_i18n_sources SOURCE_FILES )
+  #message( "i18n_sources 1: '${Monteverdi2_I18N_SOURCE_FILES}'" )
+  #message( STATUS "Adding I18N sources:'${ARGV}'" )
+  set( Monteverdi2_I18N_SOURCE_FILES
+    ${Monteverdi2_I18N_SOURCE_FILES} ${ARGV}
+    CACHE INTERNAL "" FORCE
+    )
+  #message( "i18n_sources 2: '${Monteverdi2_I18N_SOURCE_FILES}'" )
 endmacro()
