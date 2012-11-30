deaa5f725d575b1171a16ecdef9aa8f110fac6ec
Stephane Albert <stephane.albert@c-s.fr>
2012-11-30 12:46:37 +0100

    COMP: Fixed error while cmaking with '-DMERGE_TS:BOOL=ON' by removing wrapped moc_*.cxx and ui_*.h from translation build (not needed).

--- i18n/fr_FR.ts
+++ i18n/fr_FR.ts
@@ -1,4 +1,77 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.0" language="fr_FR">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>toolBar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Ctrl+Q</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Ctrl+O</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>mvd::Application</name>
+    <message>
+        <source>Running from build directory &apos;%1&apos;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Running from install directory &apos;%1&apos;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Failed to access translation-files directory &apos;%1&apos;.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Failed to load &apos;%1&apos; translation file in &apos;%2&apos;.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Successfully loaded &apos;%1&apos; translation file in &apos;%2&apos;.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&gt;DEBUG&gt; mvd::Application::OnAboutToQuit().</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>mvd::MainWindow</name>
+    <message>
+        <source>&gt;DEBUG&gt; Failed to load &apos;%s&apos; translation file.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&gt;DEBUG&gt; Loaded &apos;%s&apos; translation file.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Open file...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
 </TS>
