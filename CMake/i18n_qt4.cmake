020efcca5bc93f3785b22ac35140f51a2c64a741
Stephane Albert <stephane.albert@c-s.fr>
2012-12-10 10:59:21 +0100

    COMP: Added warning message when enabling -DMERGE-TS:BOOL=ON cmake cache variable in order to generate/merge .ts files.

--- CMake/i18n_qt4.cmake
+++ CMake/i18n_qt4.cmake
@@ -7,11 +7,8 @@
 #
 # Set locale human-readable translation files.
 macro( add_to_qt4_i18n_sources SOURCE_FILES )
-  #message( "i18n_sources 1: '${Monteverdi2_I18N_SOURCE_FILES}'" )
-  #message( STATUS "Adding I18N sources:'${ARGV}'" )
   set( Monteverdi2_I18N_SOURCE_FILES
     ${Monteverdi2_I18N_SOURCE_FILES} ${ARGV}
     CACHE INTERNAL "" FORCE
     )
-  #message( "i18n_sources 2: '${Monteverdi2_I18N_SOURCE_FILES}'" )
 endmacro()
