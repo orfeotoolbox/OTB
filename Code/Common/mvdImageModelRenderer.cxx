664931237502401b3cbbaea8ae63a1787229c834
Otmane Lahlou <otmane.lahlou@c-s.fr>
2012-12-11 13:52:24 +0100

    ADD: ImageModelRenderer skeleton

---
+++ Code/Common/mvdImageModelRenderer.cxx
@@ -0,0 +1,65 @@
+/*=========================================================================
+
+  Program:   Monteverdi2
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
+
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#include "mvdMyClass.h"
+
+//
+// System includes (sorted by alphabetic order)
+
+//
+// Qt includes (sorted by alphabetic order)
+
+//
+// ITK includes (sorted by alphabetic order)
+
+//
+// OTB includes (sorted by alphabetic order)
+
+//
+// Monteverdi includes (sorted by alphabetic order)
+
+namespace mvd
+{
+/*
+  TRANSLATOR mvd::ImageModelRender
+
+  Necessary for lupdate to be aware of C++ namespaces.
+
+  Context comment for translator.
+*/
+
+/*****************************************************************************/
+ImageModelRender
+::ImageModelRender( QObject* parent ) :
+  QObject( parent )
+{
+}
+
+/*****************************************************************************/
+ImageModelRender
+::~ImageModelRender()
+{
+}
+
+/*****************************************************************************/
+/* SLOTS                                                                     */
+/*****************************************************************************/
+
+/*****************************************************************************/
+
+} // end namespace 'mvd'
---
+++ Code/Common/mvdImageModelRenderer.h
@@ -0,0 +1,104 @@
+/*=========================================================================
+
+  Program:   Monteverdi2
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
+
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#ifndef __mvdImageModelRenderer_h
+#define __mvdImageModelRenderer_h
+
+//
+// System includes (sorted by alphabetic order)
+
+//
+// Qt includes (sorted by alphabetic order)
+#include <QObject>
+
+//
+// ITK includes (sorted by alphabetic order)
+#include "itkImageRegion.h"
+
+//
+// OTB includes (sorted by alphabetic order)
+
+//
+// Monteverdi includes (sorted by alphabetic order)
+
+//
+// External classes pre-declaration.
+namespace
+{
+}
+
+namespace mvd
+{
+//
+// Internal classes pre-declaration.
+
+/** \class ImageModelRenderer
+ *
+ */
+class ImageModelRenderer :
+    public QObject
+{
+  Q_OBJECT;
+
+//
+// Public methods.
+public:
+
+  typedef itk::ImageRegion<2>           ImageRegionType;
+
+  /** Constructor */
+  ImageModelRenderer( QObject* parent = NULL );
+
+  /** Destructor */
+  virtual ~ImageModelRenderer();
+
+  /** */
+  void paintGL(){}
+
+public slots:
+  void updateGL(const ImageRegionType& region){}
+
+//
+// SIGNALS.
+signals:
+
+//
+// Protected methods.
+protected:
+
+//
+// Protected attributes.
+protected:
+
+//
+// Private methods.
+private:
+
+
+//
+// Private attributes.
+private:
+
+//
+// SLOTS.
+private slots:
+};
+
+} // end namespace 'mvd'
+
+#endif // __mvdImageModelRenderer_h
