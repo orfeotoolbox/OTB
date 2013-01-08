3985f9992a59fde4302dc7bf322b2785a0259be8
Manuel Grizonnet <manuel.grizonnet@orfeo-toolbox.org>
2013-01-08 11:55:01 +0100

    STY: kwstyle

--- Examples/Projections/otbProjectionsExamplesTests.cxx
+++ Examples/Projections/otbProjectionsExamplesTests.cxx
@@ -6,14 +6,16 @@
   Version:   $Revision$
 
 
-    Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
-    See OTBCopyright.txt for details.
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
 
-    This software is distributed WITHOUT ANY WARRANTY; without even
-    the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-    PURPOSE.  See the above copyright notices for more information.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
 
 =========================================================================*/
+
 
 // this file defines the otbMultiScaleTest for the test driver
 // and all it expects is that you have a function called RegisterTests
