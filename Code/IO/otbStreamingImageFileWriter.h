0779b527be7c741f93fc5922ec53d62af174d694
Julien Michel <julien.michel@orfeo-toolbox.org>
2013-01-08 15:31:22 +0100

    ENH: Cleaner StreamingImageFileWriter stub class

--- Code/IO/otbStreamingImageFileWriter.h
+++ Code/IO/otbStreamingImageFileWriter.h
@@ -22,8 +22,49 @@
 
 #include "otbImageFileWriter.h"
 
-// StreamingImageFileWriter is deprecated. It is now defined as an
-// alias of ImageFileWriter class.
-#define StreamingImageFileWriter ImageFileWriter
+namespace otb
+{
+/** \class StreamingImageFileWriter
+ *  \brief Deprecated streaming-enabled image writing class
+ *
+ *  StreamingImageFileWriter is deprecated and provided for backward
+ *  compatibility only. Full support of streaming is now provided by
+ *  otb::ImageFileWriter, and StreamingImageFileWriter is a simple
+ *  alias of this class.
+ *
+ *  \sa ImageFileWriter
+ */
+template <class TInputImage> class ITK_EXPORT StreamingImageFileWriter : public ImageFileWriter<TInputImage>
+{
+public:
+  /** Standard class typedefs. */
+  typedef StreamingImageFileWriter       Self;
+  typedef ImageFileWriter<TInputImage>   Superclass;
+  typedef itk::SmartPointer<Self>        Pointer;
+  typedef itk::SmartPointer<const Self>  ConstPointer;
+
+  /** Method for creation through the object factory. */
+  itkNewMacro(Self);
+  
+  /** Run-time type information (and related methods). */
+  itkTypeMacro(ImageFileWriter, itk::ImageFileWriter);
+
+ /** Dimension of input image. */
+  itkStaticConstMacro(InputImageDimension, unsigned int,
+                      Superclass::InputImageDimension);
+  
+protected:
+  /** Constructor */
+  StreamingImageFileWriter();
+
+  /** Destructor */
+  virtual ~StreamingImageFileWriter();
+
+private:
+  StreamingImageFileWriter(const StreamingImageFileWriter &); //purposely not implemented
+  void operator =(const StreamingImageFileWriter&); //purposely not implemented
+};
+
+} // end namespace otb
 
 #endif
