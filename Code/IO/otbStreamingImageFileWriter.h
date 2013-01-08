5222ac6de02d631875a52afea76261c9980fbc34
Julien Michel <julien.michel@orfeo-toolbox.org>
2013-01-08 13:52:35 +0100

    ENH: Adding a stub StreamingImageFileWriter header for backward compatibility (wip)

---
+++ Code/IO/otbStreamingImageFileWriter.h
@@ -0,0 +1,29 @@
+/*=========================================================================
+
+  Program:   ORFEO Toolbox
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
+
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __otbStreamingImageFileWriter_h
+#define __otbStreamingImageFileWriter_h
+
+#include "vcl_deprecated_header.h"
+
+#include "otbImageFileWriter.h"
+
+// StreamingImageFileWriter is deprecated. It is now defined as an
+// alias of ImageFileWriter class.
+#define StreamingImageFileWriter ImageFileWriter
+
+#endif
