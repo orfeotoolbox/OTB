5f0046f8cfb4e1132a854917c9f9d0028d11094c
Otmane Lahlou <otmane.lahlou@c-s.fr>
2013-02-15 17:06:33 +0100

    ENH: implement QuicklookModel and make it inherit VectorImageModel

---
+++ Code/Common/mvdQuicklookModel.cxx
@@ -0,0 +1,103 @@
+/*=========================================================================
+
+  Program:   Monteverdi2
+  Language:  C++
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See Copyright.txt for details.
+
+  Monteverdi2 is distributed under the CeCILL licence version 2. See
+  Licence_CeCILL_V2-en.txt or
+  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt for more details.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+//
+// Qt includes (sorted by alphabetic order)
+//// Must be included before system/custom includes.
+
+//
+// System includes (sorted by alphabetic order)
+
+//
+// ITK includes (sorted by alphabetic order)
+#include "itksys/SystemTools.hxx"
+
+//
+// Monteverdi includes (sorted by alphabetic order)
+#include "mvdQuicklookModel.h"
+#include "mvdAlgorithm.h"
+
+
+namespace mvd
+{
+/*
+  TRANSLATOR mvd::QuicklookModel
+
+  Necessary for lupdate to be aware of C++ namespaces.
+
+  Context comment for translator.
+*/
+
+/*******************************************************************************/
+QuicklookModel
+::QuicklookModel( QObject* parent ) :
+  VectorImageModel( parent )
+{
+}
+
+/*******************************************************************************/
+QuicklookModel
+::~QuicklookModel()
+{
+}
+
+/*******************************************************************************/
+void
+QuicklookModel
+::virtual_BuildModel()
+{
+  //
+  // Step #1: Check if it is a multi-resolution file
+  
+    
+  //
+  // Step #2: get the image used as quicklook
+  VectorImageModel* imageModel = qobject_cast< VectorImageModel* >( parent() );
+
+  // TODO : remove size
+  this->LoadFile(imageModel->GetInputFilename(), 500, 500);
+}
+
+/*******************************************************************************/
+const VectorImageModel::Settings&
+QuicklookModel
+::GetSettings() const
+{
+  // Get the parent Abstract model
+  VectorImageModel * viModel = 
+    qobject_cast< VectorImageModel* >( parent() );
+
+  // use the same settings than the vector model
+  return viModel->GetSettings();
+}
+
+/*******************************************************************************/
+VectorImageModel::Settings&
+QuicklookModel
+::GetSettings()
+{
+  // Get the parent Abstract model
+  VectorImageModel * viModel = 
+    qobject_cast< VectorImageModel* >( parent() );
+
+  // use the same settings than the vector model
+  return viModel->GetSettings();
+}
+
+} // end namespace 'mvd'
---
+++ Code/Common/mvdQuicklookModel.h
@@ -0,0 +1,174 @@
+/*=========================================================================
+
+  Program:   Monteverdi2
+  Language:  C++
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See Copyright.txt for details.
+
+  Monteverdi2 is distributed under the CeCILL licence version 2. See
+  Licence_CeCILL_V2-en.txt or
+  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt for more details.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#ifndef __mvdQuicklookModel_h
+#define __mvdQuicklookModel_h
+
+//
+// Configuration include.
+//// Included at first position before any other ones.
+#include "ConfigureMonteverdi2.h"
+
+
+/*****************************************************************************/
+/* INCLUDE SECTION                                                           */
+
+//
+// Monteverdi includes (sorted by alphabetic order)
+#include "mvdTypes.h"
+#include "mvdVectorImageModel.h"
+
+//
+// Qt includes (sorted by alphabetic order)
+//// Must be included before system/custom includes.
+
+//
+// System includes (sorted by alphabetic order)
+
+//
+// ITK includes (sorted by alphabetic order)
+
+//
+// OTB includes (sorted by alphabetic order)
+
+
+
+/*****************************************************************************/
+/* PRE-DECLARATION SECTION                                                   */
+
+//
+// External classes pre-declaration.
+namespace
+{
+}
+
+namespace mvd
+{
+//
+// Internal classes pre-declaration.
+
+
+/*****************************************************************************/
+/* CLASS DEFINITION SECTION                                                  */
+
+/** \class QuicklookModel
+ *
+ */
+class Monteverdi2_EXPORT QuicklookModel :
+    public VectorImageModel
+{
+
+  /*-[ QOBJECT SECTION ]-----------------------------------------------------*/
+
+  Q_OBJECT;
+
+  /*-[ PUBLIC SECTION ]------------------------------------------------------*/
+
+//
+// Public types.
+public:
+  /**
+   * In-memory storage type of source image (from file).
+   */
+  typedef DefaultImageType SourceImageType;
+
+//
+// Public methods.
+public:
+
+  /** Constructor */
+  QuicklookModel( QObject * parent =NULL );
+
+  /** Destructor */
+  virtual ~QuicklookModel();
+
+  /**
+   */
+  const Settings& GetSettings() const;
+
+  /** */
+   Settings& GetSettings();
+
+  /*-[ PUBLIC SLOTS SECTION ]------------------------------------------------*/
+
+//
+// Public slots.
+public slots:
+
+  /*-[ SIGNALS SECTION ]-----------------------------------------------------*/
+
+//
+// Signals.
+signals:
+  /**
+   */
+  void SettingsUpdated();
+
+  /*-[ PROTECTED SECTION ]---------------------------------------------------*/
+
+//
+// Protected methods.
+protected:
+
+  /** */
+  virtual void virtual_BuildModel();
+
+//
+// Protected attributes.
+protected:
+
+  /*-[ PRIVATE SECTION ]-----------------------------------------------------*/
+
+//
+// Private types.
+private:
+  /**
+   * Display type of source image (to OpenGL).
+   */
+  typedef RGBAImageType DisplayImageType;
+
+  // /**
+  //  * Extract filter.
+  //  */
+  // typedef
+  //   itk::ExtractImageFilter< SourceImageType, SourceImageType >
+  //   ExtractFilterType;
+
+//
+// Private methods.
+private:
+
+//
+// Private attributes.
+private:
+
+  /*-[ PRIVATE SLOTS SECTION ]-----------------------------------------------*/
+
+//
+// Slots.
+private slots:
+  /** */
+};
+
+
+/*****************************************************************************/
+
+} // end namespace 'mvd'
+
+#endif // __mvdQuicklookModel_h
