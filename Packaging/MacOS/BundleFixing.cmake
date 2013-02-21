2a1bec64182c6a4ae958a6c56a711bf7437b8df0
Julien Malik <julien.malik@c-s.fr>
2013-02-21 19:44:35 +0100

    COMP: fix MacOS bundle generation

--- Packaging/MacOS/BundleFixing.cmake
+++ Packaging/MacOS/BundleFixing.cmake
@@ -1,4 +1,4 @@
 
 include(BundleUtilities)
 set(BU_CHMOD_BUNDLE_ITEMS ON)
-fixup_bundle("${CMAKE_INSTALL_PREFIX}/bin/monteverdi" "" "${CMAKE_INSTALL_PREFIX}/lib/otb")
+fixup_bundle("${CMAKE_INSTALL_PREFIX}/bin/monteverdi2" "" "${CMAKE_INSTALL_PREFIX}/lib/otb")
