30b7a4a0fc781b37a331e5e0739d68e687564468
Otmane Lahlou <otmane.lahlou@c-s.fr>
2013-03-12 15:59:35 +0100

    TEST: init applications toolBox test

---
+++ Testing/Common/otbApplicationsToolBoxTest.cxx
@@ -0,0 +1,41 @@
+/*=========================================================================
+
+  Program:   ORFEO Toolbox
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
+
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+//
+// Configuration include.
+//// Included at first position before any other ones.
+#include "ConfigureMonteverdi2.h"
+
+//
+// Qt includes (sorted by alphabetic order)
+//// Must be included before system/custom includes.
+
+//
+// System includes (sorted by alphabetic order)
+
+// Monteverdi includes (sorted by alphabetic order)
+#include "mvdApplicationsToolBox.h"
+
+int otbApplicationsToolBoxTest(int argc, char* argv[])
+{
+  QApplication app(argc, argv);
+  mvd::ApplicationsToolBox applToolBox;
+  applToolBox.show();
+  
+  return app.exec();
+
+}
