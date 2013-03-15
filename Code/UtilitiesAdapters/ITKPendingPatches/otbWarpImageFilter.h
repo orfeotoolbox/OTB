c34d473216f800e1f22b05756638270dc54c9901
Manuel Grizonnet <manuel.grizonnet@orfeo-toolbox.org>
2013-03-15 15:32:25 +0100

    ITK4: style - bad header

--- Code/UtilitiesAdapters/ITKPendingPatches/otbWarpImageFilter.h
+++ Code/UtilitiesAdapters/ITKPendingPatches/otbWarpImageFilter.h
@@ -1,17 +1,18 @@
 /*=========================================================================
 
-  Program:   Insight Segmentation & Registration Toolkit
-  Module:    $RCSfile: itkWarpImageFilter.h,v $
+  Program:   ORFEO Toolbox
   Language:  C++
-  Date:      $Date: 2009-10-29 11:19:00 $
-  Version:   $Revision: 1.31 $
+  Date:      $Date$
+  Version:   $Revision$
 
-  Copyright (c) Insight Software Consortium. All rights reserved.
-  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
 
-     This software is distributed WITHOUT ANY WARRANTY; without even
-     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-     PURPOSE.  See the above copyright notices for more information.
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
+
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notices for more information.
 
 =========================================================================*/
 #ifndef __otbWarpImageFilter_h
