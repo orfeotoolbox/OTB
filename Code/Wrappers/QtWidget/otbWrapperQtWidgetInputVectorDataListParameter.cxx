37c41eee2dcf4642a602bb0be9832882cef5dfcf
Sebastien Dinot <sebastien.dinot@c-s.fr>
2013-03-19 17:39:12 +0100

    STYLE

--- Code/Wrappers/QtWidget/otbWrapperQtWidgetInputVectorDataListParameter.cxx
+++ Code/Wrappers/QtWidget/otbWrapperQtWidgetInputVectorDataListParameter.cxx
@@ -26,7 +26,7 @@ QtWidgetInputVectorDataListParameter::QtWidgetInputVectorDataListParameter(Input
 : QtWidgetParameterBase(param, m),
   m_InputVectorDataListParam(param)
 {
- connect( this, SIGNAL(Change()), GetModel(), SLOT(NotifyUpdate()) );
+ connect(this, SIGNAL(Change()), GetModel(), SLOT(NotifyUpdate()));
 }
 
 QtWidgetInputVectorDataListParameter::~QtWidgetInputVectorDataListParameter()
@@ -48,7 +48,7 @@ void QtWidgetInputVectorDataListParameter::DoCreateWidget()
   QHBoxLayout * hLayout = new QHBoxLayout;
   hLayout->setSpacing(sp);
   hLayout->setContentsMargins(sp, sp, sp, sp);
- 
+
   // Button layout
   QVBoxLayout * buttonLayout = new QVBoxLayout;
   buttonLayout->setSpacing(sp);
@@ -67,7 +67,7 @@ void QtWidgetInputVectorDataListParameter::DoCreateWidget()
   addButton->setText("+");
   addButton->setFixedWidth(buttonSize);
   addButton->setToolTip("Add a file selector...");
-  connect( addButton, SIGNAL(clicked()), this, SLOT(AddFile()) );
+  connect(addButton, SIGNAL(clicked()), this, SLOT(AddFile()));
   addSupLayout->addWidget(addButton);
 
   // Supress file button
@@ -75,7 +75,7 @@ void QtWidgetInputVectorDataListParameter::DoCreateWidget()
   supButton->setText("-");
   supButton->setFixedWidth(buttonSize);
   supButton->setToolTip("Supress the selected file...");
-  connect( supButton, SIGNAL(clicked()), this, SLOT(SupressFile()) );
+  connect(supButton, SIGNAL(clicked()), this, SLOT(SupressFile()));
   addSupLayout->addWidget(supButton);
   buttonLayout->addLayout(addSupLayout);
 
@@ -84,7 +84,7 @@ void QtWidgetInputVectorDataListParameter::DoCreateWidget()
   upButton->setText("Up");
   upButton->setFixedWidth(buttonSize);
   upButton->setToolTip("Up the selected file in the list...");
-  connect( upButton, SIGNAL(clicked()), this, SLOT(UpFile()) );
+  connect(upButton, SIGNAL(clicked()), this, SLOT(UpFile()));
   upDownLayout->addWidget(upButton);
 
   // Down file edit
@@ -92,7 +92,7 @@ void QtWidgetInputVectorDataListParameter::DoCreateWidget()
   downButton->setText("Down");
   downButton->setFixedWidth(buttonSize);
   downButton->setToolTip("Down the selected file in the list...");
-  connect( downButton, SIGNAL(clicked()), this, SLOT(DownFile()) );
+  connect(downButton, SIGNAL(clicked()), this, SLOT(DownFile()));
   upDownLayout->addWidget(downButton);
   buttonLayout->addLayout(upDownLayout);
 
@@ -101,7 +101,7 @@ void QtWidgetInputVectorDataListParameter::DoCreateWidget()
   eraseButton->setText("Erase");
   eraseButton->setFixedWidth(2*(buttonSize+sp));
   eraseButton->setToolTip("Erase the selected file of the list...");
-  connect( eraseButton, SIGNAL(clicked()), this, SLOT(EraseFile()) );
+  connect(eraseButton, SIGNAL(clicked()), this, SLOT(EraseFile()));
   buttonLayout->addWidget(eraseButton);
 
   QVBoxLayout * fileLayout = new QVBoxLayout();
@@ -109,9 +109,9 @@ void QtWidgetInputVectorDataListParameter::DoCreateWidget()
 
   QtFileSelectionWidget * fileSelection = new QtFileSelectionWidget();
   fileSelection->setFixedHeight(30);
-  fileLayout->addWidget( fileSelection );
+  fileLayout->addWidget(fileSelection);
   m_InputVectorDataListParam->AddNullElement();
-  connect( fileSelection->GetInput(), SIGNAL(textChanged(const QString&)), this, SLOT(UpdateVectorDataList()) );
+  connect(fileSelection->GetInput(), SIGNAL(textChanged(const QString&)), this, SLOT(UpdateVectorDataList()));
 
   m_FileSelectionList.push_back(fileSelection);
 
@@ -137,13 +137,13 @@ void QtWidgetInputVectorDataListParameter::DoCreateWidget()
 void
 QtWidgetInputVectorDataListParameter::UpdateVectorDataList()
 {
-  for(unsigned int j=0; j<m_InputVectorDataListParam->GetVectorDataList()->Size(); j++ )
+  for(unsigned int j = 0; j < m_InputVectorDataListParam->GetVectorDataList()->Size(); j++)
     {
-    if( m_InputVectorDataListParam->SetNthFileName(j, m_FileSelectionList[j]->GetFilename()) == false )
+    if(m_InputVectorDataListParam->SetNthFileName(j, m_FileSelectionList[j]->GetFilename()) == false)
       {
       std::ostringstream oss;
       oss << "The given file " << m_FileSelectionList[j]->GetFilename() << " is not valid.";
-      this->GetModel()->SendLogWARNING( oss.str() );
+      this->GetModel()->SendLogWARNING(oss.str());
       m_FileSelectionList[j]->ClearFilename();
       }
     }
@@ -155,7 +155,7 @@ QtWidgetInputVectorDataListParameter::UpdateVectorDataList()
 void
 QtWidgetInputVectorDataListParameter::UpFile()
 {
- if(m_FileSelectionList.size() < 2 )
+ if(m_FileSelectionList.size() < 2)
     return;
 
   m_FileLayout = new QVBoxLayout();
@@ -163,26 +163,26 @@ QtWidgetInputVectorDataListParameter::UpFile()
 
   // Map link between old and new index in the list
   std::map<unsigned int, unsigned int> idMap;
-  
+
   // Init map
-  for(unsigned int i=0; i<m_FileSelectionList.size(); i++ )
+  for(unsigned int i = 0; i < m_FileSelectionList.size(); i++)
     {
     idMap[i] = i;
     }
-  
+
   // If the first item is checked, uncheck it...
   // It won't be moved
-  if( m_FileSelectionList[0]->IsChecked() )
+  if(m_FileSelectionList[0]->IsChecked())
     {
     m_FileSelectionList[0]->SetChecked(false);
     }
 
- 
+
   // If other item are checked, up the index
   // Starts at 1 because the first item mustn't move
-  for(unsigned int i=1; i<m_FileSelectionList.size(); i++ )
+  for(unsigned int i = 1; i < m_FileSelectionList.size(); i++)
     {
-    if( m_FileSelectionList[i]->IsChecked() )
+    if(m_FileSelectionList[i]->IsChecked())
       {
       unsigned int tmp = idMap[i];
       idMap[i] = i-1;
@@ -190,15 +190,15 @@ QtWidgetInputVectorDataListParameter::UpFile()
       }
     }
 
-  this->UpdateFileList( idMap );
- 
+  this->UpdateFileList(idMap);
+
   this->RecreateVectorDataList();
 }
 
 void
 QtWidgetInputVectorDataListParameter::DownFile()
 {
-  if (m_FileSelectionList.size() < 2) return;
+  if(m_FileSelectionList.size() < 2) return;
 
   m_FileLayout = new QVBoxLayout();
   m_FileLayout->setSpacing(0);
@@ -207,23 +207,23 @@ QtWidgetInputVectorDataListParameter::DownFile()
   std::map<unsigned int, unsigned int> idMap;
 
   // Init map
-  for (unsigned int i = 0; i < m_FileSelectionList.size(); i++)
+  for(unsigned int i = 0; i < m_FileSelectionList.size(); i++)
     {
     idMap[i] = i;
     }
 
   // If the last item is checked, uncheck it...
   // It won't be moved
-  if (m_FileSelectionList[m_FileSelectionList.size() - 1]->IsChecked())
+  if(m_FileSelectionList[m_FileSelectionList.size() - 1]->IsChecked())
     {
     m_FileSelectionList[m_FileSelectionList.size() - 1]->SetChecked(false);
     }
 
   // If other item are checked, up the index
   // Stops at size-1 because the last item mustn't move
-  for (int i = m_FileSelectionList.size() - 2; i >= 0; i--)
+  for(int i = m_FileSelectionList.size() - 2; i >= 0; i--)
     {
-    if (m_FileSelectionList[i]->IsChecked())
+    if(m_FileSelectionList[i]->IsChecked())
       {
       unsigned int tmp = idMap[i];
       idMap[i] = i + 1;
@@ -238,18 +238,18 @@ QtWidgetInputVectorDataListParameter::DownFile()
 
 
 void
-QtWidgetInputVectorDataListParameter::UpdateFileList( std::map<unsigned int, unsigned int> idMap )
+QtWidgetInputVectorDataListParameter::UpdateFileList(std::map<unsigned int, unsigned int> idMap)
 {
   std::vector<QtFileSelectionWidget *> tmpList;
   // Keys become values and inverse
   std::map<unsigned int, unsigned int> idMapBis;
-  for (unsigned int i = 0; i < idMap.size(); i++)
+  for(unsigned int i = 0; i < idMap.size(); i++)
     {
     idMapBis[idMap[i]] = i;
     }
 
   // Create the new item list
-  for (unsigned int i = 0; i < m_FileSelectionList.size(); i++)
+  for(unsigned int i = 0; i < m_FileSelectionList.size(); i++)
     {
     m_FileLayout->addWidget(m_FileSelectionList[idMapBis[i]]);
     tmpList.push_back(m_FileSelectionList[idMapBis[i]]);
@@ -263,7 +263,7 @@ QtWidgetInputVectorDataListParameter::UpdateFileList( std::map<unsigned int, uns
   this->update();
 
   // notify of value change
-  QString key( m_InputVectorDataListParam->GetKey() );
+  QString key(m_InputVectorDataListParam->GetKey());
   emit ParameterChanged(key);
 }
 
@@ -274,7 +274,7 @@ QtWidgetInputVectorDataListParameter::AddFile()
   m_FileLayout = new QVBoxLayout();
   m_FileLayout->setSpacing(0);
 
-  for (unsigned int i = 0; i < m_FileSelectionList.size(); i++)
+  for(unsigned int i = 0; i < m_FileSelectionList.size(); i++)
     {
     m_FileLayout->addWidget(m_FileSelectionList[i]);
     }
@@ -297,11 +297,11 @@ void
 QtWidgetInputVectorDataListParameter::SupressFile()
 {
   m_FileLayout = new QVBoxLayout();
-  m_FileLayout->setSpacing(0 );
+  m_FileLayout->setSpacing(0);
   std::vector<QtFileSelectionWidget *> tmpList;
-  for (unsigned int i = 0; i < m_FileSelectionList.size(); i++)
+  for(unsigned int i = 0; i < m_FileSelectionList.size(); i++)
     {
-    if (!m_FileSelectionList[i]->IsChecked())
+    if(!m_FileSelectionList[i]->IsChecked())
       {
       m_FileLayout->addWidget(m_FileSelectionList[i]);
       tmpList.push_back(m_FileSelectionList[i]);
@@ -347,23 +347,22 @@ void QtWidgetInputVectorDataListParameter::RecreateVectorDataList()
   // save value
   m_InputVectorDataListParam->ClearValue();
 
-  if( m_FileSelectionList.size() == 0)
+  if(m_FileSelectionList.size() == 0)
     {
     this->AddFile();
     }
   else
     {
-    for (unsigned int j = 0; j < m_FileSelectionList.size(); j++)
+    for(unsigned int j = 0; j < m_FileSelectionList.size(); j++)
       {
       m_InputVectorDataListParam->AddFromFileName(m_FileSelectionList[j]->GetFilename());
       connect(m_FileSelectionList[j]->GetInput(), SIGNAL(textChanged(const QString&)), this, SLOT(UpdateVectorDataList()));
       }
-    
+
     emit Change();
     // notify of value change
-    QString key( m_InputVectorDataListParam->GetKey() );
+    QString key(m_InputVectorDataListParam->GetKey());
     emit ParameterChanged(key);
-    
     }
 }
 
