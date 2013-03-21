562980aad1cf958d421f3965eccacefbd52f2636
Manuel Grizonnet <manuel.grizonnet@orfeo-toolbox.org>
2013-03-21 18:31:50 +0100

    ITK4: fork itkBinaryDilateImageFilter and itkBinaryErodeImageFilter
    This classes have been gfork and static variables have been removed to avoid dl.close error in applications.
    I used itykv4 version of classes to do the fork.
    Forked classes are only use in the BinaryMorphological applications. It is still the internal itkv3
    which is used in OTB.
    Need to report this problem to ITK in JIRA, when the problem is solve on the ITK side we would safely remove these classes and use ITK classes

--- Applications/FeatureExtraction/otbBinaryMorphologicalOperation.cxx
+++ Applications/FeatureExtraction/otbBinaryMorphologicalOperation.cxx
@@ -22,8 +22,8 @@
 #include "itkBinaryBallStructuringElement.h"
 #include "itkBinaryCrossStructuringElement.h"
 
-#include "itkBinaryDilateImageFilter.h"
-#include "itkBinaryErodeImageFilter.h"
+#include "otbBinaryDilateImageFilter.h"
+#include "otbBinaryErodeImageFilter.h"
 #include "itkBinaryMorphologicalOpeningImageFilter.h"
 #include "itkBinaryMorphologicalClosingImageFilter.h"
 
