de56c734f5b66df525b70dc231e64fc0f968f74e
Otmane Lahlou <otmane.lahlou@c-s.fr>
2013-03-25 18:00:54 +0100

    ADD: add wrapped Applications widget model and controller to fit the used pattern

---
+++ Code/Common/Core/mvdOTBApplicationsModel.cxx
@@ -0,0 +1,108 @@
+/*=========================================================================
+
+  Program:   Monteverdi2
+  Language:  C++
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See Copyright.txt for details.
+
+  Monteverdi2 is distributed under the CeCILL licence version 2. See
+  Licence_CeCILL_V2-en.txt or
+  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt for more details.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#include "mvdOTBApplicationsModel.h"
+
+
+/*****************************************************************************/
+/* INCLUDE SECTION                                                           */
+
+//
+// Qt includes (sorted by alphabetic order)
+//// Must be included before system/custom includes.
+
+//
+// System includes (sorted by alphabetic order)
+
+//
+// ITK includes (sorted by alphabetic order)
+
+//
+// OTB includes (sorted by alphabetic order)
+
+//
+// Monteverdi includes (sorted by alphabetic order)
+#include "ApplicationsWrapper/mvdApplicationsBrowser.h"
+
+namespace mvd
+{
+/*
+  TRANSLATOR mvd::OTBApplicationsModel
+
+  Necessary for lupdate to be aware of C++ namespaces.
+
+  Context comment for translator.
+*/
+
+
+/*****************************************************************************/
+/* CONSTANTS                                                                 */
+
+
+/*****************************************************************************/
+/* STATIC IMPLEMENTATION SECTION                                             */
+
+/*****************************************************************************/
+/* CLASS IMPLEMENTATION SECTION                                              */
+
+/*******************************************************************************/
+OTBApplicationsModel
+::OTBApplicationsModel( QObject* parent ) :
+  AbstractModel( parent )
+{
+  //
+  // instanciate an app browser
+  m_ApplicationsBrowser = new ApplicationsBrowser();  
+  
+  // initialize the app browser with empty string. The default
+  // behavior use the env ITK_AUTOLOAD_PATH
+  m_ApplicationsBrowser->SetAutoLoadPath("");
+}
+
+/*******************************************************************************/
+OTBApplicationsModel
+::~OTBApplicationsModel()
+{
+}
+
+/*******************************************************************************/
+void
+OTBApplicationsModel
+::virtual_BuildModel( void* context )
+{
+  // nothing to do
+}
+
+/*******************************************************************************/
+void
+OTBApplicationsModel
+::FillTree( )
+{
+  //
+  // the method called is connected to the widget via a signal. In
+  // ApplicationBrowser, when the map is filled with tags/Apps, a
+  // signal is emitted and received by the widget to fill its tree.
+  m_ApplicationsBrowser->SearchAvailableApplicationsTags();
+}
+
+/*******************************************************************************/
+/* SLOTS                                                                       */
+/*******************************************************************************/
+
+} // end namespace 'mvd'
---
+++ Code/Common/Gui/mvdApplicationsToolBoxController.cxx
@@ -0,0 +1,152 @@
+/*=========================================================================
+
+  Program:   Monteverdi2
+  Language:  C++
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See Copyright.txt for details.
+
+  Monteverdi2 is distributed under the CeCILL licence version 2. See
+  Licence_CeCILL_V2-en.txt or
+  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt for more details.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#include "Gui/mvdApplicationsToolBoxController.h"
+
+
+/*****************************************************************************/
+/* INCLUDE SECTION                                                           */
+
+//
+// Qt includes (sorted by alphabetic order)
+//// Must be included before system/custom includes.
+
+//
+// System includes (sorted by alphabetic order)
+
+//
+// ITK includes (sorted by alphabetic order)
+
+//
+// OTB includes (sorted by alphabetic order)
+
+//
+// Monteverdi includes (sorted by alphabetic order)
+//
+#include "Core/mvdOTBApplicationsModel.h"
+#include "Core/mvdTypes.h"
+//
+#include "Gui/mvdApplicationsToolBox.h"
+#include "ApplicationsWrapper/mvdApplicationsBrowser.h"
+
+namespace mvd
+{
+/*
+  TRANSLATOR mvd::ApplicationsToolBoxController
+
+  Necessary for lupdate to be aware of C++ namespaces.
+
+  Context comment for translator.
+*/
+
+
+/*****************************************************************************/
+/* CLASS IMPLEMENTATION SECTION                                              */
+
+/*******************************************************************************/
+ApplicationsToolBoxController
+::ApplicationsToolBoxController( ApplicationsToolBox* widget, QObject* parent ) :
+  AbstractModelController( widget, parent )
+{}
+
+/*******************************************************************************/
+ApplicationsToolBoxController
+::~ApplicationsToolBoxController()
+{
+}
+
+/*******************************************************************************/
+void
+ApplicationsToolBoxController
+::Connect( AbstractModel* model )
+{
+  std::cout << "--> ApplicationsToolBoxController connection "<< std::endl;
+  
+  //
+  // Access widget
+  ApplicationsToolBox* widget = GetWidget< ApplicationsToolBox >();
+
+  //
+  // Access model.
+  OTBApplicationsModel* appModel = qobject_cast< OTBApplicationsModel* >(model);
+  assert( appModel!=NULL );
+
+  //
+  // connections
+  QObject::connect(appModel->GetApplicationsBrowser(), 
+                   SIGNAL( AvailableApplicationsTagsChanged(const ApplicationsTagContainer& ) ),
+                   widget,
+                   SLOT( OnAvailableApplicationsTagsChanged(const ApplicationsTagContainer& ) ) );
+}
+
+/*******************************************************************************/
+void
+ApplicationsToolBoxController
+::Disconnect( AbstractModel* model )
+{
+  //
+  // Access widget
+  ApplicationsToolBox* widget = GetWidget< ApplicationsToolBox >();
+
+  //
+  // Access model.
+  OTBApplicationsModel* appModel = qobject_cast< OTBApplicationsModel* >(model);
+  assert( appModel!=NULL );
+
+  //
+  // connections
+  QObject::disconnect(appModel->GetApplicationsBrowser(),
+                      SIGNAL( AvailableApplicationsTagsChanged(const ApplicationsTagContainer& ) ),
+                      widget,
+                      SLOT( OnAvailableApplicationsTagsChanged(const ApplicationsTagContainer& ) )
+    );
+}
+
+/*******************************************************************************/
+void
+ApplicationsToolBoxController
+::ResetWidget()
+{
+  // Reset widget.
+  ResetDatasetTree();
+
+  // NOTE : no signal to emit here. Done in the class ApplicationsBrowser
+  // aggregated in the model
+}
+
+/*******************************************************************************/
+void
+ApplicationsToolBoxController
+::ResetDatasetTree()
+{
+  //
+  // Access model.
+  OTBApplicationsModel* model = GetModel< OTBApplicationsModel >();
+  assert( model!=NULL );
+
+  // 
+  // Fill the widget. It also reset the datatree before filling the
+  // tree...
+  model->FillTree();
+}
+
+/*******************************************************************************/
+/* SLOTS                                                                       */
+/*******************************************************************************/
+
+} // end namespace 'mvd'
