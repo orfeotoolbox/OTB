6e7e88dddd62354e4e66b21c2390ba056c57b81d
Otmane Lahlou <otmane.lahlou@c-s.fr>
2013-03-27 16:00:56 +0100

    ENH: implement a custon QTreeWidget (prompted in DatabaseBrowserWiWidget) to handle proprely drag and MIME data to be dropped

---
+++ Code/Common/Gui/mvdDatabaseTreeWidget.cxx
@@ -0,0 +1,142 @@
+/*=========================================================================
+
+  Program:   Monteverdi2
+  Language:  C++
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See Copyright.txt for details.
+
+  Monteverdi2 is distributed under the CeCILL licence version 2. See
+  Licence_CeCILL_V2-en.txt or
+  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt for more details.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#include "Gui/mvdDatabaseTreeWidget.h"
+//#include "Gui/ui_mvdDatabaseTreeWidget.h"
+
+
+/*****************************************************************************/
+/* INCLUDE SECTION                                                           */
+
+//
+// Qt includes (sorted by alphabetic order)
+//// Must be included before system/custom includes.
+
+//
+// System includes (sorted by alphabetic order)
+
+//
+// ITK includes (sorted by alphabetic order)
+
+//
+// OTB includes (sorted by alphabetic order)
+
+//
+// Monteverdi includes (sorted by alphabetic order)
+#include "Core/mvdAlgorithm.h"
+
+namespace mvd
+{
+
+/*
+  TRANSLATOR mvd::DatabaseTreeWidget
+
+  Necessary for lupdate to be aware of C++ namespaces.
+
+  Context comment for translator.
+*/
+
+
+/*****************************************************************************/
+/* CONSTANTS                                                                 */
+
+
+/*****************************************************************************/
+/* STATIC IMPLEMENTATION SECTION                                             */
+
+
+/*****************************************************************************/
+/* CLASS IMPLEMENTATION SECTION                                              */
+
+/*******************************************************************************/
+DatabaseTreeWidget
+::DatabaseTreeWidget( QWidget* parent  ):
+  QTreeWidget( parent),
+  m_DatasetFilename("")
+{
+  //setMouseTracking(true);
+  setDragEnabled(true);
+}
+
+/*******************************************************************************/
+DatabaseTreeWidget
+::~DatabaseTreeWidget()
+{
+}
+
+/*******************************************************************************/
+void 
+DatabaseTreeWidget::mouseMoveEvent( QMouseEvent * event )
+{
+  //
+  // superclass event handling
+  QTreeWidget::mouseMoveEvent(event);
+}
+
+/*******************************************************************************/
+void
+DatabaseTreeWidget::mousePressEvent(QMouseEvent *event)
+ {
+   //
+  // superclass event handling
+   QTreeWidget::mousePressEvent(event);
+   
+   if (event->buttons() & Qt::LeftButton)
+     {
+     m_StartDragPosition = event->pos();
+     }
+
+  //
+  // 
+  if ( event->buttons() &  Qt::LeftButton )
+    {
+    // Get current selection
+    QTreeWidgetItem *selectedItem = currentItem();
+
+    if (selectedItem)
+      {
+      //
+      // TODO : get the image filename  of the selected dataset
+      
+      QByteArray itemData( ToStdString (m_DatasetFilename ).c_str() );
+ 
+      QMimeData *mimeData = new QMimeData;
+      mimeData->setData("application/x-qabstractitemmodeldatalist", itemData);
+
+      // Create drag
+      QDrag *drag = new QDrag(this);
+      drag->setMimeData(mimeData);
+
+      drag->exec(Qt::CopyAction | Qt::MoveAction);
+      }
+    }
+ }
+
+/*******************************************************************************/
+/* SLOTS                                                                       */
+/*******************************************************************************/
+void
+DatabaseTreeWidget::OnSelectedDatasetFilenameChanged(const QString& filename)
+{
+  //
+  // update the dataset image filename to be used in the dragged mime data
+  m_DatasetFilename = filename;
+}
+
+} // end namespace 'mvd'
+
---
+++ Code/Common/Gui/mvdDatabaseTreeWidget.h
@@ -0,0 +1,154 @@
+/*=========================================================================
+
+  Program:   Monteverdi2
+  Language:  C++
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See Copyright.txt for details.
+
+  Monteverdi2 is distributed under the CeCILL licence version 2. See
+  Licence_CeCILL_V2-en.txt or
+  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt for more details.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __mvdDatabaseTreeWidget_h
+#define __mvdDatabaseTreeWidget_h
+
+//
+// Configuration include.
+//// Included at first position before any other ones.
+#include "ConfigureMonteverdi2.h"
+
+
+/*****************************************************************************/
+/* INCLUDE SECTION                                                           */
+
+//
+// Qt includes (sorted by alphabetic order)
+//// Must be included before system/custom includes.
+#include <QtGui>
+
+//
+// System includes (sorted by alphabetic order)
+
+//
+// ITK includes (sorted by alphabetic order)
+
+//
+// OTB includes (sorted by alphabetic order)
+
+//
+// Monteverdi includes (sorted by alphabetic order)
+
+
+/*****************************************************************************/
+/* PRE-DECLARATION SECTION                                                   */
+
+//
+// External classes pre-declaration.
+namespace
+{
+}
+
+namespace mvd
+{
+
+//
+// Internal classes pre-declaration.
+namespace Ui
+{
+//class DatabaseTreeWidget;
+};
+
+
+/*****************************************************************************/
+/* CLASS DEFINITION SECTION                                                  */
+
+/**
+ * \class DatabaseTreeWidget
+ *
+ * \brief Widget template skeleton to copy-paste when adding a new
+ * widget class.
+ */
+class Monteverdi2_EXPORT DatabaseTreeWidget :
+    public QTreeWidget
+{
+
+  /*-[ QOBJECT SECTION ]-----------------------------------------------------*/
+
+  Q_OBJECT;
+
+  /*-[ PUBLIC SECTION ]------------------------------------------------------*/
+
+//
+// Public methods.
+public:
+
+  /** \brief Constructor. */
+  DatabaseTreeWidget( QWidget* parent =NULL );
+
+  /** \brief Destructor. */
+  virtual ~DatabaseTreeWidget();
+
+  void mouseMoveEvent( QMouseEvent * event );
+  void mousePressEvent( QMouseEvent * event );
+
+  /*-[ PUBLIC SLOTS SECTION ]------------------------------------------------*/
+
+//
+// Public SLOTS.
+public slots:
+
+    void OnSelectedDatasetFilenameChanged(const QString& filename);
+
+  /*-[ SIGNALS SECTION ]-----------------------------------------------------*/
+
+//
+// Signals.
+signals:
+  
+  /*-[ PROTECTED SECTION ]---------------------------------------------------*/
+
+//
+// Protected methods.
+protected:
+
+  /*-[ PRIVATE SECTION ]-----------------------------------------------------*/
+
+//
+// Protected attributes.
+protected:
+
+//
+// Private methods.
+private:
+
+//
+// Private attributes.
+private:
+
+  QPoint   m_StartDragPosition;
+  QString  m_DatasetFilename; 
+
+  /*-[ PRIVATE SLOTS SECTION ]-----------------------------------------------*/
+
+//
+// Slots.
+private slots:
+};
+
+} // end namespace 'mvd'
+
+/*****************************************************************************/
+/* INLINE SECTION                                                            */
+
+namespace mvd
+{
+} // end namespace 'mvd'
+
+#endif // __mvdDatabaseTreeWidget_h
