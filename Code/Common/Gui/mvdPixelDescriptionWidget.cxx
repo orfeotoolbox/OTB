881b5520bebfcd946889425da77a94356014eca4
Otmane Lahlou <otmane.lahlou@c-s.fr>
2013-05-31 14:15:55 +0200

    ADD: add necessary classes for PixelDescription

---
+++ Code/Common/Gui/mvdPixelDescriptionWidget.cxx
@@ -0,0 +1,182 @@
+/*=========================================================================
+
+  Program:   Monteverdi2
+  Language:  C++
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See Copyright.txt for details.
+
+  Monteverdi2 is distributed under the CeCILL licence version 2. See
+  Licence_CeCILL_V2-en.txt or
+  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt for more details.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#include "Gui/mvdPixelDescriptionWidget.h"
+#include "Gui/ui_mvdPixelDescriptionWidget.h"
+
+
+/*****************************************************************************/
+/* INCLUDE SECTION                                                           */
+
+//
+// Qt includes (sorted by alphabetic order)
+//// Must be included before system/custom includes.
+
+//
+// System includes (sorted by alphabetic order)
+
+//
+// ITK includes (sorted by alphabetic order)
+
+//
+// OTB includes (sorted by alphabetic order)
+
+//
+// Monteverdi includes (sorted by alphabetic order)
+#include "Core/mvdAlgorithm.h"
+
+namespace mvd
+{
+/*
+  TRANSLATOR mvd::PixelDescriptionWidget
+
+  Necessary for lupdate to be aware of C++ namespaces.
+
+  Context comment for translator.
+*/
+
+
+/*****************************************************************************/
+/* CONSTANTS                                                                 */
+
+
+/*****************************************************************************/
+/* STATIC IMPLEMENTATION SECTION                                             */
+
+
+/*****************************************************************************/
+/* CLASS IMPLEMENTATION SECTION                                              */
+
+/*******************************************************************************/
+PixelDescriptionWidget
+::PixelDescriptionWidget( QWidget* parent, Qt::WindowFlags flags  ):
+  QWidget( parent, flags ),
+  m_UI( new mvd::Ui::PixelDescriptionWidget() )
+{
+  m_UI->setupUi( this );
+
+  SetupUI();
+}
+
+/*******************************************************************************/
+PixelDescriptionWidget
+::~PixelDescriptionWidget()
+{
+}
+
+/*******************************************************************************/
+void
+PixelDescriptionWidget
+::SetupUI()
+{
+  //
+  // Cartographic coordiantes
+  m_CartographicRootItem = new QTreeWidgetItem( GetDescriptionTree() ); 
+  m_CartographicRootItem->setText(0, tr("Cartographic"));
+  m_CartographicRootItem->setExpanded(true);
+
+  m_CartographicItem = new QTreeWidgetItem( m_CartographicRootItem );
+  m_CartographicItem->setText(0, tr("Coordinates"));
+
+  //
+  // Geographic coordiantes
+  m_GeographicRootItem = new QTreeWidgetItem( GetDescriptionTree() );
+  m_GeographicRootItem->setText(0, tr("Geographic"));
+  m_GeographicRootItem->setExpanded(true);
+
+  m_GeographicItem = new QTreeWidgetItem( m_GeographicRootItem );  
+  m_GeographicItem->setText(0, tr("Coordinates"));
+
+  //
+  // Child items will be created + updated in a dedicated slot
+  m_PixelValueRootItem = new QTreeWidgetItem( GetDescriptionTree() ); 
+  m_PixelValueRootItem->setText(0, tr("Pixel Values"));
+  m_PixelValueRootItem->setExpanded(true);
+}
+
+/*******************************************************************************/
+QTreeWidget *
+PixelDescriptionWidget
+::GetDescriptionTree()
+{
+  return m_UI->m_DescriptionTree;
+}
+
+/*******************************************************************************/
+/* SLOTS                                                                       */
+/*******************************************************************************/
+void
+PixelDescriptionWidget
+::OnCurrentPhysicalUpdated(const QString & currentPhysical)
+{
+  m_CartographicItem->setText(1, currentPhysical);
+}
+
+/*******************************************************************************/
+void
+PixelDescriptionWidget
+::OnCurrentGeographicUpdated(const QString & currentGeo)
+{
+  m_GeographicItem->setText(1, currentGeo);
+}
+
+/*******************************************************************************/
+void
+PixelDescriptionWidget
+::OnCurrentPixelValueUpdated(const VectorImageType::PixelType & currentPixel, 
+                             const QStringList& bandNames)
+{
+  // 
+  qDebug()<<this<<"::OnCurrentPixelValueUpdated currenPixel size : " <<currentPixel.GetSize()
+          << ", bandName size " << bandNames.size();
+  assert( bandNames.size()==currentPixel.GetSize() );
+
+  //
+  // remove the previous QTreeWidgetItem  of m_PixelValueRootItem
+  while( m_PixelValueRootItem->childCount()>0 )
+    {
+    // Remove QTreeWidgetItem
+    QTreeWidgetItem* child = m_PixelValueRootItem->takeChild( 0 );
+
+    // Delete it from memory.
+    delete child;
+    child = NULL;
+    }
+  
+  // fill with the new values
+  for (unsigned int idx = 0; idx < currentPixel.GetSize(); idx++)
+    {
+    QTreeWidgetItem * iBandItem = new QTreeWidgetItem( m_PixelValueRootItem );
+    
+    // figure out if a band name is available, if not use Band idx
+    if ( bandNames[idx].isEmpty() )
+      {
+      iBandItem->setText(0, QString( tr("Band") )+ QString(" %1").arg(idx) );
+      }
+    else
+      {
+      iBandItem->setText(0, bandNames[ idx ] );
+      }
+    // set the value
+    iBandItem->setText(1, QString("%1").arg(currentPixel.GetElement( idx )) );
+    }
+}
+
+/*******************************************************************************/
+
+} // end namespace 'mvd'
---
+++ Code/Common/Gui/mvdPixelDescriptionWidget.h
@@ -0,0 +1,184 @@
+/*=========================================================================
+
+  Program:   Monteverdi2
+  Language:  C++
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See Copyright.txt for details.
+
+  Monteverdi2 is distributed under the CeCILL licence version 2. See
+  Licence_CeCILL_V2-en.txt or
+  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt for more details.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __mvdPixelDescriptionWidget_h
+#define __mvdPixelDescriptionWidget_h
+
+//
+// Configuration include.
+//// Included at first position before any other ones.
+#include "ConfigureMonteverdi2.h"
+
+
+/*****************************************************************************/
+/* INCLUDE SECTION                                                           */
+
+//
+// Qt includes (sorted by alphabetic order)
+//// Must be included before system/custom includes.
+#include <QtGui>
+
+//
+// System includes (sorted by alphabetic order)
+
+//
+// ITK includes (sorted by alphabetic order)
+
+//
+// OTB includes (sorted by alphabetic order)
+
+//
+// Monteverdi includes (sorted by alphabetic order)
+#include "Core/mvdTypes.h"
+
+/*****************************************************************************/
+/* PRE-DECLARATION SECTION                                                   */
+
+//
+// External classes pre-declaration.
+namespace
+{
+}
+
+namespace mvd
+{
+//
+// Internal classes pre-declaration.
+namespace Ui
+{
+class PixelDescriptionWidget;
+};
+
+
+/*****************************************************************************/
+/* CLASS DEFINITION SECTION                                                  */
+
+/**
+ * \class PixelDescriptionWidget
+ *
+ * \brief Widget to show current pixel under cursor description 
+ */
+class Monteverdi2_EXPORT PixelDescriptionWidget :
+    public QWidget
+{
+
+  /*-[ QOBJECT SECTION ]-----------------------------------------------------*/
+
+  Q_OBJECT;
+
+  /*-[ PUBLIC SECTION ]------------------------------------------------------*/
+
+//
+// Public methods.
+public:
+
+  /** \brief Constructor. */
+  PixelDescriptionWidget( QWidget* parent =NULL, Qt::WindowFlags flags =0 );
+
+  /** \brief Destructor. */
+  virtual ~PixelDescriptionWidget();
+
+  /** Get TreeWidget */
+  QTreeWidget * GetDescriptionTree();
+
+  /*-[ PUBLIC SLOTS SECTION ]------------------------------------------------*/
+
+//
+// Public SLOTS.
+public slots:
+
+  void OnCurrentPhysicalUpdated(const QString &);
+  
+  void OnCurrentGeographicUpdated(const QString &);
+
+  void OnCurrentPixelValueUpdated(const VectorImageType::PixelType &, 
+                                  const QStringList& bandNames);
+
+  /*-[ SIGNALS SECTION ]-----------------------------------------------------*/
+
+//
+// Signals.
+signals:
+
+  /*-[ PROTECTED SECTION ]---------------------------------------------------*/
+
+//
+// Protected methods.
+protected:
+
+  void SetupUI();
+
+  /*-[ PRIVATE SECTION ]-----------------------------------------------------*/
+
+//
+// Protected attributes.
+protected:
+
+//
+// Private methods.
+private:
+
+//
+// Private attributes.
+private:
+  /**
+   * \brief uic generated.
+   */
+  Ui::PixelDescriptionWidget* m_UI;
+
+  // /**
+  //  * \brief map storing applications and its tags
+  //  */
+  // ApplicationsTagContainer m_AppTags;
+
+  // /**
+  //  * \brief map storing applications docName and appName association
+  //  */
+  // ApplicationDocNameToNameMap m_AppsDocNameToNameMap;
+
+  /**
+   * \brief text to search in the widget tree
+   */
+  QString                  m_SearchText;  
+
+  /**
+   * \brief Root items
+   */
+  QTreeWidgetItem*    m_CartographicRootItem;
+  QTreeWidgetItem*    m_GeographicRootItem;
+  QTreeWidgetItem*    m_PixelValueRootItem;
+
+   /**
+   * \brief child items
+   */ 
+  QTreeWidgetItem*    m_CartographicItem;
+  QTreeWidgetItem*    m_GeographicItem;
+
+  /*-[ PRIVATE SLOTS SECTION ]-----------------------------------------------*/
+
+//
+// Slots.
+private slots:
+};
+
+/*****************************************************************************/
+/* INLINE SECTION                                                            */
+
+} // end namespace 'mvd'
+
+#endif // __mvdPixelDescriptionWidget_h
