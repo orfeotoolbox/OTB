ba83fddeccbb03ac98744900def9c0cd0498096d
Mickael Savinaud <mickael.savinaud@c-s.fr>
2013-06-27 19:17:53 +0200

    ENH: rename mvd2-catalogue also into packaging script for windows

--- Packaging/Windows/BundleFixing.cmake
+++ Packaging/Windows/BundleFixing.cmake
@@ -1,3 +1,3 @@
 include(BundleUtilities)
 fixup_bundle("${CMAKE_INSTALL_PREFIX}/bin/mvd2-viewer.exe" "" "")
-fixup_bundle("${CMAKE_INSTALL_PREFIX}/bin/mvd2-catalogue.exe" "" "")
+fixup_bundle("${CMAKE_INSTALL_PREFIX}/bin/monteverdi2.exe" "" "")
