cdd23ba22bb279288578b048403e7eab6658b976
Julien Michel <julien.michel@orfeo-toolbox.org>
2013-06-27 16:44:08 +0200

    BUG: Removing annoying blinking texture borders

--- Code/Common/Gui/mvdImageModelRenderer.cxx
+++ Code/Common/Gui/mvdImageModelRenderer.cxx
@@ -196,6 +196,9 @@ void ImageModelRenderer::paintGL( const RenderingContext& context )
 #endif
       glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
       glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S,GL_CLAMP_TO_BORDER);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T,GL_CLAMP_TO_BORDER);
+
       glBindTexture (GL_TEXTURE_2D, m_Texture);
       }
   
