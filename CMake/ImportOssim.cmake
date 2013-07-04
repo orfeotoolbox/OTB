68f73c9511700dcfbd8df2be45779ef61d3a6377
Julien Malik <julien.malik@c-s.fr>
2013-07-04 11:56:13 +0200

    COMP: on windows, enable external ossim by default since it is available with OSGeo4W

--- CMake/ImportOssim.cmake
+++ CMake/ImportOssim.cmake
@@ -1,6 +1,14 @@
 MESSAGE(STATUS "Importing Ossim...")
-# Option for internal/external GDAL
-OPTION(OTB_USE_EXTERNAL_OSSIM "Use an outside build of Ossim." OFF)
+
+SET(DEFAULT_OTB_USE_EXTERNAL_OSSIM  OFF)
+
+# OSGeo4W provides an "ossim" package : use it otherwise if it is installed and not used by OTB
+# we get conflicts (because OSGeo4W include dir is included for other dependencies 
+IF(WIN32)
+  SET(DEFAULT_OTB_USE_EXTERNAL_OSSIM  ON)
+ENDIF()
+
+OPTION(OTB_USE_EXTERNAL_OSSIM "Use an outside build of Ossim." ${DEFAULT_OTB_USE_EXTERNAL_OSSIM})
 MARK_AS_ADVANCED(OTB_USE_EXTERNAL_OSSIM)
 
 IF(OTB_USE_EXTERNAL_OSSIM)
