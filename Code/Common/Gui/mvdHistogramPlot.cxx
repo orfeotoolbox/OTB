82cad9857626105824c6a7c00d1b283957f55ec8
Stephane Albert <stephane.albert@c-s.fr>
2013-08-29 17:29:38 +0200

    ENH: Added HistogramPlot class.

---
+++ Code/Common/Gui/mvdHistogramPlot.cxx
@@ -0,0 +1,84 @@
+/*=========================================================================
+
+  Program:   Monteverdi2
+  Language:  C++
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See Copyright.txt for details.
+
+  Monteverdi2 is distributed under the CeCILL licence version 2. See
+  Licence_CeCILL_V2-en.txt or
+  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt for more details.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#include "mvdHistogramPlot.h"
+
+
+/*****************************************************************************/
+/* INCLUDE SECTION                                                           */
+
+//
+// Qt includes (sorted by alphabetic order)
+//// Must be included before system/custom includes.
+
+//
+// System includes (sorted by alphabetic order)
+
+//
+// ITK includes (sorted by alphabetic order)
+
+//
+// OTB includes (sorted by alphabetic order)
+
+//
+// Monteverdi includes (sorted by alphabetic order)
+
+namespace mvd
+{
+/*
+  TRANSLATOR mvd::HistogramPlot
+
+  Necessary for lupdate to be aware of C++ namespaces.
+
+  Context comment for translator.
+*/
+
+
+/*****************************************************************************/
+/* CONSTANTS                                                                 */
+
+namespace
+{
+} // end of anonymous namespace.
+
+
+/*****************************************************************************/
+/* STATIC IMPLEMENTATION SECTION                                             */
+
+
+/*****************************************************************************/
+/* CLASS IMPLEMENTATION SECTION                                              */
+
+/*******************************************************************************/
+HistogramPlot
+::HistogramPlot( QObject* parent ) :
+  QObject( parent )
+{
+}
+
+/*******************************************************************************/
+HistogramPlot
+::~HistogramPlot()
+{
+}
+
+/*******************************************************************************/
+/* SLOTS                                                                       */
+/*******************************************************************************/
+
+} // end namespace 'mvd'
