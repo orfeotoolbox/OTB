c702bf4b5a09b22a025a34e59c517498c5373a25
Rashad Kanavath <mohammedrashadkm@gmail.com>
2013-10-08 16:26:16 +0200

    Adding correct headers for CE application

--- Applications/ContrastEnhancement/otbColorSpaceTransform.cxx
+++ Applications/ContrastEnhancement/otbColorSpaceTransform.cxx
@@ -1,4 +1,23 @@
+/*=========================================================================
 
+  Program:   ORFEO Toolbox
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
+
+  Some parts of this code are derived from ITK. See ITKCopyright.txt
+  for details.
+
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
 #include <iostream>
 #include <sstream>
 
--- Applications/ContrastEnhancement/otbGammaCorrection.cxx
+++ Applications/ContrastEnhancement/otbGammaCorrection.cxx
@@ -1,7 +1,25 @@
+/*=========================================================================
 
+  Program:   ORFEO Toolbox
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
+
+  Some parts of this code are derived from ITK. See ITKCopyright.txt
+  for details.
+
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
 #include <iostream>
 #include <sstream>
-
 
 #include "otbImage.h"
 #include "otbVectorImage.h"
@@ -29,7 +47,6 @@ public:
   itkNewMacro(Self);
 
   itkTypeMacro(GammaCorrection, otb::Application);
-
 
 private:
 
