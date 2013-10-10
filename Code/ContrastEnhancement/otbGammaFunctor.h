78e7fc92c820d2069851591a0852217f00a74e65
Julien Malik <julien.malik@c-s.fr>
2013-10-10 10:42:10 +0200

    COMP: fix compilation of GammaFunctor

--- Code/ContrastEnhancement/otbGammaFunctor.h
+++ Code/ContrastEnhancement/otbGammaFunctor.h
@@ -32,26 +32,25 @@ class GammaFunctor {
 
 public:
 
-  GammaFunctor() {  }
-
-  ~GammaFunctor() {  }
+  GammaFunctor() : m_Gamma(1.0)
+  {}
 
   inline void SetGamma(double gamma)
   {
     if(gamma == 0)
       {
-      std::cerr << "Error: Division by zero " << __FILE__ << ":" << __LINE__ << std::endl;
+      itkGenericExceptionMacro( << "Error: Division by zero" );
       }
     else if(gamma < 0)
       {
-      std::cerr << "Gamma Encoding"  << std::endl;
+      otbMsgDevMacro( << "Gamma Encoding" );
       }
     else
       {
-      std::cerr << "Gamma Decoding"  << std::endl;
+      otbMsgDevMacro( << "Gamma Decoding" );
       }
 
-    m_Gamma = 1.0 / gamma;
+    m_GammaInverse = 1.0 / gamma;
   }
 
   inline TOutput operator ()(const TInput& input) const
@@ -64,14 +63,13 @@ public:
 
    for (unsigned int i = 0; i < input.Size(); ++i)
      {
-     //output[i] = vcl_pow(m_Range *  (input[i] / m_Range) , m_Gamma);
-     output[i] = vcl_pow(input[i] , m_Gamma);
+     output[i] = vcl_pow(static_cast<double>(input[i]) , m_GammaInverse);
      }
     return output;
   }
 
 private:
-  double m_Gamma;
+  double m_GammaInverse;
 
 };
 
