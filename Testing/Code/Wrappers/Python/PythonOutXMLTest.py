2749aaeb2501d9beaf7e7c8a1bbf186842d77486
Rashad Kanavath <mohammed.rashad-km@cnes.fr>
2013-10-17 11:17:47 +0200

    ENH: Updated outxml testing for python with wrapper extended SetParameter function

--- Testing/Code/Wrappers/Python/PythonOutXMLTest.py
+++ Testing/Code/Wrappers/Python/PythonOutXMLTest.py
@@ -11,14 +11,14 @@ exp = "cos(im1b1)+im2b1*im3b1-im3b2+ndvi(im3b3, im3b4)"
 out = argv[-2]
 outxml = argv[-1]
 app = otb.Registry_CreateApplication('BandMath')
-app.SetParameterStringList("il", imagelist)
-app.SetParameterString("out", out)
-app.SetParameterString("exp", exp)
-app.SetParameterString("outxml", outxml)
+app.SetParameterStringList("il", imagelist, True)
+app.SetParameterString("out", out, True)
+app.SetParameterString("exp", exp, True)
+app.SetParameterString("outxml", outxml, True)
 app.UpdateParameters()
-for paramKey in app.GetParametersKeys():
+#for paramKey in app.GetParametersKeys():
   #app.GetParameterValue(paramKey)
-  print app.HasUserValue(paramKey)
+  #print app.HasUserValue(paramKey)
 app.ExecuteAndWriteOutput()
 
 
