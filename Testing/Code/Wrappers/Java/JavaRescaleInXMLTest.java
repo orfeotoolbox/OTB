42dbc6489d0651c1da9052176c3909982b49190e
Rashad Kanavath <mohammed.rashad-km@cnes.fr>
2013-10-17 11:19:20 +0200

    TEST: XML test for Java wrapper

---
+++ Testing/Code/Wrappers/Java/JavaRescaleInXMLTest.java
@@ -0,0 +1,21 @@
+/*
+ * Example on the use of the Rescale with inxml 
+ */
+ 
+import java.lang.Float;
+import org.otb.application.*;
+
+class RescaleInXMLTest {
+
+  public static void main( String argv[] ) {
+
+    Application app = Registry.CreateApplication("Rescale");
+    app.SetParameterString("inxml", argv[0]);
+    app.SetParameterString("in",     argv[1]);
+    app.SetParameterString("out",    argv[2]);
+    int result = app.ExecuteAndWriteOutput();
+
+    System.exit( result );
+  }
+
+}
