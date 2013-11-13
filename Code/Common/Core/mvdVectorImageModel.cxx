d24d84c785aff9b88c466e27c255902a7507f64f
Stephane Albert <stephane.albert@c-s.fr>
2013-11-13 16:07:02 +0100

    STYLE: Replaced tabs by spaces.

--- Code/Common/Core/mvdVectorImageModel.cxx
+++ Code/Common/Core/mvdVectorImageModel.cxx
@@ -66,7 +66,6 @@ namespace mvd
 
 /*****************************************************************************/
 /* CLASS IMPLEMENTATION SECTION                                              */
-
 /*****************************************************************************/
 VectorImageModel
 ::VectorImageModel( QObject* parent ) :
