4067944fcdb5e4da150189ad51ffe062b1ba5b90
Stephane Albert <stephane.albert@c-s.fr>
2013-11-13 12:51:20 +0100

    ENH: Configured MVD2 to build and deploy (Linux, Windows) with QtSql and SQLite driver plugin.

--- CMake/DeployQtConf.cmake
+++ CMake/DeployQtConf.cmake
@@ -6,13 +6,13 @@ macro( deploy_qt_conf TARGET_NAME )
       TARGET ${TARGET_NAME}
       PRE_BUILD 
       COMMAND ${CMAKE_COMMAND}
-        ARGS -D QT_TRANSLATIONS_DIR:PATH=${QT_TRANSLATIONS_DIR}
-            -D Monteverdi2_SOURCE_DIR:PATH=${Monteverdi2_SOURCE_DIR}
-            -D TARGET_DIR:PATH=$<TARGET_FILE_DIR:${TARGET_NAME}>
-            -P ${Monteverdi2_SOURCE_DIR}/CMake/DeployQtConfScript.cmake
+      ARGS -D QT_TRANSLATIONS_DIR:PATH=${QT_TRANSLATIONS_DIR}
+           -D QT_PLUGINS_DIR:PATH=${QT_PLUGINS_DIR}
+           -D Monteverdi2_SOURCE_DIR:PATH=${Monteverdi2_SOURCE_DIR}
+           -D TARGET_DIR:PATH=$<TARGET_FILE_DIR:${TARGET_NAME}>
+           -P ${Monteverdi2_SOURCE_DIR}/CMake/DeployQtConfScript.cmake
       COMMENT "Deploying qt.conf in ${TARGET_NAME} build dir"
       VERBATIM
   )
 
 endmacro()
-
