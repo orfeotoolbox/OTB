6193f80ebc2971503b25027178e9b7872c7d286f
Julien Michel <julien.michel@orfeo-toolbox.org>
2013-12-18 13:26:28 +0100

    DOC: Adding copyrights to files

--- otbGlView.cxx
+++ otbGlView.cxx
@@ -1,3 +1,20 @@
+/*=========================================================================
+
+  Program:   ORFEO Toolbox
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
+
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
 #include "otbGlView.h"
 #include <GL/gl.h> 
 #include <algorithm>
