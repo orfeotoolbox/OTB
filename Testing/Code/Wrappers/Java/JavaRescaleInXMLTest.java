fcfe59afd9050a620754bec9838b3364281cdc76
Rashad Kanavath <mohammed.rashad-km@cnes.fr>
2014-02-12 09:09:47 +0100

    BUG: mantis 884. Java Wrapper doesnt set and or override parameter from inxml

--- Testing/Code/Wrappers/Java/JavaRescaleInXMLTest.java
+++ Testing/Code/Wrappers/Java/JavaRescaleInXMLTest.java
@@ -1,7 +1,7 @@
 /*
- * Example on the use of the Rescale with inxml 
+ * Example on the use of the Rescale with inxml
  */
- 
+
 import java.lang.Float;
 import org.otb.application.*;
 
@@ -10,9 +10,13 @@ class RescaleInXMLTest {
   public static void main( String argv[] ) {
 
     Application app = Registry.CreateApplication("Rescale");
+
     app.SetParameterString("inxml", argv[0]);
+    app.UpdateParameters();
+
     app.SetParameterString("in",     argv[1]);
     app.SetParameterString("out",    argv[2]);
+
     int result = app.ExecuteAndWriteOutput();
 
     System.exit( result );
