3514a297ac498b4ae7fe2ccf22dff1ef2aaf7868
Rashad Kanavath <mohammed.rashad-km@cnes.fr>
2014-02-12 09:31:42 +0100

    BUG: mantis 883. Set InputImageList Parameter with UserValue true before inxml

--- Testing/Code/Wrappers/Python/PythonInXMLTest.py
+++ Testing/Code/Wrappers/Python/PythonInXMLTest.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 
 #
-#  Example on the use of the inxml via BandMath 
+#  Example on the use of the inxml via BandMath
 #
 from sys import argv
 import otbApplication as otb
 
 app = otb.Registry_CreateApplication('BandMath')
+
+imagelist = argv[2:-1]
+app.SetParameterStringList("il", imagelist, True)
+
 app.SetParameterString("inxml", argv[1])
 app.UpdateParameters()
-imagelist = argv[2:-1]
-app.SetParameterStringList("il", imagelist)
+
 app.SetParameterString("out"  , argv[-1])
 app.ExecuteAndWriteOutput()
-
-
-
