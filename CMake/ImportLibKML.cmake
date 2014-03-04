5b956de2541873d75869666dde8bab05bd38935f
Manuel Grizonnet <manuel.grizonnet@orfeo-toolbox.org>
2014-03-04 08:52:36 +0100

    WIP: warn user during cmake configuration that USE_EXTERNAL_ITK and USE_EXTERNAL_LIBKML does not match (bug 879/896)

--- CMake/ImportLibKML.cmake
+++ CMake/ImportLibKML.cmake
@@ -5,6 +5,13 @@ if(LIBKML_FOUND)
   option(OTB_USE_EXTERNAL_LIBKML "Use external LibKML library." ON)
 else()
   option(OTB_USE_EXTERNAL_LIBKML "Use external LibKML library." OFF)
+
+  #Known issue in OTB which does not compile with external ITK and internal libkml 
+  #Bugs 896: http://bugs.orfeo-toolbox.org/view.php?id=896
+  if (USE_EXTERNAL_ITK)
+    message(WARNING "There is a known issue which does not allow to use internal libKML with external ITK in OTB. You might consider using an external LibKML.")
+  endif()
+
   set(LIBKML_LIBRARIES otbkml)
   message(STATUS "Using LibKML internal version")
 endif()
