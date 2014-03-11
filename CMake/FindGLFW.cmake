f8c1609807de22ff3db853310d12c33319352b52
Julien Malik <julien.malik@orfeo-toolbox.org>
2014-03-11 10:40:31 +0100

    COMP: fix FindGLFW for version 3

--- CMake/FindGLFW.cmake
+++ CMake/FindGLFW.cmake
@@ -20,32 +20,24 @@
 # Allow the user to select to link to a shared library or to a static library.
 
 #Search for the include file...
-FIND_PATH(GLFW_INCLUDE_DIR GL/glfw.h DOC "Path to GLFW include directory."
+FIND_PATH(GLFW_INCLUDE_DIR GLFW/glfw3.h DOC "Path to GLFW include directory."
     HINTS
-    $ENV{GLFW_ROOT}
-    PATH_SUFFIX include # For finding the include file under the root of the glfw expanded archive, typically on Windows.
-    PATHS
-    /usr/include/
-    /usr/local/include/
-    # By default headers are under GL subfolder
-    /usr/include/GL
-    /usr/local/include/GL
-    ${GLFW_ROOT_DIR}/include/ # added by ptr
+      $ENV{GLFW_ROOT}
+    PATH_SUFFIX
+      include # For finding the include file under the root of the glfw expanded archive, typically on Windows.
 )
 
 FIND_LIBRARY(GLFW_LIBRARY DOC "Absolute path to GLFW library."
-    NAMES glfw GLFW.lib
+    NAMES glfw glfw3dll glfw3
     HINTS
-    $ENV{GLFW_ROOT}
-    PATH_SUFFIXES lib/win32 # For finding the library file under the root of the glfw expanded archive, typically on Windows.
-    PATHS
-    /usr/local/lib
-    /usr/lib
-    ${GLFW_ROOT_DIR}/lib-msvc100/release # added by ptr
+      $ENV{GLFW_ROOT}
+    PATH_SUFFIXES
+       lib # For finding the library file under the root of the glfw expanded archive, typically on Windows.
 )
 
-IF(GLFW_INCLUDE_DIR AND EXISTS "${GLFW_INCLUDE_DIR}/GL/glfw.h")
-    FILE(STRINGS "${GLFW_INCLUDE_DIR}/GL/glfw.h" glfw_version_str
+
+IF(GLFW_INCLUDE_DIR AND EXISTS "${GLFW_INCLUDE_DIR}/GLFW/glfw3.h")
+    FILE(STRINGS "${GLFW_INCLUDE_DIR}/GLFW/glfw3.h" glfw_version_str
          REGEX "^#[\t ]*define[\t ]+GLFW_VERSION_(MAJOR|MINOR|REVISION)[\t ]+[0-9]+$")
 
     UNSET(GLFW_VERSION_STRING)
@@ -56,14 +48,14 @@ IF(GLFW_INCLUDE_DIR AND EXISTS "${GLFW_INCLUDE_DIR}/GL/glfw.h")
                        GLFW_VERSION_PART "${VLINE}")
                 IF(GLFW_VERSION_STRING)
                     SET(GLFW_VERSION_STRING "${GLFW_VERSION_STRING}.${GLFW_VERSION_PART}")
-                ELSE(GLFW_VERSION_STRING)
+                ELSE()
                     SET(GLFW_VERSION_STRING "${GLFW_VERSION_PART}")
-                ENDIF(GLFW_VERSION_STRING)
+                ENDIF()
                 UNSET(GLFW_VERSION_PART)
             ENDIF()
-        ENDFOREACH(VLINE)
-    ENDFOREACH(VPART)
-ENDIF(GLFW_INCLUDE_DIR AND EXISTS "${GLFW_INCLUDE_DIR}/GL/glfw.h")
+        ENDFOREACH()
+    ENDFOREACH()
+ENDIF()
 
 INCLUDE(${CMAKE_ROOT}/Modules/FindPackageHandleStandardArgs.cmake)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(GLFW
