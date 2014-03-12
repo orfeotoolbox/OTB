869efc11ae82d408d7fe479bd32ae2ce8610bd1e
Julien Malik <julien.malik@c-s.fr>
2014-03-12 18:12:17 +0100

    COMP: wrong variable test in ImportLibKML

--- CMake/ImportLibKML.cmake
+++ CMake/ImportLibKML.cmake
@@ -9,10 +9,11 @@ else()
   message(STATUS "Using LibKML internal version")
 endif()
 
-#Known issue in OTB which does not compile with external ITK and internal libkml 
-#Bugs 896: http://bugs.orfeo-toolbox.org/view.php?id=896
-if (USE_EXTERNAL_ITK)
-  message(WARNING "There is a known issue which does not allow to use internal libKML with external ITK in OTB. You might consider using an external LibKML.")
+# Known issue in OTB which does not compile with external ITK and internal libkml 
+if (OTB_USE_EXTERNAL_ITK AND NOT OTB_USE_EXTERNAL_LIBKML)
+  message(WARNING "There is a known issue which does not allow to use internal libKML with external ITK in OTB."
+                  "You might consider using an external LibKML. "
+                  "See http://bugs.orfeo-toolbox.org/view.php?id=896 for details")
 endif()
 
 mark_as_advanced(OTB_USE_EXTERNAL_LIBKML)
