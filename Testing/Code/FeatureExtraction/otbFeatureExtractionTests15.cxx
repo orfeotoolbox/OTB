ba38866e44aea662c8eecb74823eacf6e2dd00d8
Rashad Kanavath <mohammed.rashad-km@cnes.fr>
2014-05-14 15:11:05 +0200

    ENH: readded otbGreyLevelCooccurrenceIndexedListNew in Testing after backout

--- Testing/Code/FeatureExtraction/otbFeatureExtractionTests15.cxx
+++ Testing/Code/FeatureExtraction/otbFeatureExtractionTests15.cxx
@@ -31,6 +31,7 @@ void RegisterTests()
   REGISTER_TEST(otbScalarImageToTexturesFilter);
   REGISTER_TEST(otbGreyLevelCooccurrenceMatrixAdvancedTextureCoefficientsCalculatorNew);
   REGISTER_TEST(otbGreyLevelCooccurrenceMatrixAdvancedTextureCoefficientsCalculator);
+  REGISTER_TEST(otbGreyLevelCooccurrenceIndexedListNew);
   REGISTER_TEST(otbScalarImageToAdvancedTexturesFilterNew);
   REGISTER_TEST(otbScalarImageToAdvancedTexturesFilter);
   REGISTER_TEST(otbScalarImageToPanTexTextureFilterNew);
