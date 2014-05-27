7a56d7293591c3af517cddcbc91d03bf441b0ff8
Guillaume Pasero <guillaume.pasero@c-s.fr>
2014-05-27 15:47:09 +0200

    BUG: Mantis-856: Fix TinyXML import script and enhance documentation

--- CMake/ImportTinyXML.cmake
+++ CMake/ImportTinyXML.cmake
@@ -3,19 +3,27 @@ find_package(TinyXML)
 
 if(TINYXML_FOUND)
   option(OTB_USE_EXTERNAL_TINYXML "Use external TinyXML library." ON)
-
-  # Check if STL support is enabled with macro TIXML_USE_STL
-  try_compile(TIXML_USE_STL
-              ${CMAKE_CURRENT_BINARY_DIR}/CMake
-              ${CMAKE_CURRENT_SOURCE_DIR}/CMake/otbTestTinyXMLUseSTL.cxx
-              CMAKE_FLAGS "-DINCLUDE_DIRECTORIES:PATH=${TINYXML_INCLUDE_DIRS}" "-DLINK_LIBRARIES:STRING=${TINYXML_LIBRARIES}"
-              COMPILE_DEFINITIONS "-DTIXML_USE_STL"
-              OUTPUT_VARIABLE OUTPUT)
-
 else()
   option(OTB_USE_EXTERNAL_TINYXML "Use external TinyXML library." OFF)
-  set(TINYXML_LIBRARIES otbtinyXML)
-  set(TIXML_USE_STL)
-  message(STATUS "Using TinyXML internal version")
 endif()
 mark_as_advanced(OTB_USE_EXTERNAL_TINYXML)
+
+if(OTB_USE_EXTERNAL_TINYXML)
+  if(TINYXML_FOUND)
+    # Check if STL support is enabled with macro TIXML_USE_STL
+    try_compile(TIXML_USE_STL
+                ${CMAKE_CURRENT_BINARY_DIR}/CMake
+                ${CMAKE_CURRENT_SOURCE_DIR}/CMake/otbTestTinyXMLUseSTL.cxx
+                CMAKE_FLAGS "-DINCLUDE_DIRECTORIES:PATH=${TINYXML_INCLUDE_DIRS}" "-DLINK_LIBRARIES:STRING=${TINYXML_LIBRARIES}"
+                COMPILE_DEFINITIONS "-DTIXML_USE_STL"
+                OUTPUT_VARIABLE OUTPUT)
+  else()
+    message(FATAL_ERROR "Can't build OTB without TinyXML. Install it "
+                        "on your system, or disable the option "
+                        "OTB_USE_EXTERNAL_TINYXML to use the internal one")
+  endif()
+else()
+  set(TINYXML_LIBRARIES otbtinyXML)
+  set(TIXML_USE_STL)
+  message(STATUS "  Using TinyXML internal version")
+endif()
