ac00b68d1a6dc5d4589685f67fe8d7747e27096f
Manuel Grizonnet <manuel.grizonnet@orfeo-toolbox.org>
2014-07-18 10:03:34 +0200

    ENH: change dummy code in cmake to test if gdal has ogr

--- CMake/TestGDALHasOGR.cxx
+++ CMake/TestGDALHasOGR.cxx
@@ -1,10 +1,26 @@
 #ifdef WIN32
 #define CPL_DISABLE_DLL
 #endif
-#include "ogrsf_frmts.h"
+#include "ogr_api.h"
 
 int main(int argc,char * argv[])
 {
-  OGRSFDriverRegistrar::Open(argv[1], FALSE);
+  // OGRSFDriverRegistrar::Open(argv[1], FALSE);
+
+  // return 0;
+
+  OGRDataSourceH	hDS;
+  OGRSFDriverH        *pahDriver;
+  
+  OGRRegisterAll(); 	
+
+  hDS = OGROpen(argv[1], FALSE, pahDriver );
+  if( hDS == NULL )
+    {
+    return -1;
+    }
+
+  OGRReleaseDataSource( hDS );
+  
   return 0;
 }
