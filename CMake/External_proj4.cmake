1dd9a9ebf9620260d76c8a17d6b1a44699a4d7ad
Rashad Kanavath <rashad.kanavath@c-s.fr>
2015-01-29 16:47:09 +0100

    source not copied to build

--- CMake/External_proj4.cmake
+++ CMake/External_proj4.cmake
@@ -35,17 +35,13 @@ else()
       SOURCE_DIR ${proj}/_install
       BINARY_DIR ${PROJ_SB_BUILD_DIR}
       INSTALL_DIR ${CMAKE_INSTALL_PREFIX}
-    CMAKE_CACHE_ARGS
-      -DCMAKE_INSTALL_PREFIX:STRING=${CMAKE_INSTALL_PREFIX}
-      -DCMAKE_BUILD_TYPE:STRING=Release
-      -DPROJ4_BUILD_DIR:STRING=${PROJ_SB_SRC}/src
+      CMAKE_CACHE_ARGS
+        -DCMAKE_INSTALL_PREFIX:STRING=${CMAKE_INSTALL_PREFIX}
+        -DCMAKE_BUILD_TYPE:STRING=Release
+        -DPROJ4_BUILD_DIR:STRING=${PROJ_SB_BUILD_DIR}/src
       DEPENDS ${proj}_build
       CMAKE_COMMAND
     )
-
-    
-
-        
     
     ExternalProject_Add_Step(${proj}_build copy_source
       COMMAND ${CMAKE_COMMAND} -E copy_directory 
@@ -69,14 +65,14 @@ else()
       BUILD_COMMAND $(MAKE)
       INSTALL_COMMAND $(MAKE) install
     )
-    
+
     ExternalProject_Add_Step(${proj} copy_source
       COMMAND ${CMAKE_COMMAND} -E copy_directory 
         ${PROJ_SB_SRC} ${PROJ_SB_BUILD_DIR}
       DEPENDEES patch update
       DEPENDERS configure
-    )
-    
+    )    
   endif()
+  
   message(STATUS "  Using Proj4 SuperBuild version")
 endif()
