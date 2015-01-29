aee6a2e4090310d86bb14f31cf886655ec6cada4
Rashad Kanavath <rashad.kanavath@c-s.fr>
2015-01-29 18:37:24 +0100

    MSVC: update expat superbuild config for LibKML

--- CMake/External_libKML.cmake
+++ CMake/External_libKML.cmake
@@ -21,6 +21,12 @@ else()
       -DEXPAT_INCLUDE_DIR:STRING=${CMAKE_INSTALL_PREFIX}/include
       -DEXPAT_LIBRARY:STRING=${CMAKE_INSTALL_PREFIX}/lib/libexpat.so
       )
+      if(MSVC)
+        set(LIBKML_SB_EXPAT_CONFIG 
+            -DEXPAT_INCLUDE_DIR:STRING=${CMAKE_INSTALL_PREFIX}/include
+            -DEXPAT_LIBRARY:STRING=${CMAKE_INSTALL_PREFIX}/lib/expat.lib
+        )  
+      endif()
     list(APPEND ${proj}_DEPENDENCIES EXPAT)
   endif()
   
@@ -38,28 +44,25 @@ else()
   else()
     set(LIBKML_SB_ZLIB_CONFIG 
       -DZLIB_ROOT:STRING=${CMAKE_INSTALL_PREFIX}
-      )
+      )      
     list(APPEND ${proj}_DEPENDENCIES ZLIB)
   endif()
   
   if(MSVC)
     set(${proj}_DEPENDENCIES)
-    set(LIBKML_SB_EXPAT_CONFIG)
-    #set(LIBKML_SB_BOOST_CONFIG)
+
+    set(LIBKML_SB_BOOST_CONFIG)
     set(LIBKML_SB_ZLIB_CONFIG)
-    #try to use github clone
-    #URIPARSER, GOOGLETEST, EXPAT, MINIZIP dependencies
- ### endif()
-       message(STATUS "  LibKML SuperBuild is not available yet...")
-  
-  else(UNIX)
-  
+    
+
   ExternalProject_Add(${proj}
     PREFIX ${proj}
     URL "http://ftp.de.debian.org/debian/pool/main/libk/libkml/libkml_1.3.0~r863.orig.tar.gz"
     URL_MD5 211ed5fdf2dd45aeb9c0abc8e1fe42be
     BINARY_DIR ${LIBKML_SB_BUILD_DIR}
     INSTALL_DIR ${CMAKE_INSTALL_PREFIX}
+    DEPENDS ${${proj}_DEPENDENCIES}
+    PATCH_COMMAND ${CMAKE_COMMAND} -E copy_directory  ${CMAKE_SOURCE_DIR}/patches/${proj} ${LIBKML_SB_SRC}    
     CMAKE_CACHE_ARGS
       -DCMAKE_INSTALL_PREFIX:STRING=${CMAKE_INSTALL_PREFIX}
       -DCMAKE_BUILD_TYPE:STRING=Release
@@ -67,14 +70,7 @@ else()
       ${LIBKML_SB_EXPAT_CONFIG}
       ${LIBKML_SB_BOOST_CONFIG}
       ${LIBKML_SB_ZLIB_CONFIG}
-    #CMAKE_COMMAND
-    #  # use 'env' because CTest launcher doesn't perform shell interpretation
-    #  env LD_LIBRARY_PATH=${CMAKE_INSTALL_PREFIX}/lib
-    #  ${CMAKE_COMMAND}
-    DEPENDS ${${proj}_DEPENDENCIES}
-    PATCH_COMMAND ${CMAKE_COMMAND} -E copy_directory 
-      ${CMAKE_SOURCE_DIR}/patches/${proj}
-      ${LIBKML_SB_SRC}
+    
     )
   
   message(STATUS "  Using libKML SuperBuild version")
