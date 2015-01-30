8d4e3fbb7985384bcddd044a49cf22b152cf04f5
Rashad Kanavath <rashad.kanavath@c-s.fr>
2015-01-30 18:05:12 +0100

    force static build of Monteverdi on MSVC

--- CMake/External_Monteverdi.cmake
+++ CMake/External_Monteverdi.cmake
@@ -7,7 +7,7 @@ message(STATUS "Setup Monteverdi...")
 set(MVD_SB_SRC ${CMAKE_BINARY_DIR}/${proj}/src/${proj})
 set(MVD_SB_BUILD_DIR ${CMAKE_BINARY_DIR}/${proj}/build)
 
-set(${proj}_DEPENDENCIES OTB)
+set(${proj}_DEPENDENCIES)# OTB)
 
 if(USE_SYSTEM_FLTK)
   message(STATUS "  Using FLTK system version")
@@ -16,6 +16,10 @@ else()
     list(APPEND ${proj}_DEPENDENCIES FLTK)
 endif()
 
+set(MVD_SB_BUILD_SHARED ON)
+if(MSVC)
+  set(MVD_SB_BUILD_SHARED OFF)
+endif()
 
 ExternalProject_Add(${proj}
    PREFIX ${proj}
@@ -26,7 +30,7 @@ ExternalProject_Add(${proj}
    DEPENDS ${${proj}_DEPENDENCIES}
    CMAKE_CACHE_ARGS
     -DCMAKE_BUILD_TYPE:STRING=Release
-    -DBUILD_SHARED_LIBS:BOOL=ON
+    -DBUILD_SHARED_LIBS:BOOL=${MVD_SB_BUILD_SHARED}
     -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_INSTALL_PREFIX}
     ${MVD_SB_FLTK_CONFIG}
 )
