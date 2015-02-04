b0e31cd3df0faf4dc61bfb0397e979e9df89c408
Rashad Kanavath <rashad.kanavath@c-s.fr>
2015-02-04 17:17:10 +0100

    link libjpeg against DLL version of mscvrt library
    makefile.vc builds libjpeg static with /MT flags that
    link with static mscvt. This libjpeg when linking against
    gdal throws link error as gdal is linked against MSVCRT
    dll version so as all other dependencies in superbuild.
    This makes external libjpeg currently unusabe with gdal
    and end upwith two libjpeg in the build. This commit
    build libjpeg as cmake project which automatically add /MD
    depending on configuration and finally to have a single libjpeg
    used in GDAL and libtiff
    
    http://dash.orfeo-toolbox.org/testDetails.php?test=28153014&build=170814

--- CMake/External_jpeg.cmake
+++ CMake/External_jpeg.cmake
@@ -15,43 +15,27 @@ else()
   set(JPEG_SB_SRC ${CMAKE_BINARY_DIR}/${proj}/src/${proj})
   
   if(WIN32)
-
-   
-   ExternalProject_Add(${proj}_build
+      
+    ExternalProject_Add(${proj}
       PREFIX ${proj}
       URL "http://www.ijg.org/files/jpegsr8c.zip"
       URL_MD5 2af71a567af8f60a649b2fcc87c88192
       SOURCE_DIR ${JPEG_SB_SRC}
       BINARY_DIR ${JPEG_SB_BUILD_DIR}
       INSTALL_DIR ${CMAKE_INSTALL_PREFIX}
-      PATCH_COMMAND ${CMAKE_COMMAND} -E copy_directory ${JPEG_SB_SRC} ${JPEG_SB_BUILD_DIR}
-      CONFIGURE_COMMAND ""
-      BUILD_COMMAND nmake /f ${JPEG_SB_BUILD_DIR}/makefile.vc nodebug=1
-      INSTALL_COMMAND ${CMAKE_COMMAND} -E copy  ${CMAKE_SOURCE_DIR}/patches/${proj}/CMakeLists.txt
-      ${CMAKE_BINARY_DIR}/${proj}/_install
-    )
-
-    ExternalProject_Add_Step(${proj}_build copy_jconfig
-      COMMAND ${CMAKE_COMMAND} -E copy 
-        ${JPEG_SB_BUILD_DIR}/jconfig.vc ${JPEG_SB_BUILD_DIR}/jconfig.h
-      DEPENDEES configure
-      DEPENDERS build
-    )
-      
-      
-    ExternalProject_Add(${proj}
-      PREFIX ${proj}/_install
-      DOWNLOAD_COMMAND ""
-      SOURCE_DIR ${proj}/_install
-      BINARY_DIR ${JPEG_SB_BUILD_DIR}
-      INSTALL_DIR ${CMAKE_INSTALL_PREFIX}
+      PATCH_COMMAND ${CMAKE_COMMAND} -E copy  ${CMAKE_SOURCE_DIR}/patches/${proj}/CMakeLists.txt ${JPEG_SB_SRC}
       CMAKE_CACHE_ARGS
         -DCMAKE_INSTALL_PREFIX:STRING=${CMAKE_INSTALL_PREFIX}
         -DCMAKE_BUILD_TYPE:STRING=Release
-        -DJPEG_BUILD_DIR:STRING=${JPEG_SB_BUILD_DIR}
-      DEPENDS ${proj}_build
-      CMAKE_COMMAND
+        -DCMAKE_VERBOSE_MAKEFILE:BOOL=OFF
     )
+
+    ExternalProject_Add_Step(${proj} copy_jconfig
+      COMMAND ${CMAKE_COMMAND} -E copy 
+        ${JPEG_SB_SRC}/jconfig.vc ${JPEG_SB_SRC}/jconfig.h
+      DEPENDEES patch
+      DEPENDERS build
+    )        
       
 
   else()
