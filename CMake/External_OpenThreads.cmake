d2b51f044bbaae25154208d81f13ccfd9b8220b6
Rashad Kanavath <rashad.kanavath@c-s.fr>
2015-02-04 17:47:47 +0100

    ossim link error when using OpenThreads archive on windows

--- CMake/External_OpenThreads.cmake
+++ CMake/External_OpenThreads.cmake
@@ -18,25 +18,42 @@ else()
   set(OPENTHREADS_SB_BUILD_DIR ${CMAKE_BINARY_DIR}/${proj}/build)
   set(OPENTHREADS_SB_SRC ${CMAKE_BINARY_DIR}/${proj}/src/${proj})
   
-  ExternalProject_Add(${proj}
-    PREFIX ${proj}
-    #SVN_REPOSITORY "http://svn.openscenegraph.org/osg/OpenThreads/tags/OpenThreads-2.3.0/"
-    #SVN_REVISION -r 14667
-    URL "http://www.openscenegraph.org/downloads/developer_releases/OpenSceneGraph-3.2.0.zip"
-    URL_MD5 4980f8692712a24d4c99f363f80c6814
-    SOURCE_DIR ${OPENTHREADS_SB_SRC}
-    BINARY_DIR ${OPENTHREADS_SB_BUILD_DIR}
-    INSTALL_DIR ${CMAKE_INSTALL_PREFIX}
-    CMAKE_CACHE_ARGS
-      -DCMAKE_INSTALL_PREFIX:STRING=${CMAKE_INSTALL_PREFIX}
-      -DCMAKE_BUILD_TYPE:STRING=Release
-      -DBUILD_SHARED_LIBS:BOOL=ON
-    CMAKE_COMMAND ${SB_CMAKE_COMMAND}
-    DEPENDS ${${proj}_DEPENDENCIES}
-    PATCH_COMMAND ${CMAKE_COMMAND} -E copy 
-      ${CMAKE_SOURCE_DIR}/patches/${proj}/CMakeLists.txt 
-      ${OPENTHREADS_SB_SRC}
+  #TODO: try to use cmake variable for DOWNLOAD_COMMAND for different platforms
+  if(MSVC)
+    ExternalProject_Add(${proj}
+      PREFIX ${proj}
+      SVN_REPOSITORY "http://svn.openscenegraph.org/osg/OpenThreads/tags/OpenThreads-2.3.0/"
+      SVN_REVISION -r 14667
+      SOURCE_DIR ${OPENTHREADS_SB_SRC}
+      BINARY_DIR ${OPENTHREADS_SB_BUILD_DIR}
+      INSTALL_DIR ${CMAKE_INSTALL_PREFIX}
+      CMAKE_CACHE_ARGS
+        -DCMAKE_INSTALL_PREFIX:STRING=${CMAKE_INSTALL_PREFIX}
+        -DCMAKE_BUILD_TYPE:STRING=Release
+        -DBUILD_SHARED_LIBS:BOOL=ON
+        PATCH_COMMAND ${CMAKE_COMMAND} -E copy 
+        ${CMAKE_SOURCE_DIR}/patches/${proj}/CMakeLists.txt ${OPENTHREADS_SB_SRC}
+        CMAKE_COMMAND ${SB_CMAKE_COMMAND}
     )
+  else(UNIX or APPLE)
+    ExternalProject_Add(${proj}
+      PREFIX ${proj}
+      URL "http://www.openscenegraph.org/downloads/developer_releases/OpenSceneGraph-3.2.0.zip"
+      URL_MD5 4980f8692712a24d4c99f363f80c6814
+      SOURCE_DIR ${OPENTHREADS_SB_SRC}
+      BINARY_DIR ${OPENTHREADS_SB_BUILD_DIR}
+      INSTALL_DIR ${CMAKE_INSTALL_PREFIX}
+      CMAKE_CACHE_ARGS
+        -DCMAKE_INSTALL_PREFIX:STRING=${CMAKE_INSTALL_PREFIX}
+        -DCMAKE_BUILD_TYPE:STRING=Release
+        -DBUILD_SHARED_LIBS:BOOL=ON
+      CMAKE_COMMAND ${SB_CMAKE_COMMAND}
+      DEPENDS ${${proj}_DEPENDENCIES}
+      PATCH_COMMAND ${CMAKE_COMMAND} -E copy 
+        ${CMAKE_SOURCE_DIR}/patches/${proj}/CMakeLists.txt 
+        ${OPENTHREADS_SB_SRC}
+    )
+  endif()
   
   message(STATUS "  Using OpenThreads SuperBuild version")
 
