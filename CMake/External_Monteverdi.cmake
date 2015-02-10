6da90eb6d1dac59d956b3020e2570f895d007558
Rashad Kanavath <rashad.kanavath@c-s.fr>
2015-02-10 17:15:47 +0100

    set proper dependencies for Monterverdi and force static build

--- CMake/External_Monteverdi.cmake
+++ CMake/External_Monteverdi.cmake
@@ -7,18 +7,13 @@ message(STATUS "Setup Monteverdi...")
 set(MVD_SB_SRC ${CMAKE_BINARY_DIR}/${proj}/src/${proj})
 set(MVD_SB_BUILD_DIR ${CMAKE_BINARY_DIR}/${proj}/build)
 
-set(${proj}_DEPENDENCIES)# OTB)
+set(${proj}_DEPENDENCIES OTB)
 
 if(USE_SYSTEM_FLTK)
   message(STATUS "  Using FLTK system version")
 else()
     set(MVD_SB_FLTK_CONFIG)
     list(APPEND ${proj}_DEPENDENCIES FLTK)
-endif()
-
-set(MVD_SB_BUILD_SHARED ON)
-if(MSVC)
-  set(MVD_SB_BUILD_SHARED OFF)
 endif()
 
 ExternalProject_Add(${proj}
@@ -30,7 +25,8 @@ ExternalProject_Add(${proj}
    DEPENDS ${${proj}_DEPENDENCIES}
    CMAKE_CACHE_ARGS
     -DCMAKE_BUILD_TYPE:STRING=Release
-    -DBUILD_SHARED_LIBS:BOOL=${MVD_SB_BUILD_SHARED}
+    -DBUILD_SHARED_LIBS:BOOL=${SB_BUILD_SHARED_LIBS}
+    -DBUILD_TESTING:BOOL=ON
     -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_INSTALL_PREFIX}
     ${MVD_SB_FLTK_CONFIG}
 )
