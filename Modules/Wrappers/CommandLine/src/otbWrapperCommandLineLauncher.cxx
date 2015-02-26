b7eded3e31d41c7fa1dd422e2fc650094e888ea3
Julien Michel <julien.michel@orfeo-toolbox.org>
2015-02-26 11:31:33 +0100

    COMP: fixing small comp error

--- Modules/Wrappers/CommandLine/src/otbWrapperCommandLineLauncher.cxx
+++ Modules/Wrappers/CommandLine/src/otbWrapperCommandLineLauncher.cxx
@@ -64,7 +64,7 @@ CommandLineLauncher::CommandLineLauncher() :
   m_AddProcessCommand->SetCallbackFunction(this, &CommandLineLauncher::LinkWatchers);
 }
 
-CommandLineLauncher::CommandLineLauncher(const char * itkNotUsed(exp)) /*:
+CommandLineLauncher::CommandLineLauncher(const char *) /*:
   m_Expression(exp)*/
 {
   m_Application = NULL;
