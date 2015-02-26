157ec3dea904c710e283adcf3c6ec79b3d001729
Christophe Palmann <christophe.palmann@c-s.fr>
2015-02-26 14:58:48 +0100

    COMP:dependencies instructions missing

--- Modules/Adapters/OpenThreadsAdapters/otb-module.cmake
+++ Modules/Adapters/OpenThreadsAdapters/otb-module.cmake
@@ -2,6 +2,8 @@ set(DOCUMENTATION "Adapters for the OpenThreads library (thread interface for
 C++).")
 
 otb_module(OTBOpenThreadsAdapters
+  DEPENDS
+    OTBOpenThreads
 
   TEST_DEPENDS
     OTBTestKernel
