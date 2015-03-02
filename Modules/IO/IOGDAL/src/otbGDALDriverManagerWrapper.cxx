a186d3c42f1b0cf942b951c81e9ebec4e31b7291
Julien Michel <julien.michel@orfeo-toolbox.org>
2015-03-02 15:52:06 +0100

    ENH: Getting jasper blacklisted again, enabling HDF

--- Modules/IO/IOGDAL/src/otbGDALDriverManagerWrapper.cxx
+++ Modules/IO/IOGDAL/src/otbGDALDriverManagerWrapper.cxx
@@ -65,23 +65,20 @@ GDALDatasetWrapper::IsJPEG2000() const
 GDALDriverManagerWrapper::GDALDriverManagerWrapper()
 {
     GDALAllRegister();
+    
+    GDALDriver* driver = 0;
 
     // Ignore incompatible Jpeg2000 drivers (Jasper)
-    /*
     driver = GetGDALDriverManager()->GetDriverByName( "JPEG2000" );
     if (driver)
       GetGDALDriverManager()->DeregisterDriver( driver );
-    */
 
-#ifndef CHECK_HDF4OPEN_SYMBOL
-
-    GDALDriver* driver = 0;
-
-    // Get rid of the HDF4 driver when it is buggy
-    driver = GetGDALDriverManager()->GetDriverByName( "hdf4" );
-    if (driver)
-      GetGDALDriverManager()->DeregisterDriver( driver );
-#endif
+// #ifndef CHECK_HDF4OPEN_SYMBOL
+//     // Get rid of the HDF4 driver when it is buggy
+//     driver = GetGDALDriverManager()->GetDriverByName( "hdf4" );
+//     if (driver)
+//       GetGDALDriverManager()->DeregisterDriver( driver );
+// #endif
 }
 
 GDALDriverManagerWrapper::~GDALDriverManagerWrapper()
