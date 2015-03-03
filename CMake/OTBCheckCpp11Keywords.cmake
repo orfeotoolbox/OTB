f804900b10ed28f788db1fdcb3c202e37a9791a0
Christophe Palmann <christophe.palmann@c-s.fr>
2015-03-03 17:07:17 +0100

    COMP:Check Cpp11 Keywords

--- CMake/OTBCheckCpp11Keywords.cmake
+++ CMake/OTBCheckCpp11Keywords.cmake
@@ -1,32 +1,5 @@
-include(CheckCXXSourceRuns)
 
-unset(CMAKE_REQUIRED_INCLUDES)
-unset(CMAKE_REQUIRED_LIBRARIES)
-set(CMAKE_REQUIRED_FLAGS ${CMAKE_CXX_FLAGS})
-unset(CMAKE_REQUIRED_DEFINES)
+try_compile(OTB_IS_UNIQUE_PTR_DEFINED ${CMAKE_CURRENT_BINARY_DIR}/CMake ${CMAKE_SOURCE_DIR}/CMake/otbTestUniquePtr.cpp)
+try_compile(OTB_IS_OVERRIDE_DEFINED ${CMAKE_CURRENT_BINARY_DIR}/CMake ${CMAKE_SOURCE_DIR}/CMake/otbTestUniquePtr.cpp)
+try_compile(OTB_IS_NULLPTR_DEFINED ${CMAKE_CURRENT_BINARY_DIR}/CMake ${CMAKE_SOURCE_DIR}/CMake/otbTestUniquePtr.cpp)
 
-# check if unique_ptr is defined
-file(READ ${CMAKE_SOURCE_DIR}/CMake/otbTestUniquePtr.cpp
-  OTB_TEST_UNIQUE_PTR_SOURCE)
-check_cxx_source_runs(
-  "${OTB_TEST_UNIQUE_PTR_SOURCE}"
-  OTB_IS_UNIQUE_PTR_DEFINED
-  )
-
-# check if override keyword is known
-file(READ ${CMAKE_SOURCE_DIR}/CMake/otbTestOverride.cpp
-  OTB_TEST_OVERRIDE_SOURCE)
-check_cxx_source_runs(
-  "${OTB_TEST_OVERRIDE_SOURCE}"
-  OTB_IS_OVERRIDE_DEFINED
-  )
-
-# check if nullptr keyword is known
-file(READ ${CMAKE_SOURCE_DIR}/CMake/otbTestNullPtr.cpp
-  OTB_TEST_NULLPTR_SOURCE)
-check_cxx_source_runs(
-  "${OTB_TEST_NULLPTR_SOURCE}"
-  OTB_IS_NULLPTR_DEFINED
-  )
-
-unset(CMAKE_REQUIRED_FLAGS)
