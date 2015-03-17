9971138ff4a31054f237baf6de626ad70ac1e106
Guillaume Pasero <guillaume.pasero@c-s.fr>
2015-03-17 14:30:18 +0100

    ENH: add ITK compilation flags in OTB use file

--- CMake/UseOTB.cmake
+++ CMake/UseOTB.cmake
@@ -12,9 +12,19 @@ set(CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS} ${OTB_REQUIRED_LINK_
 
 
 # Add include directories needed to use OTB.
-include_directories(BEFORE ${OTB_INCLUDE_DIRS})
+include_directories(${OTB_INCLUDE_DIRS})
 
 # Add link directories needed to use OTB.
 link_directories(${OTB_LIBRARY_DIRS})
 
-# TODO : find_package() & include(UseFile) for all external dependencies
+# find_package() are already done (in OTBConfig.cmake)
+# perform include(UseFile) for all external dependencies
+
+# ITK : don't call UseFile to avoid the include_directorties(BEFORE ...)
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${ITK_REQUIRED_C_FLAGS}")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${ITK_REQUIRED_CXX_FLAGS}")
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${ITK_REQUIRED_LINK_FLAGS}")
+set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} ${ITK_REQUIRED_LINK_FLAGS}")
+set(CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS} ${ITK_REQUIRED_LINK_FLAGS}")
+
+# OpenCV : no USE_FILE defined
