c16dee7b59a557f1a8dc86dc1a2d78354abbf778
Guillaume Pasero <guillaume.pasero@c-s.fr>
2015-03-19 18:31:38 +0100

    ENH: superbuild: qt4 on msvc uses a wrapper script to configure and build

--- SuperBuild/CMake/External_qt4.cmake
+++ SuperBuild/CMake/External_qt4.cmake
@@ -29,42 +29,37 @@ else()
 
     STRING(REGEX REPLACE "/$" "" CMAKE_WIN_INSTALL_PREFIX ${SB_INSTALL_PREFIX})    
     STRING(REGEX REPLACE "/" "\\\\" CMAKE_WIN_INSTALL_PREFIX ${CMAKE_WIN_INSTALL_PREFIX})
+  
+  if(MSVC)
+    configure_file(${CMAKE_SOURCE_DIR}/patches/${proj}/configure_qt4.bat.in
+                  ${CMAKE_BINARY_DIR}/configure_qt4.bat
+                  @ONLY)
     
-  ExternalProject_Add(${proj}
-    PREFIX ${proj}
-    URL "http://download.qt-project.org/official_releases/qt/4.8/4.8.6/qt-everywhere-opensource-src-4.8.6.tar.gz"
-    URL_MD5 2edbe4d6c2eff33ef91732602f3518eb
-    BINARY_DIR ${QT4_SB_SRC}
-    INSTALL_DIR ${SB_INSTALL_PREFIX}
-    CONFIGURE_COMMAND
-      configure
-        -prefix ${CMAKE_WIN_INSTALL_PREFIX}
-        -opensource
-        -confirm-license
-        -release
-        -shared
-        -nomake demos
-        -nomake examples
-        -nomake tools
-        -no-phonon-backend
-        -no-phonon
-        -no-script
-        -no-scripttools
-        -no-multimedia
-        -no-webkit
-        -qt-sql-sqlite
-        -plugin-sql-sqlite
-        -no-nis
-        -no-qt3support
-        -system-zlib
-        -system-libpng
-        -system-libtiff
-        -system-libjpeg
-        #-system-sqlite
-        -L ${CMAKE_WIN_INSTALL_PREFIX}\\lib
-        -I ${CMAKE_WIN_INSTALL_PREFIX}\\include
-    DEPENDS ${${proj}_DEPENDENCIES}
-    )
+    ExternalProject_Add(${proj}
+      PREFIX ${proj}
+      URL "http://download.qt-project.org/official_releases/qt/4.8/4.8.6/qt-everywhere-opensource-src-4.8.6.tar.gz"
+      URL_MD5 2edbe4d6c2eff33ef91732602f3518eb
+      BINARY_DIR ${QT4_SB_SRC}
+      INSTALL_DIR ${SB_INSTALL_PREFIX}
+      CONFIGURE_COMMAND
+        configure_qt4.bat
+      PATCH_COMMAND ${CMAKE_COMMAND} -E copy
+        ${CMAKE_BINARY_DIR}/configure_qt4.bat
+        ${QT4_SB_SRC}
+      DEPENDS ${${proj}_DEPENDENCIES}
+      )
+  else()
+    ExternalProject_Add(${proj}
+      PREFIX ${proj}
+      URL "http://download.qt-project.org/official_releases/qt/4.8/4.8.6/qt-everywhere-opensource-src-4.8.6.tar.gz"
+      URL_MD5 2edbe4d6c2eff33ef91732602f3518eb
+      BINARY_DIR ${QT4_SB_SRC}
+      INSTALL_DIR ${SB_INSTALL_PREFIX}
+      CONFIGURE_COMMAND
+        configure -prefix ${CMAKE_WIN_INSTALL_PREFIX} -opensource -confirm-license -release -shared -nomake demos -nomake examples -nomake tools -no-phonon-backend -no-phonon -no-script -no-scripttools -no-multimedia -no-webkit -qt-sql-sqlite -plugin-sql-sqlite -no-nis -no-qt3support  -system-zlib -system-libpng -system-libtiff -system-libjpeg -system-sqlite -L ${CMAKE_WIN_INSTALL_PREFIX}\\lib -I ${CMAKE_WIN_INSTALL_PREFIX}\\include
+      DEPENDS ${${proj}_DEPENDENCIES}
+      )
+  endif()
   
   #copying Qt4 to have out-of-source build takes too much disk space
   # ExternalProject_Add_Step(${proj} copy_source
