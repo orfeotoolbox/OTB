1693ab51d825e6afbb6968e01075961f9400fafb
Julien Michel <julien.michel@orfeo-toolbox.org>
2015-03-24 17:08:36 +0100

    COMP: Fixing missing https targets

--- Utilities/Maintenance/SuperbuildDownloadList.sh
+++ Utilities/Maintenance/SuperbuildDownloadList.sh
@@ -1,2 +1,2 @@
 #/bin/bash
-grep "http://" ../../SuperBuild/CMake/*.cmake | grep URL | cut -d "\"" -f 2 | sed "s/\/download$//" | sort | uniq
+grep "http" ../../SuperBuild/CMake/*.cmake | grep URL | cut -d "\"" -f 2 | sed "s/\/download$//" | sort | uniq
