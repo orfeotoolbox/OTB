72eb670e36d6c4952623ac55189c45af2f2e8b18
Manuel Grizonnet <manuel.grizonnet@orfeo-toolbox.org>
2015-04-13 14:22:40 +0200

    ENH: refine boost third party find_package

--- Modules/ThirdParty/Boost/otb-module-init.cmake
+++ Modules/ThirdParty/Boost/otb-module-init.cmake
@@ -1,7 +1,3 @@
-if (BUILD_TESTING)
-  set(OTB_Boost_OPTIONAL_COMPONENTS unit_test_framework)
-endif()
-
 # Required
 message(STATUS "Looking for required Boost headers")
 find_package ( Boost
@@ -10,8 +6,16 @@ find_package ( Boost
   )
 
 # Optional components
+# Boost (OPTIONAL_COMPONENTS does not work with Boost find_package)
+# unit_test_framework component is used only in GdalAdapters module
+if (BUILD_TESTING)
+  set(OTB_Boost_OPTIONAL_COMPONENTS unit_test_framework)
+endif()
+
+#Quiet find package to avoid message "Could not find boost" because unit_testing_framework is not installed
 message(STATUS "Looking for optional Boost components : ${OTB_Boost_OPTIONAL_COMPONENTS}")
 find_package ( Boost
+  QUIET
   1.35.0
   COMPONENTS ${OTB_Boost_OPTIONAL_COMPONENTS}
   )
