a376e3ffd1adf5c9b79e31c2a00bbec25dc46b91
Guillaume Pasero <guillaume.pasero@c-s.fr>
2015-04-16 16:47:57 +0200

    Remove EdisonMeanShift module

--- Modules/Segmentation/EdisonMeanShift/test/otbEdisonMeanShiftTestDriver.cxx
+++
@@ -1,8 +0,0 @@
-#include "otbTestMain.h"
-void RegisterTests()
-{
-  REGISTER_TEST(otbMeanShiftImageFilterNew);
-  REGISTER_TEST(otbMeanShiftImageFilter);
-  REGISTER_TEST(otbMeanShiftVectorImageFilterNew);
-  REGISTER_TEST(otbMeanShiftVectorImageFilter);
-}
