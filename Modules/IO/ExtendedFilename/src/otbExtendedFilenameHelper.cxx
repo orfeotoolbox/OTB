269e8baf50a4aaa35aee643f05cf3360cf9c5143
Christophe Palmann <christophe.palmann@c-s.fr>
2015-04-29 14:59:57 +0200

    ENH: extendedfilename, check if values are set

--- Modules/IO/ExtendedFilename/src/otbExtendedFilenameHelper.cxx
+++ Modules/IO/ExtendedFilename/src/otbExtendedFilenameHelper.cxx
@@ -40,20 +40,26 @@ ExtendedFilenameHelper
       {
       boost::split(tmp2, tmp1[1], boost::is_any_of("&"), boost::token_compress_on);
       for (unsigned int i=0; i<tmp2.size(); i++)
+       if (tmp2[i].length() >0)
         {
-        std::vector<std::string> tmp;
-        boost::split(tmp, tmp2[i], boost::is_any_of("="), boost::token_compress_on);
-        if (tmp.size()>1)
-          {
-          if (m_OptionMap[tmp[0]].empty())
-            {
-            m_OptionMap[tmp[0]]=tmp[1];
-            }
-          else
-            {
-            itkWarningMacro("Duplicated option detected: " << tmp[0] << ". Using value " << tmp[1] << ".");
-            }
-          }
+			std::vector<std::string> tmp;
+			boost::split(tmp, tmp2[i], boost::is_any_of("="), boost::token_compress_on);
+			if (tmp.size()>1)
+				{
+				  if (tmp[1].length()>0)
+					  {
+						  if (m_OptionMap[tmp[0]].empty())
+							{
+								m_OptionMap[tmp[0]]=tmp[1];
+							}
+						  else
+							{
+								itkWarningMacro("Duplicated option detected: " << tmp[0] << ". Using value " << tmp[1] << ".");
+							}
+					  }
+				  else 
+					itkGenericExceptionMacro( << "Value for option '" << tmp[0] << "' is not set.");
+				}
         }
       }
     }
