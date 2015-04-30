9e26004b49c0300fbfa851e7408a2954081f686a
Stephane Albert <stephane.albert@c-s.fr>
2015-04-30 11:24:59 +0200

    COMP: Added mv2.bat starter script for Windows platform.

---
+++ Packaging/Windows/mv2.bat
@@ -0,0 +1,16 @@
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: Monteverdi launcher to set up proper GDAL environment
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+:: Get the directory of the current script 
+@set CURRENT_SCRIPT_DIR=%~dp0
+
+:: Set GDAL_DATA env. variable
+@set GDAL_DATA=%CURRENT_SCRIPT_DIR%..\share\gdal
+@set ITK_AUTOLOAD_PATH=%CURRENT_SCRIPT_DIR%..\lib\otb\applications
+
+:: Set current dir to HOME dir because Monteverdi generates temporary files and need write access
+@cd %HOMEDRIVE%%HOMEPATH%
+
+:: Start Monteverdi
+@start "Monteverdi2 Viewer" /B "%CURRENT_SCRIPT_DIR%mv2.exe" %*
