8919a3bc466e7aa06d872cac0ee20c4b933af4a7
Rashad Kanavath <rashad.kanavath@c-s.fr>
2015-05-20 10:56:21 +0200

    BUG: deactivate two convolution tests if ITK_USE_FFTWD is not set

--- Modules/Filtering/Convolution/test/otbConvolutionTestDriver.cxx
+++ Modules/Filtering/Convolution/test/otbConvolutionTestDriver.cxx
@@ -4,8 +4,10 @@ void RegisterTests()
   REGISTER_TEST(otbConvolutionImageFilter);
   REGISTER_TEST(otbConvolutionImageFilterNew);
   REGISTER_TEST(otbOverlapSaveConvolutionImageFilterNew);
+#if defined(ITK_USE_FFTWD)
   REGISTER_TEST(otbOverlapSaveConvolutionImageFilter);
   REGISTER_TEST(otbCompareOverlapSaveAndClassicalConvolutionWithGaborFilter);
+#endif
   REGISTER_TEST(otbGaborFilterGenerator);
   REGISTER_TEST(otbGaborFilterGeneratorNew);
 }
