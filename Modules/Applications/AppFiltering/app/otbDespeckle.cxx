2811a18b9f9fa1d1c937cb0f9e9ca788ac8556d5
Christophe Palmann <christophe.palmann@c-s.fr>
2015-07-06 15:59:13 +0200

    ENH: added GammaMAP and Kuan filters to the despeckling app + some tests (SAR)

--- Modules/Applications/AppFiltering/app/otbDespeckle.cxx
+++ Modules/Applications/AppFiltering/app/otbDespeckle.cxx
@@ -20,6 +20,8 @@
 
 #include "otbFrostImageFilter.h"
 #include "otbLeeImageFilter.h"
+#include "otbGammaMAPImageFilter.h"
+#include "otbKuanImageFilter.h"
 #include "otbMultiToMonoChannelExtractROI.h"
 
 namespace otb
@@ -38,8 +40,10 @@ public:
 
   typedef itk::ImageToImageFilter<FloatImageType, FloatImageType> SpeckleFilterType;
 
-  typedef LeeImageFilter<FloatImageType, FloatImageType>   LeeFilterType;
-  typedef FrostImageFilter<FloatImageType, FloatImageType> FrostFilterType;
+  typedef LeeImageFilter<FloatImageType, FloatImageType>        LeeFilterType;
+  typedef FrostImageFilter<FloatImageType, FloatImageType>      FrostFilterType;
+  typedef GammaMAPImageFilter<FloatImageType, FloatImageType>   GammaMAPFilterType;
+  typedef KuanImageFilter<FloatImageType, FloatImageType>       KuanFilterType;
 
   /** Standard macro */
   itkNewMacro(Self);
@@ -54,7 +58,7 @@ private:
 
     // Documentation
     SetDocName("Despeckle");
-    SetDocLongDescription("This application reduce speckle noise. Two methods are available: Lee and Frost.");
+    SetDocLongDescription("This application reduce speckle noise. Four methods are available: Lee, Frost, GammaMAP and Kuan.");
     SetDocLimitations("None");
     SetDocAuthors("OTB-Team");
     SetDocSeeAlso(" ");
@@ -74,6 +78,10 @@ private:
     SetParameterDescription("filter.lee","Lee filter");
     AddChoice("filter.frost","Frost");
     SetParameterDescription("filter.frost","Frost filter");
+    AddChoice("filter.gammamap","GammaMap");
+    SetParameterDescription("filter.gammamap","GammaMap filter");
+    AddChoice("filter.kuan","Kuan");
+    SetParameterDescription("filter.kuan","Kuan filter");
 
     AddParameter(ParameterType_Int,"filter.lee.rad","Radius");
     SetParameterDescription("filter.lee.rad","Radius for lee filter");
@@ -86,12 +94,26 @@ private:
 
     AddParameter(ParameterType_Float,"filter.frost.deramp","deramp");
     SetParameterDescription("filter.frost.deramp","Decrease factor declaration");
+    
+    AddParameter(ParameterType_Int,"filter.gammamap.rad","Radius");
+    SetParameterDescription("filter.gammamap.rad","Radius for GammaMAP filter");
+
+    AddParameter(ParameterType_Float,"filter.gammamap.nblooks","nb looks");
+    SetParameterDescription("filter.gammamap.nblooks","Nb looks for GammaMAP filter");
+    
+    AddParameter(ParameterType_Int,"filter.kuan.rad","Radius");
+    SetParameterDescription("filter.kuan.rad","Radius for Kuan filter");
+
+    AddParameter(ParameterType_Float,"filter.kuan.nblooks","nb looks");
+    SetParameterDescription("filter.kuan.nblooks","Nb looks for Kuan filter");
 
     // Default values
     SetDefaultParameterInt("filter.lee.rad", 1);
     SetDefaultParameterFloat("filter.lee.nblooks", 1.);
     SetDefaultParameterInt("filter.frost.rad",  1);
     SetDefaultParameterFloat("filter.frost.deramp",  0.1);
+    SetDefaultParameterInt("filter.gammamap.rad", 1);
+    SetDefaultParameterFloat("filter.gammamap.nblooks", 1.);
 
 
     // Doc example parameter settings
@@ -155,6 +177,40 @@ private:
       filter->SetDeramp(GetParameterFloat("filter.frost.deramp"));
 
       otbAppLogINFO( << "Frost filter" );
+      m_SpeckleFilter = filter;
+      break;
+      }
+      case 2:
+      {
+      GammaMAPFilterType::Pointer  filter = GammaMAPFilterType::New();
+      m_Ref.push_back(filter.GetPointer());
+
+      filter->SetInput(inImage);
+
+      GammaMAPFilterType::SizeType lradius;
+      lradius.Fill(GetParameterInt("filter.gammamap.rad"));
+
+      filter->SetRadius(lradius);
+      filter->SetNbLooks(GetParameterFloat("filter.gammamap.nblooks"));
+
+      otbAppLogINFO( << "GammaMAP filter" );
+      m_SpeckleFilter = filter;
+      break;
+      }
+      case 3:
+      {
+      KuanFilterType::Pointer  filter = KuanFilterType::New();
+      m_Ref.push_back(filter.GetPointer());
+
+      filter->SetInput(inImage);
+
+      KuanFilterType::SizeType lradius;
+      lradius.Fill(GetParameterInt("filter.kuan.rad"));
+
+      filter->SetRadius(lradius);
+      filter->SetNbLooks(GetParameterFloat("filter.kuan.nblooks"));
+
+      otbAppLogINFO( << "Kuan filter" );
       m_SpeckleFilter = filter;
       break;
       }
