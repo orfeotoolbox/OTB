06cbb9fec253e60684e8ced1e149812f1b9ad336
Rashad M <rashad.kanavath@c-s.fr>
2015-07-08 14:05:54 +0200

    ENH: add custom target to build portable binary package for OTB

---
+++ SuperBuild/CMake/External_pkgtools.cmake
@@ -0,0 +1,37 @@
+include(ExternalProject)
+set(proj PACKAGE-TOOLS)
+
+if(NOT __EXTERNAL_${proj}__)
+set(__EXTERNAL_${proj}__ 1)
+
+set(PKGTOOLS_SB_SRC "${CMAKE_BINARY_DIR}/${proj}/src/${proj}")
+set(PKGTOOLS_SB_BUILD_DIR "${CMAKE_BINARY_DIR}/${proj}/build/")
+
+ExternalProject_Add(${proj}
+  PREFIX ${proj}
+  URL "http://nixos.org/releases/patchelf/patchelf-0.8/patchelf-0.8.tar.gz"
+  URL_MD5 407b229e6a681ffb0e2cdd5915cb2d01
+  SOURCE_DIR ${PKGTOOLS_SB_SRC}
+  BINARY_DIR ${PKGTOOLS_SB_BUILD_DIR}
+  INSTALL_DIR ${CMAKE_INSTALL_PREFIX}
+  PATCH_COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_INSTALL_PREFIX}/tools
+  UPDATE_COMMAND ${CMAKE_COMMAND} -E copy_directory ${PKGTOOLS_SB_SRC} ${PKGTOOLS_SB_BUILD_DIR}
+  CONFIGURE_COMMAND ${PKGTOOLS_SB_BUILD_DIR}/configure --prefix ${CMAKE_INSTALL_PREFIX}/tools
+  BUILD_COMMAND $(MAKE)
+  INSTALL_COMMAND ${CMAKE_COMMAND} -E copy ${PKGTOOLS_SB_BUILD_DIR}/src/patchelf ${CMAKE_INSTALL_PREFIX}/tools )
+
+ExternalProject_Add(makeself
+  PREFIX makeself
+  URL "https://www.orfeo-toolbox.org/packages/makeself-2.2.0.tar.gz"
+  URL_MD5 3c61df934b0c61ddcd7bd63b391e951d
+  DOWNLOAD_DIR ${DOWNLOAD_LOCATION}
+  SOURCE_DIR ${PKGTOOLS_SB_BUILD_DIR}/makeself/src
+  BINARY_DIR ${PKGTOOLS_SB_BUILD_DIR}/makeself/build
+  INSTALL_DIR ${CMAKE_INSTALL_PREFIX}
+  ####${CMAKE_COMMAND} -E copy ${PKGTOOLS_SB_BUILD_DIR}/makeself/src/makeself.lsm ${CMAKE_BINARY_DIR}/makeself.lsm
+  CONFIGURE_COMMAND ""
+  BUILD_COMMAND ${CMAKE_COMMAND} -E copy ${PKGTOOLS_SB_BUILD_DIR}/makeself/src/makeself-header.sh ${CMAKE_BINARY_DIR}/makeself-header.sh
+  INSTALL_COMMAND ${CMAKE_COMMAND} -E copy ${PKGTOOLS_SB_BUILD_DIR}/makeself/src/makeself.sh ${CMAKE_BINARY_DIR}/makeself.sh  
+  DEPENDS ${proj})
+
+endif()
--- SuperBuild/CMake/Package_Macro.cmake
+++ SuperBuild/CMake/Package_Macro.cmake
@@ -2,18 +2,28 @@ macro(superbuild_package)
   cmake_parse_arguments(PACKAGE  "" "OUTDIR;INSTALLDIR" "SEARCHDIRS;PEFILES" ${ARGN} )
 
   find_program(OBJDUMP_PROGRAM "objdump")
-  set(PATCHELF_PROGRAM "/home/mrashad/Tools/patchelf-0.8/src/patchelf")
+
+  include(GetPrerequisites)
 
   list(APPEND PACKAGE_SEARCHDIRS "${PACKAGE_INSTALLDIR}/bin") #exe
   list(APPEND PACKAGE_SEARCHDIRS "${PACKAGE_INSTALLDIR}/lib") #so
   list(APPEND PACKAGE_SEARCHDIRS "${PACKAGE_INSTALLDIR}/lib/otb/applications") #otb apps
 
-#  install_common()
-
   execute_process(COMMAND ${CMAKE_COMMAND} -E remove_directory "${PACKAGE_INSTALLDIR}/${PACKAGE_OUTDIR}")
 
+  #install share/gdal, otbcli* otbgui*
+  install_common()
+
+  list(APPEND PACKAGE_PEFILES ${PACKAGE_INSTALLDIR}/bin/otbApplicationLauncherCommandLine)
+  
+  list(APPEND PACKAGE_PEFILES ${PACKAGE_INSTALLDIR}/bin/otbTestDriver)
+
+  if(OTB_USE_QT4)
+    list(APPEND PACKAGE_PEFILES ${PACKAGE_INSTALLDIR}/bin/otbApplicationLauncherQt)
+  endif()
+  
   file(GLOB otbapps_list ${PACKAGE_INSTALLDIR}/lib/otb/applications/otbapp_*so) # /lib/otb
-#  list(APPEND PACKAGE_PEFILES ${otbapps_list})
+  list(APPEND PACKAGE_PEFILES ${otbapps_list})
 
   set(alldlls)
   set(notfound_dlls)
@@ -28,8 +38,12 @@ macro(superbuild_package)
     message(FATAL_ERROR "Following dlls were not found: ${notfound_dlls}. Please consider adding their paths to SEARCHDIRS when calling superbuild_package macro.")
   endif()
 
-endmacro(superbuild_package)
+  set(PKG_OUTPUT_DIR "${PACKAGE_INSTALLDIR}/${PACKAGE_OUTDIR}")
+  configure_file(${SETUP_SCRIPT_SRC}
+    ${PACKAGE_INSTALLDIR}/${PACKAGE_OUTDIR}/pkgsetup @ONLY)
 
+  
+endmacro(superbuild_package)
 
 SET(SYSTEM_DLLS
   libm.so
@@ -40,6 +54,7 @@ SET(SYSTEM_DLLS
   libdl.so
   libpthread.so
   libidn.so
+  libgomp.so*
   )
 
 ## http://www.cmake.org/Wiki/CMakeMacroListOperations
@@ -61,6 +76,10 @@ macro(list_contains var value)
   endforeach(value2)
 endmacro()
 
+macro(install_rpath_code src_filename)
+
+endmacro()
+
 function(process_deps infile)
 
   get_filename_component(bn ${infile} NAME)
@@ -73,29 +92,28 @@ function(process_deps infile)
         if(EXISTS ${SEARCHDIR}/${infile})
           set(DLL_FOUND TRUE)
           message(STATUS "Processing ${SEARCHDIR}/${infile}")
-          #get_filename_component(file_ext ${infile} EXT)
-          # if("${file_ext}" STREQUAL "${CMAKE_SHARED_LIBRARY_SUFFIX}")
-          #   install(FILES "${SEARCHDIR}/${infile}"
-          #     DESTINATION ${PACKAGE_OUTDIR}/lib)
-
-
-          # add_custom_target(RPATH_${bn}
-          #   INSTALL
-          #     POST_BUILD
-          #     COMMAND ${PATCHELF_PROGRAM} "--set-rpath" "../lib" "${PACKAGE_INSTALLDIR}/${PACKAGE_OUTDIR}/bin/${infile}")
-
-          #else() #we assume it is executable
-            # install(FILES "${SEARCHDIR}/${infile}"
-            #   DESTINATION ${PACKAGE_OUTDIR}/bin)
-
-            file(GLOB sofiles "${SEARCHDIR}/${bn}*")
-            foreach(sofile ${sofiles})
-            install(TARGETS "${sofile}"
+          is_file_executable("${SEARCHDIR}/${infile}" is_executable)
+          if(is_executable)
+            install(FILES "${SEARCHDIR}/${infile}"
               DESTINATION ${PACKAGE_OUTDIR}/bin
-              PERMISSIONS )
+              PERMISSIONS OWNER_EXECUTE OWNER_WRITE OWNER_READ GROUP_EXECUTE GROUP_READ)
+            
+          else(is_executable)
+            if(${infile} MATCHES "otbapp_")
+              install(FILES "${PACKAGE_INSTALLDIR}/lib/otb/applications/${infile}"
+                DESTINATION ${PACKAGE_OUTDIR}/lib/otb/applications
+                PERMISSIONS OWNER_EXECUTE OWNER_WRITE OWNER_READ GROUP_EXECUTE GROUP_READ)
+            else()
+            get_filename_component(bn_we ${infile} NAME_WE)
+            file(GLOB sofiles "${SEARCHDIR}/${bn_we}*")
+            foreach(sofile ${sofiles})
+              install(FILES "${sofile}"
+                DESTINATION ${PACKAGE_OUTDIR}/lib
+                PERMISSIONS OWNER_EXECUTE OWNER_WRITE OWNER_READ GROUP_EXECUTE GROUP_READ)
             endforeach()
-          #endif()
-
+            
+            endif()
+          endif(is_executable)
           if(NOT EXISTS ${OBJDUMP_PROGRAM})
             message(FATAL_ERROR "objdump executable not found. please check OBJDUMP_PROGRAM is set to correct cross compiled executable")
           endif()
@@ -124,44 +142,40 @@ function(process_deps infile)
     set(notfound_dlls "${notfound_dlls}" PARENT_SCOPE )
   endif()
 
-
 endfunction()
 
 function(install_common)
   set(OUT_DIR "${PACKAGE_OUTDIR}")
-  set(BIN_DIR "${OUTDIR}/bin")
-  set(OTBLIBS_DIR "${OUTDIR}/lib/otb")
-  set(DATA_DIR "${OUTDIR}/share")
+  set(BIN_DIR "${OUT_DIR}/bin")
+  set(OTBAPPS_DIR "${OUT_DIR}/lib/otb")
+  set(DATA_DIR "${OUT_DIR}/share")
 
-  ####################### install GDAL data #######################
+  ####################### install GDAL data ###########################
   find_path(GDAL_DATA epsg.wkt ${PACKAGE_INSTALLDIR}/share/gdal)
   install(DIRECTORY ${GDAL_DATA}
     DESTINATION ${DATA_DIR})
 
-  ####################### Check otb applications #######################
+  ####################### install patchelf #####################
+  install(FILES ${PACKAGE_INSTALLDIR}/tools/patchelf
+    DESTINATION ${OUT_DIR}/tools
+    PERMISSIONS OWNER_EXECUTE OWNER_WRITE OWNER_READ GROUP_EXECUTE GROUP_READ
+    )
 
-  file(GLOB OTB_APPS_LIST ${PACKAGE_INSTALLDIR}/lib/otb/applications/otbapp_*so) # /lib/otb
-  if(NOT OTB_APPS_LIST)
-    message(FATAL_ERROR "No OTB-applications detected")
-  endif()
-
-  ####################### install otb applications #######################
-  install(DIRECTORY "${PACKAGE_INSTALLDIR}/lib/otb/applications"
-    DESTINATION ${OTBLIBS_DIR})
-
-  ####################### install otbcli scripts #######################
-  file(GLOB CLI_SCRIPTS ${SB_INSTALL_DIR}/bin/otbcli*)
+  ####################### install otbcli scripts ######################
+  file(GLOB CLI_SCRIPTS ${PACKAGE_INSTALLDIR}/bin/otbcli*)
   foreach(CLI_SCRIPT ${CLI_SCRIPTS})
     install(FILES "${CLI_SCRIPT}"
-      DESTINATION ${BIN_DIR})
+      DESTINATION ${BIN_DIR}
+    PERMISSIONS OWNER_EXECUTE OWNER_WRITE OWNER_READ GROUP_EXECUTE GROUP_READ)
   endforeach()
 
-  ####################### install otbgui scripts #######################
+  ####################### install otbgui scripts ######################
   if(OTB_USE_QT4)
-    file(GLOB GUI_SCRIPTS ${SB_INSTALL_DIR}/bin/otbgui*)
+    file(GLOB GUI_SCRIPTS ${PACKAGE_INSTALLDIR}/bin/otbgui*)
     foreach(GUI_SCRIPT ${GUI_SCRIPTS})
       install(FILES "${GUI_SCRIPT}"
-        DESTINATION ${BIN_DIR})
+        DESTINATION ${BIN_DIR}
+        PERMISSIONS OWNER_EXECUTE OWNER_WRITE OWNER_READ GROUP_EXECUTE GROUP_READ)
     endforeach()
   endif()
 
