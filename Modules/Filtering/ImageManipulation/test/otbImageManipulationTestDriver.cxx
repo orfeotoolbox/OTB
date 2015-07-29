27f75a1f93c76ec60e864152549446253ccaa784
Christophe Palmann <christophe.palmann@c-s.fr>
2015-07-29 18:17:35 +0200

    ENH: Im manipulation, complex band vs RI band conversion filters (SAR)

--- Modules/Filtering/ImageManipulation/test/otbImageManipulationTestDriver.cxx
+++ Modules/Filtering/ImageManipulation/test/otbImageManipulationTestDriver.cxx
@@ -76,4 +76,7 @@ void RegisterTests()
   REGISTER_TEST(otbFunctionWithNeighborhoodToImageFilterNew);
   REGISTER_TEST(otbEuclideanDistanceMetricWithMissingValue);
   REGISTER_TEST(otbEuclideanDistanceMetricWithMissingValueNew);
+  REGISTER_TEST(otbNRIBandImagesToOneNComplexBandsImage);
+  REGISTER_TEST(otbOneRIBandImageToOneComplexBandImage);
+  REGISTER_TEST(otbTwoNRIBandsImageToNComplexBandsImage);
 }
