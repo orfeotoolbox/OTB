e1e46886d2417de5068ddb1601682d9164cd5075
Rashad Kanavath <rashad.kanavath@c-s.fr>
2015-09-21 14:44:19 +0200

    ENH: register sentinel1 metadatainterface class

--- Modules/Core/Metadata/src/otbImageMetadataInterfaceFactory.cxx
+++ Modules/Core/Metadata/src/otbImageMetadataInterfaceFactory.cxx
@@ -32,6 +32,7 @@
 
 // SAR Sensors
 #include "otbTerraSarImageMetadataInterfaceFactory.h"
+#include "otbSentinel1ImageMetadataInterfaceFactory.h"
 
 #include "itkMutexLock.h"
 #include "itkMutexLockHolder.h"
@@ -109,7 +110,9 @@ ImageMetadataInterfaceFactory
       itk::ObjectFactoryBase::RegisterFactory(FormosatImageMetadataInterfaceFactory::New());
       itk::ObjectFactoryBase::RegisterFactory(QuickBirdImageMetadataInterfaceFactory::New());
       itk::ObjectFactoryBase::RegisterFactory(WorldView2ImageMetadataInterfaceFactory::New());
-      itk::ObjectFactoryBase::RegisterFactory( TerraSarImageMetadataInterfaceFactory::New() );
+      itk::ObjectFactoryBase::RegisterFactory(TerraSarImageMetadataInterfaceFactory::New());
+      itk::ObjectFactoryBase::RegisterFactory(Sentinel1ImageMetadataInterfaceFactory::New());
+
       firstTime = false;
       }
     }
