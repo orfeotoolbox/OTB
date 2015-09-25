2b87a89996e657fbee8a31f7f5a892044f27e7b7
Rashad Kanavath <rashad.kanavath@c-s.fr>
2015-09-25 17:09:23 +0200

    update string_utils functions from 'string_utils' branch

--- Modules/Adapters/BoostAdapters/include/otbStringUtils.h
+++ Modules/Adapters/BoostAdapters/include/otbStringUtils.h
@@ -20,149 +20,108 @@
 
 #include <string>
 #include <vector>
-#include <cstdlib>
-#include <iostream>
+#include <limits>
+#include <stdexcept>
 #include <boost/lexical_cast.hpp>
 #include <boost/algorithm/string.hpp>
 #include <boost/algorithm/string/predicate.hpp>
-
-
-namespace boost {
-
-/* I want raw performance and I don't want to use strtod, atoi at different
-* places. so here we are. */
-template<>
-inline int lexical_cast(const std::string& arg)
-{
-  char* stop;
-  int res = std::strtol( arg.c_str(), &stop, 10);
-    if ( *stop != 0 ) throw_exception(bad_lexical_cast(typeid(int), typeid(std::string)));
-    return res;
-}
-
-template<>
-inline float lexical_cast(const std::string& arg)
-{
-    char* stop;
-    float res = std::strtof( arg.c_str(), &stop);
-    if ( *stop != 0 ) throw_exception(bad_lexical_cast(typeid(float), typeid(std::string)));
-    return res;
-}
-
-template<>
-inline double lexical_cast(const std::string& arg)
-{
-    char* stop;
-    double res = std::strtod( arg.c_str(), &stop);
-    if ( *stop != 0 ) throw_exception(bad_lexical_cast(typeid(double), typeid(std::string)));
-    return res;
-}
-
-
-/* there could also be specialised vice-versa conversion below */
-// template<>
-// inline std::string lexical_cast(const int& arg)
-// {
-//     char buffer[20]; // large enough for arg < 2^200
-
-//     ltoa( arg, buffer, 10 );
-//     return std::string( buffer ); // RVO will take place here
-// }
-
-}//namespace boost
-
+#include <boost/utility/enable_if.hpp>
 
 namespace otb
 {
 namespace Utils
 {
+template <typename Res, typename In >
+inline
+Res LexicalCast(In const& in, std::string const& kind) {
+    try
+    {
+        return boost::lexical_cast<Res>(in);
+    }
+    catch (boost::bad_lexical_cast &) {
+        std::ostringstream oss;
+        oss << "Cannot decode " << in << " as this is not a valid value for " << kind;
+        throw std::runtime_error(oss.str());
+    }
+}
 
 /**\ingroup Utils
-   * - convert a delimitter seperated string to std::vector of type T
-   * \tparam vec - std::vector of type T. Specialized boost::lexical_cast is used
-   * \tparam str - input string
-   * \tparam delims - delimitter space is default
-   * \t param T& ret - std::vector of type T
-   */
-
+ * Converts a delimiter separated string into a collection of \c T's.
+ * \tparam Collection type. It shall define \c value_type and \c push_back()
+ * \param[out] ret output collection.
+ * \param[in] str  input string
+ * \param[in] errmsg a msg complement used to build the error message to
+be shown if there is \c lexical_cast exception. See \c
+otb:Utils::LexicalCast().
+ * \param delims  delimitter characters (space is default)
+ * \throw std::bad_alloc in case of exhausted memory
+ * \throw std::runtime_error in case an element from the input string
+cannot be converted into a valid \c T instance.
+ * \pre delims shall not be null (untested)
+ *
+ * \see \c otb::Utils::LexicalCast()
+ */
 template<typename T>
 void
-ConvertStringToVector(std::string const &str, T& ret, const char *  delims=" ")
+ConvertStringToVector(std::string const &str, T& ret, std::string const& errmsg, const char *  delims=" ")
 {
-  std::vector<std::string> splitted;
+  typedef std::vector<boost::iterator_range<std::string::const_iterator> > ListType;
+
+  ListType splitted;
 
   boost::split(splitted, str, boost::is_any_of(delims));
 
-//  split(str, delims, splitted);
-
   for(size_t i = 0; i < splitted.size(); i++)
     {
-    typename T::value_type value;
-    try
-      {
-      value = boost::lexical_cast<typename T::value_type> (splitted[i]);
-      }
-    catch (boost::bad_lexical_cast &)
-      {
-      std::cerr << "Error getting value" << value << "  at " << __FILE__ << ":" << __LINE__ << std::endl;
-      }
-
+    typename T::value_type value = LexicalCast<typename T::value_type> (splitted[i], errmsg);
     ret.push_back(value);
     }
 }
 
-/*
-SplitStringToSingleKeyValue - split a given std::string of into key value based
-on given delimitter string. default delimitter is '='. If the string doesnot
-have a delimitter the key is set to input string and value is set to defValue.
-
-This function is templated over value type. It uses specialized
-boost::lexical_cast if the T is not of type std::string. only primitive types
-
-Arugments are:
-  str      - reference to input string of type std::string
-  key      - reference to std::string where key will be stored
-  value    - reference to T where value will be stored
-  defValue - default value if there is no delimitter found. (informally TRUE)
-  delims   - const std::string which contains the delimtter used. Default is '='
-  doTrim   - option to perform boost::trim() over key and values. Default is true.
-*/
+/**\ingroup Utils
+ * split a given std::string of into key value based on given delimitter
+ * string. default delimitter is '='. If the string does not have a delimitter
+ * the key is set to input string and value is set to defValue.
+ * \param[in] str  input string
+ * \param key[out] An std::string reference where key will be stored
+ * \param value[out] a reference of \cT where value will be stored
+ * \param defValue[in] a default value if there is no delimitter fo
+ * \param[in] errmsg a msg complement used to build the error message to
+be shown if there is \c lexical_cast exception. See \cotb:Utils::LexicalCast().
+ * \param delims  delimitter characters (space is default)
+ * \throw std::bad_alloc in case of exhausted memory
+ * \throw std::runtime_error in case an element from the input string
+cannot be converted into a valid \c T instance.
+ * \pre delims shall not be null (untested)
+ *
+ * \see \c otb::Utils::LexicalCast()
+ */
 template<typename T>
 void SplitStringToSingleKeyValue(const std::string& str,
                                  std::string& key, T& value, const T& defValue,
-                                 const std::string delims="=", bool doTrim=true)
+                                 std::string const& errmsg, const std::string delims="=")
 {
-  std::size_t pos = str.find(delims);
-  if (pos == std::string::npos)
-    {
-    key = str;
-    value = defValue;
-    }
-  key = str.substr(0, pos);
 
-  std::string value_ = str.substr(pos+delims.size(), str.size() - 1 );
+  typedef boost::iterator_range<std::string::const_iterator> BoostRangeIteratorType;
+  typedef std::list< BoostRangeIteratorType > ListType;
 
-  //do optional trim
-  if(doTrim)
-    {
-    boost::trim_left(key);
-    boost::trim_right(value_);
-    }
+  ListType splitted;
 
-  if(typeid(value) != typeid(key))
+  boost::split( splitted, str, boost::is_any_of(delims), boost::token_compress_on );
+
+  typename ListType::iterator it = splitted.begin();
+  BoostRangeIteratorType kIt = boost::trim_copy((*it));
+  key.assign( kIt.begin(), kIt.end());
+  ++it;
+
+  if( it != splitted.end())
     {
-    try
-      {
-      value = boost::lexical_cast<T>(value_);
-      }
-    catch (boost::bad_lexical_cast &)
-      {
-      std::cerr << "Error getting value" << value_ << "  at " << __FILE__ << ":" << __LINE__ << std::endl;
-      }
+    value =  LexicalCast<T>(boost::trim_copy(*it), errmsg);
+    ++it;
     }
   else
     {
-    value.swap(value_);
+    value = defValue;
     }
 }
 } // end namespace Utils
