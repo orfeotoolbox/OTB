f87710d30c76c78e8ee6ecb24bd51db9f4986757
Guillaume Pasero <guillaume.pasero@c-s.fr>
2015-10-06 17:11:41 +0200

    TEST: improve test, no baseline

--- Modules/Filtering/ImageManipulation/test/otbChangeInformationImageFilter.cxx
+++ Modules/Filtering/ImageManipulation/test/otbChangeInformationImageFilter.cxx
@@ -17,37 +17,50 @@
 =========================================================================*/
 #include "otbChangeInformationImageFilter.h"
 #include "otbImageFileReader.h"
-#include "otbImageFileWriter.h"
 #include "otbImage.h"
 
 int otbChangeInformationImageFilter(int itkNotUsed(argc), char * argv[])
 {
   const char *inputFilename(argv[1]);
-  const char *outputFilename(argv[2]);
-  
+
   typedef otb::Image<float,2> ImageType;
   typedef otb::ChangeInformationImageFilter<ImageType> FilterType;
   typedef otb::ImageFileReader<ImageType> ReaderType;
-  typedef otb::ImageFileWriter<ImageType> WriterType;
   
   ReaderType::Pointer reader = ReaderType::New();
   reader->SetFileName(inputFilename);
   
   FilterType::Pointer filter = FilterType::New();
   filter->SetInput(reader->GetOutput());
+  // try to set a different ProjRef
+  std::string newProj("Fake ProjRef");
+  filter->SetOutputMetaData<std::string>(otb::MetaDataKey::ProjectionRefKey,&newProj);
+  // erase that choice
   filter->SetOutputMetaData<std::string>(otb::MetaDataKey::ProjectionRefKey,NULL);
+  // add a no data to the image
   std::vector<bool> flags;
   flags.push_back(true);
   std::vector<double> nodata;
   nodata.push_back(0.0);
   filter->SetOutputMetaData<std::vector<bool> >(otb::MetaDataKey::NoDataValueAvailable,&flags);
   filter->SetOutputMetaData<std::vector<double> >(otb::MetaDataKey::NoDataValue,&nodata);
-  
-  WriterType::Pointer writer = WriterType::New();
-  writer->SetInput(filter->GetOutput());
-  writer->SetFileName(outputFilename);
-  writer->Update();
-  
+  filter->UpdateOutputInformation();
+
+  ImageType::Pointer outImage = filter->GetOutput();
+  if (! outImage->GetProjectionRef().empty())
+    {
+    std::cout << "Projection is supposed to be removed but is still present !" << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  itk::MetaDataDictionary &dict = outImage->GetMetaDataDictionary();
+  if (!dict.HasKey(otb::MetaDataKey::NoDataValueAvailable) ||
+      !dict.HasKey(otb::MetaDataKey::NoDataValue))
+    {
+    std::cout << "Missing no data metadata !" << std::endl;
+    return EXIT_FAILURE;
+    }
+
   return EXIT_SUCCESS;
 }
 
