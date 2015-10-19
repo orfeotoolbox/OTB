012114837b328755b2989ee37c241955a43af546
Stephane ALBERT <stephane.albert@c-s.fr>
2015-10-19 16:25:14 +0200

    ENH: Added FilenameInterface.

---
+++ Code/Common/Core/mvdFilenameInterface.cxx
@@ -0,0 +1,100 @@
+/*=========================================================================
+
+  Program:   Monteverdi
+  Language:  C++
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See Copyright.txt for details.
+
+  Monteverdi is distributed under the CeCILL licence version 2. See
+  Licence_CeCILL_V2-en.txt or
+  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt for more details.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#include "mvdFilenameInterface.h"
+
+
+/*****************************************************************************/
+/* INCLUDE SECTION                                                           */
+
+//
+// Qt includes (sorted by alphabetic order)
+//// Must be included before system/custom includes.
+
+//
+// System includes (sorted by alphabetic order)
+
+//
+// ITK includes (sorted by alphabetic order)
+
+//
+// OTB includes (sorted by alphabetic order)
+
+//
+// Monteverdi includes (sorted by alphabetic order)
+
+namespace mvd
+{
+/*
+  TRANSLATOR mvd::FilenameInterface
+
+  Necessary for lupdate to be aware of C++ namespaces.
+
+  Context comment for translator.
+*/
+
+
+/*****************************************************************************/
+/* CONSTANTS                                                                 */
+
+namespace
+{
+} // end of anonymous namespace.
+
+
+/*****************************************************************************/
+/* STATIC IMPLEMENTATION SECTION                                             */
+
+
+/*****************************************************************************/
+/* CLASS IMPLEMENTATION SECTION                                              */
+
+/*****************************************************************************/
+FilenameInterface
+::FilenameInterface() :
+  m_Filename()
+{
+}
+
+/*****************************************************************************/
+FilenameInterface
+::~FilenameInterface()
+{
+}
+
+/*****************************************************************************/
+void
+FilenameInterface
+::SetFilename( const QString & filename )
+{
+  m_Filename = filename;
+}
+
+/*****************************************************************************/
+const QString &
+FilenameInterface
+::GetFilename() const
+{
+  return m_Filename;
+}
+
+/*****************************************************************************/
+/* SLOTS                                                                     */
+/*****************************************************************************/
+
+} // end namespace 'mvd'
