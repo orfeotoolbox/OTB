5999005eda228ade87e6259fd222a22b9fc3748c
Grizonnet Manuel <manuel.grizonnet@cnes.fr>
2015-10-30 15:33:33 +0100

    BUG: fix several problems in detection of GDAL version
    - Move declaration of GDAL_CONFIG_CHECKING in otb-module-init.cmake to ensure gdal configuration in all cases (if statement)
    - OTBCommon module needs to depend on OTBGDAL as GDAL module has to set OTB_USE_GDAL_20 before configuring otbConfigure.h
    - Set mode to STATUS for GDAL version >= to 2.0
    
    All these issues were found compiling gdal trunk (2.1.0dev) with clang 3.5.

--- Modules/Core/Common/otb-module.cmake
+++ Modules/Core/Common/otb-module.cmake
@@ -4,6 +4,8 @@ otb_module(OTBCommon
   DEPENDS
     OTBBoost
     OTBITK
+    #Add dependency to OTBGDAL as GDAL module need to set OTB_USE_GDAL_20 before configuring otbConfigure.h
+    OTBGDAL
 
   TEST_DEPENDS
     OTBImageBase
