98349fbcb21c2e477f3b3dfd4cdf45613758b9da
Rashad Kanavath <mohammedrashadkm@gmail.com>
2015-11-22 01:04:46 +0100

    WRG: fix shadow warning from mapnik #3

--- Modules/Filtering/VectorDataRendering/include/otbVectorDataToMapFilter.h
+++ Modules/Filtering/VectorDataRendering/include/otbVectorDataToMapFilter.h
@@ -26,6 +26,7 @@
 #if defined(__GNUC__) || defined(__clang__)
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wshadow"
+#pragma GCC diagnostic ignored "-Wunused-parameter"
 #include <mapnik/memory_datasource.hpp>
 #include <mapnik/map.hpp>
 #pragma GCC diagnostic pop
