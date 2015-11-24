9287d30b7b0c0cf4a2b72c85b36d330f6cc9e0eb
Rashad Kanavath <rashad.kanavath@c-s.fr>
2015-11-24 15:24:08 +0100

    OSSIM: fix build on superbuild

--- SuperBuild/CMake/External_ossim.cmake
+++ SuperBuild/CMake/External_ossim.cmake
@@ -48,12 +48,13 @@ else()
   ExternalProject_Add(${proj}
     PREFIX ${proj}
     URL "http://download.osgeo.org/ossim/source/ossim-1.8.20/ossim-1.8.20-1.tar.gz"
-    URL_MD5 87c861c005a180343a2fdaf1d54d9b58
+    URL_MD5 19bd40f12d65c2e5b2963466dc838854
     BINARY_DIR ${OSSIM_SB_BUILD_DIR}
     INSTALL_DIR ${SB_INSTALL_PREFIX}
     DOWNLOAD_DIR ${DOWNLOAD_LOCATION}
-    CMAKE_CACHE_ARGS
+    CONFIGURE_COMMAND ${SB_CMAKE_COMMAND} ${OSSIM_SB_SRC}/ossim
     -DProject_WC_REVISION:STRING=23537
+    -DSUBVERSION_FOUND:BOOL=FALSE
     -DCMAKE_INSTALL_PREFIX:STRING=${SB_INSTALL_PREFIX}
     -DCMAKE_BUILD_TYPE:STRING=Release
     -DCMAKE_CXX_FLAGS:STRING=${OSSIM_CXX_FLAGS}
@@ -62,40 +63,24 @@ else()
     -DBUILD_OSSIM_APPS:BOOL=OFF
     -DBUILD_OSSIM_TEST_APPS:BOOL=OFF
     -DBUILD_OSSIM_FRAMEWORKS:BOOL=OFF
-    -DBUILD_OMS:BOOL=OFF
-    -DBUILD_CSMAPI:BOOL=OFF
-    -DBUILD_OSSIMGUI:BOOL=OFF
-    -DBUILD_OSSIMPLANET:BOOL=OFF
-    -DBUILD_OSSIMPLANETQT:BOOL=OFF
-    -DBUILD_OSSIMPREDATOR:BOOL=OFF
-    -DBUILD_OSSIMQT:BOOL=OFF
-    -DBUILD_WMS:BOOL=OFF
     -DINSTALL_ARCHIVE_DIR:STRING=lib
     -DINSTALL_LIBRARY_DIR:STRING=lib
+    -DCMAKE_MODULE_PATH:PATH=${OSSIM_SB_SRC}/ossim_package_support/cmake/CMakeModules
     -DCMAKE_PREFIX_PATH:STRING=${SB_INSTALL_PREFIX};${CMAKE_PREFIX_PATH}
     ${OSSIM_SB_CONFIG}
     DEPENDS ${${proj}_DEPENDENCIES}
-    CMAKE_COMMAND ${SB_CMAKE_COMMAND}
     )
-
-  ExternalProject_Add_Step(${proj} patch_cmakelist
-      COMMAND ${CMAKE_COMMAND} -E copy
-      ${OSSIM_SB_SRC}/ossim_package_support/cmake/CMakeLists.txt ${OSSIM_SB_SRC}
-      COMMAND ${CMAKE_COMMAND} -E copy_directory
-      ${OSSIM_SB_SRC}/ossim_package_support/cmake/CMakeModules ${OSSIM_SB_SRC}/CMakeModules
-      DEPENDEES patch
-      DEPENDERS configure )
 
   if(MSVC)
     ExternalProject_Add_Step(${proj} patch_ossim_src
       COMMAND ${CMAKE_COMMAND} -E copy_directory
-      ${CMAKE_SOURCE_DIR}/patches/ossim/src ${OSSIM_SB_SRC}/src
+      ${CMAKE_SOURCE_DIR}/patches/ossim/src ${OSSIM_SB_SRC}/ossim/src
       DEPENDEES patch_ossim_include
       DEPENDERS configure )
 
     ExternalProject_Add_Step(${proj} patch_ossim_include
       COMMAND ${CMAKE_COMMAND} -E copy_directory
-      ${CMAKE_SOURCE_DIR}/patches/ossim/include ${OSSIM_SB_SRC}/include
+      ${CMAKE_SOURCE_DIR}/patches/ossim/include ${OSSIM_SB_SRC}/ossim/include
       DEPENDEES patch
       DEPENDERS patch_ossim_src )
   endif()
