32ff73f2aa6a81670c0daf995f7c51caadb349be
Manuel Grizonnet <manuel.grizonnet@cnes.fr>
2015-12-03 16:33:45 +0100

    ENH: Add remote modules cmake files (Mosaic and otbGRM)

---
+++ Modules/Remote/Mosaic.remote.cmake
@@ -0,0 +1,9 @@
+#Contact: Rémi Cresson <remi.cresson@teledetection.fr>
+otb_fetch_module(Mosaic
+  "This module provides one application dedicated to images mosaicking
+A more detailed description can be found on the project website:
+https://github.com/remicres/otb-mosaic
+"
+  GIT_REPOSITORY https://github.com/remicres/otb-mosaic.git
+  GIT_TAG 1406369c1ec9accffc0b3f40d0411e78bed2d893
+)
