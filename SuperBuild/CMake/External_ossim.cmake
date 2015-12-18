4ed22941f5268518946d8c269b1851a29eb938fd
Rashad Kanavath <rashad.kanavath@c-s.fr>
2015-12-18 16:13:42 +0100

    update to ossim r23537 until we are ready for 1.8.20-1

--- SuperBuild/CMake/External_ossim.cmake
+++ SuperBuild/CMake/External_ossim.cmake
@@ -48,58 +48,27 @@ else()
   # archive version
   ExternalProject_Add(${proj}
     PREFIX ${proj}
-    URL "http://download.osgeo.org/ossim/source/ossim-1.8.20/ossim-1.8.20-1.tar.gz"
-    URL_MD5 19bd40f12d65c2e5b2963466dc838854
+    URL "https://www.orfeo-toolbox.org/packages/ossim-minimal-r23537.tar.gz"
+    URL_MD5 40f406c0da9cc188092582b3bdb20a80
     BINARY_DIR ${OSSIM_SB_BUILD_DIR}
     INSTALL_DIR ${SB_INSTALL_PREFIX}
     DOWNLOAD_DIR ${DOWNLOAD_LOCATION}
-    CONFIGURE_COMMAND ${SB_CMAKE_COMMAND} ${OSSIM_SB_SRC}/ossim
+    CMAKE_CACHE_ARGS
     -DProject_WC_REVISION:STRING=23537
-    -DSUBVERSION_FOUND:BOOL=FALSE
     -DCMAKE_INSTALL_PREFIX:STRING=${SB_INSTALL_PREFIX}
     -DCMAKE_BUILD_TYPE:STRING=Release
     -DCMAKE_CXX_FLAGS:STRING=${OSSIM_CXX_FLAGS}
     -DBUILD_OSSIM_MPI_SUPPORT:BOOL=OFF
     -DBUILD_OSSIM_FREETYPE_SUPPORT:BOOL=ON
     -DBUILD_OSSIM_APPS:BOOL=OFF
-    -DBUILD_OSSIM_TEST_APPS:BOOL=OFF
     -DBUILD_OSSIM_FRAMEWORKS:BOOL=OFF
     -DINSTALL_ARCHIVE_DIR:STRING=lib
     -DINSTALL_LIBRARY_DIR:STRING=lib
-    -DCMAKE_MODULE_PATH:PATH=${OSSIM_SB_SRC}/ossim_package_support/cmake/CMakeModules
-    -DCMAKE_PREFIX_PATH:STRING=${SB_INSTALL_PREFIX};${CMAKE_PREFIX_PATH}
+    -DCMAKE_PREFIX_PATH:STRING=${SB_INSTALL_PREFIX}
     ${OSSIM_SB_CONFIG}
     DEPENDS ${${proj}_DEPENDENCIES}
+    CONFIGURE_COMMAND ${SB_CMAKE_COMMAND} ${OSSIM_SB_SRC}/ossim
     )
-
-  if(WIN32)
-    if(MSVC)
-      ExternalProject_Add_Step(${proj} patch_ossim_src
-        COMMAND ${CMAKE_COMMAND} -E copy_directory
-        ${CMAKE_SOURCE_DIR}/patches/ossim/src ${OSSIM_SB_SRC}/ossim/src
-        DEPENDEES patch_ossim_include
-        DEPENDERS configure )
-
-      ExternalProject_Add_Step(${proj} patch_ossim_include
-        COMMAND ${CMAKE_COMMAND} -E copy_directory
-        ${CMAKE_SOURCE_DIR}/patches/ossim/include ${OSSIM_SB_SRC}/ossim/include
-        DEPENDEES patch_ossim_cmake1
-        DEPENDERS patch_ossim_src )
-        
-    endif()
-    
-    ExternalProject_Add_Step(${proj} patch_ossim_cmake1
-      COMMAND ${CMAKE_COMMAND} -E copy
-      ${CMAKE_SOURCE_DIR}/patches/ossim/CMakeLists.txt ${OSSIM_SB_SRC}/ossim/
-      DEPENDEES patch_ossim_cmake2
-      DEPENDERS patch_ossim_include )
-      
-    ExternalProject_Add_Step(${proj} patch_ossim_cmake2
-      COMMAND ${CMAKE_COMMAND} -E copy
-      ${CMAKE_SOURCE_DIR}/patches/ossim/OssimUtilities.cmake ${OSSIM_SB_SRC}/ossim_package_support/cmake/CMakeModules
-      DEPENDEES patch
-      DEPENDERS patch_ossim_cmake1 )      
-  endif()
 
   set(_SB_${proj}_INCLUDE_DIR ${SB_INSTALL_PREFIX}/include)
   if(WIN32)
