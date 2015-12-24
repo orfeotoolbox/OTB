a00810e2618dde40a1c9d0c3e7ae77f124f92ffa
Rashad Kanavath <rashad.kanavath@c-s.fr>
2015-12-24 16:24:54 +0100

    BUG: mantis-1125 use OTB_APPLICATION_PATH in startup files

--- Packaging/Windows/monteverdi.bat
+++ Packaging/Windows/monteverdi.bat
@@ -2,13 +2,13 @@
 :: Monteverdi launcher to set up proper GDAL environment
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-:: Get the directory of the current script 
+:: Get the directory of the current script
 @set CURRENT_SCRIPT_DIR=%~dp0
 
 :: Set GDAL_DATA env. variable
 @set GDAL_DATA=%CURRENT_SCRIPT_DIR%..\share\gdal
 :: Performance issue: do not set ITK_AUTOLOAD_PATH to avoid loading of OTB-Applications.
-:: @set ITK_AUTOLOAD_PATH=%CURRENT_SCRIPT_DIR%..\lib\otb\applications
+@set OTB_APPLICATION_PATH=%CURRENT_SCRIPT_DIR%..\lib\otb\applications
 
 :: Set current dir to HOME dir because Monteverdi generates temporary files and need write access
 @cd %HOMEDRIVE%%HOMEPATH%
