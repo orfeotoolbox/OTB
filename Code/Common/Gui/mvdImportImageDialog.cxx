b8115128238bdc7f409a94bd1a10d629e5774fe4
Stephane ALBERT <stephane.albert@c-s.fr>
2016-01-18 18:04:01 +0100

    ENH: Added ImportImageDialog class.

---
+++ Code/Common/Gui/mvdImportImageDialog.cxx
@@ -0,0 +1,85 @@
+/*=========================================================================
+
+  Program:   Monteverdi
+  Language:  C++
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See Copyright.txt for details.
+
+  Monteverdi is distributed under the CeCILL licence version 2. See
+  Licence_CeCILL_V2-en.txt or
+  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt for more details.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#include "mvdImportImageDialog.h"
+#include "ui_mvdImportImageDialog.h"
+
+
+/*****************************************************************************/
+/* INCLUDE SECTION                                                           */
+
+//
+// Qt includes (sorted by alphabetic order)
+//// Must be included before system/custom includes.
+
+//
+// System includes (sorted by alphabetic order)
+
+//
+// ITK includes (sorted by alphabetic order)
+
+//
+// OTB includes (sorted by alphabetic order)
+
+//
+// Monteverdi includes (sorted by alphabetic order)
+
+namespace mvd
+{
+
+/*
+  TRANSLATOR mvd::ImportImageDialog
+
+  Necessary for lupdate to be aware of C++ namespaces.
+
+  Context comment for translator.
+*/
+
+
+/*****************************************************************************/
+/* CONSTANTS                                                                 */
+
+
+/*****************************************************************************/
+/* STATIC IMPLEMENTATION SECTION                                             */
+
+
+/*****************************************************************************/
+/* CLASS IMPLEMENTATION SECTION                                              */
+/*****************************************************************************/
+ImportImageDialog
+::ImportImageDialog( QWidget * parent, Qt::WindowFlags flags  ):
+  QDialog( parent, flags ),
+  m_UI( new mvd::Ui::ImportImageDialog() )
+{
+  m_UI->setupUi( this );
+}
+
+/*****************************************************************************/
+ImportImageDialog
+::~ImportImageDialog()
+{
+  delete m_UI;
+  m_UI = NULL;
+}
+
+/*****************************************************************************/
+/* SLOTS                                                                     */
+/*****************************************************************************/
+
+} // end namespace 'mvd'
