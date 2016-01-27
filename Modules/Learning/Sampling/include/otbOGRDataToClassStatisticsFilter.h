74e487654a16b1293e29c798f05a7f3b897543f3
Guillaume Pasero <guillaume.pasero@c-s.fr>
2016-01-27 18:12:33 +0100

    ENH: OGRDataToClassStatisticsFilter still incomplete but compile

--- Modules/Learning/Sampling/include/otbOGRDataToClassStatisticsFilter.h
+++ Modules/Learning/Sampling/include/otbOGRDataToClassStatisticsFilter.h
@@ -21,80 +21,12 @@
 #include "otbPersistentImageFilter.h"
 #include "otbPersistentFilterStreamingDecorator.h"
 #include "otbOGRDataSourceWrapper.h"
-#include "otbMaskedIteratorDecorator.h"
+// #include "otbMaskedIteratorDecorator.h"
+#include "otbPolygonClassStatisticsAccumulator.h"
+#include "itkSimpleDataObjectDecorator.h"
 
 namespace otb
 {
-
-/**
- * \class PolygonClassStatisticsAccumulator
- *
- * \brief This class stores statistics of OGR data for each thread
- *
- */
-class PolygonClassStatisticsAccumulator : public itk::Object
-{
-public:
-  typedef PolygonClassStatisticsAccumulator  Self;
-  typedef itk::SmartPointer<Self>            Pointer;
-  itkNewMacro(Self);
-
-  /** Runtime information support. */
-  itkTypeMacro(PolygonClassStatisticsAccumulator, itk::Object);
-
-  template <typename TInputImage>
-  void Add(otb::ogr::Layer::const_iterator& featIt,
-           otb::MaskedIteratorDecorator<itk::ImageRegionIterator<TInputImage> >& pixelIt,
-           const std::string &fieldName)
-  {
-    // TODO : move code to txx
-
-    // TODO : switch case with feature type
-
-    //OGRPolygon* inPolygon = dynamic_cast<OGRPolygon *>(geom);
-    //OGRLinearRing* exteriorRing = inPolygon->getExteriorRing();
-      //itk::Point<double, 2> point;
-      //inputImage->TransformIndexToPhysicalPoint(it.GetIndex(), point);
-      // ->Test if the current pixel is in a polygon hole
-      // If point is in feature
-      //if(exteriorRing->isPointInRing(&pointOGR, TRUE) && isNotInHole)
-      //{
-      //}
-      // Count
-      //nbOfPixelsInGeom++;
-      //nbPixelsGlobal++;
-
-    //Class name recuperation
-    //int className = featIt->ogr().GetFieldAsInteger(GetParameterString("cfield").c_str());
-
-    //Counters update, number of pixel in each classes and in each polygons
-    //polygon[featIt->ogr().GetFID()] += nbOfPixelsInGeom;
-
-    //Generation of a random number for the sampling in a polygon where we only need one pixel, it's choosen randomly
-    //elmtsInClass[className] = elmtsInClass[className] + nbOfPixelsInGeom;
-  }
-
-  /** Reset the accumulator */
-  void Reset();
-
-protected:
-  /** Constructor */
-  PolygonClassStatisticsAccumulator() {}
-  /** Destructor */
-  virtual ~PolygonClassStatisticsAccumulator() {}
-
-private:
-  //Number of pixels in all the polygons
-  unsigned long nbPixelsGlobal;
-  //Number of pixels in each classes
-  std::map<int, int> m_elmtsInClass;
-  //Number of pixels in each polygons
-  std::map<unsigned long, int> m_polygon;  // check the feature id
-
-  // Not implemented
-  PolygonClassStatisticsAccumulator(const Self&);
-  void operator=(const Self&);
-};
 
 /**
  * \class PersistentOGRDataToClassStatisticsFilter
@@ -112,11 +44,14 @@ public:
   typedef PersistentImageFilter<TInputImage, TInputImage> Superclass;
   typedef itk::SmartPointer<Self>                         Pointer;
   typedef itk::SmartPointer<const Self>                   ConstPointer;
-  
+
   typedef TInputImage                                     InputImageType;
   typedef typename InputImageType::Pointer                InputImagePointer;
-  typedef typename InputImageType::RegionType             
-  
+  typedef typename InputImageType::RegionType             RegionType;
+
+  typedef TMaskImage                                      MaskImageType;
+  typedef typename MaskImageType::Pointer                 MaskImagePointer;
+
   typedef otb::ogr::DataSource                            OGRDataType;
   typedef otb::ogr::DataSource::Pointer                   OGRDataPointer;
 
@@ -127,46 +62,42 @@ public:
   typedef itk::SimpleDataObjectDecorator<ClassCountMapType>  ClassCountObjectType;
   typedef itk::SimpleDataObjectDecorator<PolygonSizeMapType> PolygonSizeObjectType;
 
+  typedef itk::DataObject::DataObjectPointerArraySizeType DataObjectPointerArraySizeType;
+
   /** Method for creation through the object factory. */
   itkNewMacro(Self);
 
   /** Runtime information support. */
   itkTypeMacro(PersistentOGRDataToClassStatisticsFilter, PersistentImageFilter);
-  
-  // TODO : store in m_InputImage
-  void SetInput(const TInputImage* mask);
-  const TInputImage* GetInput();
-  
-  // TODO : input #2
-  void SetMask(const TMaskImage* mask);
-  const TMaskImage* GetMask();
-  
-  // TODO : input # 1
+
   void SetOGRData(const otb::ogr::DataSource* vector);
   const otb::ogr::DataSource* GetOGRData();
-  
+
+  void SetMask(const TMaskImage* mask);
+  const TMaskImage* GetMask();
+
   void Synthetize(void);
-  
+
   /** Reset method called before starting the streaming*/
   // TODO : inputImage->UpdateOutputInformation()
   void Reset(void);
-  
+
   // TODO : prevent loading of data into output
 
   itkSetMacro(ClassKey, std::string);
   itkGetMacro(ClassKey, std::string);
-  
+
   // TODO: store the class count map as output #2
-  const ClassCountObjectType* GetClassCountOutput();
+  const ClassCountObjectType* GetClassCountOutput() const;
   ClassCountObjectType* GetClassCountOutput();
 
   // TODO: store the polygon size map as output #3
-  const PolygonSizeObjectType* GetPolygonSizeOutput();
+  const PolygonSizeObjectType* GetPolygonSizeOutput() const;
   PolygonSizeObjectType* GetPolygonSizeOutput();
 
   /** Make a DataObject of the correct type to be used as the specified
    * output. */
-  virtual DataObjectPointer MakeOutput(DataObjectPointerArraySizeType idx);
+  virtual itk::DataObject::Pointer MakeOutput(DataObjectPointerArraySizeType idx);
   using Superclass::MakeOutput;
 
 protected:
@@ -175,19 +106,17 @@ protected:
   /** Destructor */
   virtual ~PersistentOGRDataToClassStatisticsFilter() {}
 
-  // TODO : call UpdateOutputInfo on input image
-  // TODO : check that image and mask have the same size
   void GenerateOutputInformation();
-  
+
+  void GenerateInputRequestedRegion();
+
   void ThreadedGenerateData(const RegionType& outputRegionForThread,
                             itk::ThreadIdType threadId);
 
 private:
   PersistentOGRDataToClassStatisticsFilter(const Self &); //purposely not implemented
   void operator =(const Self&); //purposely not implemented
-  
-  InputImagePointer m_InputImage;
-  
+
   std::string m_ClassKey;
 
   std::vector<PolygonClassStatisticsAccumulator::Pointer> m_TemporaryStats;
@@ -226,7 +155,9 @@ public:
   /** Creation through object factory macro */
   itkTypeMacro(OGRDataToClassStatisticsFilter, PersistentFilterStreamingDecorator);
 
-  void SetInput(const TInputImage* image);
+  using Superclass::SetInput;
+  virtual void SetInput(const TInputImage* image);
+
   const TInputImage* GetInput();
 
   void SetOGRData(const otb::ogr::DataSource* data);
@@ -237,8 +168,6 @@ public:
 
   void SetClassKey(std::string &key);
   std::string GetClassKey();
-  
-  
 
 protected:
   /** Constructor */
--- Modules/Learning/Sampling/include/otbOGRDataToClassStatisticsFilter.txx
+++ Modules/Learning/Sampling/include/otbOGRDataToClassStatisticsFilter.txx
@@ -20,7 +20,242 @@
 
 namespace otb
 {
-  
+
+// Members from otb::PersistentOGRDataToClassStatisticsFilter
+
+template<class TInputImage, class TMaskImage>
+void
+PersistentOGRDataToClassStatisticsFilter<TInputImage,TMaskImage>
+::SetOGRData(const otb::ogr::DataSource* vector)
+{
+  this->SetNthInput(1, const_cast<otb::ogr::DataSource *>( vector ));
+}
+
+template<class TInputImage, class TMaskImage>
+const otb::ogr::DataSource*
+PersistentOGRDataToClassStatisticsFilter<TInputImage,TMaskImage>
+::GetOGRData()
+{
+  if (this->GetNumberOfInputs()<2)
+    {
+    return 0;
+    }
+  return static_cast<const otb::ogr::DataSource *>(this->itk::ProcessObject::GetInput(1));
+}
+
+template<class TInputImage, class TMaskImage>
+void
+PersistentOGRDataToClassStatisticsFilter<TInputImage,TMaskImage>
+::SetMask(const TMaskImage* mask)
+{
+  this->SetNthInput(2, const_cast<TMaskImage *>( mask ));
+}
+
+template<class TInputImage, class TMaskImage>
+const TMaskImage*
+PersistentOGRDataToClassStatisticsFilter<TInputImage,TMaskImage>
+::GetMask()
+{
+  if (this->GetNumberOfInputs()<3)
+    {
+    return 0;
+    }
+  return static_cast<const TMaskImage *>(this->itk::ProcessObject::GetInput(2));
+}
+
+template<class TInputImage, class TMaskImage>
+void
+PersistentOGRDataToClassStatisticsFilter<TInputImage,TMaskImage>
+::Synthetize(void)
+{
+  // TODO
+}
+
+template<class TInputImage, class TMaskImage>
+void
+PersistentOGRDataToClassStatisticsFilter<TInputImage,TMaskImage>
+::Reset(void)
+{
+  // TODO :
+}
+
+template<class TInputImage, class TMaskImage>
+const typename PersistentOGRDataToClassStatisticsFilter<TInputImage,TMaskImage>::ClassCountObjectType*
+PersistentOGRDataToClassStatisticsFilter<TInputImage,TMaskImage>
+::GetClassCountOutput() const
+{
+  if (this->GetNumberOfOutputs()<2)
+    {
+    return 0;
+    }
+  return static_cast<const ClassCountObjectType *>(this->itk::ProcessObject::GetOutput(1));
+}
+
+
+template<class TInputImage, class TMaskImage>
+typename PersistentOGRDataToClassStatisticsFilter<TInputImage,TMaskImage>::ClassCountObjectType* 
+PersistentOGRDataToClassStatisticsFilter<TInputImage,TMaskImage>
+::GetClassCountOutput()
+{
+  if (this->GetNumberOfOutputs()<2)
+    {
+    return 0;
+    }
+  return static_cast<ClassCountObjectType *>(this->itk::ProcessObject::GetOutput(1));
+}
+
+// TODO: store the polygon size map as output #3
+template<class TInputImage, class TMaskImage>
+const typename PersistentOGRDataToClassStatisticsFilter<TInputImage,TMaskImage>::PolygonSizeObjectType*
+PersistentOGRDataToClassStatisticsFilter<TInputImage,TMaskImage>
+::GetPolygonSizeOutput() const
+{
+  if (this->GetNumberOfOutputs()<3)
+    {
+    return 0;
+    }
+  return static_cast<const PolygonSizeObjectType *>(this->itk::ProcessObject::GetOutput(2));
+}
+
+
+template<class TInputImage, class TMaskImage>
+typename PersistentOGRDataToClassStatisticsFilter<TInputImage,TMaskImage>::PolygonSizeObjectType*
+PersistentOGRDataToClassStatisticsFilter<TInputImage,TMaskImage>
+::GetPolygonSizeOutput()
+{
+    if (this->GetNumberOfOutputs()<3)
+    {
+    return 0;
+    }
+  return static_cast<PolygonSizeObjectType *>(this->itk::ProcessObject::GetOutput(2));
+}
+
+template<class TInputImage, class TMaskImage>
+itk::DataObject::Pointer
+PersistentOGRDataToClassStatisticsFilter<TInputImage,TMaskImage>
+::MakeOutput(DataObjectPointerArraySizeType idx)
+{
+  // TODO
+  return NULL;
+}
+
+
+template<class TInputImage, class TMaskImage>
+void
+PersistentOGRDataToClassStatisticsFilter<TInputImage,TMaskImage>
+::GenerateOutputInformation()
+{
+  const MaskImageType *mask = this->GetMask();
+  if (mask)
+    {
+    const InputImageType *input = this->GetInput();
+    if (mask->GetLargestPossibleRegion() !=
+        input->GetLargestPossibleRegion() )
+      {
+      itkGenericExceptionMacro("Mask and input image have a different size!");
+      }
+    }
+}
+
+template<class TInputImage, class TMaskImage>
+void
+PersistentOGRDataToClassStatisticsFilter<TInputImage,TMaskImage>
+::GenerateInputRequestedRegion()
+{
+  InputImageType *input = const_cast<InputImageType*>(this->GetInput());
+  MaskImageType *mask = const_cast<MaskImageType*>(this->GetMask());
+
+  RegionType requested = this->GetOutput()->GetRequestedRegion();
+  RegionType emptyRegion = input->GetLargestPossibleRegion();
+  emptyRegion.SetSize(0,0);
+  emptyRegion.SetSize(1,0);
+
+  input->SetRequestedRegion(emptyRegion);
+
+  if (mask)
+    {
+    mask->SetRequestedRegion(requested);
+    }
+}
+
+template<class TInputImage, class TMaskImage>
+void
+PersistentOGRDataToClassStatisticsFilter<TInputImage,TMaskImage>
+::ThreadedGenerateData(const RegionType& outputRegionForThread,
+                       itk::ThreadIdType threadId)
+{
+  // TODO
+}
+
+// Members from otb::OGRDataToClassStatisticsFilter
+
+template<class TInputImage, class TMaskImage>
+void
+OGRDataToClassStatisticsFilter<TInputImage,TMaskImage>
+::SetInput(const TInputImage* image)
+{
+  // TODO
+}
+
+template<class TInputImage, class TMaskImage>
+const TInputImage*
+OGRDataToClassStatisticsFilter<TInputImage,TMaskImage>
+::GetInput()
+{
+  // TODO
+  return NULL;
+}
+
+template<class TInputImage, class TMaskImage>
+void
+OGRDataToClassStatisticsFilter<TInputImage,TMaskImage>
+::SetOGRData(const otb::ogr::DataSource* data)
+{
+  // TODO
+}
+
+template<class TInputImage, class TMaskImage>
+const otb::ogr::DataSource*
+OGRDataToClassStatisticsFilter<TInputImage,TMaskImage>
+::GetOGRData()
+{
+  // TODO
+  return NULL;
+}
+
+template<class TInputImage, class TMaskImage>
+void
+OGRDataToClassStatisticsFilter<TInputImage,TMaskImage>
+::SetMask(const TMaskImage* mask)
+{
+  // TODO
+}
+
+template<class TInputImage, class TMaskImage>
+const TMaskImage*
+OGRDataToClassStatisticsFilter<TInputImage,TMaskImage>
+::GetMask()
+{
+  // TODO
+  return NULL;
+}
+
+template<class TInputImage, class TMaskImage>
+void
+OGRDataToClassStatisticsFilter<TInputImage,TMaskImage>
+::SetClassKey(std::string &key)
+{
+  // TODO
+}
+
+template<class TInputImage, class TMaskImage>
+std::string
+OGRDataToClassStatisticsFilter<TInputImage,TMaskImage>
+::GetClassKey()
+{
+  // TODO
+  return NULL;
+}
 
 
 } // end of namespace otb
