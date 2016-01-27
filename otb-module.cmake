1bcb302c648c2754ec979c96e85077a8c39cd84a
Julien Michel <julien.michel@cnes.fr>
2016-01-27 07:49:37 +0100

    ENH: Adding otb-module.cmake

---
+++ otb-module.cmake
@@ -0,0 +1,15 @@
+set(DOCUMENTATION "This module provides Ice, a ligthweight efficient visualization framework for remote sensing images.")
+
+otb_module(OTBSupervised
+  DEPENDS
+    OTBCommon
+    OTBGLEW
+    OTBOpenGL
+    
+  OPTIONAL_DEPENDS
+    OTBGlut
+    OTBGLFW
+    
+  DESCRIPTION
+    "${DOCUMENTATION}"
+)
