38d3ad7369b26d5e98e661f72a6d444ca0a1564b
remicres <remi.cresson@teledetection.fr>
2016-02-22 10:14:18 +0000

    Add CMakeLists in src

---
+++ Modules/ThirdParty/SPTW/otb-module.cmake
@@ -0,0 +1,13 @@
+set(DOCUMENTATION "This module imports SPTW to the build system")
+
+otb_module(OTBSPTW
+  DEPENDS
+    OTBMPI
+    
+  TEST_DEPENDS
+
+  DESCRIPTION
+    "${DOCUMENTATION}"
+  )
+
+
