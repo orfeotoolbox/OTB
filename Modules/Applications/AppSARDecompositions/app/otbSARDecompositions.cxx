d70402cd476930f06d3d8a129aefc52e356ea5e0
Christophe Palmann <christophe.palmann@c-s.fr>
2016-03-23 10:15:35 +0100

    ENH : typo

--- Modules/Applications/AppSARDecompositions/app/otbSARDecompositions.cxx
+++ Modules/Applications/AppSARDecompositions/app/otbSARDecompositions.cxx
@@ -134,7 +134,7 @@ private:
     SetParameterDescription("decomp.pauli","Pauli coherent decomposition");
     
     AddParameter(ParameterType_Group,"inco","Incoherent decompositions");
-    SetParameterDescription("inco","This group allows setting parameters related to the incoherent decompositions.");
+    SetParameterDescription("inco","This group allows to set parameters related to the incoherent decompositions.");
     
     AddParameter(ParameterType_Int, "inco.kernelsize",   "Kernel size for spatial incoherent averaging.");
     SetParameterDescription("inco.kernelsize", "Minute (0-59)");
