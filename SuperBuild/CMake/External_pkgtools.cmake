75e6ef9f0d51a342653507c241c27b6119656764
Rashad Kanavath <rashad.kanavath@c-s.fr>
2016-04-13 11:19:19 +0200

    PKG: JIRA 975 do not build patchelf on osx

--- SuperBuild/CMake/External_pkgtools.cmake
+++ SuperBuild/CMake/External_pkgtools.cmake
@@ -26,22 +26,33 @@ ExternalProject_Add(MAKESELF
   ${CMAKE_BINARY_DIR}/PACKAGE-OTB/build/makeself.sh
 
   )
-
-ExternalProject_Add(PATCHELF
-  PREFIX "${PKGTOOLS_SB_PREFIX_DIR}"
-  URL "http://nixos.org/releases/patchelf/patchelf-0.9/patchelf-0.9.tar.bz2"
-  URL_MD5 d02687629c7e1698a486a93a0d607947
-  DOWNLOAD_DIR   ${DOWNLOAD_LOCATION}
-  SOURCE_DIR "${PKGTOOLS_SB_PREFIX_DIR}/src-patchelf"
-  BINARY_DIR "${PKGTOOLS_SB_PREFIX_DIR}/build-patchelf"
-  TMP_DIR  "${PKGTOOLS_SB_PREFIX_DIR}/tmp-patchelf"
-  STAMP_DIR "${PKGTOOLS_SB_PREFIX_DIR}/stamp-patchelf"
-  PATCH_COMMAND  ${CMAKE_COMMAND} -E make_directory ${CMAKE_INSTALL_PREFIX}/tools
-  CONFIGURE_COMMAND ${CMAKE_COMMAND} -E chdir ${PKGTOOLS_SB_PREFIX_DIR}/src-patchelf
-  ./configure --prefix ${CMAKE_INSTALL_PREFIX}/tools
-  BUILD_COMMAND ${CMAKE_COMMAND} -E chdir ${PKGTOOLS_SB_PREFIX_DIR}/src-patchelf ${CMAKE_MAKE_PROGRAM}
-  INSTALL_COMMAND ${CMAKE_COMMAND} -E copy ${PKGTOOLS_SB_PREFIX_DIR}/src-patchelf/src/patchelf ${CMAKE_INSTALL_PREFIX}/tools
-  )
+if(UNIX)
+  if(APPLE)
+    ExternalProject_Add( PATCHELF
+      PREFIX             "${PKGTOOLS_SB_PREFIX_DIR}"
+      DOWNLOAD_COMMAND   ""
+      CONFIGURE_COMMAND  ""
+      BUILD_COMMAND      ""
+      INSTALL_COMMAND    ""
+      )
+  else()
+    ExternalProject_Add(PATCHELF
+      PREFIX "${PKGTOOLS_SB_PREFIX_DIR}"
+      URL "http://nixos.org/releases/patchelf/patchelf-0.9/patchelf-0.9.tar.bz2"
+      URL_MD5 d02687629c7e1698a486a93a0d607947
+      DOWNLOAD_DIR   ${DOWNLOAD_LOCATION}
+      SOURCE_DIR "${PKGTOOLS_SB_PREFIX_DIR}/src-patchelf"
+      BINARY_DIR "${PKGTOOLS_SB_PREFIX_DIR}/build-patchelf"
+      TMP_DIR  "${PKGTOOLS_SB_PREFIX_DIR}/tmp-patchelf"
+      STAMP_DIR "${PKGTOOLS_SB_PREFIX_DIR}/stamp-patchelf"
+      PATCH_COMMAND  ${CMAKE_COMMAND} -E make_directory ${CMAKE_INSTALL_PREFIX}/tools
+      CONFIGURE_COMMAND ${CMAKE_COMMAND} -E chdir ${PKGTOOLS_SB_PREFIX_DIR}/src-patchelf
+      ./configure --prefix ${CMAKE_INSTALL_PREFIX}/tools
+      BUILD_COMMAND ${CMAKE_COMMAND} -E chdir ${PKGTOOLS_SB_PREFIX_DIR}/src-patchelf ${CMAKE_MAKE_PROGRAM}
+      INSTALL_COMMAND ${CMAKE_COMMAND} -E copy ${PKGTOOLS_SB_PREFIX_DIR}/src-patchelf/src/patchelf ${CMAKE_INSTALL_PREFIX}/tools
+      )
+  endif(APPLE)
+endif(UNIX)
 
 ExternalProject_Add( ${proj}
   PREFIX             "${PKGTOOLS_SB_PREFIX_DIR}"
