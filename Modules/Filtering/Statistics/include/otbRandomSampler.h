15246186f82b786fa57ca8d21488b05b23c52ffe
Guillaume Pasero <guillaume.pasero@c-s.fr>
2016-04-22 17:34:38 +0200

    ADD: random sampler, with an adaptative mode

---
+++ Modules/Filtering/Statistics/include/otbRandomSampler.h
@@ -0,0 +1,95 @@
+/*=========================================================================
+
+  Program:   ORFEO Toolbox
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
+
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __otbRandomSampler_h
+#define __otbRandomSampler_h
+
+#include "otbSamplerBase.h"
+#include "itkMersenneTwisterRandomVariateGenerator.h"
+
+namespace otb
+{
+
+/**
+ * \class RandomSampler
+ *
+ * \brief Random sampler for iteration loops
+ * 
+ * This class allows to do random sampling during an iteration loop.
+ * It uses the MersenneTwisterRandomGenerator.
+ *
+ * \ingroup OTBStatistics
+ */
+class RandomSampler : public SamplerBase
+{
+public:
+  typedef RandomSampler  Self;
+  typedef SamplerBase      Superclass;
+  typedef itk::SmartPointer<Self>       Pointer;
+  typedef itk::SmartPointer<const Self> ConstPointer;
+
+  /** Internal parameters, only contains an offset to shift the periodic 
+   * sampling
+   */
+  typedef struct Parameter
+    {
+    bool Adaptative;
+    
+    bool operator!=(const struct Parameter  & param) const;
+    } ParameterType; 
+
+  /** Method for creation through the object factory. */
+  itkNewMacro(Self);
+
+  /** Runtime information support. */
+  itkTypeMacro(RandomSampler,SamplerBase);
+  
+  /** Setter/Getter for internal parameters */
+  itkSetMacro(Parameters,ParameterType);
+  itkGetMacro(Parameters,ParameterType);
+
+  /**
+   * Reset internal counter (to be called before starting iteration)
+   */
+  virtual void Reset(void);
+
+  /**
+   * Method to call during iteration, returns true if the sample is selected,
+   * and false otherwise.
+   */
+  bool TakeSample(void);
+
+protected:
+  /** Constructor */
+  RandomSampler();
+   
+  /** Destructor */
+  virtual ~RandomSampler() {}
+
+private:
+  // Not implemented
+  RandomSampler(const Self&);
+  void operator=(const Self&);
+  
+  /** Internal parameters for the sampler */
+  ParameterType m_Parameters;
+  
+  itk::Statistics::MersenneTwisterRandomVariateGenerator::Pointer m_Generator;
+};
+
+} // namespace otb
+#endif
---
+++ Modules/Filtering/Statistics/src/otbRandomSampler.cxx
@@ -0,0 +1,74 @@
+/*=========================================================================
+
+  Program:   ORFEO Toolbox
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
+
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#include "otbRandomSampler.h"
+#include "otbMath.h"
+
+namespace otb
+{
+
+bool
+RandomSampler::ParameterType::operator!=(const RandomSampler::ParameterType & param) const
+{
+  return bool(Adaptative != param.Adaptative);
+}
+
+void
+RandomSampler::Reset(void)
+{
+  Superclass::Reset();
+  if (this->m_Parameters.Adaptative && this->GetTotalElements() == 0UL)
+    {
+    itkExceptionMacro("Adaptative mode needs the total number of elements to be set!");
+    }
+}
+
+bool
+RandomSampler::TakeSample(void)
+{
+  bool ret = false;
+  double rate = this->GetRate();
+  if (this->m_Parameters.Adaptative)
+    {
+    if (this->m_ProcessedElements >= this->GetTotalElements() ||
+        this->m_ChosenElements >= this->GetNeededElements())
+      {
+      rate = 0.0;
+      }
+    else
+      {
+      rate = double(this->GetNeededElements() - this->m_ChosenElements) /
+            double(this->GetTotalElements() - this->m_ProcessedElements);
+      }
+    }
+  if (rate > m_Generator->GetUniformVariate(0.0, 1.0))
+    {
+    ret = true;
+    this->m_ChosenElements += 1UL;
+    }
+  this->m_ProcessedElements += 1UL;
+  return ret;
+}
+
+RandomSampler::RandomSampler()
+{
+  this->m_Parameters.Adaptative = false;
+  m_Generator = itk::Statistics::MersenneTwisterRandomVariateGenerator::GetInstance();
+}
+
+}
