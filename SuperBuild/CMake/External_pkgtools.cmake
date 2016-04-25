78b484f985198748382c37d9ca3147e19ff7d6bb
Rashad Kanavath <rashad.kanavath@c-s.fr>
2016-04-25 18:43:02 +0200

    PKG: patchelf and makeself must be built first

--- SuperBuild/CMake/External_pkgtools.cmake
+++ SuperBuild/CMake/External_pkgtools.cmake
@@ -4,28 +4,6 @@ set(__EXTERNAL_PACKAGE_TOOLS__ 1)
 
 set(PKGTOOLS_SB_PREFIX_DIR "${CMAKE_BINARY_DIR}/TOOLS-PACKAGE")
 
-ExternalProject_Add(MAKESELF
-  PREFIX "${PKGTOOLS_SB_PREFIX_DIR}"
-  URL "https://www.orfeo-toolbox.org/packages/makeself-2.2.0.tar.gz"
-  URL_MD5 3c61df934b0c61ddcd7bd63b391e951d
-  DOWNLOAD_DIR ${DOWNLOAD_LOCATION}
-  SOURCE_DIR "${PKGTOOLS_SB_PREFIX_DIR}/src-makeself"
-  BINARY_DIR "${PKGTOOLS_SB_PREFIX_DIR}/build-makeself"
-  TMP_DIR  "${PKGTOOLS_SB_PREFIX_DIR}/tmp-makeself"
-  STAMP_DIR "${PKGTOOLS_SB_PREFIX_DIR}/stamp-makeself"
-  CONFIGURE_COMMAND ""
-  BUILD_COMMAND ${CMAKE_COMMAND}
-  -E copy
-  ${PKGTOOLS_SB_PREFIX_DIR}/src-makeself/makeself-header.sh
-  ${PKGTOOLS_SB_PREFIX_DIR}/makeself-header.sh
-  INSTALL_COMMAND ${CMAKE_COMMAND}
-  -E copy
-  ${PKGTOOLS_SB_PREFIX_DIR}/src-makeself/makeself.sh
-  ${PKGTOOLS_SB_PREFIX_DIR}/makeself.sh
-  )
-
-set(MAKESELF_SCRIPT ${PKGTOOLS_SB_PREFIX_DIR}/makeself.sh)
-
 if(UNIX)
   if(APPLE)
     add_custom_target(PATCHELF)
@@ -48,6 +26,29 @@ if(UNIX)
   endif(APPLE)
 endif(UNIX)
 
-add_custom_target(PACKAGE-TOOLS DEPENDS MAKESELF PATCHELF)
+ExternalProject_Add(MAKESELF
+  PREFIX "${PKGTOOLS_SB_PREFIX_DIR}"
+  URL "https://www.orfeo-toolbox.org/packages/makeself-2.2.0.tar.gz"
+  URL_MD5 3c61df934b0c61ddcd7bd63b391e951d
+  DOWNLOAD_DIR ${DOWNLOAD_LOCATION}
+  SOURCE_DIR "${PKGTOOLS_SB_PREFIX_DIR}/src-makeself"
+  BINARY_DIR "${PKGTOOLS_SB_PREFIX_DIR}/build-makeself"
+  TMP_DIR  "${PKGTOOLS_SB_PREFIX_DIR}/tmp-makeself"
+  STAMP_DIR "${PKGTOOLS_SB_PREFIX_DIR}/stamp-makeself"
+  CONFIGURE_COMMAND ""
+  BUILD_COMMAND ${CMAKE_COMMAND}
+  -E copy
+  ${PKGTOOLS_SB_PREFIX_DIR}/src-makeself/makeself-header.sh
+  ${PKGTOOLS_SB_PREFIX_DIR}/makeself-header.sh
+  INSTALL_COMMAND ${CMAKE_COMMAND}
+  -E copy
+  ${PKGTOOLS_SB_PREFIX_DIR}/src-makeself/makeself.sh
+  ${PKGTOOLS_SB_PREFIX_DIR}/makeself.sh
+  DEPENDS PATCHELF
+  )
+
+set(MAKESELF_SCRIPT ${PKGTOOLS_SB_PREFIX_DIR}/makeself.sh)
+
+add_custom_target(PACKAGE-TOOLS DEPENDS MAKESELF)
 
 endif()
