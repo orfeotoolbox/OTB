601df516322d87bd81c8ed02c3f96af1df5f3ad2
Rashad Kanavath <rashad.kanavath@c-s.fr>
2016-05-03 16:54:55 +0200

    PKG: extra tools for windows XDK package

--- SuperBuild/CMake/External_pkgtools.cmake
+++ SuperBuild/CMake/External_pkgtools.cmake
@@ -1,15 +1,24 @@
-if(WIN32 OR CMAKE_CROSSCOMPILING)
-  add_custom_target(PACKAGE-TOOLS)
-  return()
-endif()
-
-include(ExternalProject)
 if(NOT __EXTERNAL_PACKAGE_TOOLS__)
 set(__EXTERNAL_PACKAGE_TOOLS__ 1)
 
 set(PKGTOOLS_SB_PREFIX_DIR "${CMAKE_BINARY_DIR}/PACKAGE-TOOLS")
 
-if(UNIX)
+include(ExternalProject)
+
+if(WIN32)
+  ExternalProject_Add(PACKAGE-TOOLS
+    PREFIX "${PKGTOOLS_SB_PREFIX_DIR}"
+    URL "https://www.orfeo-toolbox.org/packages/qt4-native-tools-win64.zip"
+    URL_MD5 0e4bfd5677eb63ae691f4615a4338490
+    DOWNLOAD_DIR ${DOWNLOAD_LOCATION}
+    CONFIGURE_COMMAND  ""
+    BUILD_COMMAND      ""
+    INSTALL_COMMAND    ""
+    )
+endif()
+
+
+if(UNIX AND NOT WIN32)
   if(APPLE)
     add_custom_target(PATCHELF)
   else()
@@ -29,31 +38,32 @@ if(UNIX)
       INSTALL_COMMAND ${CMAKE_COMMAND} -E copy ${PKGTOOLS_SB_PREFIX_DIR}/src-patchelf/src/patchelf ${CMAKE_INSTALL_PREFIX}/tools
       )
   endif(APPLE)
-endif(UNIX)
 
-ExternalProject_Add(MAKESELF
-  PREFIX "${PKGTOOLS_SB_PREFIX_DIR}"
-  URL "https://www.orfeo-toolbox.org/packages/makeself-2.2.0.tar.gz"
-  URL_MD5 3c61df934b0c61ddcd7bd63b391e951d
-  DOWNLOAD_DIR ${DOWNLOAD_LOCATION}
-  SOURCE_DIR "${PKGTOOLS_SB_PREFIX_DIR}/src-makeself"
-  BINARY_DIR "${PKGTOOLS_SB_PREFIX_DIR}/build-makeself"
-  TMP_DIR  "${PKGTOOLS_SB_PREFIX_DIR}/tmp-makeself"
-  STAMP_DIR "${PKGTOOLS_SB_PREFIX_DIR}/stamp-makeself"
-  CONFIGURE_COMMAND ""
-  BUILD_COMMAND ${CMAKE_COMMAND}
-  -E copy
-  ${PKGTOOLS_SB_PREFIX_DIR}/src-makeself/makeself-header.sh
-  ${PKGTOOLS_SB_PREFIX_DIR}/makeself-header.sh
-  INSTALL_COMMAND ${CMAKE_COMMAND}
-  -E copy
-  ${PKGTOOLS_SB_PREFIX_DIR}/src-makeself/makeself.sh
-  ${PKGTOOLS_SB_PREFIX_DIR}/makeself.sh
-  DEPENDS PATCHELF
-  )
+  ExternalProject_Add(MAKESELF
+    PREFIX "${PKGTOOLS_SB_PREFIX_DIR}"
+    URL "https://www.orfeo-toolbox.org/packages/makeself-2.2.0.tar.gz"
+    URL_MD5 3c61df934b0c61ddcd7bd63b391e951d
+    DOWNLOAD_DIR ${DOWNLOAD_LOCATION}
+    SOURCE_DIR "${PKGTOOLS_SB_PREFIX_DIR}/src-makeself"
+    BINARY_DIR "${PKGTOOLS_SB_PREFIX_DIR}/build-makeself"
+    TMP_DIR  "${PKGTOOLS_SB_PREFIX_DIR}/tmp-makeself"
+    STAMP_DIR "${PKGTOOLS_SB_PREFIX_DIR}/stamp-makeself"
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ${CMAKE_COMMAND}
+    -E copy
+    ${PKGTOOLS_SB_PREFIX_DIR}/src-makeself/makeself-header.sh
+    ${PKGTOOLS_SB_PREFIX_DIR}/makeself-header.sh
+    INSTALL_COMMAND ${CMAKE_COMMAND}
+    -E copy
+    ${PKGTOOLS_SB_PREFIX_DIR}/src-makeself/makeself.sh
+    ${PKGTOOLS_SB_PREFIX_DIR}/makeself.sh
+    DEPENDS PATCHELF
+    )
 
-set(MAKESELF_SCRIPT ${PKGTOOLS_SB_PREFIX_DIR}/makeself.sh)
+  set(MAKESELF_SCRIPT ${PKGTOOLS_SB_PREFIX_DIR}/makeself.sh)
 
-add_custom_target(PACKAGE-TOOLS DEPENDS MAKESELF)
+  add_custom_target(PACKAGE-TOOLS DEPENDS MAKESELF)
 
-endif()
+endif(UNIX AND NOT WIN32)
+
+endif() #idef guard
