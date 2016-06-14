6f4c97fcccb6a3bbaf1bcf7db6ad33fd5205881a
Rashad Kanavath <rashad.kanavath@c-s.fr>
2016-06-14 15:35:31 +0200

    SuperBuild: revert geos to release archive

--- SuperBuild/CMake/External_geos.cmake
+++ SuperBuild/CMake/External_geos.cmake
@@ -14,8 +14,8 @@ message(STATUS "  Using GEOS SuperBuild version")
 
 ExternalProject_Add(GEOS
   PREFIX GEOS
-  GIT_REPOSITORY "https://github.com/libgeos/libgeos"
-  GIT_TAG 714127ef49087634e7f6687424fdf041d18c3d0f
+  URL "http://download.osgeo.org/geos/geos-3.4.2.tar.bz2"
+  URL_MD5 fc5df2d926eb7e67f988a43a92683bae
   SOURCE_DIR ${GEOS_SB_SRC}
   BINARY_DIR ${GEOS_SB_BUILD_DIR}
   INSTALL_DIR ${SB_INSTALL_PREFIX}
