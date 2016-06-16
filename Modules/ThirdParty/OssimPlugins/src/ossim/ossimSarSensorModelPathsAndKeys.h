e974e69257a2e2956eda1060d321ae91cc909dea
Luc Hermitte <luc.hermitte@c-s.fr>
2016-06-16 12:12:02 +0200

    COMP: Add `extern` to constant definitions

--- Modules/ThirdParty/OssimPlugins/src/ossim/ossimSarSensorModelPathsAndKeys.h
+++ Modules/ThirdParty/OssimPlugins/src/ossim/ossimSarSensorModelPathsAndKeys.h
@@ -14,38 +14,38 @@
 #include <string>
 
 namespace ossimplugins {
-   const std::string HEADER_PREFIX       ;
-   const std::string SUPPORT_DATA_PREFIX ;
-   const std::string GEOM_PREFIX         ;
-   const std::string SRD_PREFIX          ;
-   const std::string SR_PREFIX           ;
-   const std::string GR_PREFIX           ;
-   const std::string BURST_PREFIX        ;
-   const std::string BURST_NUMBER_KEY    ;
-   const std::string GCP_PREFIX          ;
-   const std::string GCP_NUMBER_KEY      ;
-   const std::string NUMBER_KEY          ;
+   extern const std::string HEADER_PREFIX       ;
+   extern const std::string SUPPORT_DATA_PREFIX ;
+   extern const std::string GEOM_PREFIX         ;
+   extern const std::string SRD_PREFIX          ;
+   extern const std::string SR_PREFIX           ;
+   extern const std::string GR_PREFIX           ;
+   extern const std::string BURST_PREFIX        ;
+   extern const std::string BURST_NUMBER_KEY    ;
+   extern const std::string GCP_PREFIX          ;
+   extern const std::string GCP_NUMBER_KEY      ;
+   extern const std::string NUMBER_KEY          ;
 
-   const std::string keySr0              ;
-   const std::string keyGr0              ;
-   const std::string keyAzimuthTime      ;
-   const std::string keySlantRangeTime   ;
-   const std::string keyImPtX            ;
-   const std::string keyImPtY            ;
-   const std::string keyWorldPtLat       ;
-   const std::string keyWorldPtLon       ;
-   const std::string keyWorldPtHgt       ;
-   const std::string keyTime             ;
-   const std::string keyPosX             ;
-   const std::string keyPosY             ;
-   const std::string keyPosZ             ;
-   const std::string keyVelX             ;
-   const std::string keyVelY             ;
-   const std::string keyVelZ             ;
-   const std::string keyStartLine        ;
-   const std::string keyEndLine          ;
-   const std::string keyAzimuthStartTime ;
-   const std::string keyAzimuthStopTime  ;
+   extern const std::string keySr0              ;
+   extern const std::string keyGr0              ;
+   extern const std::string keyAzimuthTime      ;
+   extern const std::string keySlantRangeTime   ;
+   extern const std::string keyImPtX            ;
+   extern const std::string keyImPtY            ;
+   extern const std::string keyWorldPtLat       ;
+   extern const std::string keyWorldPtLon       ;
+   extern const std::string keyWorldPtHgt       ;
+   extern const std::string keyTime             ;
+   extern const std::string keyPosX             ;
+   extern const std::string keyPosY             ;
+   extern const std::string keyPosZ             ;
+   extern const std::string keyVelX             ;
+   extern const std::string keyVelY             ;
+   extern const std::string keyVelZ             ;
+   extern const std::string keyStartLine        ;
+   extern const std::string keyEndLine          ;
+   extern const std::string keyAzimuthStartTime ;
+   extern const std::string keyAzimuthStopTime  ;
 } // ossimplugins namespace
 
 
