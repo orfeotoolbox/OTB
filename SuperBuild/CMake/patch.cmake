ca2e2ee26285bee0315354c0a7fb27ed72689eec
Rashad Kanavath <rashad.kanavath@c-s.fr>
2016-06-20 19:52:00 +0200

    SuperBuild: save from a if loop. exit if patch command is not found

--- SuperBuild/CMake/patch.cmake
+++ SuperBuild/CMake/patch.cmake
@@ -1,46 +1,48 @@
 
 find_program(PATCH_PROGRAM NAMES patch)
-if(PATCH_PROGRAM)
-  set(OPTS)
-  if(WIN32)
-    set(OPTS "--binary")
-  endif()
-
-  separate_arguments(DOT_DIFF_FILES)
-
-  list(SORT DOT_DIFF_FILES)
-
-  foreach(dot_diff_file ${DOT_DIFF_FILES})
-    message("Input patch file: ${dot_diff_file}")
-    execute_process(
-      COMMAND
-      ${CMAKE_COMMAND} -E chdir ${SOURCE_DIR} 
-      ${PATCH_PROGRAM} "-p1" "-i" "${dot_diff_file}" ${OPTS}
-      RESULT_VARIABLE patch_rv
-      OUTPUT_VARIABLE patch_ov
-      OUTPUT_STRIP_TRAILING_WHITESPACE
-      ERROR_STRIP_TRAILING_WHITESPACE)
-    set(PATCHING_FAILED FALSE)
-    if(NOT "${patch_rv}" STREQUAL "0")
-      if( NOT "${patch_ov}" MATCHES "previously applied")
-        set(PATCHING_FAILED TRUE)
-      endif()
-    endif()
-
-    if( "${patch_ov}" MATCHES "FAILED")
-      set(PATCHING_FAILED TRUE)
-    endif()
-
-    if(PATCHING_FAILED)
-      message(FATAL_ERROR "${PATCH_PROGRAM} returned non-zero exit status \n ${patch_ov} \n")
-    else()
-      message("${patch_ov}")      
-    endif()
-    
-    unset(patch_rv)
-    unset(patch_ov)
-  endforeach()
-else()
+if(NOT PATCH_PROGRAM)
   message(FATAL_ERROR "patch program not found. PATCH_PROGRAM. search names :' patch'")
 endif()
+
+set(OPTS)
+if(WIN32)
+  set(OPTS "--binary")
+endif()
+
+separate_arguments(DOT_DIFF_FILES)
+
+list(SORT DOT_DIFF_FILES)
+
+foreach(dot_diff_file ${DOT_DIFF_FILES})
+  message("Input patch file: ${dot_diff_file}")
+  execute_process(
+    COMMAND
+    ${CMAKE_COMMAND} -E chdir ${SOURCE_DIR} 
+    ${PATCH_PROGRAM} "-p1" "-i" "${dot_diff_file}" ${OPTS}
+    RESULT_VARIABLE patch_rv
+    OUTPUT_VARIABLE patch_ov
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    ERROR_STRIP_TRAILING_WHITESPACE)
+  
+  set(PATCHING_FAILED FALSE)
+  
+  if(NOT "${patch_rv}" STREQUAL "0")
+    if( NOT "${patch_ov}" MATCHES "previously applied")
+      set(PATCHING_FAILED TRUE)
+    endif()
+  endif()
+
+  if( "${patch_ov}" MATCHES "FAILED")
+    set(PATCHING_FAILED TRUE)
+  endif()
+  
+  if(PATCHING_FAILED)
+    message(FATAL_ERROR "${PATCH_PROGRAM} returned non-zero exit status \n ${patch_ov} \n")
+  else()
+    message("${patch_ov}")      
+  endif()
+  
+  unset(patch_rv)
+  unset(patch_ov)
+endforeach()
 
