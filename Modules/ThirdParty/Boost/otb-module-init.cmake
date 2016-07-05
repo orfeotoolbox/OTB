a444ac8e41dee6ac4a3bacf82defc6b1e203e74a
Julien Michel <julien.michel@cnes.fr>
2016-07-05 15:33:42 +0200

    ENH: We do not need boost::date_time

--- Modules/ThirdParty/Boost/otb-module-init.cmake
+++ Modules/ThirdParty/Boost/otb-module-init.cmake
@@ -1,9 +1,6 @@
-# Mandatory components
-# - date_time -> ossim plugins
-set(OTB_Boost_COMPONENTS date_time)
 # Optional components
 if (BUILD_TESTING)
-  list(APPEND OTB_Boost_COMPONENTS unit_test_framework)
+  set(OTB_Boost_COMPONENTS unit_test_framework)
 endif()
 
 message(STATUS "Looking for boost components: ${OTB_Boost_COMPONENTS}")
