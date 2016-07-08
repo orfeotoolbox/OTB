0e8e95f4f91c6cb4883f7defadd754871b3fe2ab
Rashad Kanavath <rashad.kanavath@c-s.fr>
2016-07-08 14:36:51 +0200

    CMake: check otb and otb-data git status and report their log & branch names

--- CMake/SourceStatus.cmake
+++ CMake/SourceStatus.cmake
@@ -1,86 +1,65 @@
-if(EXISTS "${PROJECT_SOURCE_DIR}/.hg")
-find_package(Mercurial)
-if(Mercurial_FOUND)
-  message(STATUS "Repository status :")
-  Mercurial_WC_IDENTIFY(${PROJECT_SOURCE_DIR} OTB)
-  message(STATUS "  Mercurial version is ${Mercurial_VERSION_HG}")
+function(repository_status root_repo_dir result_var1)
+  if(NOT EXISTS "${root_repo_dir}/.git")
+    return()
+  endif()
+
+  find_package(Git)
+  if(NOT GIT_FOUND)
+    message(STATUS "git not found. Make sure git can be found in your PATH. (source status will not be reported)")
+    set(${result_var1})
+    return()
+  endif()
+
+  execute_process(COMMAND ${GIT_EXECUTABLE} config --get remote.origin.url
+    WORKING_DIRECTORY ${root_repo_dir}
+    OUTPUT_VARIABLE git_remote_origin_url
+    OUTPUT_STRIP_TRAILING_WHITESPACE ERROR_QUIET)
+  if(git_remote_origin_url)
+    get_filename_component(dir_name ${git_remote_origin_url} NAME)
+  else()
+    get_filename_component(dir_name ${root_repo_dir} NAME)
+  endif()
+
+  execute_process(COMMAND ${GIT_EXECUTABLE} log -1 --pretty=format:%H --oneline
+    WORKING_DIRECTORY ${root_repo_dir}
+    OUTPUT_VARIABLE last_commit_id
+    OUTPUT_STRIP_TRAILING_WHITESPACE ERROR_QUIET)
+
+  #git symbolic-ref --short -q HEAD does not work on older git version
+  #rather than checking for git version. it is better to not use that
+  #option and employ cmake stuff to do the work
+  execute_process(COMMAND ${GIT_EXECUTABLE} symbolic-ref -q HEAD
+    WORKING_DIRECTORY ${root_repo_dir}
+    OUTPUT_VARIABLE git_symbolic_ref_output
+    OUTPUT_STRIP_TRAILING_WHITESPACE ERROR_QUIET)
+
+  set(branch_name)
+  if(git_symbolic_ref_output)
+    get_filename_component(branch_name ${git_symbolic_ref_output} NAME)
+  endif()
+
+  set(return_msg "${dir_name} repository info: \n")
+  set(return_msg "${return_msg} git log -1 --oneline     = [${last_commit_id}] \n")
+  set(return_msg "${return_msg} git symbolic-ref -q HEAD = ${branch_name}")
+  set(${result_var1} ${return_msg} PARENT_SCOPE)
+
+  #print other information inside function call
+  execute_process(COMMAND ${GIT_EXECUTABLE} log -1 --pretty=format:%H
+    WORKING_DIRECTORY ${root_repo_dir}
+    OUTPUT_VARIABLE OTB_WC_REVISION
+    OUTPUT_STRIP_TRAILING_WHITESPACE ERROR_QUIET)
+  execute_process(COMMAND ${GIT_EXECUTABLE} status -s
+    WORKING_DIRECTORY ${root_repo_dir}
+    OUTPUT_VARIABLE OTB_WC_STATUS
+    OUTPUT_STRIP_TRAILING_WHITESPACE ERROR_QUIET)
+  message(STATUS "Repository status (${dir_name}) :")
   message(STATUS "  Repository revision is ${OTB_WC_REVISION}")
-  set(OTB_WC_REVISION ${OTB_WC_REVISION} CACHE STRING "Repository version" FORCE)
-  mark_as_advanced(OTB_WC_REVISION)
   if(OTB_WC_STATUS)
-    message(STATUS "  Local file modifications:")
+    message(STATUS "  Local file modifications (${dir_name}) :")
     string(REPLACE "\n" "\n--     " OTB_WC_STATUS_PRETTYPRINT "    ${OTB_WC_STATUS}")
     message(STATUS "${OTB_WC_STATUS_PRETTYPRINT}")
   else()
-    message(STATUS "  No files modified locally")
-  endif()
-  string(REGEX REPLACE "\n" ";" OTB_WC_STATUS "${OTB_WC_STATUS}")
-  set(OTB_WC_STATUS ${OTB_WC_STATUS} CACHE STRING "Repository status" FORCE)
-  mark_as_advanced(OTB_WC_STATUS)
-endif()
-endif()
-
-if(EXISTS "${PROJECT_SOURCE_DIR}/.git")
-  find_package(Git)
-  if(GIT_FOUND)
-    execute_process(COMMAND ${GIT_EXECUTABLE} log -1 --pretty=format:%H --oneline
-      WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-      OUTPUT_VARIABLE OTB_GIT_LAST_COMMIT
-      OUTPUT_STRIP_TRAILING_WHITESPACE ERROR_QUIET)
-
-    #git symbolic-ref --short -q HEAD does not work on older git version
-    #rather than checking for git version. it is better to not use that
-    #option and employ cmake stuff to do the work
-    execute_process(COMMAND ${GIT_EXECUTABLE} symbolic-ref -q HEAD
-      WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-      OUTPUT_VARIABLE OTB_GIT_SYMBOLIC_REF_OUTPUT
-      OUTPUT_STRIP_TRAILING_WHITESPACE ERROR_QUIET)
-
-    get_filename_component(OTB_GIT_BRANCH ${OTB_GIT_SYMBOLIC_REF_OUTPUT} NAME)
-
-    execute_process(COMMAND ${GIT_EXECUTABLE} log -1 --pretty=format:%H
-      WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-      OUTPUT_VARIABLE OTB_WC_REVISION
-      OUTPUT_STRIP_TRAILING_WHITESPACE ERROR_QUIET)
-    execute_process(COMMAND ${GIT_EXECUTABLE} status -s
-      WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-      OUTPUT_VARIABLE OTB_WC_STATUS
-      OUTPUT_STRIP_TRAILING_WHITESPACE ERROR_QUIET)
-    message(STATUS "Repository status :")
-    message(STATUS "  Repository revision is ${OTB_WC_REVISION}")
-    if(OTB_WC_STATUS)
-      message(STATUS "  Local file modifications:")
-      string(REPLACE "\n" "\n--     " OTB_WC_STATUS_PRETTYPRINT "    ${OTB_WC_STATUS}")
-      message(STATUS "${OTB_WC_STATUS_PRETTYPRINT}")
-    else()
-      message(STATUS "  No files modified locally")
-    endif()
-  endif()
-endif()
-
-if(OTB_DATA_ROOT)
-  if(EXISTS "${OTB_DATA_ROOT}/.hg")
-    if(NOT Mercurial_FOUND)
-      find_package(Mercurial)
-    endif()
-    if(Mercurial_FOUND)
-      Mercurial_WC_IDENTIFY(${OTB_DATA_ROOT} OTB_DATA)
-      set(OTB_DATA_WC_REVISION ${OTB_DATA_WC_REVISION} CACHE STRING "Repository version" FORCE)
-      mark_as_advanced(OTB_DATA_WC_REVISION)
-    endif()
+    message(STATUS "  No files modified locally (${dir_name})")
   endif()
 
-  if(EXISTS "${OTB_DATA_ROOT}/.git")
-    if(NOT GIT_FOUND)
-      find_package(Git)
-    endif()
-    if(GIT_FOUND)
-      execute_process(COMMAND ${GIT_EXECUTABLE} log -1 --pretty=format:%H
-        WORKING_DIRECTORY ${OTB_DATA_ROOT}
-        OUTPUT_VARIABLE OTB_DATA_WC_REVISION
-        OUTPUT_STRIP_TRAILING_WHITESPACE ERROR_QUIET)
-      set(OTB_DATA_WC_REVISION ${OTB_DATA_WC_REVISION} CACHE STRING "Repository version" FORCE)
-      mark_as_advanced(OTB_DATA_WC_REVISION)
-    endif()
-  endif()
-endif()
+endfunction()
