7e9ae4575787fe24074ff09a338e91b4e587cd23
Stephane ALBERT <stephane.albert@c-s.fr>
2016-07-28 17:15:10 +0200

    ENH: Moved Monteverdi/Core into Modules.

---
+++ Modules/MonteverdiCore/otb-module.cmake
@@ -0,0 +1,15 @@
+set(
+  DOCUMENTATION
+  "This module provides MonteverdiCore, the core library of the Monteverdi applications."
+  )
+
+otb_module( OTBIce
+  DEPENDS
+
+  OTBCommon
+
+  OPTIONAL_DEPENDS
+  
+  DESCRIPTION
+  "${DOCUMENTATION}"
+  )
