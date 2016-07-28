f015dc76e8053fb75300679b16683024b9ffc09c
Stephane ALBERT <stephane.albert@c-s.fr>
2016-07-28 17:34:19 +0200

    ENH: Added MonteverdiGui empty module.

---
+++ Modules/Visualization/MonteverdiGui/otb-module.cmake
@@ -0,0 +1,15 @@
+set(
+  DOCUMENTATION
+  "This module provides MonteverdiGui, the GUI library of the Monteverdi applications."
+  )
+
+otb_module( MonteverdiGUI
+  DEPENDS
+
+  OTBCommon
+
+  OPTIONAL_DEPENDS
+  
+  DESCRIPTION
+  "${DOCUMENTATION}"
+  )
