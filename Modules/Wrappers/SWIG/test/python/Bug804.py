450616575c5c4a8b02064663127f740db77b3257
Rashad Kanavath <rashad.kanavath@c-s.fr>
2016-09-08 11:01:19 +0200

    TEST: fix print syntax for python3

--- Modules/Wrappers/SWIG/test/python/Bug804.py
+++ Modules/Wrappers/SWIG/test/python/Bug804.py
@@ -1,4 +1,4 @@
 def test(otb, argv):
 	app = otb.Registry.CreateApplication('TrainImagesClassifier')
 	app.SetParameterFloat('classifier.libsvm.opt',True)
-	print app.GetParameterValue('classifier.libsvm.opt')
+	print( app.GetParameterValue('classifier.libsvm.opt') )
