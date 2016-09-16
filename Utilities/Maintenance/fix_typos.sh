6fdc06fc3c630706ad5236bb9efe917ce7c887b5
Manuel Grizonnet <manuel.grizonnet@cnes.fr>
2016-09-16 14:13:51 +0200

    STYLE: use Even Rouault fork of codespell and improve fix_typos.sh

--- Utilities/Maintenance/fix_typos.sh
+++ Utilities/Maintenance/fix_typos.sh
@@ -23,24 +23,41 @@
 #  DEALINGS IN THE SOFTWARE.
 ###############################################################################
 
-#Portion of the script is based on fix_typos.sh in gdal source
+#FIXME
+#Rename AssymetricFusionOfDetectorImageFilter to AsymetricFusionOfDetectorImageFilter
+#Rename Restauration to Restoration
+#Rename InstanciateTransform() to InstantiateTransform()
+#Rename Adressing  ==> Addressing
+# ../../Modules/Core/Metadata/include/otbDefaultImageMetadataInterface.h:155: clas  ==> class  | disabled because of name clash in c++
+# m_ShrinkedOutput -> m_ShrunkOutput
+# convertor -> converter
 
-#Install codespell (pip install codespell or git https://github.com/lucasdemarchi/codespell.git)
-# Note GDAL is using a patched version to support white list and full filename for exclude files
+if ! test -d fix_typos; then
+    # Get even rouault fork of codespell that adds --words-white-list and full filename support for -S option
+    mkdir fix_typos
+    cd fix_typos
+    git clone https://github.com/rouault/codespell
+    cd codespell
+    git checkout gdal_improvements
+    cd ..
+    # Aggregate base dictionary + QGIS one + Debian Lintian one
+    curl https://raw.githubusercontent.com/qgis/QGIS/master/scripts/spelling.dat | sed "s/:/->/" | grep -v "colour->" | grep -v "colours->" > qgis.txt
+    curl https://anonscm.debian.org/cgit/lintian/lintian.git/plain/data/spelling/corrections| grep "||" | grep -v "#" | sed "s/||/->/" > debian.txt
+    cat codespell/data/dictionary.txt qgis.txt debian.txt | awk 'NF' > otb_dict.txt
 
-# Aggregate base dictionary + QGIS one + Debian Lintian one (same as in gdal)
-curl https://raw.githubusercontent.com/qgis/QGIS/master/scripts/spelling.dat | sed "s/:/->/" | grep -v "colour->" | grep -v "colours->" > qgis.txt
-curl https://anonscm.debian.org/cgit/lintian/lintian.git/plain/data/spelling/corrections| grep "||" | grep -v "#" | sed "s/||/->/" > debian.txt
-cat codespell/data/dictionary.txt qgis.txt debian.txt | awk 'NF' > otb_dict.txt
+    #echo "difered->deferred" >> gdal_dict.txt
+    #echo "differed->deferred" >> gdal_dict.txt
+    cd ..
+fi
 
 #Build exclude files and directory list
-EXCLUDED_FILES="*/.git*
+EXCLUDED_FILES="*/.git*"
 #Exclude third parties code, patches from superbuild and doxygen files
-EXCLUDED_FILES="$EXCLUDED_FILES,*/Modules/ThirdParty/6S/*,*/Modules/ThirdParty/SiftFast/*,*/SuperBuild/patches/*,*/Utilities/Doxygen/*,"
+EXCLUDED_FILES="$EXCLUDED_FILES,*/Modules/ThirdParty/6S/*,*/Modules/ThirdParty/SiftFast/*,*/Modules/ThirdParty/SPTW/*,*/SuperBuild/patches/*,*/Utilities/Doxygen/*"
 #Exclude also documentation for now
 EXCLUDED_FILES="$EXCLUDED_FILES,*/Documentation/*"
 #Exclude also Copyright folder for now
-EXCLUDED_FILES="$EXCLUDED_FILES,*/Copyright/*,*/SuperBuild/Copyright/*,*/Utilities/Maintenance/*"
+EXCLUDED_FILES="$EXCLUDED_FILES,*/Copyright/*,*/SuperBuild/Copyright/*,*/Utilities/Maintenance/*,LICENSE,*/i18n/*"
 
-codespell.py -w -i 3 -q 2 -S $EXCLUDED_FILES \
-             -D otb_dict.txt ../..
+
+python3 fix_typos/codespell/codespell.py codespell.py -w -i 3 -q 2 -S $EXCLUDED_FILES -D fix_typos/otb_dict.txt ../..
