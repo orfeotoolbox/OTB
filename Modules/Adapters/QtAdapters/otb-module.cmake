f6d88f761ee3004753e1ec13770ed77229d6e5ce
Rashad Kanavath <rashad.kanavath@c-s.fr>
2016-10-03 14:57:08 +0200

    BUG: include otb common binary dir to get auto-generated export macro

--- Modules/Adapters/QtAdapters/otb-module.cmake
+++ Modules/Adapters/QtAdapters/otb-module.cmake
@@ -2,12 +2,6 @@ set( DOCUMENTATION "Adapters for the Qt Library.")
 
 otb_module( OTBQtAdapters
   ENABLE_SHARED
-  DEPENDS
-    OTBQt4
-
-  # TEST_DEPENDS
-  #   OTBCommon
-
-  DESCRIPTION
-    "${DOCUMENTATION}"
+  DEPENDS  OTBQt4
+  DESCRIPTION "${DOCUMENTATION}"
 )
