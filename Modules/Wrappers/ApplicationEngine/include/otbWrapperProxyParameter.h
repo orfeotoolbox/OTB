d82ac2e04976fa2762b90c058a5090687e40fa23
Guillaume Pasero <guillaume.pasero@c-s.fr>
2016-10-06 10:47:34 +0200

    ENH: next version of ProxyParameter mechanism : allow proxy chains

--- Modules/Wrappers/ApplicationEngine/include/otbWrapperProxyParameter.h
+++ Modules/Wrappers/ApplicationEngine/include/otbWrapperProxyParameter.h
@@ -3,12 +3,21 @@
 
 #include "itkObject.h"
 #include "otbWrapperParameter.h"
+#include <utility>
 
 namespace otb
 {
 namespace Wrapper
 {
-
+/**
+ * \class ProxyParameter
+ *
+ * \brief Parameter class acting as a proxy to a different parameter
+ *
+ * The target parameter of this proxy is defined as a pair of a group parameter
+ * containing the target and the targets key. It allows to define proxies on
+ * parameters that may be themselves replaced by a proxy
+ */
 class ProxyParameter : public Parameter
 {
 public:
@@ -21,14 +30,22 @@ public:
 
   itkTypeMacro(ProxyParameter, Parameter);
 
-  void SetInternalParameter(Parameter *param)
+  typedef std::pair<Parameter::Pointer, std::string> ProxyTargetType;
+
+  /** Set the target parameter of the proxy
+   *  \param target pair of a group parameter containing the target and its key
+   */
+  void SetTarget(const ProxyTargetType& target)
     {
-    m_InternalParam = param;
+    m_Target = target;
     }
 
-  Parameter* GetInternalParameter(void)
+  /** Get the target parameter of the proxy
+   *  First part is the group parameter containing the target
+   *  Second part is the key of the target */
+  const ProxyTargetType & GetTarget(void)
     {
-    return m_InternalParam.GetPointer();
+    return m_Target;
     }
 
 protected:
@@ -39,7 +56,7 @@ private:
   ProxyParameter(const Self &); //purposely not implemented
   void operator =(const Self&); //purposely not implemented
 
-  typename Parameter::Pointer m_InternalParam;
+  ProxyTargetType m_Target;
 };
 
 }
