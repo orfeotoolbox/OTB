300021714dddbc58600808f5f0e8738058b55eeb
Manuel Grizonnet <manuel.grizonnet@cnes.fr>
2016-10-25 09:24:41 +0200

    ENH: minor update in phenotb module description

--- Modules/Remote/phenotb.remote.cmake
+++ Modules/Remote/phenotb.remote.cmake
@@ -1,6 +1,6 @@
 #Contact: Jordi Inglada  <jordi.inglada@cesbio.eu>
 otb_fetch_module(OTBPhenology
-  "This module implements a several algorithms allowing to extract phenological
+  "This module implements several algorithms allowing to extract phenological
   information from time profiles. These time profiles should represent
   vegetation status as for instance NDVI, LAI, etc.
 A more detailed description can be found on the project website:
