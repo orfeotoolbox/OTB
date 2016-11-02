0d467145638f44a5e700ee4b8fa9d0ecb16cb812
Guillaume Pasero <guillaume.pasero@c-s.fr>
2016-11-02 12:05:41 +0100

    PKG: warning when non-rpath depencencies are found on macosx

--- SuperBuild/Packaging/PackageHelper.cmake
+++ SuperBuild/Packaging/PackageHelper.cmake
@@ -803,6 +803,10 @@ function(func_process_deps infile)
 
   is_system_dll(is_system "${infile}")
   if(EXISTS ${infile} AND NOT is_system)
+    if(APPLE)
+      # We want all non-system deps to start with rpath
+      message(WARNING "Found dependency without relative path : ${infile}")
+    endif()
     get_filename_component(infile ${infile} NAME)
   endif()
 
