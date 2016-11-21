5c3112a5dd1105479ef1b5851404c94a9c859c9f
Rashad Kanavath <rashad.kanavath@c-s.fr>
2016-11-21 11:14:26 +0100

    ENH: cleanup package and xdk for all platforms

--- SuperBuild/CMake/External_pkgtools.cmake
+++ SuperBuild/CMake/External_pkgtools.cmake
@@ -1,3 +1,24 @@
+
+set(PKGTOOLS_SB_PREFIX_DIR "${CMAKE_BINARY_DIR}/PACKAGE-TOOLS")
+
+set(PATCHELF_SOURCE_DIR  "${PKGTOOLS_SB_PREFIX_DIR}/src/patchelf")
+# PATCHELF_BINARY_DIR is same as PATCHELF_SOURCE_DIR
+set(PATCHELF_INSTALL_DIR "${PKGTOOLS_SB_PREFIX_DIR}/install/patchelf")
+set(PATCHELF_STAMP_DIR   "${PKGTOOLS_SB_PREFIX_DIR}/stamp/patchelf")
+set(PATCHELF_TMP_DIR     "${PKGTOOLS_SB_PREFIX_DIR}/tmp/patchelf")
+
+#This variable is referenced in PMacros.cmake
+set(PATCHELF_PROGRAM "${PATCHELF_SOURCE_DIR}/src/patchelf")
+
+
+set(MAKESELF_SOURCE_DIR  "${PKGTOOLS_SB_PREFIX_DIR}/src/makeself")
+# MAKESELF_SOURCE_DIR is same as MAKESELF_BINARY_DIR and MAKESELF_INSTALL_DIR
+set(MAKESELF_STAMP_DIR   "${PKGTOOLS_SB_PREFIX_DIR}/stamp/makeself")
+set(MAKESELF_TMP_DIR     "${PKGTOOLS_SB_PREFIX_DIR}/tmp/makeself")
+
+#This variable is referenced in PMacros.cmake
+set(MAKESELF_SCRIPT "${MAKESELF_SOURCE_DIR}/makeself.sh")
+
 if( __EXTERNAL_PACKAGE_TOOLS__)
   return()
 else()
@@ -9,57 +30,41 @@ if(WIN32)
   return()
 endif()
 
-if(UNIX AND NOT WIN32)
+include(ExternalProject)
 
-  include(ExternalProject)
+ExternalProject_Add(MAKESELF
+  PREFIX            "${PKGTOOLS_SB_PREFIX_DIR}"
+  URL               "https://www.orfeo-toolbox.org/packages/makeself-2.2.0.tar.gz"
+  URL_MD5           3c61df934b0c61ddcd7bd63b391e951d
+  DOWNLOAD_DIR      "${DOWNLOAD_LOCATION}"
+  SOURCE_DIR        "${MAKESELF_SOURCE_DIR}"
+  BINARY_DIR        "${MAKESELF_SOURCE_DIR}"
+  INSTALL_DIR       "${MAKESELF_SOURCE_DIR}"
+  STAMP_DIR         "${MAKESELF_STAMP_DIR}"
+  TMP_DIR           "${MAKESELF_TMP_DIR}"
+  CONFIGURE_COMMAND ""
+  BUILD_COMMAND ""
+  INSTALL_COMMAND ""
+  DEPENDS PATCHELF
+  )
 
-  set(PKGTOOLS_SB_PREFIX_DIR "${CMAKE_BINARY_DIR}/PACKAGE-TOOLS")
+if(APPLE)
+  add_custom_target(PATCHELF)
+  return()
+endif()
 
-  if(APPLE)
-    add_custom_target(PATCHELF)
-  else()
-    set(PATCHELF_INSTALL_DIR ${PKGTOOLS_SB_PREFIX_DIR}/install-patchelf)
-    set(PATCHELF_PROGRAM ${PATCHELF_INSTALL_DIR}/patchelf)
-    ExternalProject_Add(PATCHELF
-      PREFIX "${PKGTOOLS_SB_PREFIX_DIR}"
-      URL "http://nixos.org/releases/patchelf/patchelf-0.9/patchelf-0.9.tar.bz2"
-      URL_MD5 d02687629c7e1698a486a93a0d607947
-      DOWNLOAD_DIR   ${DOWNLOAD_LOCATION}
-      SOURCE_DIR "${PKGTOOLS_SB_PREFIX_DIR}/src-patchelf"
-      BINARY_DIR "${PKGTOOLS_SB_PREFIX_DIR}/build-patchelf"
-      TMP_DIR  "${PKGTOOLS_SB_PREFIX_DIR}/tmp-patchelf"
-      STAMP_DIR "${PKGTOOLS_SB_PREFIX_DIR}/stamp-patchelf"
-      PATCH_COMMAND  ${CMAKE_COMMAND} -E make_directory ${PKGTOOLS_SB_PREFIX_DIR}/install
-      CONFIGURE_COMMAND ${CMAKE_COMMAND} -E chdir ${PKGTOOLS_SB_PREFIX_DIR}/src-patchelf
-      ./configure --prefix ${PATCHELF_INSTALL_DIR}
-      BUILD_COMMAND ${CMAKE_COMMAND} -E chdir ${PKGTOOLS_SB_PREFIX_DIR}/src-patchelf ${CMAKE_MAKE_PROGRAM}
-      INSTALL_COMMAND ${CMAKE_COMMAND} -E copy ${PKGTOOLS_SB_PREFIX_DIR}/src-patchelf/src/patchelf ${PATCHELF_PROGRAM}
-      )
-  endif(APPLE)
+ExternalProject_Add(PATCHELF
+  PREFIX "${PKGTOOLS_SB_PREFIX_DIR}"
+  URL                 "http://nixos.org/releases/patchelf/patchelf-0.9/patchelf-0.9.tar.bz2"
+  URL_MD5             d02687629c7e1698a486a93a0d607947
+  DOWNLOAD_DIR        "${DOWNLOAD_LOCATION}"
+  SOURCE_DIR          "${PATCHELF_SOURCE_DIR}"
+  BINARY_DIR          "${PATCHELF_SOURCE_DIR}"
+  INSTALL_DIR         "${PATCHELF_INSTALL_DIR}"
+  STAMP_DIR           "${PATCHELF_STAMP_DIR}"
+  TMP_DIR             "${PATCHELF_TMP_DIR}"
+  CONFIGURE_COMMAND "./configure" "--prefix" "${PATCHELF_INSTALL_DIR}"
+  INSTALL_COMMAND ""
+  )
 
-  set(MAKESELF_INSTALL_DIR ${PKGTOOLS_SB_PREFIX_DIR}/install-makeself)
-  set(MAKESELF_SCRIPT ${MAKESELF_INSTALL_DIR}/makeself.sh)
-  set(MAKESELF_HEADER_SCRIPT ${MAKESELF_INSTALL_DIR}/makeself-header.sh)
-
-  ExternalProject_Add(MAKESELF
-    PREFIX "${PKGTOOLS_SB_PREFIX_DIR}"
-    URL "https://www.orfeo-toolbox.org/packages/makeself-2.2.0.tar.gz"
-    URL_MD5 3c61df934b0c61ddcd7bd63b391e951d
-    DOWNLOAD_DIR ${DOWNLOAD_LOCATION}
-    SOURCE_DIR "${PKGTOOLS_SB_PREFIX_DIR}/src-makeself"
-    BINARY_DIR "${PKGTOOLS_SB_PREFIX_DIR}/build-makeself"
-    TMP_DIR  "${PKGTOOLS_SB_PREFIX_DIR}/tmp-makeself"
-    STAMP_DIR "${PKGTOOLS_SB_PREFIX_DIR}/stamp-makeself"
-    CONFIGURE_COMMAND ""
-    BUILD_COMMAND ${CMAKE_COMMAND} -E copy
-    ${PKGTOOLS_SB_PREFIX_DIR}/src-makeself/makeself-header.sh
-    ${MAKESELF_HEADER_SCRIPT}
-    INSTALL_COMMAND ${CMAKE_COMMAND} -E copy
-    ${PKGTOOLS_SB_PREFIX_DIR}/src-makeself/makeself.sh
-    ${MAKESELF_SCRIPT}
-    DEPENDS PATCHELF
-    )
-
-  add_custom_target(PACKAGE-TOOLS DEPENDS MAKESELF)
-
-endif(UNIX AND NOT WIN32)
+add_custom_target(PACKAGE-TOOLS DEPENDS MAKESELF)
