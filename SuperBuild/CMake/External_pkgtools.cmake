beb006c8dd460308d79a1e0b7322ea407950c91d
Rashad Kanavath <rashad.kanavath@c-s.fr>
2016-11-22 04:01:51 +0100

    pathelf -> makeself -> package-tools dependency for unix

--- SuperBuild/CMake/External_pkgtools.cmake
+++ SuperBuild/CMake/External_pkgtools.cmake
@@ -7,7 +7,7 @@ set(PATCHELF_INSTALL_DIR "${PKGTOOLS_SB_PREFIX_DIR}/install/patchelf")
 set(PATCHELF_STAMP_DIR   "${PKGTOOLS_SB_PREFIX_DIR}/stamp/patchelf")
 set(PATCHELF_TMP_DIR     "${PKGTOOLS_SB_PREFIX_DIR}/tmp/patchelf")
 
-#This variable is referenced in PMacros.cmake
+#This variable is referenced in CreateCMakeProjects.cmake
 set(PATCHELF_PROGRAM "${PATCHELF_SOURCE_DIR}/src/patchelf")
 
 
@@ -16,7 +16,7 @@ set(MAKESELF_SOURCE_DIR  "${PKGTOOLS_SB_PREFIX_DIR}/src/makeself")
 set(MAKESELF_STAMP_DIR   "${PKGTOOLS_SB_PREFIX_DIR}/stamp/makeself")
 set(MAKESELF_TMP_DIR     "${PKGTOOLS_SB_PREFIX_DIR}/tmp/makeself")
 
-#This variable is referenced in PMacros.cmake
+#This variable is referenced in CreateCMakeProjects.cmake
 set(MAKESELF_SCRIPT "${MAKESELF_SOURCE_DIR}/makeself.sh")
 
 if( __EXTERNAL_PACKAGE_TOOLS__)
@@ -32,6 +32,25 @@ endif()
 
 include(ExternalProject)
 
+
+if(APPLE)
+  add_custom_target(PATCHELF)
+else()
+  ExternalProject_Add(PATCHELF
+    PREFIX "${PKGTOOLS_SB_PREFIX_DIR}"
+    URL                 "http://nixos.org/releases/patchelf/patchelf-0.9/patchelf-0.9.tar.bz2"
+    URL_MD5             d02687629c7e1698a486a93a0d607947
+    DOWNLOAD_DIR        "${DOWNLOAD_LOCATION}"
+    SOURCE_DIR          "${PATCHELF_SOURCE_DIR}"
+    BINARY_DIR          "${PATCHELF_SOURCE_DIR}"
+    INSTALL_DIR         "${PATCHELF_INSTALL_DIR}"
+    STAMP_DIR           "${PATCHELF_STAMP_DIR}"
+    TMP_DIR             "${PATCHELF_TMP_DIR}"
+    CONFIGURE_COMMAND "./configure" "--prefix" "${PATCHELF_INSTALL_DIR}"
+    INSTALL_COMMAND ""
+    )
+endif()
+
 ExternalProject_Add(MAKESELF
   PREFIX            "${PKGTOOLS_SB_PREFIX_DIR}"
   URL               "https://www.orfeo-toolbox.org/packages/makeself-2.2.0.tar.gz"
@@ -45,26 +64,7 @@ ExternalProject_Add(MAKESELF
   CONFIGURE_COMMAND ""
   BUILD_COMMAND ""
   INSTALL_COMMAND ""
+  DEPENDS PATCHELF
   )
 
-if(APPLE)
-  add_custom_target(PATCHELF)
-  return()
-endif()
-
-ExternalProject_Add(PATCHELF
-  PREFIX "${PKGTOOLS_SB_PREFIX_DIR}"
-  URL                 "http://nixos.org/releases/patchelf/patchelf-0.9/patchelf-0.9.tar.bz2"
-  URL_MD5             d02687629c7e1698a486a93a0d607947
-  DOWNLOAD_DIR        "${DOWNLOAD_LOCATION}"
-  SOURCE_DIR          "${PATCHELF_SOURCE_DIR}"
-  BINARY_DIR          "${PATCHELF_SOURCE_DIR}"
-  INSTALL_DIR         "${PATCHELF_INSTALL_DIR}"
-  STAMP_DIR           "${PATCHELF_STAMP_DIR}"
-  TMP_DIR             "${PATCHELF_TMP_DIR}"
-  CONFIGURE_COMMAND "./configure" "--prefix" "${PATCHELF_INSTALL_DIR}"
-  INSTALL_COMMAND ""
-  DEPENDS MAKESELF
-  )
-
-add_custom_target(PACKAGE-TOOLS DEPENDS PATCHELF)
+add_custom_target(PACKAGE-TOOLS DEPENDS MAKESELF)
