4ed97d82afe53e8f1e004ecc4314ceebadb6f3dc
Guillaume Pasero <guillaume.pasero@c-s.fr>
2017-01-18 14:55:39 +0100

    PKG: choose the revision to package for each remote module

--- Modules/Remote/otb-bv.remote.cmake
+++ Modules/Remote/otb-bv.remote.cmake
@@ -5,5 +5,5 @@ A more detailed description can be found on the project website:
 http://tully.ups-tlse.fr/jordi/otb-bv
 "
   GIT_REPOSITORY http://tully.ups-tlse.fr/jordi/otb-bv.git
-  GIT_TAG master
+  GIT_TAG d13a3b3febe61c3c67777eac8261e07a6257a519
 )
