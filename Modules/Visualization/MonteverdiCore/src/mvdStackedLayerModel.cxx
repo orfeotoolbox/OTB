orfeotoolbox/OTB

commit 38ba398ba46ee6baae9a5e95327cb14b0348b0e5

BUG: When deleting a layer fallback to Projection=None only if there are no more images left in the layer stack.

Modules/Visualization/MonteverdiCore/src/mvdStackedLayerModel.cxx
@@ -253,7 +253,7 @@ StackedLayerModel
     ? m_Current
     : ( m_Current>0
         ? m_Current - 1
-        : StackedLayerModel::NIL_INDEX );
+        : GetCount()>0 ? 0 : StackedLayerModel::NIL_INDEX );
 
   //
   // Emit signals.
@@ -287,7 +287,7 @@ StackedLayerModel
       ? m_Reference
       : ( m_Reference > 0
 	  ? m_Reference - 1
-	  : StackedLayerModel::NIL_INDEX ),
+          : GetCount() > 0 ? 0 : StackedLayerModel::NIL_INDEX ),
       true
     );
 

commit a35865d23594c92ea3b5fce4d56f01758b039ba5

BUG: fix regression introduced by 38ba398

Modules/Visualization/MonteverdiCore/src/mvdStackedLayerModel.cxx
@@ -253,7 +253,7 @@ StackedLayerModel
     ? m_Current
     : ( m_Current>0
         ? m_Current - 1
-        : GetCount()>0 ? 0 : StackedLayerModel::NIL_INDEX );
+        : GetCount()>1 ? 0 : StackedLayerModel::NIL_INDEX );
 
   //
   // Emit signals.