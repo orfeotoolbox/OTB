290e1a084f958ee3b7548cb80ad1aab9b63c8e4a
Guillaume Pasero <guillaume.pasero@c-s.fr>
2017-02-07 17:36:41 +0100

    Revert "PKG: include libstdc++ for better compatibility"
    This reverts commit f618e6c6c9af41b0a8ed6809aa750b8803045ed3.

--- SuperBuild/Packaging/PackageGlobals.cmake
+++ SuperBuild/Packaging/PackageGlobals.cmake
@@ -47,6 +47,7 @@ endif()
 set(LINUX_SYSTEM_DLLS
   libm.so
   libc.so
+  libstdc*
   libgcc_s.so
   librt.so
   libdl.so
@@ -133,8 +134,6 @@ set(GTK_LIB_LIST_2
   libpng12.so.0
   libexpat.so.1
   libfontconfig.so.1
-  libstdc\\+\\+.so.6
   )
-# libstdc++ is not actually part of GTK but we want to include it anyway in linux package
 
 set(ALLOWED_SYSTEM_DLLS ${GTK_LIB_LIST_1} ${GTK_LIB_LIST_2})
