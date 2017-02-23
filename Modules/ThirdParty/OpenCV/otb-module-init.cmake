056ff067c08f02c577841b157189e8bcae8c5b65
Guillaume Pasero <guillaume.pasero@c-s.fr>
2017-02-23 15:19:17 +0100

    BUG: make the OTB_OPENCV_3 variable more visible for test screening

--- Modules/ThirdParty/OpenCV/otb-module-init.cmake
+++ Modules/ThirdParty/OpenCV/otb-module-init.cmake
@@ -5,3 +5,9 @@ if("${OpenCV_VERSION}" VERSION_LESS "2.4.0")
   message(WARNING "The OpenCV version detected (${OpenCV_VERSION}) is older than 2.4.0, "
     "it may not be fully compatible with OTB")
 endif()
+
+if(OpenCV_VERSION_MAJOR EQUAL 3)
+set(OTB_OPENCV_3 1)
+else()
+set(OTB_OPENCV_3 0)
+endif()
