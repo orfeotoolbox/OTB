cb1af1ef3b29b0a615f2b919ddbcffe8eb672e4c
Manuel Grizonnet <manuel.grizonnet@cnes.fr>
2017-06-07 16:26:06 +0200

    SUPERBUILD: update gdal to 2.2.0

--- SuperBuild/CMake/External_gdal.cmake
+++ SuperBuild/CMake/External_gdal.cmake
@@ -133,8 +133,8 @@ endif()
 
 ExternalProject_Add(GDAL
   PREFIX GDAL
-  URL "http://download.osgeo.org/gdal/2.1.0/gdal-2.1.0.tar.gz"
-  URL_MD5 0fc165cd947c54b132204233dfb243f1
+  URL "http://download.osgeo.org/gdal/2.2.0/gdal-2.2.0.tar.gz"
+  URL_MD5 37d98d32d6ab0b2d7026a6089c3d17ba
   SOURCE_DIR ${GDAL_SB_SRC}
   BINARY_DIR ${GDAL_SB_SRC}
   INSTALL_DIR ${SB_INSTALL_PREFIX}
