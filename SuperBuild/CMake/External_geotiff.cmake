5503f0c5bdc6f411f58604bb714c4763191ad29f
Rashad Kanavath <rashad.kanavath@c-s.fr>
2017-07-21 14:28:36 +0200

    SuperBuild: geotiff: try cmake build on all platforms

--- SuperBuild/CMake/External_geotiff.cmake
+++ SuperBuild/CMake/External_geotiff.cmake
@@ -26,49 +26,49 @@ SETUP_SUPERBUILD(GEOTIFF)
 ADDTO_DEPENDENCIES_IF_NOT_SYSTEM(GEOTIFF TIFF PROJ JPEG ZLIB)
 
 set(GEOTIFF_SB_CONFIG)
-ADD_SUPERBUILD_CMAKE_VAR(GEOTIFF TIFF_INCLUDE_DIR)
-ADD_SUPERBUILD_CMAKE_VAR(GEOTIFF TIFF_LIBRARY)
-ADD_SUPERBUILD_CMAKE_VAR(GEOTIFF PROJ_INCLUDE_DIR)
-ADD_SUPERBUILD_CMAKE_VAR(GEOTIFF PROJ_LIBRARY)
-ADD_SUPERBUILD_CMAKE_VAR(GEOTIFF ZLIB_INCLUDE_DIR)
-ADD_SUPERBUILD_CMAKE_VAR(GEOTIFF ZLIB_LIBRARY)
-ADD_SUPERBUILD_CMAKE_VAR(GEOTIFF JPEG_INCLUDE_DIR)
-ADD_SUPERBUILD_CMAKE_VAR(GEOTIFF JPEG_LIBRARY)
+# ADD_SUPERBUILD_CMAKE_VAR(GEOTIFF TIFF_INCLUDE_DIR)
+# ADD_SUPERBUILD_CMAKE_VAR(GEOTIFF TIFF_LIBRARY)
+# ADD_SUPERBUILD_CMAKE_VAR(GEOTIFF PROJ_INCLUDE_DIR)
+# ADD_SUPERBUILD_CMAKE_VAR(GEOTIFF PROJ_LIBRARY)
+# ADD_SUPERBUILD_CMAKE_VAR(GEOTIFF ZLIB_INCLUDE_DIR)
+# ADD_SUPERBUILD_CMAKE_VAR(GEOTIFF ZLIB_LIBRARY)
+# ADD_SUPERBUILD_CMAKE_VAR(GEOTIFF JPEG_INCLUDE_DIR)
+# ADD_SUPERBUILD_CMAKE_VAR(GEOTIFF JPEG_LIBRARY)
 
-#RK: we are forced to use autoconf on osx due to cmake's find_* functions
-#Do we need to use the same of Linux ?
-if(UNIX)
-  set(GEOTIFF_SB_CONFIG)
-  ADD_SUPERBUILD_CONFIGURE_VAR(GEOTIFF PROJ_ROOT     --with-proj)
-  ADD_SUPERBUILD_CONFIGURE_VAR(GEOTIFF TIFF_ROOT     --with-libtiff)
-  ADD_SUPERBUILD_CONFIGURE_VAR(GEOTIFF JPEG_ROOT     --with-jpeg)
-  ADD_SUPERBUILD_CONFIGURE_VAR(GEOTIFF ZLIB_ROOT     --with-libz)
+# #RK: we are forced to use autoconf on osx due to cmake's find_* functions
+# #Do we need to use the same of Linux ?
+# if(UNIX)
+#   set(GEOTIFF_SB_CONFIG)
+#   ADD_SUPERBUILD_CONFIGURE_VAR(GEOTIFF PROJ_ROOT     --with-proj)
+#   ADD_SUPERBUILD_CONFIGURE_VAR(GEOTIFF TIFF_ROOT     --with-libtiff)
+#   ADD_SUPERBUILD_CONFIGURE_VAR(GEOTIFF JPEG_ROOT     --with-jpeg)
+#   ADD_SUPERBUILD_CONFIGURE_VAR(GEOTIFF ZLIB_ROOT     --with-libz)
 
-  ExternalProject_Add(GEOTIFF
-    PREFIX GEOTIFF
-    URL "http://download.osgeo.org/geotiff/libgeotiff/libgeotiff-1.4.2.tar.gz"
-    URL_MD5 96ab80e0d4eff7820579957245d844f8
-    SOURCE_DIR ${GEOTIFF_SB_SRC}
-    BINARY_DIR ${GEOTIFF_SB_SRC}
-    INSTALL_DIR ${SB_INSTALL_PREFIX}
-    DOWNLOAD_DIR ${DOWNLOAD_LOCATION}
-    DEPENDS ${GEOTIFF_DEPENDENCIES}
-    CONFIGURE_COMMAND
-    ${SB_ENV_CONFIGURE_CMD}
-    ${GEOTIFF_SB_SRC}/configure
-    ${SB_CONFIGURE_ARGS}
-    ${GEOTIFF_SB_CONFIG}
-    LOG_BUILD 1
-    LOG_INSTALL 1
-    )
+#   ExternalProject_Add(GEOTIFF
+#     PREFIX GEOTIFF
+#     URL "http://download.osgeo.org/geotiff/libgeotiff/libgeotiff-1.4.2.tar.gz"
+#     URL_MD5 96ab80e0d4eff7820579957245d844f8
+#     SOURCE_DIR ${GEOTIFF_SB_SRC}
+#     BINARY_DIR ${GEOTIFF_SB_SRC}
+#     INSTALL_DIR ${SB_INSTALL_PREFIX}
+#     DOWNLOAD_DIR ${DOWNLOAD_LOCATION}
+#     DEPENDS ${GEOTIFF_DEPENDENCIES}
+#     CONFIGURE_COMMAND
+#     ${SB_ENV_CONFIGURE_CMD}
+#     ${GEOTIFF_SB_SRC}/configure
+#     ${SB_CONFIGURE_ARGS}
+#     ${GEOTIFF_SB_CONFIG}
+#     LOG_BUILD 1
+#     LOG_INSTALL 1
+#     )
 
-  # [LOG_CONFIGURE 1]           # Wrap configure in script to log output
-  # [LOG_BUILD 1]               # Wrap build in script to log output
-  # [LOG_TEST 1]                # Wrap test in script to log output
-  # [LOG_INSTALL 1]             # Wrap install in script to log output
-  SUPERBUILD_PATCH_SOURCE(GEOTIFF)
+#   # [LOG_CONFIGURE 1]           # Wrap configure in script to log output
+#   # [LOG_BUILD 1]               # Wrap build in script to log output
+#   # [LOG_TEST 1]                # Wrap test in script to log output
+#   # [LOG_INSTALL 1]             # Wrap install in script to log output
+#   SUPERBUILD_PATCH_SOURCE(GEOTIFF)
 
-else()
+# else()
 
   ExternalProject_Add(GEOTIFF
     PREFIX GEOTIFF
@@ -91,7 +91,7 @@ else()
     LOG_INSTALL 1
     )
 
-endif()
+#endif()
 
 set(_SB_GEOTIFF_INCLUDE_DIR ${SB_INSTALL_PREFIX}/include)
 if(WIN32)
