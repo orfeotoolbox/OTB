bc42575865700e5961e69761873f9cb056f30423
Rashad Kanavath <rashad.kanavath@c-s.fr>
2017-07-31 15:32:49 +0200

    TEST: run selftester script in testing

--- Packaging/testing.cmake
+++ Packaging/testing.cmake
@@ -9,20 +9,22 @@ set(pkg_extracted_dir "${testing_dir}/${PKG_STAGE_DIR}")
 if(WIN32)
   set(extract_opts "x;${PACKAGE_OUTPUT_FILE}")
   set(extract_cmd "${ZIP_EXECUTABLE}")
+  set(my_ext ".bat")
 else()
   set(extract_opts "--target;${pkg_extracted_dir}")
   set(extract_cmd "${PACKAGE_OUTPUT_FILE}")
+  set(my_ext ".sh")
 endif()
 
-add_test(TuExtractPackage
+add_test(Tu_install_package
   COMMAND ${extract_cmd}
   ${extract_opts}
   WORKING_DIRECTORY ${testing_dir}
   )
 
-add_test(TuOtbLoader
-  COMMAND ${pkg_extracted_dir}/bin/otb_loader
-  WORKING_DIRECTORY ${testing_dir}
+add_test(Tu_selftester
+  COMMAND ${pkg_extracted_dir}/bin/selftester${my_ext}
+  WORKING_DIRECTORY ${pkg_extracted_dir}
   DEPENDS
   )
 
