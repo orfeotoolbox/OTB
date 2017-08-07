629293d69178d38d773544e746c2e9cf601654c6
Stephane ALBERT <stephane.albert@c-s.fr>
2017-08-07 15:44:52 +0200

    ENH: improved otb::Wrapper::StringListInterface.

--- Modules/Wrappers/ApplicationEngine/include/otbWrapperStringListInterface.h
+++ Modules/Wrappers/ApplicationEngine/include/otbWrapperStringListInterface.h
@@ -68,6 +68,11 @@ public:
   /** Retrieve number of elements */
   virtual unsigned int Size() const = 0;
 
+  /** */
+  virtual bool IsActive( unsigned int i ) const = 0;
+
+  /** */
+  virtual const std::string & GetToolTip( unsigned int ) const = 0;
 
 protected:
   /** Constructor */
