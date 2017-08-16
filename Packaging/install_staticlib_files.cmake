f090fde60b956e6706a5d7b2577a54ee8fa486b5
Rashad Kanavath <rashad.kanavath@c-s.fr>
2017-08-16 10:19:26 +0200

    BUG: avoif including all static libs from lib
    This will break ofcourse building external modules for OTB.
    But now that we have a test Tu_build_examples, these issues
    will not go un-noticed or fixed with a hack!. We find the
    bug and fix right way

--- Packaging/install_staticlib_files.cmake
+++ Packaging/install_staticlib_files.cmake
@@ -9,25 +9,25 @@ function(install_staticlib_files)
       # )
 
       
-    file(GLOB MY_LIB_FILES 
-    "${SUPERBUILD_INSTALL_DIR}/lib/*${STATIC_LIB_EXT}"
-    "${SUPERBUILD_INSTALL_DIR}/${DEST_LIB_DIR}/${LIB_PREFIX}itk*${LIB_EXT}*"
-    "${SUPERBUILD_INSTALL_DIR}/${DEST_LIB_DIR}/${LIB_PREFIX}ITK*${LIB_EXT}*"
-    )
-    foreach(MY_LIB_FILE ${MY_LIB_FILES})
-    set(a_symlink FALSE)
-     isfile_symlink("${itk_lib_file}" a_symlink itk_lib_file_target)
-     if(a_symlink)
-          #TODO
-     else()
-       install_rule(${MY_LIB_FILE})
-       #list(APPEND file_list "${itk_lib_file}")
-     endif()
+    # file(GLOB MY_LIB_FILES 
+    # "${SUPERBUILD_INSTALL_DIR}/lib/*${STATIC_LIB_EXT}"
+    # "${SUPERBUILD_INSTALL_DIR}/${DEST_LIB_DIR}/${LIB_PREFIX}itk*${LIB_EXT}*"
+    # "${SUPERBUILD_INSTALL_DIR}/${DEST_LIB_DIR}/${LIB_PREFIX}ITK*${LIB_EXT}*"
+    # )
+    # foreach(MY_LIB_FILE ${MY_LIB_FILES})
+    # set(a_symlink FALSE)
+    #  isfile_symlink("${itk_lib_file}" a_symlink itk_lib_file_target)
+    #  if(a_symlink)
+    #       #TODO
+    #  else()
+    #    install_rule(${MY_LIB_FILE})
+    #    #list(APPEND file_list "${itk_lib_file}")
+    #  endif()
      
       #get_filename_component(MY_LIB_FILE_name ${MY_LIB_FILE} NAME)
       #list(APPEND file_list "${MY_LIB_FILE_name}")
     
-    endforeach()  
+    #endforeach()  
 
     #RK: there is a bug in itk cmake files in install tree 
     #we workaround with below code
