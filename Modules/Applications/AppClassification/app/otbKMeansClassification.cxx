d695331f9d82aed685d83caa4a92d9f7132d39c4
Marina Bertolino <marina.bertolino@c-s.fr>
2017-08-16 10:45:46 +0200

    REFAC: move ClassKMeansBase->KMeansApplicationBase class in otbKMeansClassification.cxx

--- Modules/Applications/AppClassification/app/otbKMeansClassification.cxx
+++ Modules/Applications/AppClassification/app/otbKMeansClassification.cxx
@@ -18,19 +18,376 @@
  * limitations under the License.
  */
 
-#include "otbClassKMeansBase.h"
+#include "otbWrapperCompositeApplication.h"
+#include "otbWrapperApplicationFactory.h"
+
+#include "otbOGRDataToSamplePositionFilter.h"
 
 namespace otb
 {
 namespace Wrapper
 {
 
-class KMeansClassification: public ClassKMeansBase
+class KMeansApplicationBase : public CompositeApplication
+{
+public:
+  /** Standard class typedefs. */
+  typedef KMeansApplicationBase Self;
+  typedef CompositeApplication Superclass;
+  typedef itk::SmartPointer<Self> Pointer;
+  typedef itk::SmartPointer<const Self> ConstPointer;
+
+  /** Standard macro */
+  itkTypeMacro( KMeansApplicationBase, Superclass )
+
+protected:
+  void InitKMParams()
+  {
+    AddApplication("ImageEnvelope", "imgenvelop", "mean shift smoothing");
+    AddApplication("PolygonClassStatistics", "polystats", "Polygon Class Statistics");
+    AddApplication("SampleSelection", "select", "Sample selection");
+    AddApplication("SampleExtraction", "extraction", "Sample extraction");
+
+    AddApplication("TrainVectorClassifier", "training", "Model training");
+    AddApplication("ComputeImagesStatistics", "imgstats", "Compute Images second order statistics");
+    AddApplication("ImageClassifier", "classif", "Performs a classification of the input image");
+
+    ShareParameter("in", "imgenvelop.in");
+    ShareParameter("out", "classif.out");
+
+    InitKMSampling();
+    InitKMClassification();
+
+    // init at the end cleanup
+    AddParameter( ParameterType_Empty, "cleanup", "Temporary files cleaning" );
+    EnableParameter( "cleanup" );
+    SetParameterDescription( "cleanup",
+                           "If activated, the application will try to clean all temporary files it created" );
+    MandatoryOff( "cleanup" );
+  }
+
+  void InitKMSampling()
+  {
+    AddParameter(ParameterType_Int, "nc", "Number of classes");
+    SetParameterDescription("nc", "Number of modes, which will be used to generate class membership.");
+    SetDefaultParameterInt("nc", 5);
+
+    AddParameter(ParameterType_Int, "ts", "Training set size");
+    SetParameterDescription("ts", "Size of the training set (in pixels).");
+    SetDefaultParameterInt("ts", 100);
+    MandatoryOff("ts");
+
+    AddParameter(ParameterType_Int, "maxit", "Maximum number of iterations");
+    SetParameterDescription("maxit", "Maximum number of iterations for the learning step.");
+    SetDefaultParameterInt("maxit", 1000);
+    MandatoryOff("maxit");
+
+    AddParameter(ParameterType_OutputFilename, "outmeans", "Centroid filename");
+    SetParameterDescription("outmeans", "Output text file containing centroid positions");
+    MandatoryOff("outmeans");
+
+    ShareKMSamplingParameters();
+    ConnectKMSamplingParams();
+  }
+
+  void InitKMClassification()
+  {
+    ShareKMClassificationParams();
+    ConnectKMClassificationParams();
+  }
+
+  void ShareKMSamplingParameters()
+  {
+    ShareParameter("ram", "polystats.ram");
+    ShareParameter("sampler", "select.sampler");
+    ShareParameter("vm", "polystats.mask", "Validity Mask",
+      "Validity mask, only non-zero pixels will be used to estimate KMeans modes.");
+  }
+
+  void ShareKMClassificationParams()
+  {
+    ShareParameter("nodatalabel", "classif.nodatalabel", "Label mask value",
+      "By default, hidden pixels will have the assigned label 0 in the output image. "
+      "It's possible to define the label mask by another value, "
+      "but be careful to not take a label from another class. "
+      "This application initalize the labels from 0 to N-1, "
+      "N is the number of class (defined by 'nc' parameter).");
+  }
+
+  void ConnectKMSamplingParams()
+  {
+    Connect("polystats.in", "imgenvelop.in");
+
+    Connect("select.in", "polystats.in");
+    Connect("select.vec", "polystats.vec");
+    Connect("select.ram", "polystats.ram");
+
+    Connect("extraction.in", "select.in");
+    Connect("extraction.field", "select.field");
+    Connect("extraction.vec", "select.out");
+    Connect("extraction.ram", "polystats.ram");
+  }
+
+  void ConnectKMClassificationParams()
+  {
+    Connect("training.cfield", "extraction.field");
+    Connect("training.io.stats","imgstats.out");
+
+    Connect("classif.in", "imgenvelop.in");
+    Connect("classif.model", "training.io.out");
+    Connect("classif.ram", "polystats.ram");
+    Connect("classif.imstat", "imgstats.out");
+  }
+
+  void ConnectKMClassificationMask()
+  {
+    otbAppLogINFO("Using input mask ...");
+    Connect("select.mask", "polystats.mask");
+    Connect("classif.mask", "select.mask");
+  }
+
+  void ComputeImageEnvelope(const std::string &vectorFileName)
+  {
+    GetInternalApplication("imgenvelop")->SetParameterString("out", vectorFileName, false);
+    GetInternalApplication("imgenvelop")->ExecuteAndWriteOutput();
+  }
+
+  void ComputeAddField(const std::string &vectorFileName,
+                       const std::string &fieldName)
+  {
+    otbAppLogINFO("add field in the layer ...");
+    otb::ogr::DataSource::Pointer ogrDS;
+    ogrDS = otb::ogr::DataSource::New(vectorFileName, otb::ogr::DataSource::Modes::Update_LayerUpdate);
+    otb::ogr::Layer layer = ogrDS->GetLayer(0);
+
+    OGRFieldDefn confidenceField(fieldName.c_str(), OFTInteger);
+    confidenceField.SetWidth(confidenceField.GetWidth());
+    confidenceField.SetPrecision(confidenceField.GetPrecision());
+    ogr::FieldDefn confFieldDefn(confidenceField);
+    layer.CreateField(confFieldDefn);
+
+    // Complete field
+    layer.ogr().ResetReading();
+    otb::ogr::Feature feature = layer.ogr().GetNextFeature();
+    if(feature.addr())
+    {
+      feature.ogr().SetField(fieldName.c_str(), 0);
+      layer.SetFeature(feature);
+    }
+    const OGRErr err = layer.ogr().CommitTransaction();
+    if (err != OGRERR_NONE)
+      itkExceptionMacro(<< "Unable to commit transaction for OGR layer " << layer.ogr().GetName() << ".");
+    ogrDS->SyncToDisk();
+  }
+
+  void ComputePolygonStatistics(const std::string &statisticsFileName,
+                                const std::string &fieldName)
+  {
+    std::vector<std::string> fieldList = {fieldName};
+
+    GetInternalApplication("polystats")->SetParameterStringList("field", fieldList, false);
+    GetInternalApplication("polystats")->SetParameterString("out", statisticsFileName, false);
+
+    ExecuteInternal("polystats");
+  }
+
+  void SelectAndExtractSamples(const std::string &statisticsFileName,
+                               const std::string &fieldName,
+                               const std::string &sampleFileName,
+                               int NBSamples)
+  {
+    /* SampleSelection */
+    GetInternalApplication("select")->SetParameterString("out", sampleFileName, false);
+
+    UpdateInternalParameters("select");
+    GetInternalApplication("select")->SetParameterString("instats", statisticsFileName, false);
+    GetInternalApplication("select")->SetParameterString("field", fieldName, false);
+
+    GetInternalApplication("select")->SetParameterString("strategy", "constant", false);
+    GetInternalApplication("select")->SetParameterInt("strategy.constant.nb", NBSamples, false);
+
+    if( IsParameterEnabled("rand"))
+      GetInternalApplication("select")->SetParameterInt("rand", GetParameterInt("rand"), false);
+
+    // select sample positions
+    ExecuteInternal("select");
+
+    /* SampleExtraction */
+    UpdateInternalParameters("extraction");
+
+    GetInternalApplication("extraction")->SetParameterString("outfield", "prefix", false);
+    GetInternalApplication("extraction")->SetParameterString("outfield.prefix.name", "value_", false);
+
+    // extract sample descriptors
+    GetInternalApplication("extraction")->ExecuteAndWriteOutput();
+  }
+
+  void TrainKMModel(FloatVectorImageType *image,
+                    const std::string &sampleTrainFileName,
+                    const std::string &modelFileName)
+  {
+    std::vector<std::string> extractOutputList = {sampleTrainFileName};
+    GetInternalApplication("training")->SetParameterStringList("io.vd", extractOutputList, false);
+    UpdateInternalParameters("training");
+
+    // set field names
+    std::string selectPrefix = GetInternalApplication("extraction")->GetParameterString("outfield.prefix.name");
+    unsigned int nbBands = image->GetNumberOfComponentsPerPixel();
+    std::vector<std::string> selectedNames;
+    for( unsigned int i = 0; i < nbBands; i++ )
+      {
+      std::ostringstream oss;
+      oss << i;
+      selectedNames.push_back( selectPrefix + oss.str() );
+      }
+    GetInternalApplication("training")->SetParameterStringList("feat", selectedNames, false);
+
+    GetInternalApplication("training")->SetParameterString("classifier", "sharkkm", false);
+    GetInternalApplication("training")->SetParameterInt("classifier.sharkkm.maxiter",
+                                                        GetParameterInt("maxit"), false);
+    GetInternalApplication("training")->SetParameterInt("classifier.sharkkm.k",
+                                                        GetParameterInt("nc"), false);
+
+    if( IsParameterEnabled("rand"))
+      GetInternalApplication("training")->SetParameterInt("rand", GetParameterInt("rand"), false);
+    GetInternalApplication("training")->GetParameterByKey("v")->SetActive(false);
+
+    GetInternalApplication("training")->SetParameterString("io.out", modelFileName, false);
+
+    ExecuteInternal( "training" );
+    otbAppLogINFO("output model : " << GetInternalApplication("training")->GetParameterString("io.out"));
+  }
+
+  void ComputeImageStatistics(const std::string &imageFileName,
+                                               const std::string &imagesStatsFileName)
+  {
+    std::vector<std::string> imageFileNameList = {imageFileName};
+    GetInternalApplication("imgstats")->SetParameterStringList("il", imageFileNameList, false);
+    GetInternalApplication("imgstats")->SetParameterString("out", imagesStatsFileName, false);
+
+    ExecuteInternal( "imgstats" );
+    otbAppLogINFO("image statistics file : " << GetInternalApplication("imgstats")->GetParameterString("out"));
+  }
+
+
+  void KMeansClassif()
+  {
+    ExecuteInternal( "classif" );
+  }
+
+  void CreateOutMeansFile(FloatVectorImageType *image,
+                          const std::string &modelFileName,
+                          unsigned int nbClasses)
+  {
+    if (IsParameterEnabled("outmeans"))
+    {
+      unsigned int nbBands = image->GetNumberOfComponentsPerPixel();
+      unsigned int nbElements = nbClasses * nbBands;
+      // get the line in model file that contains the centroids positions
+      std::ifstream infile(modelFileName);
+      if(!infile)
+      {
+        itkExceptionMacro(<< "File : " << modelFileName << " couldn't be opened");
+      }
+
+      // get the end line with the centroids
+      std::string line, centroidLine;
+      while(std::getline(infile,line))
+      {
+        if (!line.empty())
+          centroidLine = line;
+      }
+
+      std::vector<std::string> centroidElm;
+      boost::split(centroidElm,centroidLine,boost::is_any_of(" "));
+
+      // remove the first elements, not the centroids positions
+      int nbWord = centroidElm.size();
+      int beginCentroid = nbWord-nbElements;
+      centroidElm.erase(centroidElm.begin(), centroidElm.begin()+beginCentroid);
+
+      // write in the output file
+      std::ofstream outfile;
+      outfile.open(GetParameterString("outmeans"));
+
+      for (unsigned int i = 0; i < nbClasses; i++)
+      {
+        for (unsigned int j = 0; j < nbBands; j++)
+        {
+          outfile << std::setw(8) << centroidElm[i * nbBands + j] << " ";
+        }
+        outfile << std::endl;
+      }
+      outfile.close();
+
+      infile.close();
+
+    }
+  }
+
+  class KMeansFileNamesHandler
+    {
+    public :
+      KMeansFileNamesHandler(const std::string &outPath)
+      {
+        tmpVectorFile = outPath + "_imgEnvelope.shp";
+        polyStatOutput = outPath + "_polyStats.xml";
+        sampleOutput = outPath + "_sampleSelect.shp";
+        modelFile = outPath + "_model.txt";
+        imgStatOutput = outPath + "_imgstats.xml";
+      }
+
+      void clear()
+      {
+        RemoveFile(tmpVectorFile);
+        RemoveFile(polyStatOutput);
+        RemoveFile(sampleOutput);
+        RemoveFile(modelFile);
+        RemoveFile(imgStatOutput);
+      }
+
+      std::string tmpVectorFile;
+      std::string polyStatOutput;
+      std::string sampleOutput;
+      std::string modelFile;
+      std::string imgStatOutput;
+
+    private:
+      bool RemoveFile(const std::string &filePath)
+      {
+        bool res = true;
+        if( itksys::SystemTools::FileExists( filePath.c_str() ) )
+          {
+          size_t posExt = filePath.rfind( '.' );
+          if( posExt != std::string::npos && filePath.compare( posExt, std::string::npos, ".shp" ) == 0 )
+            {
+            std::string shxPath = filePath.substr( 0, posExt ) + std::string( ".shx" );
+            std::string dbfPath = filePath.substr( 0, posExt ) + std::string( ".dbf" );
+            std::string prjPath = filePath.substr( 0, posExt ) + std::string( ".prj" );
+            RemoveFile( shxPath );
+            RemoveFile( dbfPath );
+            RemoveFile( prjPath );
+            }
+          res = itksys::SystemTools::RemoveFile( filePath.c_str() );
+          if( !res )
+            {
+            //otbAppLogINFO( <<"Unable to remove file  "<<filePath );
+            }
+          }
+        return res;
+      }
+
+    };
+
+};
+
+
+class KMeansClassification: public KMeansApplicationBase
 {
 public:
   /** Standard class typedefs. */
   typedef KMeansClassification Self;
-  typedef ClassKMeansBase Superclass;
+  typedef KMeansApplicationBase Superclass;
   typedef itk::SmartPointer<Self> Pointer;
   typedef itk::SmartPointer<const Self> ConstPointer;
 
@@ -78,7 +435,7 @@ private:
     ClearApplications();
 
     // initialisation parameters and synchronizes parameters
-    initKMParams();
+    Superclass::InitKMParams();
 
     AddRANDParameter();
 
@@ -98,20 +455,20 @@ private:
 
   void DoExecute() ITK_OVERRIDE
   {
-    if (IsParameterEnabled("vm") && HasValue("vm")) ConnectKMClassificationMask();
+    if (IsParameterEnabled("vm") && HasValue("vm")) Superclass::ConnectKMClassificationMask();
 
     KMeansFileNamesHandler fileNames(GetParameterString("out"));
 
     const std::string fieldName = "field";
 
     // Create an image envelope
-    ComputeImageEnvelope(fileNames.tmpVectorFile);
+    Superclass::ComputeImageEnvelope(fileNames.tmpVectorFile);
     // Add a new field at the ImageEnvelope output file
-    ComputeAddField(fileNames.tmpVectorFile, fieldName);
+    Superclass::ComputeAddField(fileNames.tmpVectorFile, fieldName);
 
     // Compute PolygonStatistics app
     UpdateKMPolygonClassStatisticsParameters(fileNames.tmpVectorFile);
-    ComputePolygonStatistics(fileNames.polyStatOutput, fieldName);
+    Superclass::ComputePolygonStatistics(fileNames.polyStatOutput, fieldName);
 
     // Compute number of sample max for KMeans
     const int theoricNBSamplesForKMeans = GetParameterInt("ts");
@@ -122,22 +479,22 @@ private:
                   << std::endl);
 
     // Compute SampleSelection and SampleExtraction app
-    SelectAndExtractSamples(fileNames.polyStatOutput, fieldName,
-                            fileNames.sampleOutput,
-                            actualNBSamplesForKMeans);
+    Superclass::SelectAndExtractSamples(fileNames.polyStatOutput, fieldName,
+                                        fileNames.sampleOutput,
+                                        actualNBSamplesForKMeans);
 
     // Compute Images second order statistics
-    ComputeImageStatistics(GetParameterString("in"), fileNames.imgStatOutput);
+    Superclass::ComputeImageStatistics(GetParameterString("in"), fileNames.imgStatOutput);
 
     // Compute a train model with TrainVectorClassifier app
-    TrainKMModel(GetParameterImage("in"), fileNames.sampleOutput,
-                 fileNames.modelFile);
+    Superclass::TrainKMModel(GetParameterImage("in"), fileNames.sampleOutput,
+                             fileNames.modelFile);
 
     // Compute a classification of the input image according to a model file
-    KMeansClassif();
+    Superclass::KMeansClassif();
 
     // Create the output text file containing centroids positions
-    CreateOutMeansFile(GetParameterImage("in"), fileNames.modelFile, GetParameterInt("nc"));
+    Superclass::CreateOutMeansFile(GetParameterImage("in"), fileNames.modelFile, GetParameterInt("nc"));
 
     // Remove all tempory files
     if( IsParameterEnabled( "cleanup" ) )
@@ -147,7 +504,6 @@ private:
       }
   }
 
-private :
   void UpdateKMPolygonClassStatisticsParameters(const std::string &vectorFileName)
   {
     GetInternalApplication( "polystats" )->SetParameterString( "vec", vectorFileName, false );
