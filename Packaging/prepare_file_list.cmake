21d4cb7dfb32c6c016869649d70f730462088a23
Rashad Kanavath <rashad.kanavath@c-s.fr>
2017-08-16 10:18:17 +0200

    STYLE: indent-region

--- Packaging/prepare_file_list.cmake
+++ Packaging/prepare_file_list.cmake
@@ -24,10 +24,10 @@ function(prepare_file_list file_list_result)
   if(HAVE_PYTHON)
     list(APPEND file_list "_otbApplication${PYMODULE_EXT}")
   endif()
-    
+  
   foreach(exe_file "iceViewer" "otbTestDriver" "SharkVersion")
     if(EXISTS "${SUPERBUILD_INSTALL_DIR}/bin/${exe_file}${EXE_EXT}")
-        list(APPEND file_list "${exe_file}${EXE_EXT}")
+      list(APPEND file_list "${exe_file}${EXE_EXT}")
     else()
       message(STATUS "${exe_file}${EXE_EXT} not found in ${SUPERBUILD_INSTALL_DIR}/bin. (skipping)")
     endif()
@@ -44,39 +44,37 @@ function(prepare_file_list file_list_result)
     list(APPEND file_list "cs2cs${EXE_EXT}")
   endif()
   
+  file(GLOB otb_test_exe_list 
+    "${SUPERBUILD_INSTALL_DIR}/bin/gdal*${EXE_EXT}"
+    "${OTB_BINARY_DIR}/bin/*[T|t]est*${EXE_EXT}"
+    )
 
-   
-   file(GLOB otb_test_exe_list 
-     "${SUPERBUILD_INSTALL_DIR}/bin/gdal*${EXE_EXT}"
-     "${OTB_BINARY_DIR}/bin/*[T|t]est*${EXE_EXT}"
-     )
-
-  #special items to be removed!
-   list(REMOVE_ITEM otb_test_exe_list
-     "${SUPERBUILD_INSTALL_DIR}/bin/gdal-config"
-     )
-   list(REMOVE_ITEM otb_test_exe_list
-     "${OTB_BINARY_DIR}/bin/otbcli_TestApplication")
-
-   list(REMOVE_ITEM otb_test_exe_list
-     "${OTB_BINARY_DIR}/bin/otbgui_TestApplication")
-
-   foreach(otb_test_exe   ${otb_test_exe_list})
-     get_filename_component(otb_test_exe_name ${otb_test_exe} NAME)
-     list(APPEND file_list ${otb_test_exe_name})
-   endforeach()
-
-   # special case for msvc: ucrtbase.dll must be explicitly vetted.
-   # for proj.dll, see Mantis-1424
-   if(MSVC)
-     list(APPEND file_list "ucrtbase.dll")
-     list(APPEND file_list "proj.dll")
-   endif()  
+  #special items to be removed!. hackety-hack
+  list(REMOVE_ITEM otb_test_exe_list
+    "${SUPERBUILD_INSTALL_DIR}/bin/gdal-config"
+    )
+  list(REMOVE_ITEM otb_test_exe_list
+    "${OTB_BINARY_DIR}/bin/otbcli_TestApplication")
+  
+  list(REMOVE_ITEM otb_test_exe_list
+    "${OTB_BINARY_DIR}/bin/otbgui_TestApplication")
+  
+  foreach(otb_test_exe   ${otb_test_exe_list})
+    get_filename_component(otb_test_exe_name ${otb_test_exe} NAME)
+    list(APPEND file_list ${otb_test_exe_name})
+  endforeach()
+  
+  # special case for msvc: ucrtbase.dll must be explicitly vetted.
+  # for proj.dll, see Mantis-1424
+  if(MSVC)
+    list(APPEND file_list "ucrtbase.dll")
+    list(APPEND file_list "proj.dll")
+  endif()  
 
   set(otb_applications_dir "${SUPERBUILD_INSTALL_DIR}/lib/otb/applications")
   file(GLOB OTB_APPS_LIST "${otb_applications_dir}/otbapp_*${LIB_EXT}") # /lib/otb
   list(APPEND  file_list ${OTB_APPS_LIST})
 
 
-    set(${file_list_result} ${file_list} PARENT_SCOPE)
+  set(${file_list_result} ${file_list} PARENT_SCOPE)
 endfunction()
