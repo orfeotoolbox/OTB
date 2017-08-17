417fbb50a92ea61b42d4c3b0fbfe66e59cf328da
Rashad Kanavath <rashad.kanavath@c-s.fr>
2017-08-17 11:38:04 +0200

    BUG: windows need all .lib files for devel.

--- Packaging/install_importlibs.cmake
+++ Packaging/install_importlibs.cmake
@@ -5,37 +5,18 @@ function(install_importlibs)
     return()
   endif()
 
-  set(search_regex  "..IMPORTED_IMPLIB_([A-Z]+)...{_IMPORT_PREFIX}\/lib\/")
-  if(NOT WIN32)
-    set(search_regex    "..IMPORTED_LOCATION_([A-Z]+)...{_IMPORT_PREFIX}\/lib\/")
-  endif()
-
-  file(GLOB_RECURSE
-    target_cmake_files
-    "${SUPERBUILD_INSTALL_DIR}/lib/cmake/ITK*/*Targets-*.*"
-    "${SUPERBUILD_INSTALL_DIR}/lib/cmake/OTB*/*Targets-*.*")
-
-  set(lib_files)
-  foreach( target_cmake_file ${target_cmake_files})
-    if(PKG_DEBUG)
-      message("checking IMPORTED_IMPLIB_RELEASE in ${target_cmake_file}")
-    endif()
-    file(STRINGS "${target_cmake_file}" matched_items REGEX "${search_regex}")
-    foreach(matched_item ${matched_items})
-      if(matched_item MATCHES "otbapp_")
-	continue()
-      endif()
-      string(REGEX REPLACE "${search_regex}" "" lib_file "${matched_item}")
-      string(REGEX REPLACE "\"" "" lib_file "${lib_file}")
-      list(APPEND lib_files "${SUPERBUILD_INSTALL_DIR}/lib/${lib_file}")
-    endforeach()
-  endforeach()
-
-  foreach(lib_file ${lib_files})
-    if(PKG_DEBUG)
-      message("SPECIAL install_rule for lib_file: ${lib_file}")
-    endif()
-    install_rule(${lib_file})
+  #ALL .lib files are required for building
+  #eg: gdal.dll will have gdal_i.lib which is
+  #needed when you do cmake configure for projects
+  #using OTB. Hence the *.lib regex is non optional
+  #and cannot be optimized at any level.
+  #Only thing you can control is to create only those
+  #required .lib files in install directory. This exclude
+  #all otb*.*testdriver.lib etc..
+  file(GLOB import_lib_files 
+    "${SUPERBUILD_INSTALL_DIR}/lib/*.lib")
+  foreach( import_lib_file ${import_lib_files})
+    install_rule(${import_lib_file})
   endforeach()
 
 
