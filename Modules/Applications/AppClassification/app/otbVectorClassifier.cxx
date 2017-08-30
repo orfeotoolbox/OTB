7d8e959c4e2597f2018acde67ea7ea9ca3fd6e39
Julien Michel <julien.michel@cnes.fr>
2017-08-30 15:53:09 +0200

    BUG: Fixing bug #1448

--- Modules/Applications/AppClassification/app/otbVectorClassifier.cxx
+++ Modules/Applications/AppClassification/app/otbVectorClassifier.cxx
@@ -196,7 +196,7 @@ private:
 
     const int nbFeatures = GetSelectedItems("feat").size();
     input->SetMeasurementVectorSize(nbFeatures);
-
+  
     otb::ogr::Layer::const_iterator it = layer.cbegin();
     otb::ogr::Layer::const_iterator itEnd = layer.cend();
     for( ; it!=itEnd ; ++it)
@@ -205,7 +205,11 @@ private:
       mv.SetSize(nbFeatures);
       for(int idx=0; idx < nbFeatures; ++idx)
         {
-        mv[idx] = (*it)[GetSelectedItems("feat")[idx]].GetValue<double>();
+        // Beware that itemIndex differs from ogr layer field index
+        unsigned int itemIndex = GetSelectedItems("feat")[idx];
+        std::string fieldName = GetChoiceNames( "feat" )[itemIndex];
+        
+        mv[idx] = (*it)[fieldName].GetValue<double>();
         }
       input->PushBack(mv);
       }
