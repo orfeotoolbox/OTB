fbe242991ce6e6e15c70d12e95f297a7f4793762
Stephane ALBERT <stephane.albert@c-s.fr>
2017-09-04 14:44:48 +0200

    ENH: Added otb::Wrapper::ParameterList.

---
+++ Modules/Wrappers/ApplicationEngine/include/otbWrapperParameterList.h
@@ -0,0 +1,81 @@
+/*
+ * Copyright (C) 2005-2017 Centre National d'Etudes Spatiales (CNES)
+ *
+ * This file is part of Orfeo Toolbox
+ *
+ *     https://www.orfeo-toolbox.org/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef otbWrapperParameterList_h
+#define otbWrapperParameterList_h
+
+
+#include "otbWrapperParameter.h"
+
+
+namespace otb
+{
+
+namespace Wrapper
+{
+
+
+/** \class ParameterList
+ *  \brief This class represents a InputImage parameter
+ *
+ * \ingroup OTBApplicationEngine
+ */
+class OTBApplicationEngine_EXPORT ParameterList : public Parameter
+{
+public:
+  /** Standard class typedef */
+  typedef ParameterList Self;
+  typedef Parameter Superclass;
+
+  typedef itk::SmartPointer< Self > Pointer;
+  typedef itk::SmartPointer< const Self > ConstPointer;
+
+  typedef itk::ImageBase< 2 > ImageBaseType;
+
+  /** Defining ::New() static method */
+  itkNewMacro( Self );
+
+  /** RTTI support */
+  itkTypeMacro( ParameterList, Parameter );
+
+
+protected:
+  /** Constructor */
+  ParameterList();
+
+  /** Destructor */
+  ~ParameterList() ITK_OVERRIDE;
+
+private:
+  ParameterList( const Parameter & ); // purposely not implemented
+  void operator = ( const Parameter & ); // purposely not implemented
+
+}; // End class InputImage Parameter
+
+
+} // End namespace Wrapper
+
+} // End namespace otb
+
+#ifndef OTB_MANUAL_INSTANTIATION
+# include "otbWrapperParameterList.txx"
+#endif
+
+#endif
