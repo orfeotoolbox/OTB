13ffd4699151cbabe4894e4ce28885983f0da7cd
Laurentiu Nicola <lnicola@dend.ro>
2017-11-21 16:19:30 +0200

    Upgrade SuperBuild GDAL and libtiff
    Closes #1

--- SuperBuild/CMake/External_tiff.cmake
+++ SuperBuild/CMake/External_tiff.cmake
@@ -12,7 +12,7 @@ if(USE_SYSTEM_TIFF)
 else()
   SETUP_SUPERBUILD(PROJECT ${proj})
   message(STATUS "  Using libtiff SuperBuild version")
-  
+
   # declare dependencies
   set(${proj}_DEPENDENCIES ZLIB)
   if(MSVC)
@@ -21,20 +21,20 @@ else()
   INCLUDE_SUPERBUILD_DEPENDENCIES(${${proj}_DEPENDENCIES})
   # set proj back to its original value
   set(proj TIFF)
-  
+
   ADD_SUPERBUILD_CONFIGURE_VAR(ZLIB_INCLUDE_DIR --with-zlib-include-dir)
-  
+
   if(MSVC)
     #is JPEG required for linux also?
-    
-    STRING(REGEX REPLACE "/$" "" CMAKE_WIN_INSTALL_PREFIX ${SB_INSTALL_PREFIX})    
-    STRING(REGEX REPLACE "/" "\\\\" CMAKE_WIN_INSTALL_PREFIX ${CMAKE_WIN_INSTALL_PREFIX})    
+
+    STRING(REGEX REPLACE "/$" "" CMAKE_WIN_INSTALL_PREFIX ${SB_INSTALL_PREFIX})
+    STRING(REGEX REPLACE "/" "\\\\" CMAKE_WIN_INSTALL_PREFIX ${CMAKE_WIN_INSTALL_PREFIX})
     configure_file(${CMAKE_SOURCE_DIR}/patches/${proj}/nmake.opt ${CMAKE_BINARY_DIR}/nmake_libtiff_extra.opt)
-    
+
      ExternalProject_Add(${proj}_build
       PREFIX ${proj}
-      URL "http://download.osgeo.org/libtiff/tiff-4.0.3.tar.gz"
-      URL_MD5 051c1068e6a0627f461948c365290410
+      URL "http://download.osgeo.org/libtiff/tiff-4.0.9.tar.gz"
+      URL_MD5 54bad211279cc93eb4fca31ba9bfdc79
       SOURCE_DIR ${TIFF_SB_SRC}
       BINARY_DIR ${TIFF_SB_SRC}
       INSTALL_DIR ${SB_INSTALL_PREFIX}
@@ -65,8 +65,8 @@ else()
   else()
     ExternalProject_Add(${proj}
       PREFIX ${proj}
-      URL "http://download.osgeo.org/libtiff/tiff-4.0.3.tar.gz"
-      URL_MD5 051c1068e6a0627f461948c365290410
+      URL "http://download.osgeo.org/libtiff/tiff-4.0.9.tar.gz"
+      URL_MD5 54bad211279cc93eb4fca31ba9bfdc79
       BINARY_DIR ${TIFF_SB_BUILD_DIR}
       INSTALL_DIR ${SB_INSTALL_PREFIX}
       DOWNLOAD_DIR ${DOWNLOAD_LOCATION}
@@ -81,22 +81,22 @@ else()
       INSTALL_COMMAND $(MAKE) install
       DEPENDS ${${proj}_DEPENDENCIES}
       )
-    
+
     ExternalProject_Add_Step(${proj} copy_source
-      COMMAND ${CMAKE_COMMAND} -E copy_directory 
+      COMMAND ${CMAKE_COMMAND} -E copy_directory
         ${TIFF_SB_SRC} ${TIFF_SB_BUILD_DIR}
       DEPENDEES patch update
       DEPENDERS configure
     )
-    
+
   endif()
-  
+
   set(_SB_${proj}_INCLUDE_DIR ${SB_INSTALL_PREFIX}/include)
   if(WIN32)
     set(_SB_${proj}_LIBRARY ${SB_INSTALL_PREFIX}/lib/libtiff_i.lib)
   elseif(UNIX)
     set(_SB_${proj}_LIBRARY ${SB_INSTALL_PREFIX}/lib/libtiff${CMAKE_SHARED_LIBRARY_SUFFIX})
   endif()
-  
+
 endif()
 endif()
