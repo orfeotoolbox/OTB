ee51b5875cf5c486414eb85eda6427e41f98b861
Guillaume Pasero <guillaume.pasero@c-s.fr>
2017-12-12 18:58:14 +0100

    ENH: move all MachineLearningModelFactoryBase to LearningBase

--- Modules/Learning/LearningBase/otb-module.cmake
+++ Modules/Learning/LearningBase/otb-module.cmake
@@ -22,6 +22,7 @@ set(DOCUMENTATION "This module contains OTB generic Machine Learning framework
 mainly based on OpenCV.")
 
 otb_module(OTBLearningBase
+  ENABLE_SHARED
   DEPENDS
     OTBCommon
     OTBITK
