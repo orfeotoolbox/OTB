93ae5c2dc6500393fdb4c6beedf97e6ae449c7af
Yannick TANGUY <tanguyy@visu01.sis.cnes.fr>
2018-02-21 15:40:44 +0000

    Misprint in documentation

--- Modules/Applications/AppFiltering/app/otbContrastEnhancement.cxx
+++ Modules/Applications/AppFiltering/app/otbContrastEnhancement.cxx
@@ -11,7 +11,7 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreened to in writing, software
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -258,7 +258,7 @@ private:
     SetDefaultParameterFloat("mode.lum.red.coef", 0.21 );
 
     AddParameter(ParameterType_Group , "mode.lum.green" , "Green Channel" );
-    AddParameter(ParameterType_Int , "mode.lum.green.ch" , "Greenen Channel" );
+    AddParameter(ParameterType_Int , "mode.lum.green.ch" , "Green Channel" );
     SetDefaultParameterInt("mode.lum.green.ch", 1 );
     AddParameter(ParameterType_Float , "mode.lum.green.coef" ,
       "Value for luminance computation" );
