90eea80223ec5dff10475b4ae04193c7352615f2
Rashad Kanavath <rashad.kanavath@c-s.fr>
2018-04-03 16:11:09 +0200

    ENH: new module otb qgis wrapper

---
+++ Modules/Wrappers/QGIS/otb-module.cmake
@@ -0,0 +1,13 @@
+set(DOCUMENTATION "Required to generate descriptor files for QGIS processing plugin.")
+
+otb_module(OTBQgis
+  DEPENDS
+    OTBITK
+    OTBApplicationEngine
+    
+  TEST_DEPENDS
+    OTBTestKernel
+
+  DESCRIPTION
+    "${DOCUMENTATION}"
+)
