753057a6671bc747a107b22ef65413af88c33bf5
Antoine Regimbeau <antoine.regimbeau@c-s.fr>
2018-04-10 11:44:35 +0200

    SuperBuild: change QT5 build directory

--- SuperBuild/CMake/External_qt5.cmake
+++ SuperBuild/CMake/External_qt5.cmake
@@ -180,7 +180,7 @@ configure_file( ${QT5_CONFIGURE_COMMAND_IN} ${QT5_CONFIGURE_COMMAND} @ONLY )
     PREFIX QT5
     URL "http://download.qt.io/official_releases/qt/5.10/5.10.1/single/qt-everywhere-src-5.10.1.tar.xz"
     URL_MD5 7e167b9617e7bd64012daaacb85477af
-    BINARY_DIR ${QT5_SB_SRC}
+    BINARY_DIR ${QT5_SB_BUILD_DIR}
     INSTALL_DIR ${SB_INSTALL_PREFIX}
     DOWNLOAD_DIR ${DOWNLOAD_LOCATION}
     CONFIGURE_COMMAND ${QT5_CONFIGURE_COMMAND}
