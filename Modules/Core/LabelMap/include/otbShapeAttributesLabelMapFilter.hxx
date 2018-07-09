72cb66ce19854f8fb3f2d68ac8d48d95d5a396b7
Laurentiu Nicola <lnicola@dend.ro>
2018-07-09 12:13:46 +0300

    ENH: Use std::complex instead of vcl_complex
    find \( -name "*.hxx" -or -name "*.h" -or -name "*.cxx" \) -not -path "*ThirdParty*" -print0 | xargs -0 sed -sri "s/vcl_complex/std::complex/g"

--- Modules/Core/LabelMap/include/otbShapeAttributesLabelMapFilter.hxx
+++ Modules/Core/LabelMap/include/otbShapeAttributesLabelMapFilter.hxx
@@ -442,7 +442,7 @@ ShapeAttributesLabelObjectFunctor<TLabelObject, TLabelImage>
   // Add a final reflection if needed for a proper rotation,
   // by multiplying the last row by the determinant
   vnl_real_eigensystem eigenrot(principalAxes.GetVnlMatrix());
-  vnl_diag_matrix<vcl_complex<double> > eigenval = eigenrot.D;
+  vnl_diag_matrix<std::complex<double> > eigenval = eigenrot.D;
   std::complex<double> det(1.0, 0.0);
 
   for (DimensionType i = 0; i < LabelObjectType::ImageDimension; ++i)
--- Modules/Core/LabelMap/include/otbStatisticsAttributesLabelMapFilter.hxx
+++ Modules/Core/LabelMap/include/otbStatisticsAttributesLabelMapFilter.hxx
@@ -225,7 +225,7 @@ StatisticsAttributesLabelObjectFunctor<TLabelObject, TFeatureImage>
       // Add a final reflection if needed for a proper rotation,
       // by multiplying the last row by the determinant
       vnl_real_eigensystem eigenrot(principalAxes.GetVnlMatrix());
-      vnl_diag_matrix<vcl_complex<double> > eigenval = eigenrot.D;
+      vnl_diag_matrix<std::complex<double> > eigenval = eigenrot.D;
       std::complex<double> det(1.0, 0.0);
 
       for (unsigned int i = 0; i < TFeatureImage::ImageDimension; ++i)
