6b7463a0b3b57e4c9fb4fe9bd46740ca61c8b236
Antoine Regimbeau <antoine.regimbeau@c-s.fr>
2018-08-08 17:22:52 +0200

    DEP: remove test

--- Modules/IO/IOTileMap/test/otbIOTileMapTestDriver.cxx
+++ Modules/IO/IOTileMap/test/otbIOTileMapTestDriver.cxx
@@ -24,6 +24,6 @@ void RegisterTests()
 {
   REGISTER_TEST(otbTileMapImageIOHelperNew);
   REGISTER_TEST(otbTileMapImageIOHelperTest);
-  REGISTER_TEST(otbTileMapImageIOTest);
-  REGISTER_TEST(otbTileMapWriter);
+  // REGISTER_TEST(otbTileMapImageIOTest);
+  // REGISTER_TEST(otbTileMapWriter);
 }
