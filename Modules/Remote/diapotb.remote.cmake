0d8ca6c0b6b3692dc40437f765db707312d903db
Guillaume Pasero <guillaume.pasero@c-s.fr>
2018-11-19 17:18:57 +0100

    Remote: freeze diapotb version for release 6.6.1

--- Modules/Remote/diapotb.remote.cmake
+++ Modules/Remote/diapotb.remote.cmake
@@ -2,5 +2,5 @@
 otb_fetch_module(DiapOTBModule
   "OTB module for SAR processing in Diapason."
   GIT_REPOSITORY https://gitlab.orfeo-toolbox.org/remote_modules/diapotb.git
-  GIT_TAG master
+  GIT_TAG 6054f46baaf9589b3e510931568bdca750500250
 )
