c6ed48f5e20d059727b199fa520618f6e92bdb6d
Guillaume Pasero <guillaume.pasero@c-s.fr>
2019-02-14 18:06:54 +0100

    COMP: add export macros for FillIn/FillOut

--- Modules/Filtering/ImageManipulation/include/otbConvertTypeFunctor.h
+++ Modules/Filtering/ImageManipulation/include/otbConvertTypeFunctor.h
@@ -28,6 +28,7 @@
 
 #include "itkNumericTraits.h"
 #include "otbDefaultConvertPixelTraits.h"
+#include "OTBImageManipulationExport.h"
 
 namespace otb
 {
@@ -38,7 +39,7 @@ namespace Functor
 // Fill-in section
 template <class PixelType , class InternalPixelType = PixelType,
           std::enable_if_t < std::is_arithmetic < PixelType > ::value  , int > = 0 >
-void FillIn( unsigned int i ,
+void OTBImageManipulation_EXPORT_TEMPLATE FillIn( unsigned int i ,
              PixelType const & pix ,
              std::vector < double > & vPix )
 {
@@ -48,7 +49,7 @@ void FillIn( unsigned int i ,
 
 template <class PixelType, class InternalPixelType = PixelType,
           std::enable_if_t < boost::is_complex < PixelType > :: value , int > = 0 >
-void FillIn( unsigned int i ,
+void OTBImageManipulation_EXPORT_TEMPLATE FillIn( unsigned int i ,
              PixelType const & pix ,
              std::vector < double > & vPix )
 {
@@ -61,7 +62,7 @@ void FillIn( unsigned int i ,
 template <class PixelType , class InternalPixelType = PixelType,
           std::enable_if_t <  !( boost::is_complex < PixelType > :: value 
                                  || std::is_arithmetic < PixelType > ::value ) , int > = 0 > 
-void FillIn( unsigned int i ,
+void OTBImageManipulation_EXPORT_TEMPLATE FillIn( unsigned int i ,
              PixelType const & pix ,
              std::vector < double > & vPix )
 {
@@ -69,32 +70,32 @@ void FillIn( unsigned int i ,
   FillIn < PixelType, InternalType  > ( i , pix , vPix );
 }
 
-extern template void FillIn<unsigned char>(unsigned int i, unsigned char const & pix, std::vector < double > & vPix);
-extern template void FillIn<short>(unsigned int i, short const & pix, std::vector < double > & vPix);
-extern template void FillIn<unsigned short>(unsigned int i, unsigned short const & pix, std::vector < double > & vPix);
-extern template void FillIn<int>(unsigned int i, int const & pix, std::vector < double > & vPix);
-extern template void FillIn<unsigned int>(unsigned int i, unsigned int const & pix, std::vector < double > & vPix);
-extern template void FillIn<float>(unsigned int i, float const & pix, std::vector < double > & vPix);
-extern template void FillIn<double>(unsigned int i, double const & pix, std::vector < double > & vPix);
+extern template void OTBImageManipulation_EXPORT_TEMPLATE FillIn<unsigned char>(unsigned int i, unsigned char const & pix, std::vector < double > & vPix);
+extern template void OTBImageManipulation_EXPORT_TEMPLATE FillIn<short>(unsigned int i, short const & pix, std::vector < double > & vPix);
+extern template void OTBImageManipulation_EXPORT_TEMPLATE FillIn<unsigned short>(unsigned int i, unsigned short const & pix, std::vector < double > & vPix);
+extern template void OTBImageManipulation_EXPORT_TEMPLATE FillIn<int>(unsigned int i, int const & pix, std::vector < double > & vPix);
+extern template void OTBImageManipulation_EXPORT_TEMPLATE FillIn<unsigned int>(unsigned int i, unsigned int const & pix, std::vector < double > & vPix);
+extern template void OTBImageManipulation_EXPORT_TEMPLATE FillIn<float>(unsigned int i, float const & pix, std::vector < double > & vPix);
+extern template void OTBImageManipulation_EXPORT_TEMPLATE FillIn<double>(unsigned int i, double const & pix, std::vector < double > & vPix);
 
-extern template void FillIn<itk::VariableLengthVector<unsigned char> >(unsigned int i, itk::VariableLengthVector<unsigned char> const & pix, std::vector < double > & vPix);
-extern template void FillIn<itk::VariableLengthVector<short>>(unsigned int i, itk::VariableLengthVector<short> const & pix, std::vector < double > & vPix);
-extern template void FillIn<itk::VariableLengthVector<unsigned short>>(unsigned int i, itk::VariableLengthVector<unsigned short> const & pix, std::vector < double > & vPix);
-extern template void FillIn<itk::VariableLengthVector<int>>(unsigned int i, itk::VariableLengthVector<int> const & pix, std::vector < double > & vPix);
-extern template void FillIn<itk::VariableLengthVector<unsigned int>>(unsigned int i, itk::VariableLengthVector<unsigned int> const & pix, std::vector < double > & vPix);
-extern template void FillIn<itk::VariableLengthVector<float>>(unsigned int i, itk::VariableLengthVector<float> const & pix, std::vector < double > & vPix);
-extern template void FillIn<itk::VariableLengthVector<double>>(unsigned int i, itk::VariableLengthVector<double> const & pix, std::vector < double > & vPix);
+extern template void OTBImageManipulation_EXPORT_TEMPLATE FillIn<itk::VariableLengthVector<unsigned char> >(unsigned int i, itk::VariableLengthVector<unsigned char> const & pix, std::vector < double > & vPix);
+extern template void OTBImageManipulation_EXPORT_TEMPLATE FillIn<itk::VariableLengthVector<short>>(unsigned int i, itk::VariableLengthVector<short> const & pix, std::vector < double > & vPix);
+extern template void OTBImageManipulation_EXPORT_TEMPLATE FillIn<itk::VariableLengthVector<unsigned short>>(unsigned int i, itk::VariableLengthVector<unsigned short> const & pix, std::vector < double > & vPix);
+extern template void OTBImageManipulation_EXPORT_TEMPLATE FillIn<itk::VariableLengthVector<int>>(unsigned int i, itk::VariableLengthVector<int> const & pix, std::vector < double > & vPix);
+extern template void OTBImageManipulation_EXPORT_TEMPLATE FillIn<itk::VariableLengthVector<unsigned int>>(unsigned int i, itk::VariableLengthVector<unsigned int> const & pix, std::vector < double > & vPix);
+extern template void OTBImageManipulation_EXPORT_TEMPLATE FillIn<itk::VariableLengthVector<float>>(unsigned int i, itk::VariableLengthVector<float> const & pix, std::vector < double > & vPix);
+extern template void OTBImageManipulation_EXPORT_TEMPLATE FillIn<itk::VariableLengthVector<double>>(unsigned int i, itk::VariableLengthVector<double> const & pix, std::vector < double > & vPix);
 
 
-extern template void FillIn<std::complex<short> >(unsigned int i, std::complex<short> const & pix, std::vector < double > & vPix);
-extern template void FillIn<std::complex<int> >(unsigned int i, std::complex<int> const & pix, std::vector < double > & vPix);
-extern template void FillIn<std::complex<float> >(unsigned int i, std::complex<float> const & pix, std::vector < double > & vPix);
-extern template void FillIn<std::complex<double> >(unsigned int i, std::complex<double> const & pix, std::vector < double > & vPix);
+extern template void OTBImageManipulation_EXPORT_TEMPLATE FillIn<std::complex<short> >(unsigned int i, std::complex<short> const & pix, std::vector < double > & vPix);
+extern template void OTBImageManipulation_EXPORT_TEMPLATE FillIn<std::complex<int> >(unsigned int i, std::complex<int> const & pix, std::vector < double > & vPix);
+extern template void OTBImageManipulation_EXPORT_TEMPLATE FillIn<std::complex<float> >(unsigned int i, std::complex<float> const & pix, std::vector < double > & vPix);
+extern template void OTBImageManipulation_EXPORT_TEMPLATE FillIn<std::complex<double> >(unsigned int i, std::complex<double> const & pix, std::vector < double > & vPix);
 
 // Fill-out section
 template <class PixelType , class InternalPixelType = PixelType,
           std::enable_if_t < std::is_arithmetic < PixelType > ::value  , int > = 0 >
-void FillOut( unsigned int i ,
+void OTBImageManipulation_EXPORT_TEMPLATE FillOut( unsigned int i ,
               PixelType & pix ,
               std::vector < double > & vPix )
 {
@@ -104,7 +105,7 @@ void FillOut( unsigned int i ,
 
 template <class PixelType , class InternalPixelType = PixelType,
           std::enable_if_t < boost::is_complex < PixelType > :: value , int > = 0 >
-void FillOut( unsigned int i ,
+void OTBImageManipulation_EXPORT_TEMPLATE FillOut( unsigned int i ,
               PixelType & pix ,
               std::vector < double > & vPix )
 {
@@ -116,7 +117,7 @@ void FillOut( unsigned int i ,
 template <class PixelType , class InternalPixelType = PixelType,
           std::enable_if_t <  !( boost::is_complex < PixelType > :: value 
                                  || std::is_arithmetic < PixelType > ::value ) , int > = 0 > 
-void FillOut( unsigned int i ,
+void OTBImageManipulation_EXPORT_TEMPLATE FillOut( unsigned int i ,
               PixelType & pix ,
               std::vector < double > & vPix )
 {
@@ -124,27 +125,27 @@ void FillOut( unsigned int i ,
   FillOut < PixelType, InternalType > ( i , pix , vPix );
 }
 
-extern template void FillOut<unsigned char>(unsigned int i, unsigned char & pix, std::vector < double > & vPix);
-extern template void FillOut<short>(unsigned int i, short & pix, std::vector < double > & vPix);
-extern template void FillOut<unsigned short>(unsigned int i, unsigned short & pix, std::vector < double > & vPix);
-extern template void FillOut<int>(unsigned int i, int & pix, std::vector < double > & vPix);
-extern template void FillOut<unsigned int>(unsigned int i, unsigned int & pix, std::vector < double > & vPix);
-extern template void FillOut<float>(unsigned int i, float & pix, std::vector < double > & vPix);
-extern template void FillOut<double>(unsigned int i, double & pix, std::vector < double > & vPix);
+extern template void OTBImageManipulation_EXPORT_TEMPLATE FillOut<unsigned char>(unsigned int i, unsigned char & pix, std::vector < double > & vPix);
+extern template void OTBImageManipulation_EXPORT_TEMPLATE FillOut<short>(unsigned int i, short & pix, std::vector < double > & vPix);
+extern template void OTBImageManipulation_EXPORT_TEMPLATE FillOut<unsigned short>(unsigned int i, unsigned short & pix, std::vector < double > & vPix);
+extern template void OTBImageManipulation_EXPORT_TEMPLATE FillOut<int>(unsigned int i, int & pix, std::vector < double > & vPix);
+extern template void OTBImageManipulation_EXPORT_TEMPLATE FillOut<unsigned int>(unsigned int i, unsigned int & pix, std::vector < double > & vPix);
+extern template void OTBImageManipulation_EXPORT_TEMPLATE FillOut<float>(unsigned int i, float & pix, std::vector < double > & vPix);
+extern template void OTBImageManipulation_EXPORT_TEMPLATE FillOut<double>(unsigned int i, double & pix, std::vector < double > & vPix);
 
-extern template void FillOut<itk::VariableLengthVector<unsigned char> >(unsigned int i, itk::VariableLengthVector<unsigned char> & pix, std::vector < double > & vPix);
-extern template void FillOut<itk::VariableLengthVector<short>>(unsigned int i, itk::VariableLengthVector<short> & pix, std::vector < double > & vPix);
-extern template void FillOut<itk::VariableLengthVector<unsigned short>>(unsigned int i, itk::VariableLengthVector<unsigned short> & pix, std::vector < double > & vPix);
-extern template void FillOut<itk::VariableLengthVector<int>>(unsigned int i, itk::VariableLengthVector<int> & pix, std::vector < double > & vPix);
-extern template void FillOut<itk::VariableLengthVector<unsigned int>>(unsigned int i, itk::VariableLengthVector<unsigned int> & pix, std::vector < double > & vPix);
-extern template void FillOut<itk::VariableLengthVector<float>>(unsigned int i, itk::VariableLengthVector<float> & pix, std::vector < double > & vPix);
-extern template void FillOut<itk::VariableLengthVector<double>>(unsigned int i, itk::VariableLengthVector<double> & pix, std::vector < double > & vPix);
+extern template void OTBImageManipulation_EXPORT_TEMPLATE FillOut<itk::VariableLengthVector<unsigned char> >(unsigned int i, itk::VariableLengthVector<unsigned char> & pix, std::vector < double > & vPix);
+extern template void OTBImageManipulation_EXPORT_TEMPLATE FillOut<itk::VariableLengthVector<short>>(unsigned int i, itk::VariableLengthVector<short> & pix, std::vector < double > & vPix);
+extern template void OTBImageManipulation_EXPORT_TEMPLATE FillOut<itk::VariableLengthVector<unsigned short>>(unsigned int i, itk::VariableLengthVector<unsigned short> & pix, std::vector < double > & vPix);
+extern template void OTBImageManipulation_EXPORT_TEMPLATE FillOut<itk::VariableLengthVector<int>>(unsigned int i, itk::VariableLengthVector<int> & pix, std::vector < double > & vPix);
+extern template void OTBImageManipulation_EXPORT_TEMPLATE FillOut<itk::VariableLengthVector<unsigned int>>(unsigned int i, itk::VariableLengthVector<unsigned int> & pix, std::vector < double > & vPix);
+extern template void OTBImageManipulation_EXPORT_TEMPLATE FillOut<itk::VariableLengthVector<float>>(unsigned int i, itk::VariableLengthVector<float> & pix, std::vector < double > & vPix);
+extern template void OTBImageManipulation_EXPORT_TEMPLATE FillOut<itk::VariableLengthVector<double>>(unsigned int i, itk::VariableLengthVector<double> & pix, std::vector < double > & vPix);
 
 
-extern template void FillOut<std::complex<short> >(unsigned int i, std::complex<short> & pix, std::vector < double > & vPix);
-extern template void FillOut<std::complex<int> >(unsigned int i, std::complex<int> & pix, std::vector < double > & vPix);
-extern template void FillOut<std::complex<float> >(unsigned int i, std::complex<float> & pix, std::vector < double > & vPix);
-extern template void FillOut<std::complex<double> >(unsigned int i, std::complex<double> & pix, std::vector < double > & vPix);
+extern template void OTBImageManipulation_EXPORT_TEMPLATE FillOut<std::complex<short> >(unsigned int i, std::complex<short> & pix, std::vector < double > & vPix);
+extern template void OTBImageManipulation_EXPORT_TEMPLATE FillOut<std::complex<int> >(unsigned int i, std::complex<int> & pix, std::vector < double > & vPix);
+extern template void OTBImageManipulation_EXPORT_TEMPLATE FillOut<std::complex<float> >(unsigned int i, std::complex<float> & pix, std::vector < double > & vPix);
+extern template void OTBImageManipulation_EXPORT_TEMPLATE FillOut<std::complex<double> >(unsigned int i, std::complex<double> & pix, std::vector < double > & vPix);
 
 
 
