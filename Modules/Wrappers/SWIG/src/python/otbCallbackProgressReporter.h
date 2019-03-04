85ebfb004ab5eceee9c1a69935c2607b2502ad41
ctraizet <cedric.traizet@c-s.fr>
2019-03-04 11:54:56 +0100

    ENH: added class for progress reporting (wip)

---
+++ Modules/Wrappers/SWIG/src/python/otbCallbackProgressReporter.h
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2005-2019 Centre National d'Etudes Spatiales (CNES)
+ *
+ * This file is part of Orfeo Toolbox
+ *
+ *     https://www.orfeo-toolbox.org/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef otbCallbackProgressReporter_h
+#define otbCallbackProgressReporter_h
+
+#include "otbFilterWatcherBase.h"
+#include "otbLogOutputCallback.h"
+
+namespace otb
+{
+
+class CallbackProgressReporter : public FilterWatcherBase
+{
+
+  /** Default constructor */
+  CallbackProgressReporter() = default;
+
+  /** Destructor. */
+  virtual ~CallbackProgressReporter() = default;
+
+  typedef LogOutputCallback CallbackType;
+  
+  /** Set the callback method */
+  void SetCallback(CallbackType * callback)
+    {
+    m_Callback = callback;
+    }
+  
+protected:
+  /** Callback method to show the ProgressEvent */
+  virtual void ShowProgress() override;
+
+  /** Callback method to show the StartEvent */
+  virtual void StartFilter() override;
+
+  /** Callback method to show the EndEvent */
+  virtual void EndFilter() override;
+
+private:
+  CallbackType * m_Callback; 
+  
+};
+}
+
+#endif //otbCallbackProgressReporter_h
