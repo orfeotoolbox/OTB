69db9881801070c24715291e984e7f7f913c0848
Victor Poughon <victor.poughon@cnes.fr>
2019-03-11 13:55:37 +0100

    REFAC: add noreturn attribute to TypeError function

--- Modules/Wrappers/ApplicationEngine/include/otbWrapperParameter.h
+++ Modules/Wrappers/ApplicationEngine/include/otbWrapperParameter.h
@@ -136,7 +136,7 @@ public:
   virtual ParameterType GetType() const = 0;
 
   /** Error raising function to indicate a type conversion error */
-  void TypeError(const std::string& target_type) const;
+  [[noreturn]] void TypeError(const std::string& target_type) const;
 
   /** Parameter conversion functions. They are used by WrapperApplication
    * to provide functions like SetParameterInt, GetParameterString, etc.
