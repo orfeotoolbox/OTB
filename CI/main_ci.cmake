87ff8588ef68da0e1b4a1d56f209bad691828d83
Antoine Regimbeau <antoine.regimbeau@c-s.fr>
2019-03-14 12:06:56 +0100

    CI: allow job to fail if test is failing

--- CI/main_ci.cmake
+++ CI/main_ci.cmake
@@ -84,6 +84,14 @@ if ( NOT _build_rv EQUAL 0 )
   message( SEND_ERROR "An error occurs during ctest_build.")
 endif()
 
-ctest_test(PARALLEL_LEVEL 8)
+ctest_test(PARALLEL_LEVEL 8
+           RETURN_VALUE _test_rv
+           CAPTURE_CMAKE_ERROR _test_error
+           )
+
+if ( NOT _test_rv EQUAL 0 )
+  ctest_submit()
+  message( SEND_ERROR "An error occurs during ctest_test.")
+endif()
 
 ctest_submit()
