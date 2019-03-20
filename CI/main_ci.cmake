bbcd9e6fae2a032573a7e3c904683bcbed087a71
Guillaume Pasero <guillaume.pasero@c-s.fr>
2019-03-20 19:55:18 +0100

    CI: try to enable testing

--- CI/main_ci.cmake
+++ CI/main_ci.cmake
@@ -91,15 +91,14 @@ if ( NOT _build_rv EQUAL 0 )
   message( SEND_ERROR "An error occurs during ctest_build.")
 endif()
 
-# Uncomment when ready for test
-# ctest_test(PARALLEL_LEVEL 8
-#            RETURN_VALUE _test_rv
-#            CAPTURE_CMAKE_ERROR _test_error
-#            )
+ctest_test(PARALLEL_LEVEL 8
+           RETURN_VALUE _test_rv
+           CAPTURE_CMAKE_ERROR _test_error
+           )
 
-# if ( NOT _test_rv EQUAL 0 )
-#   ctest_submit()
-#   message( SEND_ERROR "An error occurs during ctest_test.")
-# endif()
+if ( NOT _test_rv EQUAL 0 )
+  ctest_submit()
+  message( SEND_ERROR "An error occurs during ctest_test.")
+endif()
 
 ctest_submit()
