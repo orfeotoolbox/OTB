6831ace47a84be6e887510936e695eabbcacba42
Antoine Regimbeau <antoine.regimbeau@c-s.fr>
2019-03-25 17:11:37 +0100

    Add header

--- CI/cdash_handler.py
+++ CI/cdash_handler.py
@@ -1,3 +1,23 @@
+#
+# Copyright (C) 2005-2019 Centre National d'Etudes Spatiales (CNES)
+#
+# This file is part of Orfeo Toolbox
+#
+#     https://www.orfeo-toolbox.org/
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 import os.path
 import requests
 import glob
@@ -47,12 +67,6 @@ class Handler:
     "Name" in it, it might be hard to find the right one.
     """
     return 
-
-  def GetProject (self , build_dir ):
-    """
-    Could not find the project name in the configure file.
-    """
-    return
 
   def GetName (self , build_dir = ""):
     """
