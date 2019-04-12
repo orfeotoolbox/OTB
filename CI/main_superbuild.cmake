123ce04b0cf431847b7e90c8c4016e3d6d5abf5e
Antoine Regimbeau <antoine.regimbeau@c-s.fr>
2019-04-12 10:25:03 +0200

    Remove hack

--- CI/main_superbuild.cmake
+++ CI/main_superbuild.cmake
@@ -108,10 +108,6 @@ include ( "${CMAKE_CURRENT_LIST_DIR}/sb_configure_options.cmake" )
 set ( CONFIGURE_OPTIONS  
   "${CONFIGURE_OPTIONS}-DCMAKE_PREFIX_PATH=${XDK_PATH};")
 
-# Hack because there is no more superbuild available (LIBKML)
-set ( CONFIGURE_OPTIONS  
-  "${CONFIGURE_OPTIONS}-DOTB_USE_LIBKML:BOOL=OFF;" )
-
 # FIX ME this part might platform dependent
 set( GDAL_DATA "${XDK_PATH}/share/gdal" )
 set( GEOTIFF_CSV "${XDK_PATH}/share/epsg_csv" )
