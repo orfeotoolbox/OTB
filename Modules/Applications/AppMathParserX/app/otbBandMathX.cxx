cf688d3ebd38ca747b4e08996d3dea616cb21bf5
Cédric Traizet <cedric.traizet@c-s.fr>
2019-05-09 18:07:25 +0200

    DOC: replace min max by vmin vmax for neighborhood min max computation

--- Modules/Applications/AppMathParserX/app/otbBandMathX.cxx
+++ Modules/Applications/AppMathParserX/app/otbBandMathX.cxx
@@ -146,8 +146,8 @@ private:
       "  - mean\n"
       "  - var\n"
       "  - median\n"
-      "  - min\n"
-      "  - max\n"
+      "  - vmin\n"
+      "  - vmax\n"
       "  - etc.\n\n"
 
       "For instance, if ``im1`` represents the pixel of a 3 bands image::\n\n"
