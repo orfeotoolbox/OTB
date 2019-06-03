535427f926088ea9605d07afd05ff81867233a81
Rashad Kanavath <rashad.kanavath@c-s.fr>
2019-06-03 07:24:06 +0200

    SuperBuild: build boost header only windows debug

--- SuperBuild/CMake/External_boost.cmake
+++ SuperBuild/CMake/External_boost.cmake
@@ -25,37 +25,47 @@ SETUP_SUPERBUILD(BOOST)
 set(_SB_BOOST_LIBRARYDIR ${SB_INSTALL_PREFIX}/lib)
 
 set(BOOST_SB_CONFIG)
+
+if(CMAKE_BUILD_TYPE MATCHES "Debug")
+	set(BOOST_SB_CONFIG variant=debug)
+else()
+	set(BOOST_SB_CONFIG variant=release
+	--libdir=${_SB_BOOST_LIBRARYDIR}
+	--with-system
+	--with-serialization
+	--with-filesystem
+	--with-test
+	--with-date_time
+	--with-program_options
+	--with-thread)
+endif()
+
 if(OTB_TARGET_SYSTEM_ARCH_IS_X64)
-  set(BOOST_SB_CONFIG address-model=64)
+  set(BOOST_SB_CONFIG ${BOOST_SB_CONFIG} address-model=64)
+endif()
+
+ if(NOT APPLE AND "${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+    set(BOOST_SB_CONFIG ${BOOST_SB_CONFIG} toolset=clang)
+endif()
+
+if(MSVC AND MSVC_VERSION GREATER 1900)
+ set(BOOST_SB_CONFIG ${BOOST_SB_CONFIG} toolset=msvc-14.1)
 endif()
 
 set(BOOST_SB_CONFIG
   ${BOOST_SB_CONFIG}
-  variant=release
   link=shared
   threading=multi
   runtime-link=shared
   --prefix=${SB_INSTALL_PREFIX}
   --includedir=${SB_INSTALL_PREFIX}/include
-  --libdir=${_SB_BOOST_LIBRARYDIR}
-  --with-system
-  --with-serialization
-  --with-filesystem
-  --with-test
-  --with-date_time
-  --with-program_options
-  --with-thread
   )
 
 set(BOOST_BOOTSTRAP_OPTIONS "")
-
 if(UNIX)
   set(BOOST_BOOTSTRAP_FILE "./bootstrap.sh")
   set(BOOST_B2_EXE "./b2")
   if(NOT APPLE AND "${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
-    set(BOOST_SB_CONFIG
-        ${BOOST_SB_CONFIG}
-        toolset=clang)
     set(BOOST_BOOTSTRAP_OPTIONS "--with-toolset=clang")
   endif()
 else()
@@ -76,11 +86,16 @@ set(BOOST_BUILD_COMMAND ${CMAKE_COMMAND}
   install
   )
 
+if(MSVC)
+  configure_file(${CMAKE_SOURCE_DIR}/patches/BOOST/project-config.jam.in
+    ${CMAKE_BINARY_DIR}/BOOST/tmp/project-config.jam)
+endif()
+
 #NOTE: update _SB_Boost_INCLUDE_DIR below when you change version number
 ExternalProject_Add(BOOST
   PREFIX BOOST
-  URL "http://downloads.sourceforge.net/project/boost/boost/1.60.0/boost_1_60_0.tar.bz2"
-  URL_MD5 65a840e1a0b13a558ff19eeb2c4f0cbe
+  URL "https://dl.bintray.com/boostorg/release/1.69.0/source/boost_1_69_0.tar.bz2"
+  URL_MD5 65a840e1a0b13a558ff19eeb2c4f0cbe #TODO: needs update
   BINARY_DIR ${BOOST_SB_BUILD_DIR}
   INSTALL_DIR ${SB_INSTALL_PREFIX}
   DOWNLOAD_DIR ${DOWNLOAD_LOCATION}
@@ -93,10 +108,19 @@ ExternalProject_Add(BOOST
   LOG_INSTALL 1  
 )
 
+if(MSVC)
+  ExternalProject_Add_Step(BOOST add_project_jam_config
+	COMMAND ${CMAKE_COMMAND} -E copy
+	${CMAKE_BINARY_DIR}/BOOST/tmp/project-config.jam
+	${BOOST_SB_SRC}
+	DEPENDEES patch
+	DEPENDERS configure)
+endif()
+
 #HINT: avoid all uses of  _SB_* in External_<project>.cmake
 # and depend on much saner CMAKE_PREFIX_PATH for cmake projects.
 if(MSVC)
-  set(_SB_Boost_INCLUDE_DIR ${SB_INSTALL_PREFIX}/include/boost-1_60)
+  set(_SB_Boost_INCLUDE_DIR ${SB_INSTALL_PREFIX}/include/boost-1_69)
 else()
   set(_SB_Boost_INCLUDE_DIR ${SB_INSTALL_PREFIX}/include)
 endif()
