4bb5d92a0fc4d53247c1cfa02b048b339340bf29
Rashad Kanavath <rashad.kanavath@c-s.fr>
2019-06-03 11:45:18 +0200

    WIP: add prefix to look like a hex

--- Modules/Segmentation/Conversion/include/otbLabelImageToVectorDataFilter.hxx
+++ Modules/Segmentation/Conversion/include/otbLabelImageToVectorDataFilter.hxx
@@ -144,7 +144,7 @@ LabelImageToVectorDataFilter<TInputImage, TPrecision>
     // integer make us pointing to an non allowed memory block => Crash.
     std::ostringstream stream;
     stream << "MEM:::"
-           <<  "DATAPOINTER=" << (unsigned long)(GUIntBig)(this->GetInput()->GetBufferPointer()) << ","
+           <<  "DATAPOINTER=0x" << this->GetInput()->GetBufferPointer() << ","
            <<  "PIXELS=" << size[0] << ","
            <<  "LINES=" << size[1] << ","
            <<  "BANDS=" << nbBands << ","
