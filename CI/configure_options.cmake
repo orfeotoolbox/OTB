964f1c332abbf46d90e921439816371a06142483
Guillaume Pasero <guillaume.pasero@c-s.fr>
2019-06-07 11:05:21 +0200

    CI: disable DiapOTBModule to compile documentation

--- CI/configure_options.cmake
+++ CI/configure_options.cmake
@@ -71,7 +71,8 @@ if(XDK_PATH)
 set(cmake_configure_option
 "${cmake_configure_option}
 CMAKE_PREFIX_PATH=${XDK_PATH}")
-foreach(remote_module DiapOTBModule OTBTemporalGapFilling Mosaic SertitObject)#otbGRM #Mosaic # #SertitObject
+# DiapOTBModule disabled until documentation can build it
+foreach(remote_module OTBTemporalGapFilling Mosaic SertitObject)#otbGRM #Mosaic # #SertitObject #DiapOTBModule
   set(cmake_configure_option
 "${cmake_configure_option}
 Module_${remote_module}:BOOL=ON")
