60f146cf5c5f6ac5f3418e8fbef76f771cb3d733
Cédric Traizet <cedric.traizet@c-s.fr>
2019-07-16 15:44:31 +0200

    BUG: add boost timer system and chrono to linked libraries of test using boost unit test framework

--- Modules/ThirdParty/Boost/otb-module-init.cmake
+++ Modules/ThirdParty/Boost/otb-module-init.cmake
@@ -24,7 +24,7 @@ set(Boost_USE_MULTITHREADED ON CACHE BOOL "use multi-threaded libraries from boo
 
 find_package (Boost 1.35.0 REQUIRED)
 if (BUILD_TESTING)
-  find_package (Boost 1.35.0 QUIET COMPONENTS unit_test_framework)
+  find_package (Boost 1.35.0 QUIET COMPONENTS unit_test_framework timer system chrono)
   if (NOT Boost_UNIT_TEST_FRAMEWORK_FOUND)
     message(STATUS "Boost unit_test_framework not found. Hence otbOGRTests will be skipped")
   else()
