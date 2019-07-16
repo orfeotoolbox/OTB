a7a44ba4479f5e970a5e126bd6be9246649281eb
Victor Poughon <victor.poughon@cnes.fr>
2019-07-16 17:05:00 +0200

    ENh: use new TerraSar-X sar sensor model

--- Modules/ThirdParty/OssimPlugins/src/ossim/ossimTerraSarXSarSensorModel.h
+++ Modules/ThirdParty/OssimPlugins/src/ossim/ossimTerraSarXSarSensorModel.h
@@ -49,12 +49,13 @@ public:
   virtual ~ossimTerraSarXSarSensorModel() = default;
 #endif
 
-  //Not implemented yet
   /** References
    * TerraSAR-X Image Product Guide
    * SNAP source code (s1tbx-io/src/main/java/org/esa/s1tbx/io/terrasarx/TerraSarXProductDirectory.java)
   */
-  void readAnnotationFile(const std::string & annotationXml, const std::string & geoXml);
+  bool readAnnotationFile(const std::string & annotationXml, const std::string & geoXml);
+
+  bool open(const ossimFilename& file);
 
 protected:
   /*
