aeea0d4f6e8d6683303f16db8de42a137029c028
Guillaume Pasero <guillaume.pasero@c-s.fr>
2019-08-06 12:04:53 +0200

    PKG: detect if env variables are empty or not

--- Packaging/Files/otbenv.bat
+++ Packaging/Files/otbenv.bat
@@ -20,11 +20,23 @@
 :: Setup environment for OTB package
 set CURRENT_SCRIPT_DIR=%~dp0
 
-set PATH=%CURRENT_SCRIPT_DIR%bin;%PATH%
+call :prefix_path PATH "%CURRENT_SCRIPT_DIR%bin"
+call :prefix_path PYTHONPATH "%CURRENT_SCRIPT_DIR%lib\python"
+call :prefix_path OTB_APPLICATION_PATH "%CURRENT_SCRIPT_DIR%lib\otb\applications"
+
 set GDAL_DATA=%CURRENT_SCRIPT_DIR%share\data
 set GEOTIFF_CSV=%CURRENT_SCRIPT_DIR%share\epsg_csv
-set PYTHONPATH=%CURRENT_SCRIPT_DIR%lib\python;%PYTHONPATH%
-set OTB_APPLICATION_PATH=%CURRENT_SCRIPT_DIR%lib\otb\applications
 set GDAL_DRIVER_PATH=disable
 :: Set numeric locale to C
 set LC_NUMERIC=C
+goto :eof
+
+:prefix_path
+setlocal enabledelayedexpansion
+set output=%~2
+set var=%1
+set content=
+if defined %var% ( set content=!%var%! )
+if not "%content%" == "" ( set output=%output%;%content% )
+endlocal & set %1=%output%
+goto :eof
