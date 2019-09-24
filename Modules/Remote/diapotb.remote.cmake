49a05bf8443621110fa0b32df8edb9c5a8764d2d
Cédric Traizet <cedric.traizet@c-s.fr>
2019-09-24 10:21:45 +0200

    CI: use diapotb master branch

--- Modules/Remote/diapotb.remote.cmake
+++ Modules/Remote/diapotb.remote.cmake
@@ -22,5 +22,5 @@
 otb_fetch_module(DiapOTBModule
   "OTB module for SAR processing in Diapason."
   GIT_REPOSITORY https://gitlab.orfeo-toolbox.org/remote_modules/diapotb.git
-  GIT_TAG d38457a019d2ef15a47d04c21f235c9e9fe4acd5
+  GIT_TAG master
 )
