e6eab5c557a3addfb231fd0bc051fd6befe8c469
Victor Poughon <victor.poughon@cnes.fr>
2019-10-01 09:24:11 +0200

    REFAC: remove more obsolete exception specifications

--- Modules/Filtering/Wavelet/include/otbWaveletFilterBank.hxx
+++ Modules/Filtering/Wavelet/include/otbWaveletFilterBank.hxx
@@ -616,7 +616,7 @@ void WaveletFilterBank<TInputImage, TOutputImage, TWaveletOperator, Wavelet::INV
 }
 
 template <class TInputImage, class TOutputImage, class TWaveletOperator>
-void WaveletFilterBank<TInputImage, TOutputImage, TWaveletOperator, Wavelet::INVERSE>::GenerateInputRequestedRegion() throw(itk::InvalidRequestedRegionError)
+void WaveletFilterBank<TInputImage, TOutputImage, TWaveletOperator, Wavelet::INVERSE>::GenerateInputRequestedRegion()
 {
   Superclass::GenerateInputRequestedRegion();
 
