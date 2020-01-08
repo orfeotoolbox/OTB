96dcf9e1ff2dd40b7a82a863c07b500784616af9
Cédric Traizet <cedric.traizet@c-s.fr>
2020-01-08 10:50:46 +0100

    WRG: update diapOTB tag

--- Modules/Remote/diapotb.remote.cmake
+++ Modules/Remote/diapotb.remote.cmake
@@ -22,5 +22,5 @@
 otb_fetch_module(DiapOTBModule
   "OTB module for SAR processing in Diapason."
   GIT_REPOSITORY https://gitlab.orfeo-toolbox.org/remote_modules/diapotb.git
-  GIT_TAG 04628a9c183883b461114ff5b099943197431f5a
+  GIT_TAG 1b46a3ca14a009779b277a9c43f4a59913462d1a
 )
