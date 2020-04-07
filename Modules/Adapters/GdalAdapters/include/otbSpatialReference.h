c035b3400cb399ef55c69bae567d2fd3df80292b
Julien Osman <julien.osman@c-s.fr>
2020-04-07 19:10:33 +0200

    ENH: Add possibility to access the OGRSpatialReference from the otbSpatialReference

--- Modules/Adapters/GdalAdapters/include/otbSpatialReference.h
+++ Modules/Adapters/GdalAdapters/include/otbSpatialReference.h
@@ -176,6 +176,11 @@ public:
    */
   static void UTMFromGeoPoint(double lon, double lat, unsigned int& zone, hemisphere& hem);
 
+  /**
+   * Returns the internal OGRSpatialReference
+   */
+  OGRSpatialReference* getOGRSpatialReference();
+
 private:
   /// Constructor from wrapped type. ref will be cloned.
   SpatialReference(const OGRSpatialReference* ref);
