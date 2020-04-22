eda6425b6e053aa9fd48e1e1396117044f827db7
Julien Osman <julien.osman@c-s.fr>
2020-04-22 17:37:38 +0200

    STYLE: Update the headers with the good date for the copyright

--- Modules/Wrappers/SWIG/test/python/Bug1498.py
+++ Modules/Wrappers/SWIG/test/python/Bug1498.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 #-*- coding: utf-8 -*-
 #
-# Copyright (C) 2005-2019 Centre National d'Etudes Spatiales (CNES)
+# Copyright (C) 2005-2020 Centre National d'Etudes Spatiales (CNES)
 #
 # This file is part of Orfeo Toolbox
 #
