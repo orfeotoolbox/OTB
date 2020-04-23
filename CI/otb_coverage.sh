9a187f989b4eaaed3e94e6d5a0d1fe01562a9869
Julien Osman <julien.osman@c-s.fr>
2020-04-23 12:23:42 +0200

    STYLE: Update the headers with the good date for the copyright

--- CI/otb_coverage.sh
+++ CI/otb_coverage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (C) 2005-2019 Centre National d'Etudes Spatiales (CNES)
+# Copyright (C) 2005-2020 Centre National d'Etudes Spatiales (CNES)
 #
 # This file is part of Orfeo Toolbox
 #
