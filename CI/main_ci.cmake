e81f995669c47c3efcf97b7e8e709d9c209f41c0
Julie Brossard <julie.brossard@c-s.fr>
2020-08-18 16:56:21 +0200

    Revert "{WIP} Added logs"
    This reverts commit 738c674681638ff2795cbfa64d0608adc2fe7c87.

--- CI/main_ci.cmake
+++ CI/main_ci.cmake
@@ -108,12 +108,6 @@ ctest_configure(BUILD "${CTEST_BINARY_DIRECTORY}"
     RETURN_VALUE _configure_rv
     CAPTURE_CMAKE_ERROR _configure_error
     )
-
-message(STATUS "CTEST_BINARY_DIRECTORY : ${CTEST_BINARY_DIRECTORY}")
-message(STATUS "CONFIGURE_OPTIONS ${CONFIGURE_OPTIONS}")
-message(STATUS "_configure_rv : ${_configure_rv}")
-message(STATUS : "_configure_error : ${_configure_error}")
-
 # Configure log
 file ( WRITE 
   "${OTB_SOURCE_DIR}/log/configure_return_value_log.txt" "${_configure_rv}")
