a013a9d031fd082b72cb7deb92a93545e5a99486
Cédric Traizet <cedric.traizet@c-s.fr>
2020-09-03 15:54:48 +0200

    Update diapotb.remote.cmake

--- Modules/Remote/diapotb.remote.cmake
+++ Modules/Remote/diapotb.remote.cmake
@@ -22,5 +22,5 @@
 otb_fetch_module(DiapOTBModule
   "OTB module for SAR processing in Diapason."
   GIT_REPOSITORY https://gitlab.orfeo-toolbox.org/remote_modules/diapotb.git
-  GIT_TAG diapotb-strptime
+  GIT_TAG fix_strptime
 )
