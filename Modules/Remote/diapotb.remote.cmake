007924bc5bc0ad964fd5ad659e73115c97867f3a
Cédric Traizet <cedric.traizet@c-s.fr>
2020-09-04 16:12:43 +0200

    REFAC: update diapotb tag for release 7.2.0

--- Modules/Remote/diapotb.remote.cmake
+++ Modules/Remote/diapotb.remote.cmake
@@ -22,5 +22,5 @@
 otb_fetch_module(DiapOTBModule
   "OTB module for SAR processing in Diapason."
   GIT_REPOSITORY https://gitlab.orfeo-toolbox.org/remote_modules/diapotb.git
-  GIT_TAG f84b85352abb75ff3eefafdf0480fcde7fa06173
+  GIT_TAG 1.0.0
 )
