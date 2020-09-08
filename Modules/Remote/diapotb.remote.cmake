fc6fa1a9615c11a38feba99bf224db6fd8e99e66
Julien Osman <julien.osman@csgroup.eu>
2020-09-08 17:15:04 +0200

    BUG: set a temporary branch for DiapOTB

--- Modules/Remote/diapotb.remote.cmake
+++ Modules/Remote/diapotb.remote.cmake
@@ -22,5 +22,5 @@
 otb_fetch_module(DiapOTBModule
   "OTB module for SAR processing in Diapason."
   GIT_REPOSITORY https://gitlab.orfeo-toolbox.org/remote_modules/diapotb.git
-  GIT_TAG 5cedc91f6cd66ecfb4adccae55f823b00db636ee
+  GIT_TAG RPCModel
 )
