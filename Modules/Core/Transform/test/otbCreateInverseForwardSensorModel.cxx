12591edbe01d15f74a1f308eaa1709ba0709c5f3
Julien Osman <julien.osman@csgroup.eu>
2020-09-14 14:55:50 +0200

    BUG: Correct test for RPC model initialization

--- Modules/Core/Transform/test/otbCreateInverseForwardSensorModel.cxx
+++ Modules/Core/Transform/test/otbCreateInverseForwardSensorModel.cxx
@@ -68,24 +68,24 @@ int otbCreateInverseForwardSensorModel(int argc, char* argv[])
   ImageType::Pointer inputImage = reader->GetOutput();
 
   otbGenericMsgDebugMacro(<< "Inverse model creation...");
-  if (inverse_rpc_model->SetMetadata(inputImage->GetImageMetadata()))
+  if (!inverse_rpc_model->SetMetadata(inputImage->GetImageMetadata()))
   {
     std::cout << "Error while reading model. This is not a RPC model!" << std::endl;
     return EXIT_FAILURE;
   }
-  if (inverse_rpc_model->IsValidSensorModel() == false)
+  if (!inverse_rpc_model->IsValidSensorModel())
   {
     std::cout << "Model not set!" << std::endl;
     return EXIT_FAILURE;
   }
 
   otbGenericMsgDebugMacro(<< "Forward model creation...");
-  if (forward_rpc_model->SetMetadata(inputImage->GetImageMetadata()))
+  if (!forward_rpc_model->SetMetadata(inputImage->GetImageMetadata()))
   {
     std::cout << "Error while reading model. This is not a RPC model!" << std::endl;
     return EXIT_FAILURE;
   }
-  if (forward_rpc_model->IsValidSensorModel() == false)
+  if (!forward_rpc_model->IsValidSensorModel())
   {
     std::cout << "Model not set!" << std::endl;
     return EXIT_FAILURE;
