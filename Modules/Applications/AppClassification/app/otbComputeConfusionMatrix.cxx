96e118d23769b030db14fbc91fdc6d2158ddfc44
Julien Cabieces <julien.cabieces@oslandia.com>
2020-11-13 17:38:40 +0100

    Fix several C++ comment

--- Modules/Applications/AppClassification/app/otbComputeConfusionMatrix.cxx
+++ Modules/Applications/AppClassification/app/otbComputeConfusionMatrix.cxx
@@ -198,13 +198,11 @@ private:
 
         OGRFieldType fieldType = feature.ogr().GetFieldDefnRef(iField)->GetType();
 
-        for (auto type : typeFilter)
-          if (fieldType == type)
-          {
-              std::string tmpKey = "ref.vector.field." + key.substr(0, end - key.begin());
-              AddChoice(tmpKey, item);
-              break;
-          }
+        if (std::find(typeFilter.begin(), typeFilter.end(), fieldType) != std::end(typeFilter))
+        {
+          std::string tmpKey = "ref.vector.field." + key.substr(0, end - key.begin());
+          AddChoice(tmpKey, item);
+        }
       }
     }
   }
