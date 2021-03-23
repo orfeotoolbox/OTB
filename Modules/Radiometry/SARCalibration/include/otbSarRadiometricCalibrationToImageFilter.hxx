b248a18e849641dafb197d9183ddc18dcafe9138
Cédric Traizet <cedric.traizet@c-s.fr>
2021-03-23 08:47:49 +0100

    BUG: only remove S1 thermal noise when at least range noise is available in the keywordlist

--- Modules/Radiometry/SARCalibration/include/otbSarRadiometricCalibrationToImageFilter.hxx
+++ Modules/Radiometry/SARCalibration/include/otbSarRadiometricCalibrationToImageFilter.hxx
@@ -93,7 +93,7 @@ void SarRadiometricCalibrationToImageFilter<TInputImage, TOutputImage>::BeforeTh
   function->SetApplyRangeSpreadLossCorrection(!apply);
   function->SetApplyRescalingFactor(!apply);
   function->SetApplyLookupDataCorrection(apply);
-  function->SetRemoveS1ThermalNoise(apply && function->GetEnableNoise());
+  function->SetRemoveS1ThermalNoise(function->GetEnableNoise());
 
   function->SetScale(imageMetadataInterface->GetRadiometricCalibrationScale());
 
