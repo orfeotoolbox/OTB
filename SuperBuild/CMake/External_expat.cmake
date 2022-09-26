72679b576e1de70ffed699d60be770876db65a36
Luc Hermitte <luc.hermitte@csgroup.eu>
2022-09-26 19:09:28 +0200

    BUILD: Use expat.tar.gz to keep md5sum
    Change back archive type from .tar.bz2 to .tar.gz to keep the same md5sum

--- SuperBuild/CMake/External_expat.cmake
+++ SuperBuild/CMake/External_expat.cmake
@@ -24,7 +24,7 @@ SETUP_SUPERBUILD(EXPAT)
 
 ExternalProject_Add(EXPAT
   PREFIX EXPAT
-  URL "https://github.com/libexpat/libexpat/releases/download/R_2_4_8/expat-2.4.8.tar.bz2"
+  URL "https://github.com/libexpat/libexpat/releases/download/R_2_4_8/expat-2.4.8.tar.gz"
   URL_MD5 ce5fa3fa4d866d83ab0cfb00bb95b77a
   BINARY_DIR ${EXPAT_SB_BUILD_DIR}
   INSTALL_DIR ${SB_INSTALL_PREFIX}
