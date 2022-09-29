d08336f6d6618be05591ecc4fe91f029fc0446d5
Remi Cresson <remi.cresson@inrae.fr>
2022-09-29 22:29:03 +0200

    ADD: otb::StreamingBufferGenerator filter

---
+++ Modules/IO/ImageIO/include/otbStreamingBufferGenerator.h
@@ -0,0 +1,92 @@
+/*
+ * Copyright (C) 2005-2022 Centre National d'Etudes Spatiales (CNES)
+ *
+ * This file is part of Orfeo Toolbox
+ *
+ *     https://www.orfeo-toolbox.org/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef otbStreamingBufferGenerator_h
+#define otbStreamingBufferGenerator_h
+
+#include "itkImageToImageFilter.h"
+#include "itkProgressReporter.h"
+#include "otbRAMDrivenAdaptativeStreamingManager.h"
+
+namespace otb
+{
+
+/**
+ * \class StreamingBufferGenerator
+ * \brief This filter generates an output image with an internal
+ * explicit streaming mechanism.
+ *
+ * \ingroup OTBImageIO
+ */
+template <class TInputImage>
+class ITK_EXPORT StreamingBufferGenerator : public itk::ImageToImageFilter<TInputImage, TInputImage>
+{
+
+public:
+  /** Standard class typedefs. */
+  typedef StreamingBufferGenerator                          Self;
+  typedef itk::ImageToImageFilter<TInputImage, TInputImage> Superclass;
+  typedef itk::SmartPointer<Self>                           Pointer;
+  typedef itk::SmartPointer<const Self>                     ConstPointer;
+
+  /** Method for creation through the object factory. */
+  itkNewMacro(Self);
+
+  /** Run-time type information (and related methods). */
+  itkTypeMacro(StreamingBufferGenerator, itk::ImageToImageFilter);
+
+  /** Images typedefs */
+  typedef typename Superclass::InputImageType       ImageType;
+  typedef typename Superclass::InputImageRegionType RegionType;
+
+  /** Streaming manager base class pointer */
+  typedef RAMDrivenAdaptativeStreamingManager<TInputImage>  RAMDrivenAdaptativeStreamingManagerType;
+  typedef typename RAMDrivenAdaptativeStreamingManagerType::Pointer StreamingManagerPointerType;
+
+protected:
+  StreamingBufferGenerator(){
+    m_StreamingManager = RAMDrivenAdaptativeStreamingManagerType::New();
+  };
+  virtual ~StreamingBufferGenerator(){};
+
+  virtual void
+  UpdateOutputData(itk::DataObject * output)
+  {
+    (void) output;
+    this->GenerateData();
+  }
+
+  virtual void
+  GenerateData();
+
+  StreamingManagerPointerType m_StreamingManager;
+
+private:
+  StreamingBufferGenerator(const Self &); // purposely not implemented
+  void
+  operator=(const Self &); // purposely not implemented
+
+}; // end class
+
+
+} // end namespace otb
+
+#include "otbStreamingBufferGenerator.hxx"
+
+#endif
---
+++ Modules/IO/ImageIO/include/otbStreamingBufferGenerator.hxx
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2005-2022 Centre National d'Etudes Spatiales (CNES)
+ *
+ * This file is part of Orfeo Toolbox
+ *
+ *     https://www.orfeo-toolbox.org/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef otbStreamingBufferGenerator_hxx
+#define otbStreamingBufferGenerator_hxx
+
+#include "otbStreamingBufferGenerator.h"
+#include "itkImageAlgorithm.h"
+
+namespace otb
+{
+
+/**
+ * Compute the output image
+ */
+template <class TInputImage>
+void
+StreamingBufferGenerator<TInputImage>::GenerateData()
+{
+  // Output pointer and requested region
+  ImageType * inputPtr = static_cast<ImageType *>(Superclass::ProcessObject::GetInput(0));
+  ImageType * outputPtr = this->GetOutput();
+  RegionType reqRegion = inputPtr->GetLargestPossibleRegion();
+  otbDebugMacro("Allocate a buffer of size " << reqRegion.GetSize(0) << "x" << reqRegion.GetSize(1));
+  outputPtr->SetBufferedRegion(reqRegion);
+  outputPtr->Allocate();
+  otbDebugMacro("Allocation ok")
+
+  m_StreamingManager->PrepareStreaming(inputPtr, reqRegion);
+  int numberOfDivisions = m_StreamingManager->GetNumberOfSplits();
+
+  const auto firstSplitSize = m_StreamingManager->GetSplit(0).GetSize();
+  otbLogMacro(Info, << "Buffer will be written in " << numberOfDivisions << " blocks of " << firstSplitSize[0] << "x" << firstSplitSize[1]
+                    << " pixels");
+
+  /**
+   * Loop over the number of pieces, execute the upstream pipeline on each
+   * piece, and copy the results into the output image.
+   */
+  itk::ProgressReporter progress(this, 0, numberOfDivisions);
+  RegionType streamRegion;
+  int currentDivision = 0;
+  for (currentDivision = 0; currentDivision < numberOfDivisions; currentDivision++)
+  {
+    otbDebugMacro("Processing region " << (currentDivision + 1) << " over " << numberOfDivisions);
+    streamRegion = m_StreamingManager->GetSplit(currentDivision);
+
+    // Propagate region
+    inputPtr->SetRequestedRegion(streamRegion);
+    inputPtr->PropagateRequestedRegion();
+    inputPtr->UpdateOutputData();
+
+    // Copy the subregion to output
+    itk::ImageAlgorithm::Copy(inputPtr, outputPtr, streamRegion, streamRegion);
+    progress.CompletedPixel();
+  }
+}
+
+
+} // end namespace otb
+
+
+#endif
