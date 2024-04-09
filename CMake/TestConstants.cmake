73d8c6525a9081d2827d3b3d3ab4c1495bd4d139
Tristan Laurent <tristan.laurent@csgroup.eu>
2024-04-09 15:27:54 +0200

    COMP: move test constants to separated cmake file accessible from every project

---
+++ CMake/TestConstants.cmake
@@ -0,0 +1,35 @@
+#
+# Copyright (C) 2005-2022 Centre National d'Etudes Spatiales (CNES)
+#
+# This file is part of Orfeo Toolbox
+#
+#     https://www.orfeo-toolbox.org/
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+set(NOTOL      0.0)
+set(EPSILON_1  0.1)
+set(EPSILON_2  0.01)
+set(EPSILON_3  0.001)
+set(EPSILON_4  0.0001)
+set(EPSILON_5  0.00001)
+set(EPSILON_6  0.000001)
+set(EPSILON_7  0.0000001)
+set(EPSILON_8  0.00000001)
+set(EPSILON_9  0.000000001)
+set(EPSILON_10 0.0000000001)
+set(EPSILON_11 0.00000000001)
+set(EPSILON_12 0.000000000001)
+set(EPSILON_13 0.0000000000001)
+set(EPSILON_14 0.00000000000001)
+set(EPSILON_15 0.000000000000001)
