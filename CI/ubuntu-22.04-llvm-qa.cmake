69f94eed2c28d0b518bb65694b492fb9dd1d0169
Thibaut ROMAIN <thibaut.romain@cs-soprasteria.com>
2024-10-09 17:29:39 +0200

    CI: remove unused files in CI

--- CI/ubuntu-22.04-llvm-qa.cmake
+++ CI/ubuntu-22.04-llvm-qa.cmake
@@ -18,13 +18,29 @@
 # limitations under the License.
 #
 
-# Configuration options for ubuntu-20.04-llvm-xdk
+# Configuration options for ubuntu-22.04-llvm-qa
 
 set(site_option
 "CMAKE_C_COMPILER:STRING=clang
 CMAKE_CXX_COMPILER:STRING=clang++
+CMAKE_C_FLAGS:STRING=--coverage
+CMAKE_CXX_FLAGS:STRING=--coverage
 CMAKE_EXE_LINKER_FLAGS:STRING=-fuse-ld=lld
 CMAKE_MODULE_LINKER_FLAGS:STRING=-fuse-ld=lld
 CMAKE_SHARED_LINKER_FLAGS:STRING=-fuse-ld=lld
+OTB_BUILD_FeaturesExtraction:BOOL=ON
+OTB_BUILD_Hyperspectral:BOOL=ON
+OTB_BUILD_Learning:BOOL=ON
+OTB_BUILD_Miscellaneous:BOOL=ON
+OTB_BUILD_Remote:BOOL=ON
+OTB_BUILD_SAR:BOOL=ON
+OTB_BUILD_Segmentation:BOOL=ON 
+OTB_BUILD_StereoProcessing:BOOL=ON
+OTB_USE_LIBSVM:BOOL=ON
+OTB_USE_MUPARSER:BOOL=ON
+OTB_USE_MUPARSERX:BOOL=ON
+OTB_USE_OPENCV:BOOL=ON
+OTB_USE_OPENMP:BOOL=OFF
+OTB_USE_SHARK:BOOL=ON
 USE_SYSTEM_OPENSSL:BOOL=OFF
 ")
