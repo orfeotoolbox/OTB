f4789c486be1054e98984a5886a009ef2217e9c8
Thibaut ROMAIN <thibaut.romain@cs-soprasteria.com>
2024-10-10 09:43:37 +0200

    CI: update gitlab-ci and all 20.04 files to 22.04

--- CI/ubuntu-22.04-llvm-xdk-full.cmake
+++ CI/ubuntu-22.04-llvm-xdk-full.cmake
@@ -18,7 +18,7 @@
 # limitations under the License.
 #
 
-# Configuration options for ubuntu-20.04-llvm-xdk
+# Configuration options for ubuntu-22.04-llvm-xdk
 
 set(site_option
 "CMAKE_C_COMPILER:STRING=clang
@@ -43,3 +43,26 @@ OTB_USE_OPENCV:BOOL=ON
 OTB_USE_OPENMP:BOOL=OFF
 OTB_USE_SHARK:BOOL=ON
 ")
+
+if(NOT ${ci_do_cookbook} EQUAL -1)
+  set(site_option
+"${site_option}
+BUILD_COOKBOOK:BOOL=ON")
+endif()
+
+if(NOT ${ci_do_doxygen} EQUAL -1)
+  set(site_option
+"${site_option}
+BUILD_DOCUMENTATION:BOOL=ON
+OTB_DOXYGEN_ITK_TAGFILE:FILEPATH=${CTEST_BINARY_DIRECTORY}/InsightDoxygenDocTag-4.13.0
+OTB_DOXYGEN_ITK_DOXYGEN_URL:STRING=\"https://itk.org/Doxygen413/html\"
+")
+  set (ENABLE_DOXYGEN ON)
+  # The ITK doxygen tag file needs to be patched before being used for OTB
+  # See otb-devutils/Scripts/tagfile_fix.py
+  message(STATUS "Get resources for Doxygen build ...")
+  execute_process(COMMAND wget https://www.orfeo-toolbox.org/packages/archives/Doxygen/InsightDoxygenDocTag-4.13.0.gz
+                  WORKING_DIRECTORY ${CTEST_BINARY_DIRECTORY})
+  execute_process(COMMAND gzip -d InsightDoxygenDocTag-4.13.0.gz
+                  WORKING_DIRECTORY ${CTEST_BINARY_DIRECTORY})
+endif()
