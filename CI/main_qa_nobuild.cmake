4edddd614da5b6c1dd0e487153c15ec562b73b9a
Tristan Laurent <tristan.laurent@cs-soprasteria.com>
2024-10-16 15:58:40 +0200

    CI: qa still need xdk when configuring

--- CI/main_qa_nobuild.cmake
+++ CI/main_qa_nobuild.cmake
@@ -1,14 +1,29 @@
-set (ENV{LANG} "C") # Only ascii output
-get_filename_component(OTB_SOURCE_DIR "${CMAKE_CURRENT_LIST_DIR}" DIRECTORY)
-
+#
+# Copyright (C) 2005-2024 Centre National d'Etudes Spatiales (CNES)
+#
+# This file is part of Orfeo Toolbox
+#
+#     https://www.orfeo-toolbox.org/
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# This script is a prototype for the future CI, it may evolve rapidly in a near future
 
 set( QA ON )
 set( ci_build_type "Debug" )
 set( CTEST_TEST_TIMEOUT 1500)
-set ( DEBUG "1" )
-set (CMAKE_COMMAND "cmake")
 set ( ci_skip_build ON)
 set ( ci_skip_install ON)
 set ( ci_skip_testing ON)
 
-include( "${CMAKE_CURRENT_LIST_DIR}/main_ci.cmake" )
+include( "${CMAKE_CURRENT_LIST_DIR}/main_superbuild.cmake" )
