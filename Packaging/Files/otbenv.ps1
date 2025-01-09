1ba6ebfc15f6fab358f5c22e60339db5fb690c14
Tristan Laurent <tristan.laurent@cs-soprasteria.com>
2025-01-09 18:22:12 +0100

    BUG: correct ps1 script with their awful syntax

--- Packaging/Files/otbenv.ps1
+++ Packaging/Files/otbenv.ps1
@@ -17,12 +17,43 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# do not add value twice
+function Add-If-Not-Exist {
+  param (
+    [Parameter(Mandatory)]
+    [string]$var_to_check,
+    [Parameter(Mandatory)]
+    [string]$content
+  )
+
+  # NOTE that here we can not use the simple $env: notation as "wonderful"
+  # powershell is not even capable to resole $env:$smthing
+  # Thus we are force to use ununderstandable ugly verbose "Get/Set" Item
+  # notation
+
+  # check if this env var exists
+  if (-Not(Test-Path Env:$var_to_check)) {
+    Set-Item Env:$var_to_check $content
+  }
+  else { # if it exists, save its value
+    $temp_var = (Get-Item Env:$var_to_check).Value
+    # check that we are not re-adding it
+    # use "SimpleMatch" option to not interpret $content as a regular expression
+    if (-Not($temp_var | Select-String -Pattern $content -SimpleMatch)) {
+      Set-Item Env:$var_to_check "$content;$temp_var"
+    }
+  #   else {
+  #     echo "$var_to_check env var already contains $content"
+  #   }
+  }
+}
+
 # Setup environment for OTB package
-$env:PATH+=";$PSScriptRoot\bin;$PSScriptRoot\lib"
-$env:PYTHONPATH+=";$PSScriptRoot\lib\otb\python"
-$env:OTB_APPLICATION_PATH+=";$PSScriptRoot\lib\otb\application"
+Add-If-Not-Exist -var_to_check PATH -content "$PSScriptRoot\bin;$PSScriptRoot\lib"
+Add-If-Not-Exist -var_to_check PYTHONPATH -content "$PSScriptRoot\lib\otb\python"
+Add-If-Not-Exist -var_to_check OTB_APPLICATION_PATH -content "$PSScriptRoot\lib\otb\applications"
 
 $env:GDAL_DATA="$PSScriptRoot\share\data"
-$env:GDAL_DRIVER_PATH=disable
+$env:GDAL_DRIVER_PATH="disable"
 $env:PROJ_LIB="$PSScriptRoot\share\proj"
-$env:LC_NUMERIC=C
+$env:LC_NUMERIC="C"
