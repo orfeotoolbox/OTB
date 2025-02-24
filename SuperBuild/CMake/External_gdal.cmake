200395921bdb21823f346bf345fddd1ff88426b2
Tristan Laurent <tristan.laurent@cs-soprasteria.com>
2025-02-24 10:28:28 +0100

    UPD: use gdal 3.10.2 Gulf of Mexico

--- SuperBuild/CMake/External_gdal.cmake
+++ SuperBuild/CMake/External_gdal.cmake
@@ -24,8 +24,8 @@ SETUP_SUPERBUILD(GDAL)
 
 ADDTO_DEPENDENCIES_IF_NOT_SYSTEM(GDAL CURL OPENJPEG TIFF GEOTIFF PNG JPEG SQLITE GEOS ZLIB EXPAT HDF5 NETCDF HDF4 PROJ)
 
-set(GDAL_URL "https://github.com/OSGeo/gdal/releases/download/v3.10.1/gdal-3.10.1.tar.gz")
-set(GDAL_MD5 45fc16b66f77dffbb4a1cd28c211e147)
+set(GDAL_URL "https://github.com/OSGeo/gdal/releases/download/v3.10.2/gdal-3.10.2.tar.gz")
+set(GDAL_MD5 882761659217c593a4a8028111b15a2a)
 
 # avoid use of system SPATIALITE which can use other libproj which cause conflict
 list(APPEND GDAL_CONFIG_LIBS
